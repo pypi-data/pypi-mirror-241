9819506e4cbfd0926db16ec2a8d1bc1fb4ea3481
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 14:04:47 +0000

    Add package DataProfileViewerAKP/0.1.6/DataProfileViewerAKP-0.1.6.tar.gz

---
+++ packages/DataProfileViewerAKP/DataProfileViewerAKP-0.1.6.tar.gz/DataProfileViewerAKP-0.1.6/setup.py
@@ -0,0 +1,13 @@
+import pathlib
+import setuptools
+
+setuptools.setup(
+	name="DataProfileViewerAKP",
+	version='0.1.6',
+	description="Data Profiler Library",
+	author="Abhijeet Subhash Kasab",
+	author_email="abhijeetkasab07@gmail.com",
+	license="The Unlicense",
+	python_requires=">=3.7"
+)
+
