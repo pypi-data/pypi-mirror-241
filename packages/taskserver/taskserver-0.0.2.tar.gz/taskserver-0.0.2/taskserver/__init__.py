c5d4bfb63811e02a1e6741d8ea98bad47ea538d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-14 14:04:20 +0000

    Add package taskserver/0.0.2/taskserver-0.0.2.tar.gz

---
+++ packages/taskserver/taskserver-0.0.2.tar.gz/taskserver-0.0.2/taskserver/__init__.py
@@ -0,0 +1,11 @@
+
+from taskserver.main import router
+
+import taskserver.main
+import taskserver.app.index
+import taskserver.app.sidenav
+import taskserver.app.list
+import taskserver.app.task
+import taskserver.app.run
+import taskserver.app.config
+import taskserver.app.run
