5562000c92dd6a823a078ad4592eb778da17c39d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 14:04:08 +0000

    Add package django-tenant-users/1.3.0/django_tenant_users-1.3.0-py3-none-any.whl

---
+++ packages/django-tenant-users/django_tenant_users-1.3.0-py3-none-any.whl/tenant_users/permissions/backend.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth.models import Permission
+
+from tenant_users.permissions.models import UserTenantPermissions
+
+
+class UserBackend(ModelBackend):
+    """
+    Authenticates against UserProfile
+    Authorizes against the UserTenantPermissions.
+    The Facade classes handle the magic of passing
+    requests to the right spot.
+    """
+
+    # We override this so that it looks for the 'groups' attribute on the
+    # UserTenantPermissions rather than from get_user_model()
+    def _get_group_permissions(self, user_obj):
+        user_groups_field = UserTenantPermissions._meta.get_field("groups")
+        user_groups_query = "group__{0}".format(
+            user_groups_field.related_query_name(),
+        )
+        return Permission.objects.filter(**{user_groups_query: user_obj})
