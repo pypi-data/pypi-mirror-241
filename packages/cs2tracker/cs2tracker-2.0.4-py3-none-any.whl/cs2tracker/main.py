c5101d8f6b561971ebfd8ec47e2b5d7e58e4cb32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 15:04:40 +0000

    Add package cs2tracker/2.0.4/cs2tracker-2.0.4-py3-none-any.whl

---
+++ packages/cs2tracker/cs2tracker-2.0.4-py3-none-any.whl/cs2tracker/main.py
@@ -0,0 +1,33 @@
+import urllib3
+from rich.console import Console
+
+from .application import Application
+
+
+def main():
+    ## disable warnings for proxy requests
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+    console = Console()
+    console.print(
+        "[bold yellow]"
+        + """
+        __   _____ _____  ______  ____    ____     __  __  _    ___  ____
+   /  ] / ___/|     T|      T|    \  /    T   /  ]|  l/ ]  /  _]|    \
+  /  / (   \_ l__/  ||      ||  D  )Y  o  |  /  / |  ' /  /  [_ |  D  )
+ /  /   \__  T|   __jl_j  l_j|    / |     | /  /  |    \ Y    _]|    /
+/   \_  /  \ ||  /  |  |  |  |    \ |  _  |/   \_ |     Y|   [_ |    \
+\     | \    ||     |  |  |  |  .  Y|  |  |\     ||  .  ||     T|  .  Y
+ \____j  \___jl_____j  l__j  l__j\_jl__j__j \____jl__j\_jl_____jl__j\_j
+
+
+"""
+        + "Version: v2.0.4 - 11/14/2023 - Jannik Novak @ashiven_"
+    )
+
+    application = Application()
+    application.run()
+
+
+if __name__ == "__main__":
+    main()
