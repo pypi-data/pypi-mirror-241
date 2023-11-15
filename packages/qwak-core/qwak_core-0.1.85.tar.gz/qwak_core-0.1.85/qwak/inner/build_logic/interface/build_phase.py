d32c9a1c52adc16ef2f9f4f518510f19a391184b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 04:06:17 +0000

    Add package qwak-core/0.1.85/qwak_core-0.1.85.tar.gz

---
+++ packages/qwak-core/qwak_core-0.1.85.tar.gz/qwak_core-0.1.85/qwak/inner/build_logic/interface/build_phase.py
@@ -0,0 +1,17 @@
+from typing import Optional
+
+
+class BuildPhase:
+    def __init__(
+        self,
+        phase_id: str,
+        name: Optional[str] = None,
+        description: Optional[str] = None,
+    ):
+        self.phase_id = phase_id
+        self.name = name
+        self.description = description
+        if not self.name:
+            self.name = phase_id.replace("_", " ").capitalize()
+        if not self.description:
+            self.description = phase_id.replace("_", " ").capitalize()
