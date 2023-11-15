e96725f4d23015576e5c775cc0630c74cdaaf26f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 03:05:45 +0000

    Add package defog/0.51.0/defog-0.51.0.tar.gz

---
+++ packages/defog/defog-0.51.0.tar.gz/defog-0.51.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+# with open("./requirements.txt", "r") as f:
+#     requirements = f.read().splitlines()
+
+setup(
+    name="defog",
+    packages=find_packages(),
+    package_data={"defog": ["gcp/*", "aws/*"]},
+    version="0.51.0",
+    description="Defog is a Python library that helps you generate data queries from natural language questions.",
+    author="Full Stack Data Pte. Ltd.",
+    license="MIT",
+    # install_requires=requirements,
+    install_requires=[
+        "requests>=2.28.2",
+        "psycopg2-binary>=2.9.5",
+        "prompt-toolkit>=3.0.38",
+    ],
+    entry_points={
+        "console_scripts": [
+            "defog=defog.cli:main",
+        ],
+    },
+    author_email="founders@defog.ai",
+    url="https://github.com/defog-ai/defog-python",
+    long_description="Defog is a Python library that helps you generate data queries from natural language questions.",
+    long_description_content_type="text/markdown",
+)
