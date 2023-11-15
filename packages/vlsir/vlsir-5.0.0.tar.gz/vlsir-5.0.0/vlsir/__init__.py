4a2b245e64a0e0bce7f36a31f26c63a55932609f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 05:05:29 +0000

    Add package vlsir/5.0.0/vlsir-5.0.0.tar.gz

---
+++ packages/vlsir/vlsir-5.0.0.tar.gz/vlsir-5.0.0/vlsir/__init__.py
@@ -0,0 +1,21 @@
+"""
+VLSIR Python Bindings 
+"""
+
+__version__ = "2.0.dev0"
+
+# Schema
+from . import utils_pb2 as utils
+from . import spice_pb2 as spice
+from . import circuit_pb2 as circuit
+from . import raw_pb2 as raw
+from . import tetris_pb2 as tetris
+
+# Add the utility-types to the top-level namespace
+from .utils_pb2 import *
+
+# But not the other stuff, as quite a few names conflict, e.g. `Library`, `Package`.
+# from .spice_pb2 import *
+# from .circuit_pb2 import *
+# from .raw_pb2 import *
+# from .tetris_pb2 import *
