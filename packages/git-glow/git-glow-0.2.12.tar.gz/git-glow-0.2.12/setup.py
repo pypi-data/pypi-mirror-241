0df5daf6bac71c7fe4907c797faa3c78a2669909
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 03:05:06 +0000

    Add package git-glow/0.2.12/git-glow-0.2.12.tar.gz

---
+++ packages/git-glow/git-glow-0.2.12.tar.gz/git-glow-0.2.12/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="git-glow",
+    version="0.2.12",
+    packages=find_packages(),
+    author="Nicolas RAMY",
+    author_email="nicolas.ramy@darkelda.com",
+    license="MIT",
+    description=(
+        "A new git extension to use git flow "
+        "branching model automatically with Jira and Github"
+    ),
+    long_description=open("DESCRIPTION.rst").read(),
+    entry_points={
+        "console_scripts": ["git-glow=glow.__main__:main"],
+    },
+    install_requires=[
+        "colorama==0.4.4",
+        "click==8.1.3",
+        "emoji==1.2.0",
+        "gitpython==3.1.37",
+        "idna==2.10",
+        "python-slugify==4.0.1",
+        "requests==2.31.0",
+        "semver==2.13.0",
+        "termcolor==1.1.0",
+        "urllib3[secure]==1.26.18",
+    ],
+    url="https://github.com/nicolasramy/git-glow",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Terminals",
+        "Topic :: Utilities",
+    ],
+)
