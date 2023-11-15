1c1a0f44be4d365a248a5ad1a74853bc48588e63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 17:04:23 +0000

    Add package lqbox/1.5.0/lqbox-1.5.0.tar.gz

---
+++ packages/lqbox/lqbox-1.5.0.tar.gz/lqbox-1.5.0/src/lqbox/oc/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+@Project : lqbox 
+@File    : __init__.py
+@Date    : 2023/8/25 10:51:57
+@Author  : zhchen
+@Desc    : 
+"""
+from lqbox.base import BaseBox
+
+
+class OcBox(BaseBox):
+    base_url = 'aHR0cHM6Ly9vYy5sZXFlZS5jb20='
