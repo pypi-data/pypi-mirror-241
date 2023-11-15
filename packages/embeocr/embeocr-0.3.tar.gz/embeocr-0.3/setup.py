6108f601e7ada8d0a1b873e6fc458aee65d54df6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 15:04:09 +0000

    Add package embeocr/0.3/embeocr-0.3.tar.gz

---
+++ packages/embeocr/embeocr-0.3.tar.gz/embeocr-0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='embeocr',
+    version='0.3',
+    author='trangvv2',
+    author_email='vantrangeuro@gmail.com',
+    description='Description of my package',
+    packages=find_packages(),    
+    install_requires=['transformers', 
+                      'torch==2.0.1', 
+                      'numpy'],
+)
