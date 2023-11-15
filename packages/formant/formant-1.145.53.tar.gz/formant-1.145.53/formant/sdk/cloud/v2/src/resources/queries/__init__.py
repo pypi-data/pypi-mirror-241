1a00607fa33222ca8c5b7eea75eb68607b49bc20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 07:05:44 +0000

    Add package formant/1.145.53/formant-1.145.53.tar.gz

---
+++ packages/formant/formant-1.145.53.tar.gz/formant-1.145.53/formant/sdk/cloud/v2/src/resources/queries/__init__.py
@@ -0,0 +1,4 @@
+
+from formant.sdk.cloud.v2.formant_query_api_client.models import StreamDataType, File, ImageAnnotation, QueryAggregate, StreamAggregateData, Query, Battery, StreamDataListResponse, VideoMimeType, Localization, Health, Odometry, Twist, Transform, BoundingBox, StreamAggregateDataType, Path, StreamDataTags, Bitset, StreamData, Image, Map, NumericSetEntry, StreamAggregateDataTags, PointCloud, Video,VideoMimeType, HealthStatus, Quaternion, Location, Video, Health,HealthStatus, Goal, TransformNode, StreamDataPointsItem, Vector3, QueryTypesItem
+from formant.sdk.cloud.v2.formant_query_api_client.api.query import query_controller_query
+from formant.sdk.cloud.v2.formant_query_api_client.types import *
---
+++ packages/formant/formant-1.145.53.tar.gz/formant-1.145.53/formant/sdk/cloud/v2/src/resources/views/__init__.py
@@ -0,0 +1,4 @@
+
+from formant.sdk.cloud.v2.formant_admin_api_client.models import LocationViewport, PartialViewLayoutType, Filter, ImageViewConfigurationMode, Camera, ViewTags, LocalizationViewConfiguration, TransformTreeViewConfiguration, View, PointCloudViewConfiguration, ImageViewConfiguration, PartialViewTags, ViewConfigurationType, PartialView, NumericViewConfiguration, LocationViewConfiguration, BitsetViewConfiguration, ViewLayoutType, LocationViewConfigurationBasemap, ViewConfiguration, Location, ViewListResponse, Vector3
+from formant.sdk.cloud.v2.formant_admin_api_client.api.view import view_controller_get_one, view_controller_get_all, view_controller_patch
+from formant.sdk.cloud.v2.formant_admin_api_client.types import *
