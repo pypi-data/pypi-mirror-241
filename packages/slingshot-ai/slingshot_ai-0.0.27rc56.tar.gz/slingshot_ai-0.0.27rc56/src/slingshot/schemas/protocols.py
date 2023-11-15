f19d25a13cdbf08f4a693bef493e12cbca3a6a81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 03:05:52 +0000

    Add package slingshot-ai/0.0.27rc56/slingshot_ai-0.0.27rc56.tar.gz

---
+++ packages/slingshot-ai/slingshot_ai-0.0.27rc56.tar.gz/slingshot_ai-0.0.27rc56/src/slingshot/schemas/protocols.py
@@ -0,0 +1,23 @@
+from abc import abstractmethod
+from typing import Any, Protocol, runtime_checkable
+
+
+@runtime_checkable
+class HasAutoscalingParams(Protocol):
+    @property
+    @abstractmethod
+    def min_replicas(self) -> int | None:
+        ...
+
+    @property
+    @abstractmethod
+    def max_replicas(self) -> int | None:
+        ...
+
+
+@runtime_checkable
+class HasComponentConfig(Protocol):
+    @property
+    @abstractmethod
+    def config_variables(self) -> dict[str, Any]:
+        ...
