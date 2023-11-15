1d2ff5e01cda37e3c759f656f4e62713ac83db50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 04:06:04 +0000

    Add package Slugifybadwendel/0.1/Slugifybadwendel-0.1.tar.gz

---
+++ packages/Slugifybadwendel/Slugifybadwendel-0.1.tar.gz/Slugifybadwendel-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Slugifybadwendel',
+    version='0.1',
+    author='Marcklens Badio && Wendel Hector',
+    author_email='baddesignhaiti@gmail.com',
+    long_description='Trans+.',
+    long_description_content_type='text/plain',  # Remplacez par le type de contenu approprié
+    url='https://github.com/wendoohector/slugify_projet.git',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ]
+)
