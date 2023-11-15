507c5d1ce4b012c326aff67fc32a36f8c4f73d7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 03:06:00 +0000

    Add package libactivation/0.1.0/libactivation-0.1.0.tar.gz

---
+++ packages/libactivation/libactivation-0.1.0.tar.gz/libactivation-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+        name="libactivation", 
+        version="0.1.0",
+        author="Bastiaan Quast",
+        author_email="<bquast@gmail.com>",
+        packages=find_packages(),
+        keywords=['sigmoid', 'ReLU'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
