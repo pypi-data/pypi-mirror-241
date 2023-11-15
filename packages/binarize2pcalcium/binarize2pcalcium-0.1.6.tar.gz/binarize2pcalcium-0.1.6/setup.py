abc7a0ad442abcd0128049cf619f24c5a0429137
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 17:03:45 +0000

    Add package binarize2pcalcium/0.1.6/binarize2pcalcium-0.1.6.tar.gz

---
+++ packages/binarize2pcalcium/binarize2pcalcium-0.1.6.tar.gz/binarize2pcalcium-0.1.6/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='binarize2pcalcium',
+    version='0.1.6',
+    packages=['binarize2pcalcium'],
+    install_requires=[
+        'numpy',
+        'tqdm',
+        'scipy',
+        'matplotlib',
+        'pyyaml',
+        'networkx',
+        'scikit-learn',
+        'pandas',
+        'opencv-python',
+        'parmap',
+    ],
+    long_description="""# Markdown supported!\n\n* Cheer\n* Celebrate\n""",
+)
+
