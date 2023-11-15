fec8b6492b9d0ef8070c3d463e01a9721cd701c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 04:06:22 +0000

    Add package gixtpy/0.1.2/gixtpy-0.1.2.tar.gz

---
+++ packages/gixtpy/gixtpy-0.1.2.tar.gz/gixtpy-0.1.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    author="Teddy Tortorici",
+    description="Tools to assist in angle tuning grazing incidence x-ray scattering (GIWAXS or GISASXS) experiments",
+    long_description="https://github.com/teddy-tort/gixtpy",
+    name="gixtpy",
+    version='0.1.2',
+    packages=find_packages(include=['gixtpy.*']),
+    install_requires=[
+        "numpy",
+        "scipy",
+        "matplotlib",
+        "tk",
+        "tifffile",
+    ],
+    python_requires=">=3.8"
+)
