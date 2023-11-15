baa43ec4f401e50ab5f9934b123971836f4b4442
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 17:04:31 +0000

    Add package github4api/1.1.13/github4api-1.1.13-py3-none-any.whl

---
+++ packages/github4api/github4api-1.1.13-py3-none-any.whl/github4api/handlers/request_handler/__init__.py
@@ -0,0 +1,41 @@
+if (__debug__):
+    try:
+        import requests
+        from typing import Self, Literal, Union
+    
+    except ModuleNotFoundError.__doc__ as mnfe:
+        raise mnfe
+
+
+
+
+
+
+
+class RequestHandler:
+    def __init__(self: Self, url: str) -> Literal[None]:
+        super(RequestHandler, self).__init__()
+        self.__url = url
+        self.__data = None
+        
+    @property
+    def url(self: Self) -> str:
+        return self.__url
+        
+    def sendGetRequest(self: Self, content: bool = False, status_code = False) -> Union[int, bytes, Literal[None]]:
+        try:
+            self.__data = requests.get(url=self.url)
+            
+        except* requests.ConnectionError as ce:
+            raise ce.__doc__
+        
+        except* requests.RequestException as re:
+            raise re.__doc__
+        
+        if (content):
+            return self.__data.content
+        
+        if (status_code):
+            return self.__data.status_code
+        
+        return self.__data
