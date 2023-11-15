0de6fbc16dcd3008a8a252c698379683d67b3bda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 17:04:11 +0000

    Add package darwin-py/0.8.49/darwin_py-0.8.49-py3-none-any.whl

---
+++ packages/darwin-py/darwin_py-0.8.49-py3-none-any.whl/darwin/future/meta/objects/workflow.py
@@ -0,0 +1,126 @@
+from __future__ import annotations
+
+from typing import List, Optional, Sequence, Union
+from uuid import UUID
+
+from darwin.cli_functions import upload_data
+from darwin.dataset.upload_manager import LocalFile
+from darwin.datatypes import PathLike
+from darwin.future.core.types.query import QueryFilter
+from darwin.future.data_objects.workflow import WFDatasetCore, WFTypeCore, WorkflowCore
+from darwin.future.meta.objects.base import MetaBase
+from darwin.future.meta.queries.item import ItemQuery
+from darwin.future.meta.queries.stage import StageQuery
+
+
+class Workflow(MetaBase[WorkflowCore]):
+    """
+    Workflow Meta object. Facilitates the creation of Query objects, lazy loading of
+    sub fields
+
+    Args:
+        MetaBase (Workflow): Generic MetaBase object expanded by Workflow core object
+            return type
+
+    Returns:
+        _type_: Workflow
+
+    Attributes:
+        name (str): The name of the workflow.
+        id (UUID): The id of the workflow
+        datasets (List[Dataset]): A list of datasets associated with the workflow.
+        stages (StageQuery): Queries stages associated with the workflow.
+
+    Methods:
+        push_from_dataset_stage() -> Workflow:
+            moves all items associated with the dataset stage to the next connected stage
+        upload_files(...): -> Workflow:
+            Uploads files to the dataset stage of the workflow
+
+    Example Usage:
+        # Get the workflow object
+        workflow = client.team.workflows.where(name='test').collect_one()
+
+        # Get the stages associated with the workflow
+        stages = workflow.stages
+
+        # Get the datasets associated with the workflow
+        datasets = workflow.datasets
+    """
+
+    @property
+    def items(self) -> ItemQuery:
+        return ItemQuery(
+            self.client,
+            meta_params=self.meta_params,
+            filters=[QueryFilter(name="workflow_id", param=str(self.id))],
+        )
+
+    @property
+    def stages(self) -> StageQuery:
+        meta_params = self.meta_params.copy()
+        meta_params["workflow_id"] = self._element.id
+        if self.datasets is not None:
+            meta_params["dataset_id"] = self.datasets[0].id
+            meta_params["dataset_name"] = self.datasets[0].name
+        return StageQuery(self.client, meta_params=meta_params)
+
+    @property
+    def datasets(self) -> List[WFDatasetCore]:
+        if self._element.dataset is None:
+            raise ValueError("WorkflowMeta has no associated dataset")
+        return [self._element.dataset]
+
+    @property
+    def id(self) -> UUID:
+        return self._element.id
+
+    @property
+    def name(self) -> str:
+        return self._element.name
+
+    def push_from_dataset_stage(self) -> Workflow:
+        assert self._element.dataset is not None
+        stages = self.stages
+        ds_stage = stages[0]
+        assert len(stages) > 1
+        assert ds_stage._element.type == WFTypeCore.DATASET
+        next_stage = ds_stage._element.edges[0].target_stage_id
+        assert next_stage is not None
+        ds_stage.move_attached_files_to_stage(next_stage)
+        return self
+
+    def upload_files(
+        self,
+        files: Sequence[Union[PathLike, LocalFile]],
+        files_to_exclude: Optional[List[PathLike]] = None,
+        fps: int = 1,
+        path: Optional[str] = None,
+        frames: bool = False,
+        extract_views: bool = False,
+        preserve_folders: bool = False,
+        verbose: bool = False,
+        auto_push: bool = True,
+    ) -> Workflow:
+        assert self._element.dataset is not None
+        upload_data(
+            self.datasets[0].name,
+            files,  # type: ignore
+            files_to_exclude,
+            fps,
+            path,
+            frames,
+            extract_views,
+            preserve_folders,
+            verbose,
+        )
+        if auto_push:
+            self.push_from_dataset_stage()
+        return self
+
+    def __str__(self) -> str:
+        return f"Workflow\n\
+- Workflow Name: {self._element.name}\n\
+- Workflow ID: {self._element.id}\n\
+- Connected Dataset ID: {self.datasets[0].id}\n\
+- Conneted Dataset Name: {self.datasets[0].name}"
