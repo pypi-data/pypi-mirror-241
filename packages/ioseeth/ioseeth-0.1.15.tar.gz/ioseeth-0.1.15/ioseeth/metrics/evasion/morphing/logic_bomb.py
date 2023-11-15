1af8d29d89b3992b9ba5401e846474e0231d90d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 03:05:45 +0000

    Add package ioseeth/0.1.15/ioseeth-0.1.15.tar.gz

---
+++ packages/ioseeth/ioseeth-0.1.15.tar.gz/ioseeth-0.1.15/ioseeth/metrics/evasion/morphing/logic_bomb.py
@@ -0,0 +1,26 @@
+"""Evaluate the probability that multiple transfers were bundled in a transaction."""
+
+from web3 import Web3
+
+import ioseeth.metrics.probabilities
+import ioseeth.parsing.bytecode
+
+# CONSTANTS ###################################################################
+
+UNUSUAL_OPCODES = (ioseeth.parsing.bytecode.BLOCKHASH,)
+RED_PILL_OPCODES = (ioseeth.parsing.bytecode.COINBASE,)
+
+# RED PILL ####################################################################
+
+def is_red_pill(
+    bytecode: str,
+    **kwargs
+) -> float:
+    """Evaluate the probability that a contract is trying to evade simulation environments."""
+    __scores = []
+    # opcodes used to detect simulation environments
+    __scores.append(ioseeth.metrics.probabilities.indicator_to_probability(
+        indicator=ioseeth.parsing.bytecode.bytecode_has_specific_opcodes(bytecode=bytecode, opcodes=RED_PILL_OPCODES, check=any),
+        true_score=0.6,
+        false_score=0.5))
+    return ioseeth.metrics.probabilities.conflation(__scores)
