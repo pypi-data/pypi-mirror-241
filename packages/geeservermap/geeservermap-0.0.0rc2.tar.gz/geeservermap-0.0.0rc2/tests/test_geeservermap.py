7c919add73bd0ea994bca72c02de2b9a20087ee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 04:06:16 +0000

    Add package geeservermap/0.0.0rc2/geeservermap-0.0.0rc2.tar.gz

---
+++ packages/geeservermap/geeservermap-0.0.0rc2.tar.gz/geeservermap-0.0.0rc2/tests/test_geeservermap.py
@@ -0,0 +1,8 @@
+"""Test the geeservermap package."""
+
+import geeservermap
+
+
+def test_author():
+    """Default test to check author exist."""
+    assert geeservermap.__author__ == "LDC Research Repository"
