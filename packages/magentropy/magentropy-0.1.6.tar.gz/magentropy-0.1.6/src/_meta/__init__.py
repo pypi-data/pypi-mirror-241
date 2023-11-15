15b3a5c2c7cb580e5be6f45bd350de6944a167fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 17:04:33 +0000

    Add package magentropy/0.1.6/magentropy-0.1.6.tar.gz

---
+++ packages/magentropy/magentropy-0.1.6.tar.gz/magentropy-0.1.6/src/_meta/__init__.py
@@ -0,0 +1,10 @@
+'''MagentroPy package metadata.'''
+
+# keep consistent with pyproject.toml
+__project__ = 'MagentroPy'
+__author__ = 'Soren Bear'
+__email__ = 'sorenbear@berkeley.edu'
+__copyright__ = f'2022-2023, {__author__}'
+__license__ = 'MIT'
+__version__= '0.1.6'
+__repo_url__ = 'https://github.com/sorenbear/magentropy'
