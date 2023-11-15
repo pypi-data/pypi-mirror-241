ec706449acf508726e54aa8b1edc7e3a3dd602f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 05:03:59 +0000

    Add package fastapi-simple-pagination/1.4.0/fastapi_simple_pagination-1.4.0-py3-none-any.whl

---
+++ packages/fastapi-simple-pagination/fastapi_simple_pagination-1.4.0-py3-none-any.whl/fastapi_simple_pagination/__init__.py
@@ -0,0 +1,10 @@
+from .cursor_pagination import CursorPaginationParams
+from .deps import PaginationRequestParams
+from .schemas import CursorPage, Page
+
+__all__ = [
+    "PaginationRequestParams",
+    "Page",
+    "CursorPage",
+    "CursorPaginationParams",
+]
