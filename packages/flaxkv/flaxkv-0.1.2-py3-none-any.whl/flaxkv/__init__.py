91a0710507f39f986af95e6ce4d966a2cdb2304e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 17:03:49 +0000

    Add package flaxkv/0.1.2/flaxkv-0.1.2-py3-none-any.whl

---
+++ packages/flaxkv/flaxkv-0.1.2-py3-none-any.whl/flaxkv/__init__.py
@@ -0,0 +1,18 @@
+from .base import LevelDBDict, LMDBDict
+
+__version__ = "0.1.2"
+
+__all__ = [
+    "dbdict",
+    "LMDBDict",
+    "LevelDBDict",
+]
+
+
+def dbdict(path, backend='lmdb', rebuild=False, **kwargs):
+    if backend == 'lmdb':
+        return LMDBDict(path, rebuild=rebuild, **kwargs)
+    elif backend == 'leveldb':
+        return LevelDBDict(path, rebuild=rebuild, **kwargs)
+    else:
+        raise ValueError(f"Unsupported DB type {backend}.")
