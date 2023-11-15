069b16a824bbca8e7137f82951ad46015ac3a059
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 16:05:07 +0000

    Add package sunholo/0.1.3.20231115065325/sunholo-0.1.3.20231115065325.tar.gz

---
+++ packages/sunholo/sunholo-0.1.3.20231115065325.tar.gz/sunholo-0.1.3.20231115065325/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages
+import datetime
+
+# Define your base version
+base_version = '0.1.3'
+
+timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+version = f"{base_version}.{timestamp}"
+
+
+setup(
+    name='sunholo',
+    version=version,
+    packages=find_packages(),
+    license='Apache License, Version 2.0',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    description='Large Language Model DevOps - a package to help deploy LLMs to the Cloud.',
+    author = 'Sunholo ApS',
+    author_email = 'llmops@sunholo.com',
+    url = 'https://github.com/sunholo-data/sunholo-py',
+    download_url=f'https://github.com/sunholo-data/sunholo-py/archive/refs/tags/v{base_version}.tar.gz',
+    keywords=['llms', 'devops','google_cloud_platform'],
+    package_data={
+        'sunholo.database': ['sql/sb/*.sql']
+    },
+    install_requires=[
+        # List your dependencies here
+        "langchain"
+        # "supabase",
+        # "openai",
+        # "tiktoken",
+        # "google-cloud-storage",
+        # "google-api-python-client",
+        # "google-auth-httplib2",
+        # "google-auth-oauthlib",
+        # "psycopg2-binary"
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Intended Audience :: Developers',      # Define that your audience are developers
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: Apache Software License',   # Again, pick a license
+        'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12'
+    ],
+)
