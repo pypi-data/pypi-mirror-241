8028b469567930b31fdb448b303a123c037b2cb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 15:04:27 +0000

    Add package streamlit-oauth/0.1.0/streamlit-oauth-0.1.0.tar.gz

---
+++ packages/streamlit-oauth/streamlit-oauth-0.1.0.tar.gz/streamlit-oauth-0.1.0/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+from pathlib import Path
+
+README = (Path(__file__).parent/"README.md").read_text()
+
+setuptools.setup(
+    name="streamlit-oauth",
+    version="0.1.0",
+    author="Dylan Lu",
+    author_email="dnplus@gmail.com",
+    description="Simple OAuth2 authorization code flow for Streamlit",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dnplus/streamlit-oauth",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.9",
+    install_requires=[
+        # By definition, a Custom Component depends on Streamlit.
+        # If your component has other Python dependencies, list
+        # them here.
+        "streamlit==1.28.2",
+        "httpx-oauth==0.14.0",
+        "python-dotenv==1.0.0"
+    ],
+)
