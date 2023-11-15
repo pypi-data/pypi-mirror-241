3a8c000fbd3850e8cf1393e5d53019a80893336c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 04:05:30 +0000

    Add package edvinspythonlib/0.1.2/edvinspythonlib-0.1.2-py3-none-any.whl

---
+++ packages/edvinspythonlib/edvinspythonlib-0.1.2-py3-none-any.whl/edvinspythonlib/machinelearning.py
@@ -0,0 +1,16 @@
+
+'''
+Functions and classes for course Machine Learning and Data Mining
+'''
+
+def multiplication(x,y):
+    '''Multiplies 2 numbers'''
+    return x*y
+
+def addition(x,y):
+    '''Adds 2 numbers'''
+    return x+y
+
+def subtraction(x,y):
+    '''Subtracts 2 numbers'''
+    return x-y
