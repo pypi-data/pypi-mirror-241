7d7a01aa66afdb36bc6be7e0764c9e9fe0a93fc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 03:04:53 +0000

    Add package xtgeo/3.4.2/xtgeo-3.4.2-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/xtgeo/xtgeo-3.4.2-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/xtgeo/plot/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""The XTGeo plot package"""
+
+
+from xtgeo.plot.grid3d_slice import Grid3DSlice
+
+# flake8: noqa
+from xtgeo.plot.xsection import XSection
+from xtgeo.plot.xtmap import Map
+
+# from ._colortables import random, random40, xtgeocolors, colorsfromfile
