3ed97741940f012d179402538d290b587d04fdff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 04:06:12 +0000

    Add package faradaysec/4.6.2/faradaysec-4.6.2.tar.gz

---
+++ packages/faradaysec/faradaysec-4.6.2.tar.gz/faradaysec-4.6.2/faraday/__init__.py
@@ -0,0 +1,8 @@
+"""
+Faraday Penetration Test IDE
+Copyright (C) 2013  Infobyte LLC (https://faradaysec.com/)
+See the file 'doc/LICENSE' for the license information
+"""
+
+__version__ = '4.6.2'
+__license_version__ = __version__
