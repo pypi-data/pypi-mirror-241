ff785f6aec0402c829c0657806b1a87588c5eecb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 03:03:29 +0000

    Add package whisper-client/0.2.14/whisper_client-0.2.14.tar.gz

---
+++ packages/whisper-client/whisper_client-0.2.14.tar.gz/whisper_client-0.2.14/src/whisper_client/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2023-present Marceau-h <pypi@marceau-h.fr>
+#
+# SPDX-License-Identifier: AGPL-V3-or-later
+#
+# from whisper_client.main import WhisperClient
+#
+# __all__ = ["WhisperClient"]
