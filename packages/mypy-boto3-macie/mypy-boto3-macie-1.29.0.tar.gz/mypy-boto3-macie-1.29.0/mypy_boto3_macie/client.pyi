a7522bf58afab7f492feb81d92c4f1abe400ebab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 07:04:54 +0000

    Add package mypy-boto3-macie/1.29.0/mypy-boto3-macie-1.29.0.tar.gz

---
+++ packages/mypy-boto3-macie/mypy-boto3-macie-1.29.0.tar.gz/mypy-boto3-macie-1.29.0/mypy_boto3_macie/client.pyi
@@ -0,0 +1,196 @@
+"""
+Type annotations for macie service client.
+
+[Open documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_macie/client/)
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from mypy_boto3_macie.client import MacieClient
+
+    session = Session()
+    client: MacieClient = session.client("macie")
+    ```
+"""
+
+import sys
+from typing import Any, Dict, Mapping, Sequence, Type, overload
+
+from botocore.client import BaseClient, ClientMeta
+
+from .paginator import ListMemberAccountsPaginator, ListS3ResourcesPaginator
+from .type_defs import (
+    AssociateS3ResourcesResultTypeDef,
+    DisassociateS3ResourcesResultTypeDef,
+    EmptyResponseMetadataTypeDef,
+    ListMemberAccountsResultTypeDef,
+    ListS3ResourcesResultTypeDef,
+    S3ResourceClassificationTypeDef,
+    S3ResourceClassificationUpdateTypeDef,
+    S3ResourceTypeDef,
+    UpdateS3ResourcesResultTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+__all__ = ("MacieClient",)
+
+class BotocoreClientError(Exception):
+    MSG_TEMPLATE: str
+
+    def __init__(self, error_response: Mapping[str, Any], operation_name: str) -> None:
+        self.response: Dict[str, Any]
+        self.operation_name: str
+
+class Exceptions:
+    AccessDeniedException: Type[BotocoreClientError]
+    ClientError: Type[BotocoreClientError]
+    InternalException: Type[BotocoreClientError]
+    InvalidInputException: Type[BotocoreClientError]
+    LimitExceededException: Type[BotocoreClientError]
+
+class MacieClient(BaseClient):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/macie.html#Macie.Client)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_macie/client/)
+    """
+
+    meta: ClientMeta
+
+    @property
+    def exceptions(self) -> Exceptions:
+        """
+        MacieClient exceptions.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/macie.html#Macie.Client.exceptions)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_macie/client/#exceptions)
+        """
+
+    def associate_member_account(self, *, memberAccountId: str) -> EmptyResponseMetadataTypeDef:
+        """
+        (Discontinued) Associates a specified Amazon Web Services account with Amazon
+        Macie Classic as a member
+        account.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/macie.html#Macie.Client.associate_member_account)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_macie/client/#associate_member_account)
+        """
+
+    def associate_s3_resources(
+        self, *, s3Resources: Sequence[S3ResourceClassificationTypeDef], memberAccountId: str = ...
+    ) -> AssociateS3ResourcesResultTypeDef:
+        """
+        (Discontinued) Associates specified S3 resources with Amazon Macie Classic for
+        monitoring and data
+        classification.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/macie.html#Macie.Client.associate_s3_resources)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_macie/client/#associate_s3_resources)
+        """
+
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        Check if an operation can be paginated.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/macie.html#Macie.Client.can_paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_macie/client/#can_paginate)
+        """
+
+    def close(self) -> None:
+        """
+        Closes underlying endpoint connections.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/macie.html#Macie.Client.close)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_macie/client/#close)
+        """
+
+    def disassociate_member_account(self, *, memberAccountId: str) -> EmptyResponseMetadataTypeDef:
+        """
+        (Discontinued) Removes the specified member account from Amazon Macie Classic.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/macie.html#Macie.Client.disassociate_member_account)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_macie/client/#disassociate_member_account)
+        """
+
+    def disassociate_s3_resources(
+        self, *, associatedS3Resources: Sequence[S3ResourceTypeDef], memberAccountId: str = ...
+    ) -> DisassociateS3ResourcesResultTypeDef:
+        """
+        (Discontinued) Removes specified S3 resources from being monitored by Amazon
+        Macie
+        Classic.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/macie.html#Macie.Client.disassociate_s3_resources)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_macie/client/#disassociate_s3_resources)
+        """
+
+    def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Mapping[str, Any] = ...,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = ...,
+    ) -> str:
+        """
+        Generate a presigned url given a client, its method, and arguments.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/macie.html#Macie.Client.generate_presigned_url)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_macie/client/#generate_presigned_url)
+        """
+
+    def list_member_accounts(
+        self, *, nextToken: str = ..., maxResults: int = ...
+    ) -> ListMemberAccountsResultTypeDef:
+        """
+        (Discontinued) Lists all Amazon Macie Classic member accounts for the current
+        Macie Classic administrator
+        account.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/macie.html#Macie.Client.list_member_accounts)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_macie/client/#list_member_accounts)
+        """
+
+    def list_s3_resources(
+        self, *, memberAccountId: str = ..., nextToken: str = ..., maxResults: int = ...
+    ) -> ListS3ResourcesResultTypeDef:
+        """
+        (Discontinued) Lists all the S3 resources associated with Amazon Macie Classic.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/macie.html#Macie.Client.list_s3_resources)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_macie/client/#list_s3_resources)
+        """
+
+    def update_s3_resources(
+        self,
+        *,
+        s3ResourcesUpdate: Sequence[S3ResourceClassificationUpdateTypeDef],
+        memberAccountId: str = ...
+    ) -> UpdateS3ResourcesResultTypeDef:
+        """
+        (Discontinued) Updates the classification types for the specified S3 resources.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/macie.html#Macie.Client.update_s3_resources)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_macie/client/#update_s3_resources)
+        """
+
+    @overload
+    def get_paginator(
+        self, operation_name: Literal["list_member_accounts"]
+    ) -> ListMemberAccountsPaginator:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/macie.html#Macie.Client.get_paginator)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_macie/client/#get_paginator)
+        """
+
+    @overload
+    def get_paginator(
+        self, operation_name: Literal["list_s3_resources"]
+    ) -> ListS3ResourcesPaginator:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/macie.html#Macie.Client.get_paginator)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_macie/client/#get_paginator)
+        """
---
+++ packages/mypy-boto3-macie/mypy-boto3-macie-1.29.0.tar.gz/mypy-boto3-macie-1.29.0/mypy_boto3_macie/paginator.py
@@ -0,0 +1,75 @@
+"""
+Type annotations for macie service client paginators.
+
+[Open documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_macie/paginators/)
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+
+    from mypy_boto3_macie.client import MacieClient
+    from mypy_boto3_macie.paginator import (
+        ListMemberAccountsPaginator,
+        ListS3ResourcesPaginator,
+    )
+
+    session = Session()
+    client: MacieClient = session.client("macie")
+
+    list_member_accounts_paginator: ListMemberAccountsPaginator = client.get_paginator("list_member_accounts")
+    list_s3_resources_paginator: ListS3ResourcesPaginator = client.get_paginator("list_s3_resources")
+    ```
+"""
+
+from typing import Generic, Iterator, TypeVar
+
+from botocore.paginate import PageIterator, Paginator
+
+from .type_defs import (
+    ListMemberAccountsResultTypeDef,
+    ListS3ResourcesResultTypeDef,
+    PaginatorConfigTypeDef,
+)
+
+__all__ = ("ListMemberAccountsPaginator", "ListS3ResourcesPaginator")
+
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+
+class _PageIterator(Generic[_ItemTypeDef], PageIterator):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+
+class ListMemberAccountsPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/macie.html#Macie.Paginator.ListMemberAccounts)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_macie/paginators/#listmemberaccountspaginator)
+    """
+
+    def paginate(
+        self, *, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> _PageIterator[ListMemberAccountsResultTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/macie.html#Macie.Paginator.ListMemberAccounts.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_macie/paginators/#listmemberaccountspaginator)
+        """
+
+
+class ListS3ResourcesPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/macie.html#Macie.Paginator.ListS3Resources)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_macie/paginators/#lists3resourcespaginator)
+    """
+
+    def paginate(
+        self, *, memberAccountId: str = ..., PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> _PageIterator[ListS3ResourcesResultTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/macie.html#Macie.Paginator.ListS3Resources.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_macie/paginators/#lists3resourcespaginator)
+        """
