ce58091a2c3d635a26ef395826dd6ad8b2e3e02a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 04:06:17 +0000

    Add package ophyd-async/0.2.0/ophyd-async-0.2.0.tar.gz

---
+++ packages/ophyd-async/ophyd-async-0.2.0.tar.gz/ophyd-async-0.2.0/src/ophyd_async/epics/areadetector/writers/_hdffile.py
@@ -0,0 +1,40 @@
+from typing import Iterator, List
+
+from event_model import StreamDatum, StreamResource, compose_stream_resource
+
+from ._hdfdataset import _HDFDataset
+
+
+class _HDFFile:
+    def __init__(self, full_file_name: str, datasets: List[_HDFDataset]) -> None:
+        self._last_emitted = 0
+        self._bundles = [
+            compose_stream_resource(
+                spec="AD_HDF5_SWMR_SLICE",
+                root="/",
+                data_key=ds.name,
+                resource_path=full_file_name,
+                resource_kwargs={
+                    "path": ds.path,
+                    "multiplier": ds.multiplier,
+                    "timestamps": "/entry/instrument/NDAttributes/NDArrayTimeStamp",
+                },
+            )
+            for ds in datasets
+        ]
+
+    def stream_resources(self) -> Iterator[StreamResource]:
+        for bundle in self._bundles:
+            yield bundle.stream_resource_doc
+
+    def stream_data(self, indices_written: int) -> Iterator[StreamDatum]:
+        # Indices are relative to resource
+        if indices_written > self._last_emitted:
+            indices = dict(
+                start=self._last_emitted,
+                stop=indices_written,
+            )
+            self._last_emitted = indices_written
+            for bundle in self._bundles:
+                yield bundle.compose_stream_datum(indices)
+        return None
---
+++ packages/ophyd-async/ophyd-async-0.2.0.tar.gz/ophyd-async-0.2.0/tests/core/test_device_collector.py
@@ -0,0 +1,16 @@
+import pytest
+
+from ophyd_async.core import Device, DeviceCollector, NotConnected
+
+
+class Dummy(Device):
+    async def connect(self, sim: bool = False):
+        raise AttributeError()
+
+
+def test_device_collector_does_not_propagate_error(RE):
+    with pytest.raises(NotConnected) as exc:
+        with DeviceCollector():
+            _ = Dummy("somename")
+
+    assert not exc.value.__cause__
