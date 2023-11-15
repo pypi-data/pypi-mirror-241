f7f20b1ab4484c337fb5138f9ae2bce1ef33339d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 06:05:07 +0000

    Add package scdiffeq/0.0.53rc0/scdiffeq-0.0.53rc0.tar.gz

---
+++ packages/scdiffeq/scdiffeq-0.0.53rc0.tar.gz/scdiffeq-0.0.53rc0/scdiffeq/io/__init__.py
@@ -0,0 +1,11 @@
+
+__module_name__ = "__init__.py"
+__doc__ = """I/O __init__ module. Sub-package of the main scdiffeq API."""
+__author__ = ", ".join(["Michael E. Vinyard"])
+__email__ = ", ".join(["mvinyard.ai@gmail.com"])
+
+
+# import functions accessed as sdq.io.<func>: --------------------------------------------
+from ._read_h5ad import read_h5ad
+from ._pickle_io import read_pickle, write_pickle
+from ._model_loader import load_diffeq, load_model, ModelLoader
