6f8bd2f617623d7fa344bc4d98ac24e4ae5f32ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 14:03:15 +0000

    Add package streamlit-superapp/1.1.32/streamlit_superapp-1.1.32-py3-none-any.whl

---
+++ packages/streamlit-superapp/streamlit_superapp-1.1.32-py3-none-any.whl/streamlit_superapp/__init__.py
@@ -0,0 +1,41 @@
+try:
+    import streamlit as st
+except ModuleNotFoundError:
+    raise ModuleNotFoundError(
+        "Streamlit is not installed. Please install it with `pip install streamlit`."
+    )
+
+try:
+    st.experimental_get_query_params
+except AttributeError:
+    raise AttributeError(
+        "Streamlit version is too old. Please upgrade it with `pip install streamlit --upgrade`."
+    )
+
+try:
+    st.session_state
+except AttributeError:
+    raise AttributeError(
+        "Streamlit version is too old. Please upgrade it with `pip install streamlit --upgrade`."
+    )
+
+
+from streamlit_superapp.navigation import Navigation
+from streamlit_superapp.state import State
+from streamlit_superapp.page import Page
+
+
+def run(
+    hide_index_description: bool = False,
+    hide_home_button: bool = False,
+    hide_back_button: bool = False,
+    hide_page_title: bool = False,
+    hide_breadcrumbs: bool = False,
+):
+    Navigation.hide_index_description = hide_index_description
+    Navigation.hide_home_button = hide_home_button
+    Navigation.hide_back_button = hide_back_button
+    Navigation.hide_page_title = hide_page_title
+    Navigation.hide_breadcrumbs = hide_breadcrumbs
+
+    Navigation.initialize()
