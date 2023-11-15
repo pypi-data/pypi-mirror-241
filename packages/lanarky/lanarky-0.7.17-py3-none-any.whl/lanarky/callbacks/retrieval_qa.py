328e9d4da54d7fc55d5b403c5971fbfe6057181b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 03:05:55 +0000

    Add package lanarky/0.7.17/lanarky-0.7.17-py3-none-any.whl

---
+++ packages/lanarky/lanarky-0.7.17-py3-none-any.whl/lanarky/callbacks/retrieval_qa.py
@@ -0,0 +1,99 @@
+from typing import Any
+
+from lanarky.register import (
+    register_streaming_callback,
+    register_streaming_json_callback,
+    register_websocket_callback,
+)
+from lanarky.schemas import BaseRetrievalQAStreamingJSONResponse
+
+from .llm import (
+    AsyncLLMChainStreamingCallback,
+    AsyncLLMChainStreamingJSONCallback,
+    AsyncLLMChainWebsocketCallback,
+)
+
+SUPPORTED_CHAINS = [
+    "RetrievalQA",
+    "ConversationRetrievalQA",
+    "VectorDBQA",
+    "QAWithSourcesChain",
+    "VectorDBQAWithSourcesChain",
+    "RetrievalQAWithSourcesChain",
+    "ConversationalRetrievalChain",
+]
+SOURCE_DOCUMENT_TEMPLATE = """
+page content: {page_content}
+{document_metadata}
+"""
+SOURCE_DOCUMENTS_KEY = "source_documents"
+
+
+@register_streaming_callback(SUPPORTED_CHAINS)
+class AsyncBaseRetrievalQAStreamingCallback(AsyncLLMChainStreamingCallback):
+    """AsyncStreamingResponseCallback handler for BaseRetrievalQA."""
+
+    source_document_template: str = SOURCE_DOCUMENT_TEMPLATE
+
+    async def on_chain_end(self, outputs: dict[str, Any], **kwargs: Any) -> None:
+        """Run when chain ends running."""
+        if self.llm_cache_enabled:
+            await super().on_chain_end(outputs, **kwargs)
+        if SOURCE_DOCUMENTS_KEY in outputs:
+            message = self._construct_message("\n\nSOURCE DOCUMENTS:\n")
+            await self.send(message)
+            for document in outputs[SOURCE_DOCUMENTS_KEY]:
+                document_metadata = "\n".join(
+                    [f"{k}: {v}" for k, v in document.metadata.items()]
+                )
+                message = self._construct_message(
+                    self.source_document_template.format(
+                        page_content=document.page_content,
+                        document_metadata=document_metadata,
+                    )
+                )
+                await self.send(message)
+
+
+@register_websocket_callback(SUPPORTED_CHAINS)
+class AsyncBaseRetrievalQAWebsocketCallback(AsyncLLMChainWebsocketCallback):
+    """AsyncWebsocketCallback handler for BaseRetrievalQA."""
+
+    source_document_template: str = SOURCE_DOCUMENT_TEMPLATE
+
+    async def on_chain_end(self, outputs: dict[str, Any], **kwargs: Any) -> None:
+        """Run when chain ends running."""
+        if self.llm_cache_enabled:
+            await super().on_chain_end(outputs, **kwargs)
+        if SOURCE_DOCUMENTS_KEY in outputs:
+            message = self._construct_message("\n\nSOURCE DOCUMENTS:\n")
+            await self.websocket.send_json(message)
+            for document in outputs[SOURCE_DOCUMENTS_KEY]:
+                document_metadata = "\n".join(
+                    [f"{k}: {v}" for k, v in document.metadata.items()]
+                )
+                message = self._construct_message(
+                    self.source_document_template.format(
+                        page_content=document.page_content,
+                        document_metadata=document_metadata,
+                    )
+                )
+                await self.websocket.send_json(message)
+
+
+@register_streaming_json_callback(SUPPORTED_CHAINS)
+class AsyncBaseRetrievalQAStreamingJSONCallback(AsyncLLMChainStreamingJSONCallback):
+    """AsyncStreamingJSONResponseCallback handler for BaseRetrievalQA."""
+
+    async def on_chain_end(self, outputs: dict[str, Any], **kwargs: Any) -> None:
+        """Run when chain ends running."""
+        if self.llm_cache_enabled:
+            await super().on_chain_end(outputs, **kwargs)
+        if SOURCE_DOCUMENTS_KEY in outputs:
+            source_documents = [
+                document.dict() for document in outputs[SOURCE_DOCUMENTS_KEY]
+            ]
+            message = self._construct_message(
+                BaseRetrievalQAStreamingJSONResponse(source_documents=source_documents)
+            )
+            await self.send(message)
