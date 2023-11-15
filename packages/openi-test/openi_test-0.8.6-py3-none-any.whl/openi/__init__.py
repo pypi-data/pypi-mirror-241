46ec2ac4f7c0e6c2595d1da216cd132b9d62da91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 15:04:45 +0000

    Add package openi-test/0.8.6/openi_test-0.8.6-py3-none-any.whl

---
+++ packages/openi-test/openi_test-0.8.6-py3-none-any.whl/openi/__init__.py
@@ -0,0 +1,5 @@
+from .context import *
+from .utils import *
+from .login import login, whoami, logout
+from .dataset import upload_file, download_file
+from .model import upload_model, download_model
