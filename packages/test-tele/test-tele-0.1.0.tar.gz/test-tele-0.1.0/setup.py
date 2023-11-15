7a2d5b63b717a549cc403652c6a86f6efe5d7c64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 15:04:36 +0000

    Add package test-tele/0.1.0/test-tele-0.1.0.tar.gz

---
+++ packages/test-tele/test-tele-0.1.0.tar.gz/test-tele-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='test-tele',
+    version='0.1.0',
+    description='Aahnik tgcf but with more beta functions',
+    packages=find_packages(),
+    install_requires=[],
+    python_requires='>=3.10',
+)
