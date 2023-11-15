0569ba7579f5a3c12a2ffec8df0fac1533c7e751
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 03:05:44 +0000

    Add package example-package-a155/0.0.6/example_package_a155-0.0.6.tar.gz

---
+++ packages/example-package-a155/example_package_a155-0.0.6.tar.gz/example_package_a155-0.0.6/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='example_package_a155',
+    version='0.0.6',
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A short description of your package',
+    packages=find_packages(),
+    install_requires= ['web3', 'requests'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
