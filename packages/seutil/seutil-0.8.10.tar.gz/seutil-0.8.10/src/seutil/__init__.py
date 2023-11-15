811d912f23c13921ae845aaf2d3d36dda524fd70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 14:03:44 +0000

    Add package seutil/0.8.10/seutil-0.8.10.tar.gz

---
+++ packages/seutil/seutil-0.8.10.tar.gz/seutil-0.8.10/src/seutil/__init__.py
@@ -0,0 +1,44 @@
+import os
+import sys
+
+module_root = os.path.dirname(os.path.realpath(__file__)) + "/.."
+if module_root not in sys.path:
+    sys.path.insert(0, module_root)
+
+import lazy_import
+
+from . import arg, bash, io, log, pbar
+from .BashUtils import BashUtils
+from .GitHubUtils import GitHubUtils
+from .LoggingUtils import LoggingUtils
+from .TimeUtils import TimeoutException, TimeUtils
+
+project = lazy_import.lazy_module("seutil.project")
+latex = lazy_import.lazy_module("seutil.latex")
+ds = lazy_import.lazy_module("seutil.ds")
+
+# tricks the IDE to recognize the lazy imports, so that it can provide code completion
+# won't be executed
+if 1.0 == 1.01:
+    from . import ds, latex, project
+
+
+__all__ = [
+    "arg",
+    "bash",
+    "io",
+    "log",
+    "latex",
+    "project",
+    "ds",
+    "GitHubUtils",
+    "MiscUtils",
+    "Stream",
+    "TimeUtils",
+    "TimeoutException",
+]
+
+# Remove temporary names
+del os
+del sys
+del module_root
