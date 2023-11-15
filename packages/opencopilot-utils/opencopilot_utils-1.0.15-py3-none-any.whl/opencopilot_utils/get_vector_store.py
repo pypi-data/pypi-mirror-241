773a0b410ba56009290c0ce1121998684c114fc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 03:04:24 +0000

    Add package opencopilot-utils/1.0.15/opencopilot_utils-1.0.15-py3-none-any.whl

---
+++ packages/opencopilot-utils/opencopilot_utils-1.0.15-py3-none-any.whl/opencopilot_utils/get_vector_store.py
@@ -0,0 +1,37 @@
+import os
+from langchain.vectorstores.pinecone import Pinecone
+
+from langchain.vectorstores.qdrant import Qdrant
+from langchain.vectorstores.base import VectorStore
+
+from .configs import ENV_CONFIGS
+from .store_type import StoreType
+from .interfaces import StoreOptions
+from .get_embeddings import get_embeddings
+
+import qdrant_client
+
+
+def get_vector_store(options: StoreOptions) -> VectorStore:
+    """Gets the vector store for the given options."""
+    vector_store: VectorStore
+    embedding = get_embeddings()
+
+    store_type = os.environ.get("STORE")
+
+    if store_type == StoreType.QDRANT.value:
+        client = qdrant_client.QdrantClient(
+            url=ENV_CONFIGS.QDRANT_URL,
+            prefer_grpc=True,
+            api_key=os.getenv("QDRANT_API_KEY", None),
+        )
+
+        vector_store = Qdrant(
+            client, collection_name=options.namespace, embeddings=embedding
+        )
+        # vector_store = Qdrant.from_documents([], embedding, url='http://localhost:6333', collection=options.namespace)
+
+    else:
+        raise ValueError("Invalid STORE environment variable value")
+
+    return vector_store
