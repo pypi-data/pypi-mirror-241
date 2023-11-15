80d1cde4249c581f42208a462e814d096ac82db4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 04:06:10 +0000

    Add package idellify/0.1.0/idellify-0.1.0.tar.gz

---
+++ packages/idellify/idellify-0.1.0.tar.gz/idellify-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='idellify',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'pagen = pagen:main',
+        ],
+    },
+
+)
