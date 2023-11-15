5f59864f8b0ce35837d86aa3dcf32cb4e89e1502
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 03:04:10 +0000

    Add package ngwidgets/0.3.6/ngwidgets-0.3.6.tar.gz

---
+++ packages/ngwidgets/ngwidgets-0.3.6.tar.gz/ngwidgets-0.3.6/tests/test_widgets.py
@@ -0,0 +1,20 @@
+'''
+Created on 2023-09-10
+
+@author: wf
+'''
+from ngwidgets.basetest import Basetest
+from ngwidgets.widgets import Link
+
+class TestNiceGuiWidgets(Basetest):
+    """
+    test nice gui widgets
+    """
+    
+    def test_widgets(self):
+        link_html=Link.create("http://nicegui.io", "nicegui", "nicegui")
+        if self.debug:
+            print(link_html)
+        expected="<a href='http://nicegui.io' title='nicegui'>nicegui</a>"
+        self.assertEqual(expected,link_html)
+
