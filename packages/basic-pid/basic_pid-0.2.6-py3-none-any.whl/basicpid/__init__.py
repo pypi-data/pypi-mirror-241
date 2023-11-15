76bb9b7f16b89469e313e640089e1967a3188f0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 14:03:28 +0000

    Add package basic-pid/0.2.6/basic_pid-0.2.6-py3-none-any.whl

---
+++ packages/basic-pid/basic_pid-0.2.6-py3-none-any.whl/basicpid/__init__.py
@@ -0,0 +1,21 @@
+
+"""
+basic-pid - PID Controller for discretized time regulators
+"""
+
+# flit uses __version__ as one source version 
+# setup.cfg uses version = attr: robopid.__version__ 
+# note: pypi pep versions like this form > 'x.y.<number>[a-c]<digit>'
+# but 0.01.08d doesnt fly building from pyproject.toml
+
+
+__version__ = 'v0.02.06'
+
+# import acts to export 
+
+from .pid import BasicPid
+from .pid import BasicPid as Pid
+from .pid import BasicPid as BasicPID
+
+
+
