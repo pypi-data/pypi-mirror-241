588306986e2aee2b3ee16159b2cb09055f13fa30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 06:04:53 +0000

    Add package stateless/0.2.0/stateless-0.2.0.tar.gz

---
+++ packages/stateless/stateless-0.2.0.tar.gz/stateless-0.2.0/src/stateless/__init__.py
@@ -0,0 +1,15 @@
+from stateless.effect import (
+    Effect,
+    Depend,
+    Try,
+    throw,
+    catch,
+    depend,
+    throws,
+    success,
+    Success,
+)
+from stateless.parallel import parallel, task
+from stateless.schedule import Schedule
+from stateless.runtime import Runtime
+from stateless.functions import repeat
