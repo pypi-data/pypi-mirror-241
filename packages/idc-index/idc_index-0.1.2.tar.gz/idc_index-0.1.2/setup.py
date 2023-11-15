24b5ed6c88135ee34f86cc4d9de931ea10671dc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 06:04:43 +0000

    Add package idc-index/0.1.2/idc_index-0.1.2.tar.gz

---
+++ packages/idc-index/idc_index-0.1.2.tar.gz/idc_index-0.1.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+# Read long description from README
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='idc_index',
+    version='0.1.2',
+    packages=find_packages(),
+    include_package_data=True,  
+    install_requires=['pandas', 'requests'],
+    # Metadata
+    author='Vamsi Thiriveedhi',
+    author_email='vthiriveedhi@mgb.org',
+    description='Package to query and download data from an index of ImagingDataCommons',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/vkt1414/idc_index',
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent"]
+)
