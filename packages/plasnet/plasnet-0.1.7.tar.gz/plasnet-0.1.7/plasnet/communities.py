7794705f861f0189177c3eb7f87e8aadc903e563
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 03:05:57 +0000

    Add package plasnet/0.1.7/plasnet-0.1.7.tar.gz

---
+++ packages/plasnet/plasnet-0.1.7.tar.gz/plasnet-0.1.7/plasnet/communities.py
@@ -0,0 +1,10 @@
+# TODO: refactor
+from plasnet.community_graph import CommunityGraph
+from plasnet.list_of_graphs import ListOfGraphs
+from plasnet.utils import DistanceDict
+
+
+class Communities(ListOfGraphs[CommunityGraph]):
+    def filter_by_distance(self, distance_dict: DistanceDict, distance_threshold: float) -> None:
+        for community in self:
+            community.filter_by_distance(distance_dict, distance_threshold)
