f42609ef5dde7b2b27719894d3c763448f7f82a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 04:05:29 +0000

    Add package json2csv-esih/0.1.0/json2csv_esih-0.1.0.tar.gz

---
+++ packages/json2csv-esih/json2csv_esih-0.1.0.tar.gz/json2csv_esih-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='json2csv_esih',
+    version='0.1.0',
+    description='Convert JSON to CSV',
+    author='Py0095 & Rubenson',
+    packages=find_packages(),  
+    install_requires=[],  
+    entry_points={ 
+        'console_scripts': [
+            'json2csv = json2csv.cli:main',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
