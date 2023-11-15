26a5f489c3242b3534b90bd50b1f2eaf6722716f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 04:06:10 +0000

    Add package extrapolation/2.1.1/extrapolation-2.1.1.tar.gz

---
+++ packages/extrapolation/extrapolation-2.1.1.tar.gz/extrapolation-2.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+VERSION = '2.1.1'
+DESCRIPTION = 'Extrapolation methods to real series'
+
+# Setting up
+setup(
+    name="extrapolation",
+    version=VERSION,
+    author="Wellington Silva",
+    author_email="<wellington.71319@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=open('README.md').read(),
+    url="https://github.com/wellington36/extrapolation_methods",
+    license='MIT',
+    packages=find_packages(),
+    install_requires=['mpmath'],
+    keywords=['python', 'series', 'extrapolation', 'numerical-analysis']
+)
