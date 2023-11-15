968538853a327c46684f02196f821e0911f55deb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 06:05:59 +0000

    Add package mknodes/0.50.2/mknodes-0.50.2-py3-none-any.whl

---
+++ packages/mknodes/mknodes-0.50.2-py3-none-any.whl/mknodes/templatenodes/mkpullrequestguidelines/__init__.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+from typing import Any
+
+from mknodes.templatenodes import mktemplate
+from mknodes.utils import log
+
+
+logger = log.get_logger(__name__)
+
+
+class MkPullRequestGuidelines(mktemplate.MkTemplate):
+    """Node showing pull request guidelines."""
+
+    ICON = "octicons/git-pull-request-24"
+
+    def __init__(self, **kwargs: Any):
+        """Constructor.
+
+        Arguments:
+            kwargs: Keyword arguments passed to parent
+        """
+        super().__init__(template="pullrequest_guidelines.jinja", **kwargs)
+
+
+if __name__ == "__main__":
+    guideline = MkPullRequestGuidelines()
+    print(guideline)
