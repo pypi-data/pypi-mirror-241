9da7f18ed14ea28c4a21a96ebf7b16c2b8e79ec7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 05:03:50 +0000

    Add package pozalabs-compose/0.20.0/pozalabs-compose-0.20.0.tar.gz

---
+++ packages/pozalabs-compose/pozalabs-compose-0.20.0.tar.gz/pozalabs-compose-0.20.0/compose/messaging/__init__.py
@@ -0,0 +1,13 @@
+from .consumer import MessageConsumer
+from .consumer_runner import MessageConsumerThreadRunner
+from .model import EventMessage, SqsEventMessage
+from .queue import MessageQueue, SqsMessageQueue
+
+__all__ = [
+    "EventMessage",
+    "SqsEventMessage",
+    "MessageQueue",
+    "SqsMessageQueue",
+    "MessageConsumer",
+    "MessageConsumerThreadRunner",
+]
