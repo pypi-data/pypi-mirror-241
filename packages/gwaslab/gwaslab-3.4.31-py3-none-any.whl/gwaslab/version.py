865c8a4c86459b9cdc512946e48d33e05821dd29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 04:05:44 +0000

    Add package gwaslab/3.4.31/gwaslab-3.4.31-py3-none-any.whl

---
+++ packages/gwaslab/gwaslab-3.4.31-py3-none-any.whl/gwaslab/version.py
@@ -0,0 +1,14 @@
+from gwaslab.Log import Log
+
+def _show_version(log=Log()):
+    # show when loading sumstats
+    log.write("GWASLab v{} https://cloufield.github.io/gwaslab/".format(gwaslab_info()["version"]))
+    log.write("(C) 2022-2023, Yunye He, Kamatani Lab, MIT License, gwaslab@gmail.com")
+
+def gwaslab_info():
+    # for output header
+    dic={
+   "version":"3.4.31",
+   "release_date":"20231113"
+    }
+    return dic   
