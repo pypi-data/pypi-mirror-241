2194676c60d06b4c29d29c18532c9f7a9ee4d526
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 16:06:28 +0000

    Add package autodp/0.2.3.1/autodp-0.2.3.1.tar.gz

---
+++ packages/autodp/autodp-0.2.3.1.tar.gz/autodp-0.2.3.1/setup.py
@@ -0,0 +1,45 @@
+
+from setuptools import setup
+import os
+
+here = os.path.dirname(os.path.abspath(__file__))
+
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+def _parse_requirements(path):
+  """Parses requirements from file."""
+  with open(os.path.join(here, path)) as f:
+    deps = []
+    for line in f:
+      deps.append(line.rstrip())
+    return depss
+
+setup(
+   name='autodp',
+   version='0.2.3.1',
+   description='Automating Differential Privacy Computation',
+   license="Apache",
+   long_description="The package helps researchers and developers to correctly use advanced methods in differential privacy and obtain provable DP guarantees. The core of the package is an analytical moments accountant that keeps track of Renyi Differential Privacy in analytical forms.",
+   author='Yu-Xiang Wang',
+   author_email='yuxiangw@cs.ucsb.edu',
+   url='https://github.com/yuxiangw/autodp',
+   download_url = 'https://github.com/yuxiangw/autodp/archive/refs/tags/v0.2.3.tar.gz',   
+   keywords = ['Differential Privacy','Moments Accountant','Renyi Differential Privacy'],
+   packages=['autodp'],  #same as name
+   install_requires=[
+     'absl-py',
+     'matplotlib',
+     'numpy>=1.18',
+     'scipy>=1.2.1'
+   ], #external packages as dependencies
+   classifiers=['Development Status :: 3 - Alpha',
+   'Intended Audience :: Developers',
+   'Intended Audience :: Science/Research',
+   'Topic :: Scientific/Engineering :: Artificial Intelligence',
+   'Topic :: Security',
+   'License :: OSI Approved :: Apache Software License',
+   'Programming Language :: Python :: 3',
+   ],
+)
