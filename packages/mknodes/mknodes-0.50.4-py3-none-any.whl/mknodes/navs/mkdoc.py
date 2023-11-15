4a636e9cfff1ad64fbfdc62702b6cf14e831c445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 06:06:01 +0000

    Add package mknodes/0.50.4/mknodes-0.50.4-py3-none-any.whl

---
+++ packages/mknodes/mknodes-0.50.4-py3-none-any.whl/mknodes/navs/mkdoc.py
@@ -0,0 +1,192 @@
+from __future__ import annotations
+
+from collections.abc import Callable, Iterator, Sequence
+import types
+
+from typing import Any
+
+from mknodes.navs import mknav
+from mknodes.pages import mkclasspage, mkmodulepage
+from mknodes.utils import classhelpers, log, reprhelpers
+
+
+logger = log.get_logger(__name__)
+
+
+class MkDoc(mknav.MkNav):
+    """Nav for showing a module documenation."""
+
+    def __init__(
+        self,
+        module: types.ModuleType | Sequence[str] | str | None = None,
+        *,
+        filter_by___all__: bool = False,
+        exclude_modules: list[str] | None = None,
+        section_name: str | None = None,
+        recursive: bool = False,
+        class_page: type[mkclasspage.MkClassPage] | str | None = None,
+        module_page: type[mkmodulepage.MkModulePage] | str | None = None,
+        flatten_nav: bool = False,
+        **kwargs: Any,
+    ):
+        """Constructor.
+
+        Arguments:
+            module: Module to document
+            filter_by___all__: Whether to filter stuff according to "__all__"
+            recursive: Whether to search modules recursively
+            exclude_modules: List of modules to exclude
+            section_name: Optional section title override
+            class_page: Override for the default ClassPage
+            module_page: Override for the default ModulePage
+            flatten_nav: Whether classes should be put into top-level of the nav
+            kwargs: Keyword arguments passed to parent
+        """
+        if module:
+            self._module = classhelpers.to_module(module, return_none=False)
+        else:
+            self._module = None
+        self.ClassPage = class_page or mkclasspage.MkClassPage
+        self.ModulePage = module_page or mkmodulepage.MkModulePage
+        self.flatten_nav = flatten_nav
+        self.recursive = recursive
+        self.klasses: set[type] = set()
+        self.submodules: set[types.ModuleType] = set()
+        self.filter_by___all__ = filter_by___all__
+        self._exclude = exclude_modules or []
+        # self.root_path = pathlib.Path(f"./{self.module_name}")
+        super().__init__(**kwargs)
+        self.title = section_name or self.module_name
+        self._collect_classes()
+
+    def __repr__(self):
+        return reprhelpers.get_repr(
+            self,
+            module=self.module_name,
+            section=self.title or "<root>",
+            filename=self.filename,
+        )
+
+    @property
+    def module(self):
+        return self.ctx.metadata.module if self._module is None else self._module
+
+    @property
+    def module_name(self) -> str:
+        return self.module.__name__.split(".")[-1] if self.module else ""
+
+    def _collect_classes(self):
+        """Collect classes from given module."""
+        for klass in self.iter_classes(recursive=self.recursive):
+            self.klasses.add(klass)
+        for klass in self.klasses:
+            self.add_class_page(klass=klass, flatten=self.flatten_nav)
+        for submod in self.submodules:
+            self.add_doc(
+                submod,
+                class_page=self.ClassPage,
+                filter_by___all__=self.filter_by___all__,
+                module_page=self.ModulePage,
+                flatten_nav=self.flatten_nav,
+            )
+        self._create_index_page()
+
+    def iter_classes(
+        self,
+        submodule: types.ModuleType | str | tuple[str, ...] | list[str] | None = None,
+        *,
+        recursive: bool = False,
+        predicate: Callable[[type], bool] | None = None,
+    ) -> Iterator[type]:
+        """Iterate over all classes of the module.
+
+        Arguments:
+            submodule: filter based on a submodule
+            recursive: whether to only iterate over members of current module
+                       or whether it should also include classes from submodules.
+            predicate: filter classes based on a predicate.
+        """
+        module = submodule or self.module
+        if module is None:
+            return
+        yield from classhelpers.list_classes(
+            module=tuple(module) if isinstance(module, list) else module,
+            recursive=recursive,
+            filter_by___all__=self.filter_by___all__,
+            predicate=predicate,
+            module_filter=self.module_name,
+        )
+
+    def add_class_page(
+        self,
+        klass: type,
+        *,
+        find_topmost: bool = True,
+        flatten: bool = False,
+        **kwargs: Any,
+    ) -> mkclasspage.MkClassPage:
+        """Add a page showing information about a class.
+
+        Arguments:
+            klass: klass to build a page for
+            find_topmost: Whether to use a module path from a parent package if available
+            flatten: Put page into top level nav if nested.
+            kwargs: keyword arguments passed to CLassPage
+        """
+        if find_topmost:
+            parts = classhelpers.get_topmost_module_path(klass).split(".")
+        else:
+            parts = klass.__module__.split(".")
+        if isinstance(self.ClassPage, str):
+            page = mkclasspage.MkClassPage(
+                klass=klass,
+                template=self.ClassPage,
+                module_path=tuple(parts),
+                parent=self,
+                **kwargs,
+            )
+        else:
+            page = self.ClassPage(klass, module_path=tuple(parts), parent=self, **kwargs)
+        section = (klass.__name__,) if flatten else (*parts[1:], klass.__name__)
+        self.nav[section] = page
+        return page
+
+    def _create_index_page(
+        self,
+        title: str | None = None,
+        **kwargs: Any,
+    ) -> mkmodulepage.MkModulePage:
+        """Add a page showing all submodules.
+
+        Arguments:
+            title: Override title for the section.
+            kwargs: kwargs passed to MkModulePage.
+        """
+        path = "index.md" if title is None else f"{title}.md"
+        if isinstance(self.ModulePage, str):
+            page = mkmodulepage.MkModulePage(
+                module=self.module,
+                title=title or self.module_name,
+                klasses=self.klasses,
+                template_name=self.ModulePage,
+                path=path,
+                parent=self,
+                **kwargs,
+            )
+        else:
+            page = self.ModulePage(
+                title=title or self.module_name,
+                module=self.module,
+                klasses=self.klasses,
+                path=path,
+                parent=self,
+                **kwargs,
+            )
+        self.index_page = page
+        return page
+
+
+if __name__ == "__main__":
+    doc = MkDoc(module="mkdocs")
+    page = doc.add_class_page(MkDoc)
+    print(page)
---
+++ packages/mknodes/mknodes-0.50.4-py3-none-any.whl/mknodes/templatenodes/mktreeview/__init__.py
@@ -0,0 +1,83 @@
+from __future__ import annotations
+
+from collections.abc import Callable
+import os
+import upath
+
+from typing import Any
+
+from mknodes import treelib
+from mknodes.basenodes import mkcode, mknode
+from mknodes.data import treestyles
+from mknodes.utils import log
+
+
+logger = log.get_logger(__name__)
+
+
+class MkTreeView(mkcode.MkCode):
+    """Node to display tree structures.
+
+    Currently supports directories and Node subclasses (including `MkNodes`).
+    """
+
+    ICON = "material/file-tree-outline"
+    STATUS = "new"
+
+    def __init__(
+        self,
+        tree: str | os.PathLike | treelib.Node,
+        *,
+        style: treestyles.TreeStyleStr | tuple[str, str, str, str] = "rounded",
+        maximum_depth: int | None = None,
+        predicate: Callable | None = None,
+        exclude_folders: list[str] | str | None = None,
+        header: str = "",
+        **kwargs: Any,
+    ):
+        """Constructor.
+
+        Arguments:
+            tree: Tree to display. Can be a path to a folder or a Node.
+            style: Print style. If tuple, parts are used for stems
+            maximum_depth: Maximum nesting depth to print
+            predicate: Predicate to filter results
+            exclude_folders: Folders to exclude from listing
+            header: Section header
+            kwargs: Keyword arguments passed to parent
+        """
+        super().__init__(header, language="", **kwargs)
+        self.tree = tree
+        self.style = style
+        self.predicate = predicate
+        self.maximum_depth = maximum_depth
+        self.exclude_folders = (
+            [exclude_folders] if isinstance(exclude_folders, str) else exclude_folders
+        )
+
+    @property
+    def text(self):
+        match self.tree:
+            case str() | os.PathLike():
+                node = treelib.FileTreeNode.from_folder(
+                    upath.UPath(self.tree),
+                    predicate=self.predicate,
+                    exclude_folders=self.exclude_folders,
+                    maximum_depth=self.maximum_depth,
+                )
+            case mknode.MkNode():
+                node = self.tree
+            case _:
+                raise TypeError(self.tree)
+        return node.get_tree_repr(style=self.style, max_depth=self.maximum_depth)
+
+
+if __name__ == "__main__":
+    opts = dict(org="mkdocstrings", repo="mkdocstrings")
+    node = MkTreeView(
+        "github://mknodes",
+        storage_options=opts,
+        style="ascii",
+        maximum_depth=2,
+    )
+    print(node.to_markdown())
