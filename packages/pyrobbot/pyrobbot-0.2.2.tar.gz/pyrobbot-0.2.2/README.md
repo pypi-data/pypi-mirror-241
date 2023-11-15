153d8dda4ba2d5c7af603403212b467b4cf9a15a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 03:03:31 +0000

    Add package pyrobbot/0.2.2/pyrobbot-0.2.2.tar.gz

---
+++ packages/pyrobbot/pyrobbot-0.2.2.tar.gz/pyrobbot-0.2.2/README.md
@@ -0,0 +1,97 @@
+[![Streamlit App](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://pyrobbot.streamlit.app)
+
+[![GitHub](https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white)](https://github.com/paulovcmedeiros/pyRobBot)
+
+[![Contributors Welcome](https://img.shields.io/badge/Contributors-welcome-<COLOR>.svg)](https://github.com/paulovcmedeiros/pyRobBot/pulls)
+[![Linting](https://github.com/paulovcmedeiros/pyRobBot/actions/workflows/linting.yaml/badge.svg)](https://github.com/paulovcmedeiros/pyRobBot/actions/workflows/linting.yaml)
+[![Tests](https://github.com/paulovcmedeiros/pyRobBot/actions/workflows/tests.yaml/badge.svg)](https://github.com/paulovcmedeiros/pyRobBot/actions/workflows/tests.yaml)
+[![codecov](https://codecov.io/gh/paulovcmedeiros/pyRobBot/graph/badge.svg?token=XI8G1WH9O6)](https://codecov.io/gh/paulovcmedeiros/pyRobBot)
+
+# pyRobBot: Talk and Chat with GPT LLMs
+
+An interface to OpenAI's [GPT large language models (LLMs)](https://platform.openai.com/docs/models) that implements:
+* A conventional chatbot that can be used either via web UI or terminal
+* A personal assistant that can actually interact with you by voice
+
+The package is written in Python. The web chatbot UI is made with [Streamlit](https://streamlit.io).
+
+**See and try the [demo web app on Streamlit](https://pyrobbot.streamlit.app)!**
+
+## Features
+- [x] Text to speech and speech to text (`rob voice`)
+  - Talk to the GPT assistant!
+  - You can choose your preferred language (e.g., `rob voice --lang pt-br`)
+- [x] Web UI
+  - Add/remove conversations dynamically
+  - Automatic/editable conversation summary title
+- [x] Fully configurable
+  - Support for multiple GPT LLMs
+  - Control over the parameters passed to the OpenAI API, with (hopefully) sensible defaults
+  - Ability to pass base directives to the LLM
+    - E.g., to make it adopt a persona, but you decide which directived to pass
+  - Dynamically modifiable AI parameters in each chat separately
+    - No need to restart the chat
+- [x] Autosave & retrieve chat history
+- [x] Chat context handling using [embeddings](https://platform.openai.com/docs/guides/embeddings)
+- [x] Estimated API token usage and associated costs
+- [x] OpenAI API key is **never** stored on disk
+
+
+
+## System Requirements
+- Python >= 3.9
+- A valid [OpenAI API key](https://platform.openai.com/account/api-keys)
+  - Set in the Web UI or through the environment variable `OPENAI_API_KEY`
+- Optionally, to enable voice chat, you also need:
+  - [PortAudio](https://www.portaudio.com/docs/v19-doxydocs/index.html)
+    - Install on Ubuntu with `sudo apt-get --assume-yes install portaudio19-dev python-all-dev`
+    - Install on CentOS/RHEL with `sudo yum install portaudio portaudio-devel`
+
+## Installation
+This, naturally, assumes your systems fulfills all [requirements](#system-requirements).
+### Using pip
+```shell
+pip install pyrobbot
+```
+
+### From source
+```shell
+pip install git+https://github.com/paulovcmedeiros/pyRobBot.git
+```
+
+## Basic Usage
+Upon succesfull installation, you should be able to run
+```shell
+rob [opts] SUBCOMMAND [subcommand_opts]
+```
+where `[opts]` and `[subcommand_opts]` denote optional command line arguments
+that apply, respectively, to `rob` in general and to `SUBCOMMAND`
+specifically.
+
+**Please run `rob -h` for information** about the supported subcommands
+and general `rob` options. For info about specific subcommands and the
+options that apply to them only, **please run `rob SUBCOMMAND -h`** (note
+that the `-h` goes after the subcommand in this case).
+
+
+### Using the Web UI
+```shell
+rob
+```
+
+### Chatting by Voice
+```shell
+rob voice
+```
+
+### Running on the Terminal
+```shell
+rob .
+```
+
+## Disclaimers
+This project's main purpose is to serve as a learning exercise for me, as well as tool for experimenting with OpenAI API, GPT LLMs and text-to-voice/voice-to-text. It does not claim to be the best or more robust OpenAI-powered chatbot out there.
+
+Having said this, this project *does* aim to provide a friendly user interface that is easy to use and configure. Feel free to open an issue or submit a pull request if you find a bug or have a suggestion.
+
+Last but not least: this project is **not** affiliated with OpenAI in any way.
---
+++ packages/pyrobbot/pyrobbot-0.2.2.tar.gz/pyrobbot-0.2.2/pyrobbot/text_to_speech.py
@@ -0,0 +1,176 @@
+"""Functions for converting text to speech and speech to text."""
+import io
+import queue
+from collections import deque
+from dataclasses import dataclass
+from datetime import datetime
+
+import numpy as np
+import pygame
+import scipy.io.wavfile as wav
+import soundfile as sf
+import speech_recognition as sr
+import webrtcvad
+from gtts import gTTS
+from loguru import logger
+from pygame import mixer
+
+try:
+    import sounddevice as sd
+
+    _sounddevice_imported = True
+except OSError as error:
+    logger.error(error)
+    logger.error(
+        "Can't use module `sounddevice`. Please check your system's PortAudio install."
+    )
+    _sounddevice_imported = False
+
+
+@dataclass
+class LiveAssistant:
+    """Class for converting text to speech and speech to text."""
+
+    language: str = "en"
+    inactivity_timeout_seconds: int = 2
+    sample_rate: int = 32000  # Hz
+    recording_duration_seconds: int = 5
+    inactivity_sound_intensity_threshold: float = 0.02
+
+    def __post_init__(self):
+        if not _sounddevice_imported:
+            logger.error(
+                "Module `sounddevice`, needed for audio recording, is not available."
+            )
+            logger.error("Cannot continue. Exiting.")
+            raise SystemExit(1)
+        mixer.init()
+        self.vad = webrtcvad.Vad(2)
+
+    def speak(self, text):
+        """Convert text to speech."""
+        logger.debug("Converting text to speech...")
+        # Initialize gTTS with the text to convert
+        speech = gTTS(text, lang=self.language)
+
+        # Convert the recorded array to an in-memory wav file
+        byte_io = io.BytesIO()
+        speech.write_to_fp(byte_io)
+        byte_io.seek(0)
+
+        logger.debug("Done converting text to speech.")
+
+        # Play the audio file
+        speech = mixer.Sound(byte_io)
+        channel = speech.play()
+        while channel.get_busy():
+            pygame.time.wait(100)
+
+    def listen_time_limited(self):
+        """Record audio from the mic, for a limited timelength, and convert it to text."""
+        n_frames = int(self.recording_duration_seconds * self.sample_rate)
+        # Record audio from the microphone
+        rec_as_array = sd.rec(
+            frames=n_frames, samplerate=self.sample_rate, channels=1, dtype="int16"
+        )
+        logger.debug("Recording Audio")
+        sd.wait()
+        logger.debug("Done Recording")
+
+        logger.debug("Converting audio to text...")
+        # Convert the recorded array to an in-memory wav file
+        byte_io = io.BytesIO()
+        wav.write(byte_io, rate=self.sample_rate, data=rec_as_array.astype(np.int16))
+        text = self._audio_buffer_to_text(self, byte_io)
+        logger.debug("Done converting audio to text.")
+
+        return text
+
+    def listen(self):
+        """Record audio from the microphone, until user stops, and convert it to text."""
+        # Adapted from
+        # <https://python-sounddevice.readthedocs.io/en/0.4.6/examples.html#
+        #  recording-with-arbitrary-duration>
+        logger.debug("The assistant is listening...")
+        q = queue.Queue()
+
+        def callback(indata, frames, time, status):  # noqa: ARG001
+            """This is called (from a separate thread) for each audio block."""
+            q.put(indata.copy())
+
+        # From webrtcvad docs: A frame must be either 10, 20, or 30 ms in duration
+        frame_duration = 30  # milliseconds
+        stream_block_size = int((self.sample_rate * frame_duration) / 1000)
+        raw_buffer = io.BytesIO()
+        with sf.SoundFile(
+            raw_buffer,
+            mode="x",
+            samplerate=self.sample_rate,
+            channels=1,
+            format="wav",
+            subtype="PCM_16",
+        ) as audio_file, sd.InputStream(
+            samplerate=self.sample_rate,
+            blocksize=stream_block_size,
+            channels=1,
+            callback=callback,
+            dtype="int16",
+        ):
+            # Recording will stop after self.inactivity_timeout_seconds of silence
+            voice_activity_detected = deque(
+                maxlen=int((1000.0 * self.inactivity_timeout_seconds) / frame_duration)
+            )
+            last_inactivity_checked = datetime.now()
+            user_is_speaking = True
+            while user_is_speaking:
+                new_data = q.get()
+                audio_file.write(new_data)
+
+                # Gather voice activity samples for the inactivity check
+                is_speech = self.vad.is_speech(
+                    _np_array_to_wav_in_memory(new_data, sample_rate=self.sample_rate),
+                    self.sample_rate,
+                )
+                voice_activity_detected.append(is_speech)
+
+                # Decide if user has been inactive for too long
+                now = datetime.now()
+                if (
+                    now - last_inactivity_checked
+                ).seconds >= self.inactivity_timeout_seconds:
+                    last_inactivity_checked = now
+                    user_is_speaking = any(voice_activity_detected)
+
+        # Detect if there was any sound at all, skip the conversion if not
+        recorded_audio = np.frombuffer(raw_buffer.getvalue(), dtype=np.int16)
+        max_intensity = np.max(np.absolute(recorded_audio))
+        if max_intensity < self.inactivity_sound_intensity_threshold:
+            logger.debug("No sound detected")
+            return ""
+
+        logger.debug("Converting audio to text...")
+        text = self._audio_buffer_to_text(byte_io=raw_buffer)
+        logger.debug("Done converting audio to text.")
+
+        return text
+
+    def _audio_buffer_to_text(self, byte_io):
+        """Use SpeechRecognition to convert the audio to text."""
+        byte_io.seek(0)  # Reset the file pointer to the beginning of the file
+        r = sr.Recognizer()
+        with sr.AudioFile(byte_io) as source:
+            audio_data = r.listen(source)
+
+        try:
+            return r.recognize_google(audio_data, language=self.language)
+        except sr.exceptions.UnknownValueError:
+            logger.debug("Could not understand audio")
+            return ""
+
+
+def _np_array_to_wav_in_memory(array: np.ndarray, sample_rate: int):
+    """Convert the recorded array to an in-memory wav file."""
+    byte_io = io.BytesIO()
+    wav.write(byte_io, rate=sample_rate, data=array)
+    byte_io.seek(44)  # Skip the WAV header
+    return byte_io.read()
