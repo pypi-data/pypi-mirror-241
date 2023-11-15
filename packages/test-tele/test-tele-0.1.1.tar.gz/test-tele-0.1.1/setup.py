c34ce80894a354af36b4ef7854c821b0adaf0570
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 16:05:53 +0000

    Add package test-tele/0.1.1/test-tele-0.1.1.tar.gz

---
+++ packages/test-tele/test-tele-0.1.1.tar.gz/test-tele-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='test-tele',
+    version='0.1.1',
+    description='test tele-bot',
+    packages=find_packages(),
+    install_requires=[],
+    python_requires='>=3.10',
+)
