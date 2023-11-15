2a7b151d34f25c49491283a4d304ba6435f28a52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 04:06:18 +0000

    Add package LFPWM/1.0.3/LFPWM-1.0.3.tar.gz

---
+++ packages/LFPWM/LFPWM-1.0.3.tar.gz/LFPWM-1.0.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+VERSION = "1.0.3"
+DESCRIPTION = "LamaForge Encrypted Password Manager"
+LONG_DESCRIPTION = "A package that allows you to store encrypted data locally."
+
+# Setting up
+setup(
+    name="LFPWM",
+    version=VERSION,
+    author="LamaForge (Jadon Lama)",
+    author_email="<lamaforgecode@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=["bcrypt"],
+    keywords=["python", "encryption", "data", "password manager", "user auth"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
