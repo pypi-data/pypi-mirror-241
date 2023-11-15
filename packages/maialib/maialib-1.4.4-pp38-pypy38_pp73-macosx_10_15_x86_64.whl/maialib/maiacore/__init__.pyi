a09b5a3262fd2557baa4e288ad857121364de89b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 16:05:48 +0000

    Add package maialib/1.4.4/maialib-1.4.4-pp38-pypy38_pp73-macosx_10_15_x86_64.whl

---
+++ packages/maialib/maialib-1.4.4-pp38-pypy38_pp73-macosx_10_15_x86_64.whl/maialib/maiacore/__init__.pyi
@@ -0,0 +1,41 @@
+from __future__ import annotations
+import maialib.maiacore
+import typing
+from maialib.maiacore.maiacore import Barline
+from maialib.maiacore.maiacore import Chord
+from maialib.maiacore.maiacore import Clef
+from maialib.maiacore.maiacore import ClefSign
+from maialib.maiacore.maiacore import Duration
+from maialib.maiacore.maiacore import HeapData
+from maialib.maiacore.maiacore import Helper
+from maialib.maiacore.maiacore import Interval
+from maialib.maiacore.maiacore import Key
+from maialib.maiacore.maiacore import Measure
+from maialib.maiacore.maiacore import Note
+from maialib.maiacore.maiacore import NoteData
+from maialib.maiacore.maiacore import NoteDataHeap
+from maialib.maiacore.maiacore import Part
+from maialib.maiacore.maiacore import Score
+from maialib.maiacore.maiacore import ScoreCollection
+
+__all__ = [
+    "Barline",
+    "Chord",
+    "Clef",
+    "ClefSign",
+    "Duration",
+    "HeapData",
+    "Helper",
+    "Interval",
+    "Key",
+    "Measure",
+    "Note",
+    "NoteData",
+    "NoteDataHeap",
+    "Part",
+    "Score",
+    "ScoreCollection",
+    "maiacore"
+]
+
+
