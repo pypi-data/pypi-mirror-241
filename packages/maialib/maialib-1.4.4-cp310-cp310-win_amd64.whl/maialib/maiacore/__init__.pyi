ad9a20b0156c9c55e562bc187ec36b7fef345153
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 16:05:45 +0000

    Add package maialib/1.4.4/maialib-1.4.4-cp310-cp310-win_amd64.whl

---
+++ packages/maialib/maialib-1.4.4-cp310-cp310-win_amd64.whl/maialib/maiacore/__init__.pyi
@@ -0,0 +1,41 @@
+from __future__ import annotations
+import maialib.maiacore
+import typing
+from maialib.maiacore.Release.maiacore import Barline
+from maialib.maiacore.Release.maiacore import Chord
+from maialib.maiacore.Release.maiacore import Clef
+from maialib.maiacore.Release.maiacore import ClefSign
+from maialib.maiacore.Release.maiacore import Duration
+from maialib.maiacore.Release.maiacore import HeapData
+from maialib.maiacore.Release.maiacore import Helper
+from maialib.maiacore.Release.maiacore import Interval
+from maialib.maiacore.Release.maiacore import Key
+from maialib.maiacore.Release.maiacore import Measure
+from maialib.maiacore.Release.maiacore import Note
+from maialib.maiacore.Release.maiacore import NoteData
+from maialib.maiacore.Release.maiacore import NoteDataHeap
+from maialib.maiacore.Release.maiacore import Part
+from maialib.maiacore.Release.maiacore import Score
+from maialib.maiacore.Release.maiacore import ScoreCollection
+
+__all__ = [
+    "Barline",
+    "Chord",
+    "Clef",
+    "ClefSign",
+    "Duration",
+    "HeapData",
+    "Helper",
+    "Interval",
+    "Key",
+    "Measure",
+    "Note",
+    "NoteData",
+    "NoteDataHeap",
+    "Part",
+    "Release",
+    "Score",
+    "ScoreCollection"
+]
+
+
