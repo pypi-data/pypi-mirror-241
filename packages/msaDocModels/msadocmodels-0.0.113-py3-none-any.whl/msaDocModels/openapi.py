d554206a5887ccacbf0771d56c8629d706541c38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 03:03:26 +0000

    Add package msaDocModels/0.0.113/msadocmodels-0.0.113-py3-none-any.whl

---
+++ packages/msaDocModels/msadocmodels-0.0.113-py3-none-any.whl/msaDocModels/openapi.py
@@ -0,0 +1,18 @@
+from typing import Any, Dict, List, Optional
+
+from pydantic import BaseModel
+
+
+class MSAOpenAPIInfo(BaseModel):
+    """
+    **MSAOpenAPIInfo** Pydantic Response Class
+    """
+
+    name: str = "msaSDK Service"
+    """Service Name."""
+    version: str = "0.0.0"
+    """API Version."""
+    url: str = "/openapi.json"
+    """OpenAPI URL."""
+    tags: Optional[List[Dict[str, Any]]] = None
+    """OpenAPI Tags."""
