43d9a8aab2b9de3b9eab35ca9ff15084ebbc6c0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 06:05:43 +0000

    Add package sklearndf/2.3rc0/sklearndf-2.3rc0.tar.gz

---
+++ packages/sklearndf/sklearndf-2.3rc0.tar.gz/sklearndf-2.3rc0/src/sklearndf/classification/__init__.py
@@ -0,0 +1,8 @@
+"""
+Extended versions of all `scikit-learn` classifiers with enhanced support for data
+frames.
+"""
+from ._classification import *
+from ._classification_v0_22 import *
+from ._classification_v0_23 import *
+from ._classification_v1_0 import *
