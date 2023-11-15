5d1f9ec73f86104f43df2be43c97a4d6e027dbb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 04:06:32 +0000

    Add package cfn-lint/1.2.0a1/cfn-lint-1.2.0a1.tar.gz

---
+++ packages/cfn-lint/cfn-lint-1.2.0a1.tar.gz/cfn-lint-1.2.0a1/test/unit/module/override/test_complete.py
@@ -0,0 +1,56 @@
+"""
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: MIT-0
+"""
+from test.testlib.testcase import BaseTestCase
+
+from cfnlint.rules import RulesCollection
+from cfnlint.rules.resources.Configuration import Configuration  # pylint: disable=E0401
+from cfnlint.rules.resources.properties.JsonSchema import JsonSchema
+from cfnlint.rules.resources.properties.Required import (
+    Required,  # pylint: disable=E0401
+)
+from cfnlint.runner import Runner
+from cfnlint.schema.manager import PROVIDER_SCHEMA_MANAGER
+
+
+class TestComplete(BaseTestCase):
+    """Used for Testing Rules"""
+
+    def setUp(self):
+        """Setup"""
+        self.collection = RulesCollection()
+        self.collection.register(JsonSchema())
+        self.collection.register(Configuration())
+        self.collection.register(Required())
+        self.region = "us-east-1"
+
+    def tearDown(self):
+        """Tear Down"""
+        # Reset the Spec override to prevent other tests to fail
+        PROVIDER_SCHEMA_MANAGER.reset()
+
+    def test_success_run(self):
+        """Success test"""
+        filename = "test/fixtures/templates/good/override/complete.yaml"
+        template = self.load_template(filename)
+
+        PROVIDER_SCHEMA_MANAGER.patch(
+            "test/fixtures/templates/override_spec/complete.json", regions=[self.region]
+        )
+
+        good_runner = Runner(self.collection, filename, template, [self.region], [])
+        self.assertEqual([], good_runner.run())
+
+    def test_fail_run(self):
+        """Failure test required"""
+        filename = "test/fixtures/templates/bad/override/complete.yaml"
+        template = self.load_template(filename)
+
+        PROVIDER_SCHEMA_MANAGER.patch(
+            "test/fixtures/templates/override_spec/complete.json", regions=[self.region]
+        )
+
+        bad_runner = Runner(self.collection, filename, template, [self.region], [])
+        errs = bad_runner.run()
+        self.assertEqual(3, len(errs))
