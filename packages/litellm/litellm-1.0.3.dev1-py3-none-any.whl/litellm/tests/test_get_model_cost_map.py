13fabbffb0488110fc3d2b483abe463d67e4f9ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 16:06:24 +0000

    Add package litellm/1.0.3.dev1/litellm-1.0.3.dev1-py3-none-any.whl

---
+++ packages/litellm/litellm-1.0.3.dev1-py3-none-any.whl/litellm/tests/test_get_model_cost_map.py
@@ -0,0 +1,23 @@
+import sys, os
+import traceback
+
+sys.path.insert(
+    0, os.path.abspath("../..")
+)  # Adds the parent directory to the system path
+import time
+from litellm import get_max_tokens, model_cost, open_ai_chat_completion_models
+
+print(get_max_tokens("gpt-3.5-turbo"))
+
+def test_get_gpt3_tokens():
+    max_tokens = get_max_tokens("gpt-3.5-turbo")
+    results = max_tokens['max_tokens']
+    print(results)
+# test_get_gpt3_tokens()
+
+def test_get_palm_tokens():
+    # # 🦄🦄🦄🦄🦄🦄🦄🦄
+    max_tokens = get_max_tokens("palm/chat-bison")
+    results = max_tokens['max_tokens']
+    print(results)
+# test_get_palm_tokens()
