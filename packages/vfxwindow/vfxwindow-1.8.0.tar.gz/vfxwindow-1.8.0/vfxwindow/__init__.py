87b5c43447542b8d23a2dc1ddecb833ed820458f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 04:05:51 +0000

    Add package vfxwindow/1.8.0/vfxwindow-1.8.0.tar.gz

---
+++ packages/vfxwindow/vfxwindow-1.8.0.tar.gz/vfxwindow-1.8.0/vfxwindow/__init__.py
@@ -0,0 +1,85 @@
+"""Set the window class to be specific to whichever program is loaded.
+
+TODO:
+    Substance callbacks
+    Add dialog code for each application
+    Revise setDefault* methods
+
+    # Potential breaking changes
+    Change setDocked to setFloating
+    Remove docked in favour of floating
+    Remove *_VERSION constants
+    Changed dialog to isDialog
+    Remove processEvents
+    Remove signalExists
+"""
+
+from __future__ import absolute_import
+
+__all__ = ['VFXWindow']
+__version__ = '1.8.0'
+
+import sys
+from .utils import software
+from . import exceptions
+
+
+def _setup_qapp():
+    """Attempt to start a QApplication automatically in batch mode.
+    The purpose of this is to override whatever the program uses.
+    This must happen before any libraries are imported, as it's usually
+    at that point when the QApplication is initialised.
+    """
+
+    from Qt import QtWidgets
+    try:
+        app = QtWidgets.QApplication(sys.argv)
+    except RuntimeError:
+        pass
+
+
+if software.isMayaBatch():
+    _setup_qapp()
+    from .maya import MayaBatchWindow as VFXWindow
+
+elif software.isMaya():
+    from .maya import MayaWindow as VFXWindow
+
+elif software.isNuke():
+    from .nuke import runningInTerminal
+
+    inTerminal = runningInTerminal(startup=True)
+    if inTerminal is None:
+        raise exceptions.NotImplementedApplicationError('Nuke GUI not supported in terminal mode, launch nuke with the --tg flag instead.')
+
+    if inTerminal:
+        from .nuke import NukeBatchWindow as VFXWindow
+    else:
+        from .nuke import NukeWindow as VFXWindow
+
+elif software.isHoudini():
+    from .houdini import HoudiniWindow as VFXWindow
+
+elif software.isBlender():
+    from .blender import BlenderWindow as VFXWindow
+
+elif software.isUnrealEngine():
+    from .unreal import UnrealWindow as VFXWindow
+
+elif software.is3dsMax():
+    from .max import MaxWindow as VFXWindow
+
+elif software.isSubstanceDesigner():
+    from .substance_designer import SubstanceDesignerWindow as VFXWindow
+
+elif software.isSubstancePainter():
+    from .substance_painter import SubstancePainterWindow as VFXWindow
+
+elif software.isBlackmagicFusion():
+    from .fusion import FusionWindow as VFXWindow
+
+elif software.isCryEngine():
+    from .cryengine import CryWindow as VFXWindow
+
+else:
+    from .standalone import StandaloneWindow as VFXWindow
---
+++ packages/vfxwindow/vfxwindow-1.8.0.tar.gz/vfxwindow-1.8.0/vfxwindow/cryengine.py
@@ -0,0 +1,117 @@
+"""Window class for CryEngine."""
+
+from __future__ import absolute_import
+
+import os
+import sys
+
+from Qt import QtWidgets
+
+import SandboxBridge
+
+from .abstract import getWindowSettings
+from .utils import setCoordinatesToScreen, hybridmethod
+from .standalone import StandaloneWindow
+
+
+try:
+    VERSION = float(sys.executable.split(os.path.sep)[-4].rsplit('.', 1)[0])
+except (TypeError, IndexError):
+    VERSION = None
+
+
+def getMainWindow():
+    """Get a pointer to the CryEngine window.
+    This doesn't appear to make any difference to a standalone window.
+    """
+
+    for widget in QtWidgets.QApplication.topLevelWidgets():
+        if type(widget) == QtWidgets.QWidget and widget.parentWidget() is None and widget.objectName() == 'mainWindow':
+            return widget
+
+
+class CryWindow(StandaloneWindow):
+    def __init__(self, parent=None, **kwargs):
+        if parent is None:
+            parent = getMainWindow()
+        super(CryWindow, self).__init__(parent, **kwargs)
+        self.cryengine = True
+        self.standalone = False
+
+    def saveWindowPosition(self):
+        """Save the window location."""
+
+        if 'cryengine' not in self.windowSettings:
+            settings = self.windowSettings['cryengine'] = {}
+
+        settings['docked'] = self.dockable(raw=True)
+
+        if self.dockable():
+            pass  # Not yet implemented
+
+        else:
+            settings['main'] = dict(
+                width=self.width(),
+                height=self.height(),
+                x=self.x(),
+                y=self.y(),
+            )
+
+        super(CryWindow, self).saveWindowPosition()
+
+    def loadWindowPosition(self):
+        """Set the position of the window when loaded."""
+
+        if self.dockable():
+            return  # Not yet implemented
+
+        try:
+            settings = self.windowSettings['cryengine']['main']
+            width = settings['width']
+            height = settings['height']
+            x = settings['x']
+            y = settings['y']
+        except KeyError:
+            super(CryWindow, self).loadWindowPosition()
+        else:
+            x, y = setCoordinatesToScreen(x, y, width, height, padding=5)
+            self.resize(width, height)
+            self.move(x, y)
+
+    @hybridmethod
+    def show(cls, self, *args, **kwargs):
+        # Window is already initialised
+        if self is not cls:
+            return super(CryWindow, self).show()
+
+        # Close down any instances of the window
+        try:
+            cls.clearWindowInstance(cls.WindowID)
+        except AttributeError:
+            settings = {}
+        else:
+            settings = getWindowSettings(cls.WindowID)
+
+        #Load settings
+        if hasattr(cls, 'WindowDockable'):
+            docked = cls.WindowDockable
+        else:
+            try:
+                docked = settings['cryengine']['docked']
+            except KeyError:
+                try:
+                    docked = cls.WindowDefaults['docked']
+                except (AttributeError, KeyError):
+                    docked = True
+
+        # Apparently this should add the window to the "Tools" menu,
+        # but I couldn't figure it out so it's disabled for now
+        if False and docked:
+            return SandboxBridge.register_window(
+                window_type=cls,
+                name=getattr(cls, 'WindowName', 'New Window'),
+            )
+
+        kwargs['exec_'] = False
+        kwargs['instance'] = True
+        return super(CryWindow, cls).show(*args, **kwargs)
