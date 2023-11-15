dfa8626dc1e70e7890e39b10e97920ce8e8146b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 04:05:30 +0000

    Add package ieee754/0.4/ieee754-0.4.tar.gz

---
+++ packages/ieee754/ieee754-0.4.tar.gz/ieee754-0.4/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+
+setup(
+    name="ieee754",
+    packages=["ieee754"],
+    version="0.4",
+    license="MIT",
+    description="A Python module which converts floating points numbers into IEEE-754 representation.",
+    author="Bora Canbula",
+    author_email="bora.canbula@cbu.edu.tr",
+    url="https://github.com/canbula/ieee754",
+    download_url="https://github.com/canbula/ieee754/archive/refs/tags/v_04.tar.gz",
+    keywords=[
+        "IEEE-754",
+        "precisions",
+        "double-precision",
+        "binary",
+        "floating-points",
+        "binary-representation",
+    ],
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 4 - Beta",  # "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+)
