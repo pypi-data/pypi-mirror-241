4726dc82249427b0e4406a169bc8f1c9f6e7c0f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 05:05:32 +0000

    Add package spicecmp/5.0.0/spicecmp-5.0.0.tar.gz

---
+++ packages/spicecmp/spicecmp-5.0.0.tar.gz/spicecmp-5.0.0/spicecmp/__init__.py
@@ -0,0 +1,12 @@
+__version__ = "2.0.dev0"
+
+# Local Imports
+from .errormode import ErrorMode
+from .corner import Corner
+from .pdk import *
+from .simulator import *
+from .pdk_sim_combo import *
+from .benches import *
+from .compare_meas import *
+from .testcase import *
+from .compare_me import *
