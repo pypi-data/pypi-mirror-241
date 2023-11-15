d7e527fa61387ab6ea338e312867a94bac151613
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 16:08:11 +0000

    Add package bark-detection/0.0.4/bark_detection-0.0.4.tar.gz

---
+++ packages/bark-detection/bark_detection-0.0.4.tar.gz/bark_detection-0.0.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.4'
+DESCRIPTION = 'Package for bark detection in audio file'
+
+# Setting up
+setup(
+    name="bark_detection",
+    version=VERSION,
+    author="jgab",
+    #author_email="<mail@neuralnine.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['librosa==0.10.1','tensorflow==2.13.0'],
+    keywords=['python', 'ML', 'bark', 'detection'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Programming Language :: Python :: 3",
+    ]
+)
