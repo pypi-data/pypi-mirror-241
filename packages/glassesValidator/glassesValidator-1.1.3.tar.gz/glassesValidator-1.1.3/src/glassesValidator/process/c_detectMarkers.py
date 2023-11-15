aa7380ed37b1da8e1adad876f308ff120b15c795
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 04:06:31 +0000

    Add package glassesValidator/1.1.3/glassesValidator-1.1.3.tar.gz

---
+++ packages/glassesValidator/glassesValidator-1.1.3.tar.gz/glassesValidator-1.1.3/src/glassesValidator/process/c_detectMarkers.py
@@ -0,0 +1,208 @@
+import pathlib
+
+import cv2
+import numpy as np
+import csv
+import threading
+
+from .. import config
+from .. import utils
+from ._image_gui import GUI, generic_tooltip, qns_tooltip
+
+
+stopAllProcessing = False
+def process(working_dir, config_dir=None, show_visualization=False, show_rejected_markers=False):
+    # if show_visualization, each frame is shown in a viewer, overlaid with info about detected markers and poster
+    # if show_rejected_markers, rejected ArUco marker candidates are also shown in the viewer. Possibly useful for debug
+    working_dir  = pathlib.Path(working_dir)
+    if config_dir is not None:
+        config_dir = pathlib.Path(config_dir)
+
+    print('processing: {}'.format(working_dir.name))
+
+    # if we need gui, we run processing in a separate thread (GUI needs to be on the main thread for OSX, see https://github.com/pthom/hello_imgui/issues/33)
+    if show_visualization:
+        gui = GUI(use_thread = False)
+        gui.set_interesting_keys('qns')
+        gui.register_draw_callback('status',lambda: generic_tooltip(qns_tooltip()))
+        gui.add_window(working_dir.name)
+
+        proc_thread = threading.Thread(target=do_the_work, args=(working_dir, config_dir, gui, show_rejected_markers))
+        proc_thread.start()
+        gui.start()
+        proc_thread.join()
+        return stopAllProcessing
+    else:
+        return do_the_work(working_dir, config_dir, None, False)
+
+
+def do_the_work(working_dir, config_dir, gui, show_rejected_markers):
+    global stopAllProcessing
+    show_visualization = gui is not None
+
+    utils.update_recording_status(working_dir, utils.Task.Markers_Detected, utils.Status.Running)
+
+    # open file with information about Aruco marker and Gaze target locations
+    validationSetup = config.get_validation_setup(config_dir)
+
+    # open video file, query it for size
+    inVideo = working_dir / 'worldCamera.mp4'
+    if not inVideo.is_file():
+        inVideo = working_dir / 'worldCamera.avi'
+    cap    = cv2.VideoCapture(str(inVideo))
+    if not cap.isOpened():
+        raise RuntimeError('the file "{}" could not be opened'.format(str(inVideo)))
+    width  = float(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = float(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    nframes= float(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    # get info about markers on our poster
+    poster          = utils.Poster(config_dir, validationSetup)
+    centerTarget    = poster.targets[validationSetup['centerTarget']].center
+    # turn into aruco board object to be used for pose estimation
+    arucoBoard      = poster.getArucoBoard()
+
+    # setup aruco marker detection
+    parameters      = cv2.aruco.DetectorParameters()
+    parameters.markerBorderBits       = validationSetup['markerBorderBits']
+    parameters.cornerRefinementMethod = cv2.aruco.CORNER_REFINE_SUBPIX
+    aruco_detector  = cv2.aruco.ArucoDetector(poster.aruco_dict, parameters)
+
+    # get camera calibration info
+    cameraMatrix,distCoeff = utils.readCameraCalibrationFile(working_dir / "calibration.xml")[0:2]
+    hasCameraMatrix = cameraMatrix is not None
+    hasDistCoeff    = distCoeff is not None
+
+    # get interval coded to be analyzed, if any
+    analyzeFrames   = utils.readMarkerIntervalsFile(working_dir / "markerInterval.tsv")
+    hasAnalyzeFrames= analyzeFrames is not None
+
+    # prep output file
+    csv_file = open(working_dir / 'posterPose.tsv', 'w', newline='')
+    csv_writer = csv.writer(csv_file, delimiter='\t')
+    header = utils.PosterPose.getWriteHeader()
+    csv_writer.writerow(header)
+
+    # prep visualization, if any
+    if show_visualization:
+        i2t = utils.Idx2Timestamp(working_dir / 'frameTimestamps.tsv')
+
+    frame_idx = -1
+    stopAllProcessing = False
+    armLength = poster.markerSize/2 # arms of axis are half a marker long
+    subPixelFac = 8   # for sub-pixel positioning
+    while True:
+        # process frame-by-frame
+        ret, frame = cap.read()
+        frame_idx += 1
+
+        # check if we're done. Can't trust ret==False to indicate we're at end of video, as it may also return false for some frames when video has errors in the middle that we can just read past
+        if (not ret and (frame_idx==0 or frame_idx/nframes>.99)) or (hasAnalyzeFrames and frame_idx > analyzeFrames[-1]):
+            # done
+            break
+        if frame_idx%100==0:
+            print('  frame {}'.format(frame_idx))
+        if not ret or frame is None:
+            # we don't have a valid frame, continue to next
+            continue
+
+        if show_visualization:
+            keys = gui.get_key_presses()
+            if 'q' in keys:
+                # quit fully
+                stopAllProcessing = True
+                break
+            if 'n' in keys:
+                # goto next
+                break
+
+        if hasAnalyzeFrames:
+            # check we're in a current interval, else skip processing
+            # NB: have to spool through like this, setting specific frame to read
+            # with cap.get(cv2.CAP_PROP_POS_FRAMES) doesn't seem to work reliably
+            # for VFR video files
+            inIval = False
+            for f in range(0,len(analyzeFrames),2):
+                if frame_idx>=analyzeFrames[f] and frame_idx<=analyzeFrames[f+1]:
+                    inIval = True
+                    break
+            if not inIval:
+                # no need to process this frame
+                continue
+
+        # detect markers, undistort
+        corners, ids, rejectedImgPoints = aruco_detector.detectMarkers(frame)
+        recoveredIds = None
+
+        if np.all(ids != None):
+            if len(ids) >= validationSetup['minNumMarkers']:
+                pose = utils.PosterPose(frame_idx)
+
+                # get camera pose
+                if hasCameraMatrix and hasDistCoeff:
+                    # Refine detected markers (eliminates markers not part of our poster, adds missing markers to the poster)
+                    corners, ids, rejectedImgPoints, recoveredIds = utils.arucoRefineDetectedMarkers(aruco_detector,
+                            image = frame, arucoBoard = arucoBoard,
+                            detectedCorners = corners, detectedIds = ids, rejectedCorners = rejectedImgPoints,
+                            cameraMatrix = cameraMatrix, distCoeffs = distCoeff)
+
+                    objP, imgP = arucoBoard.matchImagePoints(corners, ids)
+                    pose.nMarkers = 0 if objP is None else int(objP.shape[0]/4)
+                    if pose.nMarkers>0:
+                        pose.poseOk, pose.rVec, pose.tVec = cv2.solvePnP(objP, imgP, cameraMatrix, distCoeff, np.empty(1), np.empty(1))
+
+                    # draw pose if wanted
+                    if pose.poseOk and show_visualization:
+                        # draw axis indicating poster pose (origin and orientation)
+                        utils.drawOpenCVFrameAxis(frame, cameraMatrix, distCoeff, pose.rVec, pose.tVec, armLength, 3, subPixelFac)
+
+                # also get homography (direct image plane to plane in world transform). Use undistorted marker corners
+                if hasCameraMatrix and hasDistCoeff:
+                    cornersU = [cv2.undistortPoints(x, cameraMatrix, distCoeff, P=cameraMatrix) for x in corners]
+                else:
+                    cornersU = corners
+                H, status = utils.estimateHomography(poster.knownMarkers, cornersU, ids)
+
+                if status:
+                    pose.hMat = H
+                    if show_visualization:
+                        # find where target is expected to be in the image
+                        iH = np.linalg.inv(pose.hMat)
+                        target = utils.applyHomography(iH, centerTarget[0], centerTarget[1])
+                        if hasCameraMatrix and hasDistCoeff:
+                            target = utils.distortPoint(*target, cameraMatrix, distCoeff)
+                        # draw target location on image
+                        if target[0] >= 0 and target[0] < width and target[1] >= 0 and target[1] < height:
+                            utils.drawOpenCVCircle(frame, target, 3, (0,0,0), -1, subPixelFac)
+
+                if pose.poseOk or status:
+                    csv_writer.writerow( pose.getWriteData() )
+
+            # if any markers were detected, draw where on the frame
+            if show_visualization:
+                utils.drawArucoDetectedMarkers(frame, corners, ids, subPixelFac=subPixelFac, specialHighlight=[recoveredIds,(255,255,0)])
+
+        # for debug, can draw rejected markers on frame
+        if show_visualization and show_rejected_markers:
+            cv2.aruco.drawDetectedMarkers(frame, rejectedImgPoints, None, borderColor=(211,0,148))
+
+        if show_visualization:
+            # keys is populated above
+            if 's' in keys:
+                # screenshot
+                cv2.imwrite(str(working_dir / ('detect_frame_%d.png' % frame_idx)), frame)
+            frame_ts  = i2t.get(frame_idx)
+            gui.update_image(frame, frame_ts/1000., frame_idx)
+            closed, = gui.get_state()
+            if closed:
+                stopAllProcessing = True
+                break
+
+    csv_file.close()
+    cap.release()
+    if show_visualization:
+        gui.stop()
+
+    utils.update_recording_status(working_dir, utils.Task.Markers_Detected, utils.Status.Finished)
+
+    return stopAllProcessing
---
+++ packages/glassesValidator/glassesValidator-1.1.3.tar.gz/glassesValidator-1.1.3/src/glassesValidator/process/e2_determineFixationIntervals.py
@@ -0,0 +1,154 @@
+#!/usr/bin/python
+
+import pathlib
+import math
+
+import numpy as np
+import pandas as pd
+
+import I2MC
+import matplotlib.pyplot as plt
+
+from .. import config
+from .. import utils
+
+
+def process(working_dir, config_dir=None):
+    working_dir  = pathlib.Path(working_dir)
+    if config_dir is not None:
+        config_dir = pathlib.Path(config_dir)
+
+    print('processing: {}'.format(working_dir.name))
+    utils.update_recording_status(working_dir, utils.Task.Fixation_Intervals_Determined, utils.Status.Running)
+
+    # open file with information about Aruco marker and Gaze target locations
+    validationSetup = config.get_validation_setup(config_dir)
+
+    # get interval coded to be analyzed
+    analyzeFrames = utils.readMarkerIntervalsFile(working_dir / "markerInterval.tsv")
+    if analyzeFrames is None:
+        print('  no marker intervals defined for this recording, skipping')
+        return
+
+    # Read gaze on poster data
+    gazePoster = utils.GazePoster.readDataFromFile(working_dir / 'gazePosterPos.tsv',analyzeFrames[0],analyzeFrames[-1],True)
+
+    # get info about markers on our poster
+    poster    = utils.Poster(config_dir, validationSetup, imHeight=-1)
+    targets   = {ID: poster.targets[ID].center for ID in poster.targets}   # get centers of targets
+    markerHalfSizeMm = poster.markerSize/2.
+
+    # run I2MC on data in poster space
+    # set I2MC options
+    opt = {'xres': None, 'yres': None}  # dummy values for required options
+    opt['missingx']         = math.nan
+    opt['missingy']         = math.nan
+    opt['maxdisp']          = 50        # mm
+    opt['windowtimeInterp'] = .25       # s
+    opt['maxMergeDist']     = 20        # mm
+    opt['maxMergeTime']     = 81        # ms
+    opt['minFixDur']        = 50        # ms
+    # decide what sampling frequency to tell I2MC about. It doesn't work with varying sampling frequency, nor
+    # any random sampling frequency. For our purposes, getting it right is not important (internally I2MC only
+    # uses sampling frequency for converting some of the time units to samples, other things are taken directly
+    # from the time signal. So, we have working I2MC settings for a few sampling frequencies, and just choose
+    # the nearest based on empirically determined sampling frequency.
+    ts          = np.array([s.ts for v in gazePoster.values() for s in v])
+    recFreq     = np.round(np.mean(1000./np.diff(ts)))    # Hz
+    knownFreqs  = [30., 50., 60., 90., 120.]
+    opt['freq'] = knownFreqs[np.abs(knownFreqs - recFreq).argmin()]
+    if opt['freq']==120.:
+        opt['downsamples']      = [2, 3, 5]
+        opt['chebyOrder']       = 7
+    elif opt['freq'] in [50., 60.]:
+        opt['downsamples']      = [2, 5]
+        opt['downsampFilter']   = False
+    else:
+        # 90 Hz, 30 Hz
+        opt['downsamples']      = [2, 3]
+        opt['downsampFilter']   = False
+
+    # collect data
+    qHasLeft        = np.any(np.logical_not(np.isnan([s.lGaze2D          for v in gazePoster.values() for s in v])))
+    qHasRight       = np.any(np.logical_not(np.isnan([s.rGaze2D          for v in gazePoster.values() for s in v])))
+    qHasRay         = np.any(np.logical_not(np.isnan([s.gaze2DRay        for v in gazePoster.values() for s in v])))
+    qHasHomography  = np.any(np.logical_not(np.isnan([s.gaze2DHomography for v in gazePoster.values() for s in v])))
+    for ival in range(0,len(analyzeFrames)//2):
+        gazePosterToAnal = {k:v for (k,v) in gazePoster.items() if k>=analyzeFrames[ival*2] and k<=analyzeFrames[ival*2+1]}
+        data = {}
+        data['time'] = np.array([s.ts for v in gazePosterToAnal.values() for s in v])
+        if qHasLeft and qHasRight:
+            # prefer using separate left and right eye signals, if available. Better I2MC robustness
+            data['L_X']  = np.array([s.lGaze2D[0] for v in gazePosterToAnal.values() for s in v])
+            data['L_Y']  = np.array([s.lGaze2D[1] for v in gazePosterToAnal.values() for s in v])
+            data['R_X']  = np.array([s.rGaze2D[0] for v in gazePosterToAnal.values() for s in v])
+            data['R_Y']  = np.array([s.rGaze2D[1] for v in gazePosterToAnal.values() for s in v])
+        elif qHasRay:
+            data['average_X']  = np.array([s.gaze2DRay[0] for v in gazePosterToAnal.values() for s in v])
+            data['average_Y']  = np.array([s.gaze2DRay[1] for v in gazePosterToAnal.values() for s in v])
+        elif qHasHomography:
+            data['average_X']  = np.array([s.gaze2DHomography[0] for v in gazePosterToAnal.values() for s in v])
+            data['average_Y']  = np.array([s.gaze2DHomography[1] for v in gazePosterToAnal.values() for s in v])
+        else:
+            raise RuntimeError('No data available to process')
+
+        # run event classification to find fixations
+        fix,dat,par = I2MC.I2MC(data,opt,False)
+
+        # for each target, find closest fixation
+        minDur      = 150       # ms
+        used        = np.zeros((fix['start'].size),dtype='bool')
+        selected    = np.empty((len(targets),),dtype='int')
+        selected[:] = -999
+
+        for i,t in zip(range(len(targets)),targets):
+            if np.all(used):
+                # all fixations used up, can't assign anything to remaining targets
+                continue
+            # select fixation
+            dist                    = np.hypot(fix['xpos']-targets[t][0], fix['ypos']-targets[t][1])
+            dist[used]              = math.inf  # make sure fixations already bound to a target are not used again
+            dist[fix['dur']<minDur] = math.inf  # make sure that fixations that are too short are not selected
+            iFix        = np.argmin(dist)
+            selected[i] = iFix
+            used[iFix]  = True
+
+        # make plot of data overlaid on poster, and show for each target which fixation
+        # was selected
+        f       = plt.figure(dpi=300)
+        imgplot = plt.imshow(poster.getImgCopy(asRGB=True),extent=(np.array(poster.bbox)[[0,2,3,1]]),alpha=.5)
+        plt.plot(fix['xpos'],fix['ypos'],'b-')
+        plt.plot(fix['xpos'],fix['ypos'],'go')
+        plt.xlim([poster.bbox[0]-markerHalfSizeMm, poster.bbox[2]+markerHalfSizeMm])
+        plt.ylim([poster.bbox[1]-markerHalfSizeMm, poster.bbox[3]+markerHalfSizeMm])
+        plt.gca().invert_yaxis()
+        for i,t in zip(range(len(targets)),targets):
+            if selected[i]==-999:
+                continue
+            plt.plot([fix['xpos'][selected[i]], targets[t][0]], [fix['ypos'][selected[i]], targets[t][1]],'r-')
+
+        plt.xlabel('mm')
+        plt.ylabel('mm')
+
+        f.savefig(str(working_dir / 'targetSelection_I2MC_interval_{}.png'.format(ival)))
+        plt.close(f)
+
+        # also make timseries plot of gaze data with fixations
+        f = I2MC.plot.data_and_fixations(data, fix, fix_as_line=True, unit='mm', res=[[poster.bbox[0]-2*markerHalfSizeMm, poster.bbox[2]+2*markerHalfSizeMm], [poster.bbox[1]-2*markerHalfSizeMm, poster.bbox[3]+2*markerHalfSizeMm]])
+        plt.gca().invert_yaxis()
+        f.savefig(str(working_dir / 'targetSelection_I2MC_interval_{}_fixations.png'.format(ival)))
+        plt.close(f)
+
+        # store selected intervals
+        df = pd.DataFrame()
+        df.index.name = 'target'
+        for i,t in zip(range(len(targets)),targets):
+            if selected[i]==-999:
+                continue
+            df.loc[t,'marker_interval'] = ival+1
+            df.loc[t,'start_timestamp'] = fix['startT'][selected[i]]
+            df.loc[t,  'end_timestamp'] = fix[  'endT'][selected[i]]
+
+        df.to_csv(str(working_dir / 'analysisInterval.tsv'), mode='w' if ival==0 else 'a', header=ival==0, sep='\t', na_rep='nan', float_format="%.3f")
+
+    utils.update_recording_status(working_dir, utils.Task.Fixation_Intervals_Determined, utils.Status.Finished)
