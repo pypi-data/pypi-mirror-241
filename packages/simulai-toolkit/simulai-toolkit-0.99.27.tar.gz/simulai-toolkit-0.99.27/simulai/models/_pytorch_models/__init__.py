3819d66afa4f46b735b0b1ab8cf8da460cbae114
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 04:06:06 +0000

    Add package simulai-toolkit/0.99.27/simulai-toolkit-0.99.27.tar.gz

---
+++ packages/simulai-toolkit/simulai-toolkit-0.99.27.tar.gz/simulai-toolkit-0.99.27/simulai/models/_pytorch_models/__init__.py
@@ -0,0 +1,18 @@
+from ._autoencoder import (
+    AutoencoderCNN,
+    AutoencoderKoopman,
+    AutoencoderMLP,
+    AutoencoderVariational,
+    MultiScaleAutoencoder,
+)
+from ._deeponet import DeepONet, FlexibleDeepONet, ImprovedDeepONet, ResDeepONet
+from ._transformer import Transformer
+from ._unet import UNet
+from ._miscellaneous import (
+    ImprovedDenseNetwork,
+    MetaModel,
+    ModelMaker,
+    MultiNetwork,
+    MoEPool,
+    SplitPool,
+)
