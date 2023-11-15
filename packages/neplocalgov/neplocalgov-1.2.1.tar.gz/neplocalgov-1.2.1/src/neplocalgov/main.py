8d6d0ecc38b2280f5cd267da23e046c794b78735
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 16:08:21 +0000

    Add package neplocalgov/1.2.1/neplocalgov-1.2.1.tar.gz

---
+++ packages/neplocalgov/neplocalgov-1.2.1.tar.gz/neplocalgov-1.2.1/src/neplocalgov/main.py
@@ -0,0 +1,35 @@
+import csv
+import importlib.resources as pkg_resources
+
+
+def get_csv_file_path():
+    with pkg_resources.path("neplocalgov.data", "data.csv") as p:
+        return str(p)
+
+
+csv_file_path = get_csv_file_path()
+
+
+def by_province(province=None):
+    result = []
+    with open(csv_file_path, newline="", encoding="utf-8") as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            if row["Province"] == province:
+                result.append(row)
+
+    return result
+
+
+def by_district(district=None):
+    result = []
+    with open(csv_file_path, newline="", encoding="utf-8") as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            if row["District"] == district.upper():
+                result.append(row)
+    print(result)
+    return result
+
+
+print(by_district(district="Kathmandu"))
