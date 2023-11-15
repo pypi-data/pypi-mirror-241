3f47facfeb0821fb27f173b20daa65b8ae769728
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 16:05:30 +0000

    Add package lst-pressure/0.1.1/lst_pressure-0.1.1-py3-none-any.whl

---
+++ packages/lst-pressure/lst_pressure-0.1.1-py3-none-any.whl/lstpressure/lstcsv/helpers/calculate_observables.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+from functools import lru_cache
+from typing import List, Union
+from datetime import datetime
+from ...observable import Observable
+from ...observation import Observation
+from ...lstcalendar import LSTCalendar
+
+
+@lru_cache(maxsize=None)
+def calculate_observables(
+    cal_start: Union[str, datetime], cal_end: Union[str, datetime], observations: List[Observation]
+) -> List[Observable]:
+    return sorted(LSTCalendar(cal_start, cal_end, observations=observations).observables())
