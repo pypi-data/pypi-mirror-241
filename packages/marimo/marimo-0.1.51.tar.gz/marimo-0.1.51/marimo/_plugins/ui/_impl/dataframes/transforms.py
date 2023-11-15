daf9dfa4f0f81d025741ebd4af90b6eda5ef2bb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 15:04:43 +0000

    Add package marimo/0.1.51/marimo-0.1.51.tar.gz

---
+++ packages/marimo/marimo-0.1.51.tar.gz/marimo-0.1.51/marimo/_plugins/ui/_impl/dataframes/transforms.py
@@ -0,0 +1,122 @@
+# Copyright 2023 Marimo. All rights reserved.
+from __future__ import annotations
+
+from dataclasses import dataclass
+from enum import Enum
+from typing import Any, List, Literal, Optional, Union
+
+ColumnId = str
+ColumnIds = List[ColumnId]
+NumpyDataType = str
+Operator = Literal[
+    "==",
+    "!=",
+    "<",
+    ">",
+    "<",
+    "<=",
+    ">=",
+    "is_true",
+    "is_false",
+    "is_nan",
+    "is_not_nan",
+    "equals",
+    "does_not_equal",
+    "contains",
+    "regex",
+    "starts_with",
+    "ends_with",
+    "in",
+]
+Aggregation = Literal[
+    "count",
+    "sum",
+    "mean",
+    "median",
+    "min",
+    "max",
+]
+
+
+class TransformType(Enum):
+    AGGREGATE = "aggregate"
+    COLUMN_CONVERSION = "column_conversion"
+    FILTER_ROWS = "filter_rows"
+    GROUP_BY = "group_by"
+    RENAME_COLUMN = "rename_column"
+    SELECT_COLUMNS = "select_columns"
+    SORT_COLUMN = "sort_column"
+
+
+@dataclass
+class Condition:
+    column_id: ColumnId
+    operator: Operator
+    value: Optional[Any] = None
+
+
+@dataclass
+class ColumnConversionTransform:
+    type: Literal[TransformType.COLUMN_CONVERSION]
+    column_id: ColumnId
+    data_type: NumpyDataType
+    errors: Literal["ignore", "raise"]
+
+
+@dataclass
+class RenameColumnTransform:
+    type: Literal[TransformType.RENAME_COLUMN]
+    column_id: ColumnId
+    new_column_id: ColumnId
+
+
+@dataclass
+class SortColumnTransform:
+    type: Literal[TransformType.SORT_COLUMN]
+    column_id: ColumnId
+    ascending: bool
+    na_position: Literal["first", "last"]
+
+
+@dataclass
+class FilterRowsTransform:
+    type: Literal[TransformType.FILTER_ROWS]
+    operation: Literal["keep_rows", "remove_rows"]
+    where: List[Condition]
+
+
+@dataclass
+class GroupByTransform:
+    type: Literal[TransformType.GROUP_BY]
+    column_ids: ColumnIds
+    drop_na: bool
+    aggregation: Aggregation
+
+
+@dataclass
+class AggregateTransform:
+    type: Literal[TransformType.AGGREGATE]
+    column_ids: ColumnIds
+    aggregations: List[Aggregation]
+
+
+@dataclass
+class SelectColumnsTransform:
+    type: Literal[TransformType.SELECT_COLUMNS]
+    column_ids: ColumnIds
+
+
+Transform = Union[
+    AggregateTransform,
+    ColumnConversionTransform,
+    FilterRowsTransform,
+    GroupByTransform,
+    RenameColumnTransform,
+    SelectColumnsTransform,
+    SortColumnTransform,
+]
+
+
+@dataclass
+class Transformations:
+    transforms: List[Transform]
---
+++ packages/marimo/marimo-0.1.51.tar.gz/marimo-0.1.51/marimo/_server/api/set_ui_element_value.py
@@ -0,0 +1,33 @@
+# Copyright 2023 Marimo. All rights reserved.
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import Any, List
+
+import tornado.web
+
+from marimo._runtime import requests
+from marimo._server import sessions
+from marimo._utils.parse_dataclass import parse_raw
+
+
+@dataclass
+class SetUIElementValue:
+    # ids of UI elements whose values we'll set
+    object_ids: List[str]
+    # value of each UI element; same length as object_ids
+    values: List[Any]
+
+
+class SetUIElementValueHandler(tornado.web.RequestHandler):
+    def post(self) -> None:
+        session = sessions.require_session_from_header(self.request.headers)
+        args = parse_raw(self.request.body, SetUIElementValue)
+        session.queue.put(
+            requests.SetUIElementValueRequest(
+                zip(
+                    args.object_ids,
+                    args.values,
+                )
+            )
+        )
