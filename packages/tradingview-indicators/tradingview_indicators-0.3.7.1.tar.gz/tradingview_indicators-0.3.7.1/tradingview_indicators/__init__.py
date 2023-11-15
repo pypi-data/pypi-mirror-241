d0981675098cc36b1a9ead42bfe0e750ddcc280a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 05:03:49 +0000

    Add package tradingview-indicators/0.3.7.1/tradingview_indicators-0.3.7.1.tar.gz

---
+++ packages/tradingview-indicators/tradingview_indicators-0.3.7.1.tar.gz/tradingview_indicators-0.3.7.1/tradingview_indicators/__init__.py
@@ -0,0 +1,7 @@
+from .moving_average import MovingAverage
+from .CCI import CCI
+from .MACD import MACD
+from .RSI import RSI
+from .DMI import DMI
+from .stoch import SlowStochastic
+from .ichimoku import Ichimoku
