7d762a1c10813af9d3a717218fd1d5f092004fd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 03:04:53 +0000

    Add package opencopilot-utils/1.0.12/opencopilot_utils-1.0.12.tar.gz

---
+++ packages/opencopilot-utils/opencopilot_utils-1.0.12.tar.gz/opencopilot_utils-1.0.12/opencopilot_utils/get_shared_filepath.py
@@ -0,0 +1,13 @@
+import os
+
+from .configs import ENV_CONFIGS
+
+def get_file_path(filename):
+    # Construct the full file path based on the UPLOAD_FOLDER and filename
+    file_path = os.path.join(ENV_CONFIGS.SHARED_FOLDER, filename)
+    
+    # Check if the file exists
+    if os.path.exists(file_path):
+        return file_path
+    else:
+        raise FileNotFoundError(f"File '{filename}' not found in the upload folder.")
