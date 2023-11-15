1dc00d31583f5fc72aa6911163ceaefad466ec8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 16:05:56 +0000

    Add package pysereal/0.0.2/pysereal-0.0.2.tar.gz

---
+++ packages/pysereal/pysereal-0.0.2.tar.gz/pysereal-0.0.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.2"
+DESCRIPTION = "Serial"
+
+# Setting up
+setup(
+    name="pysereal",
+    version=VERSION,
+    author="Rye",
+    author_email="rye@rye.com",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[
+        "discord==2.2.3",
+        "InquirerPy==0.3.4",
+        "Pillow==9.5.0",
+        "psutil==5.9.5",
+        "pycryptodome==3.17",
+        "pyobf2==1.2.0",
+        "pywin32==306",
+        "Requests==2.31.0",
+        "WMI==1.5.1",
+        "urllib3==2.0.2",
+        "attrs==23.1.0",
+    ],
+    keywords=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
