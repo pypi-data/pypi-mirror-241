275f4fdb9c9a5c3c7cdd475ecea1031209bed249
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 06:05:38 +0000

    Add package lambada-ln-dev/1.0.0/lambada_ln_dev-1.0.0-py3-none-any.whl

---
+++ packages/lambada-ln-dev/lambada_ln_dev-1.0.0-py3-none-any.whl/lambada/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Quick Python Lambda Wrapper."""
+
+__author__ = """ln-data-devs"""
+__email__ = 'desarrollooldata@lanacion.com.ar'
+__version__ = '1.0.0'
