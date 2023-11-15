58b0cf39ae31c8b15460272c14456c6a9ce705e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 16:06:10 +0000

    Add package notevennexus/0.3.0/notevennexus-0.3.0-py3-none-any.whl

---
+++ packages/notevennexus/notevennexus-0.3.0-py3-none-any.whl/notevennexus/__main__.py
@@ -0,0 +1,32 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
+import argparse
+
+import notevennexus as nen
+
+
+def _is_text_file(path: str) -> bool:
+    """Check if file is text file"""
+    try:
+        with open(path, 'r') as f:
+            f.read()
+        return True
+    except UnicodeDecodeError:
+        return False
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Validate NeXus files.')
+    parser.add_argument('path', help='Input file')
+    args = parser.parse_args()
+    path = args.path
+    group = nen.read_json(path) if _is_text_file(path) else nen.read_hdf5(path)
+
+    validators = nen.validators.base_validators()
+    results = nen.validate(group, validators=validators)
+    print(nen.report(results=results))
+    print(nen.make_fileinfo(path))
+
+
+if __name__ == '__main__':
+    main()
