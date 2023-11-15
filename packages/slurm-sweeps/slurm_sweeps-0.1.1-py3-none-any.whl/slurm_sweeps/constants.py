52d4537f90d9cc7279c9c61bce7a91fd4ec73673
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 14:03:54 +0000

    Add package slurm-sweeps/0.1.1/slurm_sweeps-0.1.1-py3-none-any.whl

---
+++ packages/slurm-sweeps/slurm_sweeps-0.1.1-py3-none-any.whl/slurm_sweeps/constants.py
@@ -0,0 +1,21 @@
+# env variables
+STORAGE_PATH = "SLURMSWEEPS_STORAGE_PATH"
+DB_PATH = "SLURMSWEEPS_DB_PATH"
+EXPERIMENT_NAME = "SLURMSWEEPS_EXPERIMENT_NAME"
+TRIAL_ID = "SLURMSWEEPS_TRIAL_ID"
+
+# DB keys
+DB_CFG = "_cfg"
+DB_ITERATION = "_iteration"
+DB_LOGGED = "_logged"
+DB_TIMESTAMP = "_timestamp"
+DB_TRIAL_ID = "_trial_id"
+
+# Storage keys
+TRAIN_PKL = "train.pkl"
+TRAIN_PY = "train.py"
+CFG_YML = "cfg.yml"
+ASHA_PKL = "asha.pkl"
+
+# Configuration
+WAITING_TIME_IN_SEC = 0.01
