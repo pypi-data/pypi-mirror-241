abdda0b7b9bfd4f7e61a48e60cb0ccec3169d392
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 03:03:23 +0000

    Add package nlu/5.1.1rc1/nlu-5.1.1rc1.tar.gz

---
+++ packages/nlu/nlu-5.1.1rc1.tar.gz/nlu-5.1.1rc1/nlu/components/embeddings/sentence_e5/E5SentenceEmbedding.py
@@ -0,0 +1,19 @@
+from sparknlp.annotator import E5Embeddings
+
+
+class E5:
+    @staticmethod
+    def get_default_model():
+        return E5Embeddings.pretrained() \
+            .setInputCols(["document"]) \
+            .setOutputCol("e5_embeddings") \
+            .setBatchSize(1)
+
+
+    @staticmethod
+    def get_pretrained_model(name, language, bucket=None):
+        return E5Embeddings.pretrained(name,language,bucket) \
+            .setInputCols(["document"]) \
+            .setOutputCol("e5_embeddings") \
+            .setBatchSize(1)
+
---
+++ packages/nlu/nlu-5.1.1rc1.tar.gz/nlu-5.1.1rc1/nlu/components/embeddings/sentence_mpnet/MPNetSentenceEmbedding.py
@@ -0,0 +1,17 @@
+from sparknlp.annotator import MPNetEmbeddings
+
+
+class MPNetSentence:
+    @staticmethod
+    def get_default_model():
+        return MPNetEmbeddings.pretrained() \
+            .setInputCols(["documents"]) \
+            .setOutputCol("mpnet_embeddings") \
+            .setBatchSize(1)
+
+    @staticmethod
+    def get_pretrained_model(name, language, bucket=None):
+        return MPNetEmbeddings.pretrained(name, language, bucket) \
+            .setInputCols(["documents"]) \
+            .setOutputCol("mpnet_embeddings") \
+            .setBatchSize(1)
