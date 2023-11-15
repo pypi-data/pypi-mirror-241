dc21a7f689a0aeb21960c2a9a78e54b5f797409d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 06:04:52 +0000

    Add package olympict/1.3.5/olympict-1.3.5.tar.gz

---
+++ packages/olympict/olympict-1.3.5.tar.gz/olympict-1.3.5/olympict/__init__.py
@@ -0,0 +1,16 @@
+from olympict.files.o_image import OlympImage
+from olympict.files.o_video import OlympVid
+from olympict.pipelines import ImagePipeline, VideoPipeline
+from olympict.types import (
+    BBoxAbsolute,
+    BBoxRelative,
+    Color,
+    Img,
+    LineAbsolute,
+    LineRelative,
+    PointAbsolute,
+    PointRelative,
+    PolygonAbsolute,
+    PolygonRelative,
+    Size,
+)
