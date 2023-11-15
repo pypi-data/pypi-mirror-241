61409cd9044e444e587b520bbc280c90705c01a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 17:04:38 +0000

    Add package odoo13-addon-gestion-editorial/13.0.0.4.2.dev1/odoo13-addon-gestion_editorial-13.0.0.4.2.dev1.tar.gz

---
+++ packages/odoo13-addon-gestion-editorial/odoo13-addon-gestion_editorial-13.0.0.4.2.dev1.tar.gz/odoo13-addon-gestion_editorial-13.0.0.4.2.dev1/models/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from . import res_partner_descontrol
+from . import product_descontrol
+from . import liquidacion_descontrol
+from . import sale_order_descontrol
+from . import stock_picking_descontrol
+from . import purchase_order_descontrol
+from . import product_pricelist_descontrol
