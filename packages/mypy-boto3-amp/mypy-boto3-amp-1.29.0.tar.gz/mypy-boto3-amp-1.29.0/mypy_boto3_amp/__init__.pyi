6321b03ae73bec31a644fbefec335ef52cfef4d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 06:05:48 +0000

    Add package mypy-boto3-amp/1.29.0/mypy-boto3-amp-1.29.0.tar.gz

---
+++ packages/mypy-boto3-amp/mypy-boto3-amp-1.29.0.tar.gz/mypy-boto3-amp-1.29.0/mypy_boto3_amp/__init__.pyi
@@ -0,0 +1,41 @@
+"""
+Main interface for amp service.
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from mypy_boto3_amp import (
+        Client,
+        ListRuleGroupsNamespacesPaginator,
+        ListWorkspacesPaginator,
+        PrometheusServiceClient,
+        WorkspaceActiveWaiter,
+        WorkspaceDeletedWaiter,
+    )
+
+    session = Session()
+    client: PrometheusServiceClient = session.client("amp")
+
+    workspace_active_waiter: WorkspaceActiveWaiter = client.get_waiter("workspace_active")
+    workspace_deleted_waiter: WorkspaceDeletedWaiter = client.get_waiter("workspace_deleted")
+
+    list_rule_groups_namespaces_paginator: ListRuleGroupsNamespacesPaginator = client.get_paginator("list_rule_groups_namespaces")
+    list_workspaces_paginator: ListWorkspacesPaginator = client.get_paginator("list_workspaces")
+    ```
+"""
+
+from .client import PrometheusServiceClient
+from .paginator import ListRuleGroupsNamespacesPaginator, ListWorkspacesPaginator
+from .waiter import WorkspaceActiveWaiter, WorkspaceDeletedWaiter
+
+Client = PrometheusServiceClient
+
+__all__ = (
+    "Client",
+    "ListRuleGroupsNamespacesPaginator",
+    "ListWorkspacesPaginator",
+    "PrometheusServiceClient",
+    "WorkspaceActiveWaiter",
+    "WorkspaceDeletedWaiter",
+)
---
+++ packages/mypy-boto3-amp/mypy-boto3-amp-1.29.0.tar.gz/mypy-boto3-amp-1.29.0/mypy_boto3_amp/waiter.py
@@ -0,0 +1,55 @@
+"""
+Type annotations for amp service client waiters.
+
+[Open documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_amp/waiters/)
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+
+    from mypy_boto3_amp.client import PrometheusServiceClient
+    from mypy_boto3_amp.waiter import (
+        WorkspaceActiveWaiter,
+        WorkspaceDeletedWaiter,
+    )
+
+    session = Session()
+    client: PrometheusServiceClient = session.client("amp")
+
+    workspace_active_waiter: WorkspaceActiveWaiter = client.get_waiter("workspace_active")
+    workspace_deleted_waiter: WorkspaceDeletedWaiter = client.get_waiter("workspace_deleted")
+    ```
+"""
+
+from botocore.waiter import Waiter
+
+from .type_defs import WaiterConfigTypeDef
+
+__all__ = ("WorkspaceActiveWaiter", "WorkspaceDeletedWaiter")
+
+
+class WorkspaceActiveWaiter(Waiter):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/amp.html#PrometheusService.Waiter.WorkspaceActive)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_amp/waiters/#workspaceactivewaiter)
+    """
+
+    def wait(self, *, workspaceId: str, WaiterConfig: WaiterConfigTypeDef = ...) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/amp.html#PrometheusService.Waiter.WorkspaceActive.wait)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_amp/waiters/#workspaceactivewaiter)
+        """
+
+
+class WorkspaceDeletedWaiter(Waiter):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/amp.html#PrometheusService.Waiter.WorkspaceDeleted)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_amp/waiters/#workspacedeletedwaiter)
+    """
+
+    def wait(self, *, workspaceId: str, WaiterConfig: WaiterConfigTypeDef = ...) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/amp.html#PrometheusService.Waiter.WorkspaceDeleted.wait)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_amp/waiters/#workspacedeletedwaiter)
+        """
