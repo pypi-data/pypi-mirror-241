949ad317b69c92bc4d3248936106207119249e87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 06:05:45 +0000

    Add package mypy-boto3-personalize-runtime/1.29.0/mypy-boto3-personalize-runtime-1.29.0.tar.gz

---
+++ packages/mypy-boto3-personalize-runtime/mypy-boto3-personalize-runtime-1.29.0.tar.gz/mypy-boto3-personalize-runtime-1.29.0/mypy_boto3_personalize_runtime/type_defs.py
@@ -0,0 +1,105 @@
+"""
+Type annotations for personalize-runtime service type definitions.
+
+[Open documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_personalize_runtime/type_defs/)
+
+Usage::
+
+    ```python
+    from mypy_boto3_personalize_runtime.type_defs import GetPersonalizedRankingRequestRequestTypeDef
+
+    data: GetPersonalizedRankingRequestRequestTypeDef = ...
+    ```
+"""
+
+import sys
+from typing import Dict, List, Mapping, Sequence
+
+if sys.version_info >= (3, 12):
+    from typing import NotRequired
+else:
+    from typing_extensions import NotRequired
+if sys.version_info >= (3, 12):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+
+__all__ = (
+    "GetPersonalizedRankingRequestRequestTypeDef",
+    "PredictedItemTypeDef",
+    "ResponseMetadataTypeDef",
+    "PromotionTypeDef",
+    "GetPersonalizedRankingResponseTypeDef",
+    "GetRecommendationsResponseTypeDef",
+    "GetRecommendationsRequestRequestTypeDef",
+)
+
+GetPersonalizedRankingRequestRequestTypeDef = TypedDict(
+    "GetPersonalizedRankingRequestRequestTypeDef",
+    {
+        "campaignArn": str,
+        "inputList": Sequence[str],
+        "userId": str,
+        "context": NotRequired[Mapping[str, str]],
+        "filterArn": NotRequired[str],
+        "filterValues": NotRequired[Mapping[str, str]],
+    },
+)
+PredictedItemTypeDef = TypedDict(
+    "PredictedItemTypeDef",
+    {
+        "itemId": NotRequired[str],
+        "score": NotRequired[float],
+        "promotionName": NotRequired[str],
+    },
+)
+ResponseMetadataTypeDef = TypedDict(
+    "ResponseMetadataTypeDef",
+    {
+        "RequestId": str,
+        "HostId": str,
+        "HTTPStatusCode": int,
+        "HTTPHeaders": Dict[str, str],
+        "RetryAttempts": int,
+    },
+)
+PromotionTypeDef = TypedDict(
+    "PromotionTypeDef",
+    {
+        "name": NotRequired[str],
+        "percentPromotedItems": NotRequired[int],
+        "filterArn": NotRequired[str],
+        "filterValues": NotRequired[Mapping[str, str]],
+    },
+)
+GetPersonalizedRankingResponseTypeDef = TypedDict(
+    "GetPersonalizedRankingResponseTypeDef",
+    {
+        "personalizedRanking": List[PredictedItemTypeDef],
+        "recommendationId": str,
+        "ResponseMetadata": ResponseMetadataTypeDef,
+    },
+)
+GetRecommendationsResponseTypeDef = TypedDict(
+    "GetRecommendationsResponseTypeDef",
+    {
+        "itemList": List[PredictedItemTypeDef],
+        "recommendationId": str,
+        "ResponseMetadata": ResponseMetadataTypeDef,
+    },
+)
+GetRecommendationsRequestRequestTypeDef = TypedDict(
+    "GetRecommendationsRequestRequestTypeDef",
+    {
+        "campaignArn": NotRequired[str],
+        "itemId": NotRequired[str],
+        "userId": NotRequired[str],
+        "numResults": NotRequired[int],
+        "context": NotRequired[Mapping[str, str]],
+        "filterArn": NotRequired[str],
+        "filterValues": NotRequired[Mapping[str, str]],
+        "recommenderArn": NotRequired[str],
+        "promotions": NotRequired[Sequence[PromotionTypeDef]],
+    },
+)
