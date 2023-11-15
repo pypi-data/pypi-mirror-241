e80bc2410411d2ae297d45ea4e36b1d916c0945d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 04:06:32 +0000

    Add package cfn-lint/1.2.0a1/cfn_lint-1.2.0a1-py3-none-any.whl

---
+++ packages/cfn-lint/cfn_lint-1.2.0a1-py3-none-any.whl/cfnlint/rules/parameters/PropertyNames.py
@@ -0,0 +1,20 @@
+"""
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: MIT-0
+"""
+from cfnlint.rules.jsonschema.PropertyNames import PropertyNames as ParentPropertyNames
+
+
+class PropertyNames(ParentPropertyNames):
+    """Check if maximum Parameter name size limit is exceeded"""
+
+    id = "E2003"
+    shortdesc = "Parameter name limit not exceeded"
+    description = (
+        "Check the size of Parameter names in the template is less than the upper limit"
+    )
+    source_url = "https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/cloudformation-limits.html"
+    tags = ["parameters", "limits"]
+
+    def __init__(self) -> None:
+        super().__init__("I2003")
