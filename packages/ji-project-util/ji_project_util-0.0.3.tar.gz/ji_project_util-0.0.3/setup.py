773ba83d1eecc67b57ae0d5e9a65f17d6f5b377d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 04:04:40 +0000

    Add package ji-project-util/0.0.3/ji_project_util-0.0.3.tar.gz

---
+++ packages/ji-project-util/ji_project_util-0.0.3.tar.gz/ji_project_util-0.0.3/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+# 配置项比较多，有些不是必须，可参考官方文档 https://packaging.python.org/guides/distributing-packages-using-setuptools/
+setuptools.setup(
+    name="ji_project_util", # 项目的名字，将来通过pip install ji_project_util安装，不能与其他项目重复，否则上传失败
+    version="0.0.3", # 项目版本号，自己决定吧
+    author="jixiangkun", # 作者
+    author_email="youngji@yeah.net", # email
+    description="python项目的log工具和监控内容聚合工具",  # 项目描述
+    long_description=long_description, # 加载read_me的内容
+    long_description_content_type="text/markdown", # 描述文本类型
+    url="",  # 项目的地址，比如github或者gitlib地址
+    packages=setuptools.find_packages(),  # 这个函数可以帮你找到包下的所有文件，你可以手动指定
+    package_data={'': ['*.yaml', '*.csv', '*.txt', '.toml']},  # 这个很重要
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ], # 该软件包仅与Python 3兼容，根据MIT许可证进行许可，并且与操作系统无关。您应始终至少包含您的软件包所使用的Python版本，软件包可用的许可证以及您的软件包将使用的操作系统。有关分类器的完整列表，请参阅 https://pypi.org/classifiers/。
+    install_requires=[
+        'pyyaml'
+    ], # 项目依赖，也可以指定依赖版本
+)
