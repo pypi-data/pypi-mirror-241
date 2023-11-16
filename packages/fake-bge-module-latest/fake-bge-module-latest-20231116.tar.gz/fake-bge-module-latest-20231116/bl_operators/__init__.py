950558e61c7f80a2836ce0f992d62b6dca8bb328
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 16:05:07 +0000

    Add package fake-bge-module-latest/20231116/fake-bge-module-latest-20231116.tar.gz

---
+++ packages/fake-bge-module-latest/fake-bge-module-latest-20231116.tar.gz/fake-bge-module-latest-20231116/bl_operators/__init__.py
@@ -0,0 +1,49 @@
+import sys
+import typing
+from . import object
+from . import userpref
+from . import geometry_nodes
+from . import assets
+from . import mesh
+from . import wm
+from . import add_mesh_torus
+from . import anim
+from . import image
+from . import vertexpaint_dirt
+from . import uvcalc_follow_active
+from . import uvcalc_transform
+from . import console
+from . import screen_play_rendered_anim
+from . import presets
+from . import bmesh
+from . import freestyle
+from . import sequencer
+from . import file
+from . import rigidbody
+from . import object_align
+from . import uvcalc_lightmap
+from . import spreadsheet
+from . import constraint
+from . import clip
+from . import view3d
+from . import node
+from . import object_quick_effects
+from . import object_randomize_transform
+
+GenericType = typing.TypeVar("GenericType")
+
+
+def register():
+    ''' 
+
+    '''
+
+    pass
+
+
+def unregister():
+    ''' 
+
+    '''
+
+    pass
