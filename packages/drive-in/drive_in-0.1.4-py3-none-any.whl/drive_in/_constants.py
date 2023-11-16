7de414f17cbc3f6a1a77bc294f7fa41cb4710841
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 03:04:14 +0000

    Add package drive-in/0.1.4/drive_in-0.1.4-py3-none-any.whl

---
+++ packages/drive-in/drive_in-0.1.4-py3-none-any.whl/drive_in/_constants.py
@@ -0,0 +1,6 @@
+CLIENT_ID = "405335407266-bbdvf0r5df6an45o61js5dsciq0erh0s.apps.googleusercontent.com"
+REDIRECT_URI = "https://oauth.trialandsuccess.nl/callback"
+# SCOPE = "https://www.googleapis.com/auth/drive.install"
+SCOPE = "https://www.googleapis.com/auth/drive.file"
+
+AUTH_TOKEN_FILE = ".gdrive_access_token"  # nosec
