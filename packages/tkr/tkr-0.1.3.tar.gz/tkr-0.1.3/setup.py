b4bd366dfc7addba209d65482248ed2360c45421
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 14:03:58 +0000

    Add package tkr/0.1.3/tkr-0.1.3.tar.gz

---
+++ packages/tkr/tkr-0.1.3.tar.gz/tkr-0.1.3/setup.py
@@ -0,0 +1,41 @@
+import pathlib
+from setuptools import setup, find_packages
+
+def post_install():
+  print("tkr.core:tkr_setup_hook -> Thank you for using tkr! For help please use the tkr.help() method. Git Repo: https://github.com/toolkitr/tkr")
+
+setup(
+    name='tkr',
+    license='MIT',
+    version='0.1.3',
+    author='tlkr.',
+    author_email='toolkitr.email@gmail.com',
+    description='Python Toolkit',
+    long_description=pathlib.Path('README.md').read_text(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/toolkitr/tkrcord',
+    entry_points={"console_scripts": ["tkr=tkr.cli:main", 'tkr-post-install=tkr:post_install']},
+    packages=find_packages(exclude=['tests', 'examples']),
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+        'Typing :: Typed',
+    ],
+    keywords='tkr, toolkitr',
+    python_requires='>=3.10.0,<3.12',
+    install_requires=[
+    ],
+    project_urls={
+        'Bug Reports': 'https://github.com/toolkitr/tkrcord/issues',
+        'Source': 'https://github.com/toolkitr/tkrcord',
+    },
+)
---
+++ packages/tkr/tkr-0.1.3.tar.gz/tkr-0.1.3/tkr/__init__.py
@@ -0,0 +1,12 @@
+from . import core
+from . import abc
+
+__version__, __name__, __package__, __author__, __email__, __description__ = core.tkr_setup_hook()
+
+Tool = abc.Tool
+ToolProxy = abc.ToolProxy
+
+__annotations__ = {
+    'Tool': Tool,
+    'ToolProxy': ToolProxy,
+}
