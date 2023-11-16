44fc8b7b77fbd58e9a5d85f1aca4d4584f6e9270
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 16:06:01 +0000

    Add package cg-maven/0.1.2/cg_maven-0.1.2-py3-none-any.whl

---
+++ packages/cg-maven/cg_maven-0.1.2-py3-none-any.whl/maven/crud/create.py
@@ -0,0 +1,8 @@
+from maven.maven_db.maven_adapter import MavenAdapter
+from maven.models import Case
+
+
+def create_case(case: Case, maven_adapter: MavenAdapter):
+    """Create a case document in the database."""
+    case_json: dict = case.model_dump()
+    maven_adapter.maven_db.case.insert_one(document=case_json)
