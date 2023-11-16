18ca7f49a0759285e78320d614d8f12300e62f22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 14:03:53 +0000

    Add package xftsim/0.1.12.dev0/xftsim-0.1.12.dev0.tar.gz

---
+++ packages/xftsim/xftsim-0.1.12.dev0.tar.gz/xftsim-0.1.12.dev0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='xftsim',
+    
+    version='0.1.12-dev',
+    
+    author="Richard Border",
+    
+    author_email="border.richard@gmail.com",
+    
+    packages=find_packages(include=[
+        'xftsim', 'xftsim*',
+    ]),
+    
+    install_requires = [
+    "funcy",
+    "networkx",
+    "nptyping",
+    "numba==0.56.4",
+    "numpy",
+    "pandas",
+    "pandas_plink",
+    "scipy",
+    "sgkit",
+    "xarray",
+    ],
+
+    include_package_data=True,
+)
