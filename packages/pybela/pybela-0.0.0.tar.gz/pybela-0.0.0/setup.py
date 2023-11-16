589909775610a1cf1eacb6b1b86229ec443fae79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 04:04:09 +0000

    Add package pybela/0.0.0/pybela-0.0.0.tar.gz

---
+++ packages/pybela/pybela-0.0.0.tar.gz/pybela-0.0.0/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+setuptools.setup(
+    name="pybela",
+    version="0.0.0", 
+    author="Teresa Pelinski",
+    author_email="teresapelinski@gmail.com",
+    description="pybela",
+    long_description="pybela",
+    long_description_content_type="text/markdown",
+    url="https://github.com/BelaPlatform/pybela",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "jupyter",
+        "panel",
+        "jupyter-bokeh",
+        "bitarray",
+        "notebook",
+        "websockets",
+        "bokeh",
+        "ipykernel",
+        "nest-asyncio",
+        "aiofiles",
+        "paramiko"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+)
