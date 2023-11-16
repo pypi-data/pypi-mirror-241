a69f86e3e050184afde78fbc348f138004a5cbc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 03:04:39 +0000

    Add package geometry-alrandis/0.3.0/geometry_alrandis-0.3.0.tar.gz

---
+++ packages/geometry-alrandis/geometry_alrandis-0.3.0.tar.gz/geometry_alrandis-0.3.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+ 
+setup(
+    name='geometry_alrandis',
+    version='0.3.0',
+    author='me',
+    author_email='alrandisisdernii@gmail.com',
+    description='This  is very useful package',
+    long_description=open('README.md').read(),
+    packages=find_packages()
+)
