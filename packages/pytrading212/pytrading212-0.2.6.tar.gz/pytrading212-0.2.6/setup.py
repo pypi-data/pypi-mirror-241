d2f506463af315cd2d7e0a83decec5a1f777e5f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 06:04:12 +0000

    Add package pytrading212/0.2.6/pytrading212-0.2.6.tar.gz

---
+++ packages/pytrading212/pytrading212-0.2.6.tar.gz/pytrading212-0.2.6/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pytrading212",
+    version="0.2.6",
+    author="HellAmbro",
+    author_email="frambrosini1998@gmail.com",
+    description="Unofficial Trading212 API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/HellAmbro/Trading212API",
+    packages=setuptools.find_packages(),
+    dependencies=[
+        'selenium',
+        'requests',
+        'rich',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+)
