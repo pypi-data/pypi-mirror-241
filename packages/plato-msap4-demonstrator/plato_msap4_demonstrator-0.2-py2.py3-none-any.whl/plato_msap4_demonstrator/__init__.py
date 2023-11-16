7d32b285ad1e21193bdffb4461f6f131163c705f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 16:05:54 +0000

    Add package plato-msap4-demonstrator/0.2/plato_msap4_demonstrator-0.2-py2.py3-none-any.whl

---
+++ packages/plato-msap4-demonstrator/plato_msap4_demonstrator-0.2-py2.py3-none-any.whl/plato_msap4_demonstrator/__init__.py
@@ -0,0 +1,28 @@
+__author__ = 'Sylvain N. Breton'
+__uri__ = 'https://gitlab.com/sybreton/plato_rotation_pipeline.git'
+__license__ = 'CeCILL'
+__version__ = '0.2'
+__description__ = 'PLATO MSAP4 demonstrator'
+__email__ = 'sylvain.breton@cea.fr'
+
+from .rotation_pipeline import *
+
+from .wavelets import *
+
+from .correlation import *
+
+from .lomb_scargle import *
+
+from .rooster import *
+
+from .aux import *
+
+from .background import *
+
+import plato_msap4_demonstrator.timeseries 
+
+import plato_msap4_demonstrator.catalogs 
+
+import plato_msap4_demonstrator.rooster_instances 
+
+import plato_msap4_demonstrator.constants 
