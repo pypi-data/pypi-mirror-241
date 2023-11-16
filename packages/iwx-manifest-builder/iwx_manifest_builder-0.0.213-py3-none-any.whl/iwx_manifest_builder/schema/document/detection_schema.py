f868c1440fecd5c6b41401db953308b17be2ee26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 15:04:46 +0000

    Add package iwx-manifest-builder/0.0.213/iwx_manifest_builder-0.0.213-py3-none-any.whl

---
+++ packages/iwx-manifest-builder/iwx_manifest_builder-0.0.213-py3-none-any.whl/iwx_manifest_builder/schema/document/detection_schema.py
@@ -0,0 +1,19 @@
+from marshmallow import Schema, fields
+from ..bbox_coord_schema import BboxCoordAttrsSchema
+from ..bbox_pixels_coord_schema import BboxPixelsCoordAttrsSchema
+from ..keypoints_pixels_coord_schema import KeypointsPixelsCoordAttrsSchema
+from ..category_schema import CategoryAttrsSchema
+from ..image_schema import ImageAttrsSchema
+from ..inferred_schema import InferredAttrsSchema
+
+
+class DetectionSchema(Schema):
+    id = fields.String()
+    bbox_coord = fields.Nested(BboxCoordAttrsSchema())
+    bbox_pixels_coord = fields.Nested(BboxPixelsCoordAttrsSchema())
+    bbox_pixels_coord_area = fields.Int()
+    keypoints = fields.Nested(KeypointsPixelsCoordAttrsSchema())
+    category = fields.Nested(CategoryAttrsSchema())
+    image = fields.Nested(ImageAttrsSchema())
+    inferred = fields.Nested(InferredAttrsSchema())
+    score = fields.Float()
