3093ed1abba09ee7fb58d9396d4a6731168b9653
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 03:03:57 +0000

    Add package prodigy-teams-recipes/0.9.10/prodigy_teams_recipes-0.9.10-py3-none-any.whl

---
+++ packages/prodigy-teams-recipes/prodigy_teams_recipes-0.9.10-py3-none-any.whl/prodigy_teams_recipes/recipes/__init__.py
@@ -0,0 +1,43 @@
+# isort: skip_file
+# The import order determines the order of recipes shown in the UI
+from . import (
+    # Annotation recipes
+    ner,
+    spans,
+    textcat,
+    rel,
+    coref,
+    dep,
+    pos,
+    terms,
+    image,
+    audio,
+    curate,
+    review,
+    sent,
+    test_task,
+    # Action recipes
+    db_actions,
+    example_computations,
+    train,
+)
+
+__all__ = [
+    "audio",
+    "dep",
+    "image",
+    "test_task",
+    "ner",
+    "pos",
+    "rel",
+    "coref",
+    "review",
+    "terms",
+    "textcat",
+    "sent",
+    "spans",
+    "curate",
+    "db_actions",
+    "example_computations",
+    "train",
+]
