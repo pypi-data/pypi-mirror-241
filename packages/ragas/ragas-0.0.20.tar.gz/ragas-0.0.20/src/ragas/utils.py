972fb526ed07723392da05c9b358b8775b8ec167
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 05:03:46 +0000

    Add package ragas/0.0.20/ragas-0.0.20.tar.gz

---
+++ packages/ragas/ragas-0.0.20.tar.gz/ragas-0.0.20/src/ragas/utils.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+import logging
+import os
+from functools import lru_cache
+
+DEBUG_ENV_VAR = "RAGAS_DEBUG"
+# constant to tell us that there is no key passed to the llm/embeddings
+NO_KEY = "no-key"
+
+
+@lru_cache(maxsize=1)
+def get_debug_mode() -> bool:
+    if os.environ.get(DEBUG_ENV_VAR, str(False)).lower() == "true":
+        logging.basicConfig(level=logging.DEBUG)
+        return True
+    else:
+        return False
