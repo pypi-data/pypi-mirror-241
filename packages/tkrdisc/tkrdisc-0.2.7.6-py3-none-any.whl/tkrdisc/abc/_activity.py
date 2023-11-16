8ac866dd64c1b26d1d83db5cf87f2c1f8e505e1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 07:04:12 +0000

    Add package tkrdisc/0.2.7.6/tkrdisc-0.2.7.6-py3-none-any.whl

---
+++ packages/tkrdisc/tkrdisc-0.2.7.6-py3-none-any.whl/tkrdisc/abc/_activity.py
@@ -0,0 +1,9 @@
+class Activity:
+  @property
+  def playing(self): return 0
+  @property
+  def streaming(self): return 1
+  @property
+  def listening(self): return 3
+  @property
+  def watching(self): return 4
