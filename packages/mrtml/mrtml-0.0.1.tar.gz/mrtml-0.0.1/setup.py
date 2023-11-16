94b66f8ac592caaa85afd3d6443d7640564bfb47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 04:04:29 +0000

    Add package mrtml/0.0.1/mrtml-0.0.1.tar.gz

---
+++ packages/mrtml/mrtml-0.0.1.tar.gz/mrtml-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='mrtml',
+    version='0.0.1',
+    description='placeholder',
+    long_description='placeholder',
+    url='https://github.com/el-mrt/mrtml',
+    author='Martin Gruber',
+    author_email='Martin.Gruber.1112@gmail.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
