1d0d0760d3e2c991a4ed380d7976ecdaf12b6da8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 04:04:11 +0000

    Add package dysregnet/0.0.5/dysregnet-0.0.5.tar.gz

---
+++ packages/dysregnet/dysregnet-0.0.5.tar.gz/dysregnet-0.0.5/README.md
@@ -0,0 +1,152 @@
+[![PyPI version](https://badge.fury.io/py/dysregnet.svg)](https://badge.fury.io/py/dysregnet)
+
+# DysRegNet package
+
+
+DysRegNet, is a  method for inferring patient-specific regulatory alterations (dysregulations) from gene expression profiles. DysRegNet uses linear models to account for confounders and residual-derived z-scores to assess significance.
+
+
+## Installation
+To install the package from PyPI please run:
+
+`pip install dysregnet`
+
+
+or you can install it from git:
+
+`git clone https://github.com/biomedbigdata/DysRegNet_package.git  && cd DysRegNet_package`
+
+`python setup.py install`
+
+
+
+## Data input
+
+The inputs of the  package are the following Pandas DataFrame object:
+
+
+- expression_data  - Gene expression matrix with the format: patients as rows (first column - patients/samples ids), and genes as columns.
+- GRN - Gene Regulatory Network (GRN) with two columns in the following order ['TF', 'target'].
+- meta -  Metadata with the first column containing patients/samples ids and other columns for the condition and the covariates.
+
+
+The patients id or samples ids must be the same in the "expression_data" and  "meta". Additionally, gene names or ids must match the ones in the "GRN" DataFrame. 
+
+In the condition column of the meta DataFrame, the control samples should be encoded as 0 and case samples as 1.
+
+The gene regulatory network should be provided by the user. You can either use an experimental validated GRN or learn it from control samples. We recommend using software like [arboreto](https://github.com/aertslab/arboreto) since you can use its output directly to DysRegNet.
+
+
+
+
+
+## Parameters 
+
+
+Additionally, you can provide the following parameters:
+
+
+            
+- conCol: Column name for the condition in the meta DataFrame.
+
+- CatCov: List of categorical variable names. They should match the name of their columns in the meta Dataframe.
+
+- ConCov: List of continuous covariates. They should match the name of their columns in the meta Dataframe.
+
+- zscoring: Boolean, default: False. zscoring of expression data (if needed).
+
+- bonferroni_alpha: P-value threshold for multiple testing correction
+
+- normaltest: Boolean. If True, Run a normality test for residuals "scipy.stats.normaltest". If residuals are not normal, the edge will not be considered in the analysis. 
+
+- normaltest_alpha: p-value threshold for normaltest (if True).
+
+- R2_threshold: R-squared (R2) threshold from 0 to 1 (optional).  If the fit is weaker, the edge will not be considered in the analysis. 
+
+- direction_condition: Boolean. If True: only include dysregulation that are relevant for the interactions (down-regulation of an activation or up-regulation of a supressions). Please check the paper for more details.
+
+
+## Get Started
+
+
+Please note that the functions are annotated with dockstrings for more details.
+
+Import the package and pandas:
+
+
+```python
+import dysregnet
+import pandas as pd
+```
+
+
+
+Define the confounding variables or the design matrix 
+
+```python
+# The condition column
+conCol='condition'
+
+# categorical variable columns in meta dataframe.
+# these columns will be transformed to variables for regression 
+CatCov=['race','gender']  
+
+# continuous variable columns in meta dataframe.
+ConCov=['birth_days_to']
+```
+
+
+Run DysRegNet
+
+```python
+data=dysregnet.run(expression_data=expr,
+                   meta=meta, 
+                   GRN=grn,
+                   conCol=conCol
+                   CatCov=CatCov,
+                   ConCov=ConCov,
+                   direction_condition=True,
+                   normaltest=True,
+                   R2_threshold=.2 )
+
+# results table
+data.get_results()
+
+# or a binary result
+
+data.get_results_binary()
+
+# get R2 values, coefficients, and coefficient p-values for all models/edges
+data.get_model_stats()
+
+```
+
+The expected run time for the installation and running the demo dataset on a "normal" desktop computer is around 3~5 minutes.
+
+
+
+## The output
+
+The package outputs a data frame that represents patient-specific dysregulated edges. The columns represent edges, and the rows are patient IDs. 
+
+In the result table, a value of 0 means that the edge is not significantly dysregulated (different from control samples). Otherwise, the z-score is reported, with a positive in case of activation and a negative sign in case of repression (different than the sign of the residual). 
+
+The method "get_results_binary()" outputs binarized dysregulations instead of z-scores. 
+
+
+## Example
+
+A simple example for running DysRegNet:
+([Notebook](https://github.com/biomedbigdata/DysRegNet_package/blob/main/test.ipynb)/[Google Colab](https://colab.research.google.com/github/biomedbigdata/DysRegNet_package/blob/main/test.ipynb)).
+
+
+You will need to download the demo dataset and extract the files into test dataset/
+
+Link for the demo dataset: https://figshare.com/ndownloader/files/35142652
+
+
+
+## Cite
+
+"DysRegNet: Patient-specific and confounder-aware dysregulated network inference"
+Olga Lazareva*, Zakaria Louadi*, Johannes Kersting, Jan Baumbach, David B. Blumenthal, Markus List. bioRxiv 2022.04.29.490015; doi: https://doi.org/10.1101/2022.04.29.490015. * equal first-authors
---
+++ packages/dysregnet/dysregnet-0.0.5.tar.gz/dysregnet-0.0.5/src/dysregnet/functions.py
@@ -0,0 +1,178 @@
+import pandas as pd
+from scipy.stats import zscore
+from sklearn.linear_model import LinearRegression
+from tqdm import tqdm
+import numpy as np
+from scipy import stats
+import statsmodels.stats.multitest as mt
+import statsmodels.api as sm
+
+
+def process_data(data):
+       
+
+        # process covariates and design matrix
+        
+        all_covariates= data.CatCov + data.ConCov
+
+        if not all_covariates or len(data.meta)==1:
+
+                # No covariate provided
+                print('You did not input any covariates in CatCov or ConCov parameters, proceed without them.')
+                cov_df=None
+
+        else:
+
+
+                # check if covariates exist in meta
+                if not set(all_covariates).issubset(data.meta.columns):
+                    raise ValueError("Invalid elements in CatCov or ConCov. Please check that all covariates names (continuous or categorials) are in the meta DataFrame. ")
+
+                cov_df=data.meta[all_covariates]
+
+                # process categorial covariate
+                # drop_first is important to avoid multicollinear
+                cov_df=pd.get_dummies(cov_df, columns=data.CatCov, drop_first=True, dtype=int)
+                
+                
+                
+        # z scoring of expression
+        if data.zscoring: expr=data.expression_data.apply(zscore) 
+        else:  expr=data.expression_data
+        
+        
+        #get control and case sample 
+        control= data.meta[ data.meta[data.conCol]==0 ].index.values.tolist()
+        case=data.meta[ data.meta[data.conCol]==1 ].index.values.tolist()
+
+        return cov_df, expr, control, case
+    
+    
+    
+def dyregnet_model(data):
+        
+        # Fit a model for every edge
+        # Detect outliers and calculate zscore and pvalues
+        # correct for multiple testing 
+        
+        # prepare data
+        
+        if data.cov_df is not None:
+            control=pd.merge(data.cov_df.loc[data.control],data.expr, left_index=True, right_index=True).drop_duplicates()
+            case=pd.merge(data.cov_df.loc[data.case],data.expr, left_index=True, right_index=True).drop_duplicates()
+            covariate_name= list(data.cov_df.columns)
+        
+        else:
+            control=data.expr.loc[data.control]
+            case=data.expr.loc[data.case]
+            covariate_name=[]
+            
+        edges = {}
+        edges['patient id']=list(case.index.values)
+        model_stats = {}
+        for tup in tqdm(data.GRN.itertuples()):
+                    # pvalues for the same edge for all patients
+
+                    edge = (tup[1],tup[2])
+                    
+                    # skip self loops
+                    if edge[0] != edge[1]:
+
+                        # prepare control for fitting model
+                        x_train = control[  [edge[0]] + covariate_name ]
+                        x_train = sm.add_constant(x_train) # add bias
+                        y_train = control[edge[1]].values
+
+                        # fit the model
+                        model = sm.OLS(y_train, x_train)
+                        results = model.fit()
+                        
+                        model_stats[edge] = [results.rsquared] + list(results.params.values) + list(results.pvalues.values)
+                        
+
+                        # get residuals of control
+                        resid_control = results.predict(x_train) -  y_train
+
+                        # test data (case or condition)
+                        x_test = case[  [edge[0]]+ covariate_name    ]
+                        x_test = sm.add_constant(x_test) # add bias
+                        y_test = case[edge[1]].values
+
+
+                        # define residue for cases
+                        resid_case =  results.predict(x_test) - y_test
+
+                        
+                        # condition of direction
+                        cond = True
+                        direction = np.sign(results.params.iloc[1])
+                        
+                        
+                        # two sided p_value as default
+                        # if direction_condition is false calculate, two sided p value
+                        sides = 2
+
+                        if data.direction_condition: 
+                            cond = ( direction * resid_case ) > 0
+                            
+                            # if direction_condition is true only calculate one sided p value
+                            sides = 1
+
+                        
+                        # calculate zscore
+                        zscore= (resid_case - resid_control.mean()) / resid_control.std()
+      
+
+
+                        # Quality check of the fitness (optionally and must be provided by user)
+
+
+                        if (data.R2_threshold is not None) and  ( data.R2_threshold > results.rsquared ):
+                            # model fit is not that good on training
+                            # shrink the zscores
+                            edges[edge]= [0.0] * len(zscore)
+                            continue
+
+                        #normality test for residuals
+                        if  data.normaltest:
+                            pv = stats.normaltest(resid_control)[1]
+                            if pv> data.normaltest_alpha:
+                                # shrink the zscores to 0s
+                                edges[edge]= [0.0] * len(zscore)
+                                continue
+
+
+                        # zscores to p values
+                        pvalues=stats.norm.sf(abs(zscore)) * sides
+
+                        # correct for multi. testing
+                        pvalues=sm.stats.multipletests(pvalues,method='bonferroni',alpha=data.bonferroni_alpha)[1]
+
+                        pvalues= pvalues < data.bonferroni_alpha
+
+
+                        # add direction to z scores
+                        zscore=abs(zscore) * direction
+
+
+                        # direction condition and a p_value 
+                        valid= cond * pvalues
+
+
+
+                        # shrink the z scores that are not signifcant or not in the condition
+                        zscore[~valid]=0.0
+
+
+                        edges[edge] = np.round(zscore, 1)
+
+                        
+                    
+        results = pd.DataFrame.from_dict(edges)
+        results = results.set_index('patient id')
+        
+        model_stats_cols = ["R2"] + ["coef_" + coef for coef in ["intercept", "TF"] + covariate_name] + ["pval_" + coef for coef in ["intercept", "TF"] + covariate_name]
+        model_stats = pd.DataFrame([model_stats[edge] for edge in results.columns], index=results.columns, columns=model_stats_cols)
+
+        
+        return results, model_stats
