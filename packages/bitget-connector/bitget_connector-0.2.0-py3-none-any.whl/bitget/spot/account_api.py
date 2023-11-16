31003489393b64170d011147795b21b08f706d1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 04:04:29 +0000

    Add package bitget-connector/0.2.0/bitget_connector-0.2.0-py3-none-any.whl

---
+++ packages/bitget-connector/bitget_connector-0.2.0-py3-none-any.whl/bitget/spot/account_api.py
@@ -0,0 +1,23 @@
+from bitget.client import Client
+from bitget.utils.consts import GET
+
+
+class AccountApi(Client):
+    def __init__(self, api_key, api_secret_key, passphrase, use_server_time=False, first=False):
+        Client.__init__(self, api_key, api_secret_key,
+                        passphrase, use_server_time, first)
+
+    def info(self, params):
+        return self._request_with_params(GET, '/api/v2/spot/account/info', params)
+
+    def assets(self, params):
+        return self._request_with_params(GET, '/api/v2/spot/account/assets', params)
+
+    def bills(self, params):
+        return self._request_with_params(GET, '/api/v2/spot/account/bills', params)
+
+    def transferRecords(self, params):
+        return self._request_with_params(GET, '/api/v2/spot/account/transferRecords', params)
+
+    def symbolInfo(self, params):
+        return self._request_with_params(GET, '/api/v2/spot/public/symbols', params)
---
+++ packages/bitget-connector/bitget_connector-0.2.0-py3-none-any.whl/bitget/utils/consts.py
@@ -0,0 +1,48 @@
+# Base Url
+API_URL = 'https://api.bitget.com'
+
+# http header
+CONTENT_TYPE = 'Content-Type'
+OK_ACCESS_KEY = 'ACCESS-KEY'
+OK_ACCESS_SIGN = 'ACCESS-SIGN'
+OK_ACCESS_TIMESTAMP = 'ACCESS-TIMESTAMP'
+OK_ACCESS_PASSPHRASE = 'ACCESS-PASSPHRASE'
+APPLICATION_JSON = 'application/json'
+
+# header key
+LOCALE = 'locale'
+
+# method
+GET = "GET"
+POST = "POST"
+DELETE = "DELETE"
+
+REQUEST_PATH = '/user/verify'
+
+# WS section
+CONTRACT_WS_URL = '	wss://ws.bitget.com/spot/v1/stream'
+
+SUBSCRIBE = 'subscribe'
+UNSUBSCRIBE = 'unsubscribe'
+LOGIN = 'login'
+
+SWAP_TICKER = 'swap/ticker'
+SWAP_CANDLES_1M = 'swap/candle60s'
+SWAP_CANDLES_5M = 'swap/candle300s'
+SWAP_CANDLES_15M = 'swap/candle900s'
+SWAP_CANDLES_30M = 'swap/candle1800s'
+SWAP_CANDLES_1H = 'swap/candle3600s'
+SWAP_CANDLES_4H = 'swap/candle14400s'
+SWAP_CANDLES_12H = 'swap/candle43200s'
+SWAP_CANDLES_1D = 'swap/candle86400s'
+SWAP_CANDLES_1W = 'swap/candle604800s'
+SWAP_TRADE = 'swap/trade'
+SWAP_FUNDING_RATE = 'swap/funding_rate'
+SWAP_PRICE_RANGE = 'swap/price_range'
+SWAP_DEPTH = 'swap/depth'
+SWAP_MARK_PRICE = 'swap/mark_price'
+SWAP_ACCOUNT = 'swap/account'
+SWAP_POSITION = 'swap/position'
+SWAP_ORDER = 'swap/order'
+
+
