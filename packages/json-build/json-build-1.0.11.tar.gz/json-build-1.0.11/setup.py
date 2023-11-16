7dd5cd5f8996f0bf940f4b6eb75e6332c1fcfb5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 06:03:52 +0000

    Add package json-build/1.0.11/json-build-1.0.11.tar.gz

---
+++ packages/json-build/json-build-1.0.11.tar.gz/json-build-1.0.11/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '1.0.11'
+DESCRIPTION = 'JSON file builder'
+LONG_DESCRIPTION = 'A package that allows developers to build and save JSON files quickly.'
+
+setup(
+    name="json-build",
+    version=VERSION,
+    author="Vince Berry",
+    author_email="vincent.berry11@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'json', 'build', 'config'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
