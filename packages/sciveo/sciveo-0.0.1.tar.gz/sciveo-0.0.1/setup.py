ce70fe37959284a936df5ac9aca0c03537a44464
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 04:04:11 +0000

    Add package sciveo/0.0.1/sciveo-0.0.1.tar.gz

---
+++ packages/sciveo/sciveo-0.0.1.tar.gz/sciveo-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sciveo',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+      'pandas>=0.0.0',
+      'numpy>=0.0.0',
+    ],
+)
---
+++ packages/sciveo/sciveo-0.0.1.tar.gz/sciveo-0.0.1/test/test_sampling.py
@@ -0,0 +1,42 @@
+import unittest
+
+from sampling import *
+
+
+class TestSampling(unittest.TestCase):
+  def test_random_1(self):
+    config = {
+        "booster": {
+            "values": ["gbtree", "gblinear"]
+        },
+        "learning_rate": {
+          "min": 0.001,
+          "max": 1.0
+        },
+        "gamma": {
+          "min": 0.001,
+          "max": 1.0
+        },
+        "max_depth": {
+            "values": [3, 5, 7]
+        },
+        "min_child_weight": {
+          "min": 1,
+          "max": 150
+        },
+        "early_stopping_rounds": {
+          "values" : [10, 20, 30, 40]
+        },
+    }
+
+    sampler = RandomSampler(config, n_samples=10)
+
+    print(sampler.samples)
+
+    self.assertTrue(len(sampler.samples) == 10)
+
+
+if __name__ == '__main__':
+    unittest.main()
+    # T = TestSampling()
+    # T.test_random_1()
