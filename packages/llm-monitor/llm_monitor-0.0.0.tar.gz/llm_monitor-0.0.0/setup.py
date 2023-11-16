618a852f9300493f5bd9fa6c6d94bb729e249522
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 06:03:50 +0000

    Add package llm-monitor/0.0.0/llm_monitor-0.0.0.tar.gz

---
+++ packages/llm-monitor/llm_monitor-0.0.0.tar.gz/llm_monitor-0.0.0/setup.py
@@ -0,0 +1,18 @@
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name='llm_monitor',
+    version='0.0.0',
+    packages=find_packages(),
+    description='https://pypi.org/project/llm-monitor/',
+    author='Franz',
+    author_email='franz@rungalileo.io',
+    url='https://pypi.org/project/llm-monitor/',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
