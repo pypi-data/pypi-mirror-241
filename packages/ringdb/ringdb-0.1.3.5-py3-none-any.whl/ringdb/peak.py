9fe2ff50c2d40dce983093982ed5b5ae8b17eb8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 16:05:43 +0000

    Add package ringdb/0.1.3.5/ringdb-0.1.3.5-py3-none-any.whl

---
+++ packages/ringdb/ringdb-0.1.3.5-py3-none-any.whl/ringdb/peak.py
@@ -0,0 +1,5 @@
+def complex_strain_peak_time_fd(sample, wf=10, f_high=1024, df=0.5, f_low=20., f_ref=100.):
+    pass
+    
+def complex_strain_peak_time_td(sample, wf=10, dt=1.0/1024.0, f_low=20., f_ref=100.):
+    pass
