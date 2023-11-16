2636cf2d0bf844becba1242d0d02942ecd211694
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 16:05:06 +0000

    Add package gptbuilder/0.0.2/gptbuilder-0.0.2-py3-none-any.whl

---
+++ packages/gptbuilder/gptbuilder-0.0.2-py3-none-any.whl/gptbuilder/__init__.py
@@ -0,0 +1,8 @@
+from gptbuilder.builder import JsonBuilder
+from gptbuilder.config import OpenaiConfig, AzureConfig
+
+__version__ = "0.0.1"
+__all__ = [
+    'JsonBuilder',
+    'OpenaiConfig',
+    'AzureConfig']
