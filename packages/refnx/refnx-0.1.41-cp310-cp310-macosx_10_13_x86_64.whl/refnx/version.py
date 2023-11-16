9f2738e206a89596b8862a2992764e3911bea081
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 15:05:15 +0000

    Add package refnx/0.1.41/refnx-0.1.41-cp310-cp310-macosx_10_13_x86_64.whl

---
+++ packages/refnx/refnx-0.1.41-cp310-cp310-macosx_10_13_x86_64.whl/refnx/version.py
@@ -0,0 +1,9 @@
+
+# THIS FILE IS GENERATED FROM REFNX SETUP.PY
+short_version = '0.1.41'
+version = '0.1.41'
+full_version = '0.1.41'
+git_revision = '116e0997d2b170c31501f075b27b8ea66034234f'
+release = True
+if not release:
+    version = full_version
