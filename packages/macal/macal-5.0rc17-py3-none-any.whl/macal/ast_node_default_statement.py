d906efe2523842b0c55bace28d40d578a738ee63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 06:03:55 +0000

    Add package macal/5.0rc17/macal-5.0rc17-py3-none-any.whl

---
+++ packages/macal/macal-5.0rc17-py3-none-any.whl/macal/ast_node_default_statement.py
@@ -0,0 +1,25 @@
+#
+# Product:   Macal
+# Author:    Marco Caspers
+# Email:     SamaDevTeam@westcon.com
+# License:   MIT License
+# Date:      24-10-2023
+#
+# Copyright 2023 Westcon-Comstor
+#
+
+# This is the AST Node for the DEFAULT statement
+
+from __future__ import annotations
+from typing import Optional
+from .ast_nodetype import AstNodetype
+from .lex_token import LexToken
+from .ast_node_statement import AstNodeStatement
+from .ast_node_block import AstNodeBlock
+
+
+class AstNodeDefaultStatement(AstNodeStatement):
+    def __init__(self, token: LexToken) -> AstNodeDefaultStatement:
+        super().__init__(token)
+        self.expr_type = AstNodetype.DEFAULT_STATEMENT
+        self.block: Optional[AstNodeBlock] = None
