74d4d247dc61836165a86a958983e31e14a4ee7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 03:04:15 +0000

    Add package start-zero/0.0.10/start-zero-0.0.10.tar.gz

---
+++ packages/start-zero/start-zero-0.0.10.tar.gz/start-zero-0.0.10/sz/core/models.py
@@ -0,0 +1,20 @@
+from sz.core.layers import Layer, Linear
+from sz.functions.ft4 import sigmoid
+
+
+class MLP(Layer):
+
+    def __init__(self, fc_output_sizes, activation=sigmoid):
+        super().__init__()
+        self.activation = activation
+        self.layers = []
+
+        for i, out_size in enumerate(fc_output_sizes):
+            layer = Linear(out_size)
+            setattr(self, 'l' + str(i), layer)
+            self.layers.append(layer)
+
+    def forward(self, x):
+        for l in self.layers[:-1]:
+            x = self.activation(l(x))
+        return self.layers[-1](x)
