ffbbec9511e236db0b6bdd2f9dbf779f60f6ec54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 14:04:15 +0000

    Add package MeowerBot/3.1.2/meowerbot-3.1.2.tar.gz

---
+++ packages/MeowerBot/meowerbot-3.1.2.tar.gz/meowerbot-3.1.2/MeowerBot/cog.py
@@ -0,0 +1,39 @@
+from .command import AppCommand, CB
+import types
+from typing import Union
+
+
+class Cog:
+	commands: dict[str, AppCommand]
+	callbacks: dict[str, list[types.CoroutineType]]
+
+	__instence__: Union["Cog", None] = None
+
+	def __init__(self) -> None:
+		if isinstance(self.__instence__, Cog):
+			return
+
+		self.__class__.__instence__ = self
+		self.commands = {}
+		self.callbacks = {}
+		self.update_commands()
+
+	def update_commands(self):
+		for command in self.__dir__():
+			attr = getattr(self, command)
+			if isinstance(attr, AppCommand):
+				attr.register_class(self)
+				self.commands = AppCommand.add_command(self.commands, attr)
+			elif isinstance(attr, CB):
+				self.callbacks[attr.id] = attr.func
+
+	def __new__(cls, *args, **kwargs):
+		if cls.__instence__ is None:
+			self = super().__new__(cls)
+
+			return self
+
+		else:
+			return cls.__instence__
+
+__all__ = ["Cog"]
