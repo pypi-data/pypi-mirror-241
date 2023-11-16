e91015900ca977c687484700b8438bd2efdd3aa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 07:04:12 +0000

    Add package tkrdisc/0.2.7.6/tkrdisc-0.2.7.6.tar.gz

---
+++ packages/tkrdisc/tkrdisc-0.2.7.6.tar.gz/tkrdisc-0.2.7.6/setup.py
@@ -0,0 +1,43 @@
+import pathlib
+from setuptools import setup, find_packages
+
+setup(
+    name='tkrdisc',
+    license='MIT',
+    version='0.2.7.6',
+    author='tlkr.',
+    author_email='toolkitr.email@gmail.com',
+    description='Custom Discord API Wrapper',
+    long_description=pathlib.Path('README.md').read_text(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/toolkitr/tkrdisc',
+    entry_points={"console_scripts": ["tkrdisc=tkrdisc.cli:main"]},
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+        'Typing :: Typed',
+    ],
+    keywords='tkrdiscord, discordtool, tkrdisc',
+    python_requires='>=3.10.0,<3.12',
+    install_requires=[
+      "aiohttp>=3.7.4",
+      "asyncio>=3.4.3",
+      "async-timeout>=3.0.1",
+      "requests>=2.26.0",
+      "datetime>=4.3.1",
+    ],
+    project_urls={
+        'Bug Reports': 'https://github.com/toolkitr/tkrdisc/issues',
+        'Source': 'https://github.com/toolkitr/tkrdisc',
+    },
+)
---
+++ packages/tkrdisc/tkrdisc-0.2.7.6.tar.gz/tkrdisc-0.2.7.6/tkrdisc/__init__.py
@@ -0,0 +1,30 @@
+import re
+
+from .wss import WSClient, Intents
+from .abc import Embed, Color, Activity
+
+Client = WSClient
+Intents = Intents()
+Color = Color()
+Activity = Activity()
+
+def set_tkrdisc_info():
+  with open('toolkitr/tkrdisc/core/setup.txt', 'r') as file:
+      config_data = file.read()
+
+  metadata_pattern = r'\[metadata\]\s*(.*?)\s*\[|\Z'
+  options_pattern = r'\[options\]\s*(.*?)\s*\[|\Z'
+  key_value_pattern = r'([\w_]+)\s*=\s*(.*)'
+
+  metadata_matches = re.findall(metadata_pattern, config_data, re.DOTALL)
+  metadata_data = '\n'.join(metadata_matches)
+  metadata_pairs = re.findall(key_value_pattern, metadata_data)
+
+  options_matches = re.findall(options_pattern, config_data, re.DOTALL)
+  options_data = '\n'.join(options_matches)
+  options_pairs = re.findall(key_value_pattern, options_data)
+
+  globals().update({f'__{key.strip()}__': value.strip() for key, value in metadata_pairs})
+  globals().update({f'__{key.strip()}__': value.strip() for key, value in options_pairs})
+
+set_tkrdisc_info()
