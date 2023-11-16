90fd9b2da1470c35a77776422fec64f62620a193
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 03:04:09 +0000

    Add package afanlife/1.1/afanlife-1.1.tar.gz

---
+++ packages/afanlife/afanlife-1.1.tar.gz/afanlife-1.1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="afanlife",
+    version="1.1",
+    author="AFAN",
+    author_email="bili_afan@163.com",
+    description="AFAN尝试的Python工具包",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(where='.'
+                                      , exclude=()
+                                      , include=('*')
+                                      ),
+    license='CC0',
+    install_requires=[
+        'scikit-learn==0.23.1'
+        , 'numpy==1.23.2'
+        , 'pandas==11.2.9'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
+    ],
+)
