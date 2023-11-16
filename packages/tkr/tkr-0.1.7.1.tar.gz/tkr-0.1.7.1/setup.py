2d2252a5943149835020eb1ffadbd8e9ca407751
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 15:04:40 +0000

    Add package tkr/0.1.7.1/tkr-0.1.7.1.tar.gz

---
+++ packages/tkr/tkr-0.1.7.1.tar.gz/tkr-0.1.7.1/setup.py
@@ -0,0 +1,45 @@
+import pathlib
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+class PostInstall(install):
+  def run(self):
+    install.run(self)
+    print("tkr.core:tkr_setup_hook -> Git Repo: https://github.com/toolkitr/tkr")
+
+setup(
+    name='tkr',
+    license='MIT',
+    version='0.1.7.1',
+    author='tlkr.',
+    author_email='toolkitr.email@gmail.com',
+    description='Python Toolkit',
+    long_description=pathlib.Path('README.md').read_text(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/toolkitr/tkr',
+    entry_points={"console_scripts": ["tkr=tkr.cli:main"]},
+    cmdclass={'install': PostInstall},
+    packages=find_packages(exclude=['tests', 'examples']),
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+        'Typing :: Typed',
+    ],
+    keywords='tkr, toolkitr',
+    python_requires='>=3.10.0,<3.12',
+    install_requires=[
+    ],
+    project_urls={
+        'Bug Reports': 'https://github.com/toolkitr/tkr/issues',
+        'Source': 'https://github.com/toolkitr/tkr',
+    },
+)
