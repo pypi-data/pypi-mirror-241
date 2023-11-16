2eb91e85053db5a1e74f3436cfe47056de088a7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 06:03:51 +0000

    Add package ufal.morphodita/1.11.2.1/ufal.morphodita-1.11.2.1.tar.gz

---
+++ packages/ufal.morphodita/ufal.morphodita-1.11.2.1.tar.gz/ufal.morphodita-1.11.2.1/setup.py
@@ -0,0 +1,38 @@
+import setuptools
+import sys
+
+with open('README') as file:
+    readme = file.read()
+
+extra_link_args = []
+extra_compile_args = ['-std=c++11', '-w']
+if sys.platform == "darwin":
+    extra_compile_args += ['-stdlib=libc++']
+    extra_link_args += ['-stdlib=libc++']
+
+setuptools.setup(
+    name             = 'ufal.morphodita',
+    version          = '1.11.2.1',
+    description      = 'Bindings to MorphoDiTa library',
+    long_description = readme,
+    author           = 'Milan Straka',
+    author_email     = 'straka@ufal.mff.cuni.cz',
+    url              = 'https://ufal.mff.cuni.cz/morphodita',
+    license          = 'MPL 2.0',
+    packages         = ['ufal', 'ufal.morphodita'],
+    package_data     = {'ufal.morphodita': ['__init__.pyi', 'py.typed']},
+    ext_modules      = [setuptools.Extension(
+        'ufal.morphodita._morphodita',
+        ['ufal/morphodita/morphodita.cpp', 'ufal/morphodita/morphodita_python.cpp'],
+        language = 'c++',
+        extra_compile_args = extra_compile_args,
+        extra_link_args = extra_link_args)],
+    classifiers      = [
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        'Programming Language :: C++',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries'
+    ]
+)
