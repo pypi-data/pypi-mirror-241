d4a78d15d03ec8c809e0ed86618de7770969e4d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 05:04:22 +0000

    Add package hsbcgui/0.0.3/hsbcgui-0.0.3.tar.gz

---
+++ packages/hsbcgui/hsbcgui-0.0.3.tar.gz/hsbcgui-0.0.3/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+
+
+class PostInstallCommand(install):
+    def run(self):
+      with open('C:\\Program Files\\example.txt', 'a') as file:
+        file.write('This line is appended.')
+        
+
+setup(
+  name='hsbcgui',
+  version='0.0.3',
+  description='Improving my Python project',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Joshua Lowe',
+  author_email='josh@edublocks.org',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='calc',
+  cmdclass={
+        'install': PostInstallCommand,
+    }, 
+  packages=find_packages(),
+  install_requires=[''] 
+)
+
