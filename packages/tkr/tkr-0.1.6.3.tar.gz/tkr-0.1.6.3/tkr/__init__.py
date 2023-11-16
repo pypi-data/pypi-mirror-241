c6c86325b447f23fe3f9268bc58aafbd3acf0bff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 15:04:35 +0000

    Add package tkr/0.1.6.3/tkr-0.1.6.3.tar.gz

---
+++ packages/tkr/tkr-0.1.6.3.tar.gz/tkr-0.1.6.3/tkr/__init__.py
@@ -0,0 +1,16 @@
+from . import core
+from . import abc
+
+__version__, __name__, __package__, __author__, __email__, __description__ = core.tkr_setup_hook()
+
+Tool = abc.Tool
+ToolProxy = abc.ToolProxy
+ToolVersion = abc.VERSION
+changelog = core.get_changelog
+
+__annotations__ = {
+    'Tool': Tool,
+    'ToolProxy': ToolProxy,
+    'ToolVersion': ToolVersion,
+    'changelog': changelog
+}
