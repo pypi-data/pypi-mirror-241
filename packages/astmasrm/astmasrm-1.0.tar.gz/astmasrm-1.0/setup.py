b359429342dae2a353344eabbfdc1084ba48e670
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 15:04:34 +0000

    Add package astmasrm/1.0/astmasrm-1.0.tar.gz

---
+++ packages/astmasrm/astmasrm-1.0.tar.gz/astmasrm-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+setup(name="astmasrm",
+      version="1.0",
+      description="package with iot codes",
+      author="Black_shark",
+      packages=['astmasrmcode'],
+      install_requires=[])
