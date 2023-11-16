f7e08bdbfe1dbbe7966ca6c8f327ec47bc700b15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 16:05:16 +0000

    Add package cambridge/3.8.0/cambridge-3.8.0-py3-none-any.whl

---
+++ packages/cambridge/cambridge-3.8.0-py3-none-any.whl/cambridge/settings.py
@@ -0,0 +1,19 @@
+"""This script contains static data."""
+
+VERSION = "3.8.0"
+
+OP = [
+    "FETCHING",
+    "PARSING",
+    "RETRY FETCHING",
+    "RETRY PARSING",
+    "PRINTING",
+    "FOUND",
+    "NOT FOUND",
+    "CACHED",
+    "CANCELLED",
+    "DELETED",
+    "UPDATED",
+]
+
+DICTS = ["CAMBRIDGE", "MERRIAM WEBSTER"]
