55ea6e7ebdc28aa57236c1a4948f7b4010006291
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 03:04:39 +0000

    Add package ipfabric/6.5.0/ipfabric-6.5.0-py3-none-any.whl

---
+++ packages/ipfabric/ipfabric-6.5.0-py3-none-any.whl/ipfabric/models/technology/sdwan.py
@@ -0,0 +1,20 @@
+import logging
+from typing import Any
+
+from pydantic import BaseModel
+
+from ipfabric import models
+
+logger = logging.getLogger("ipfabric")
+
+
+class Sdwan(BaseModel):
+    client: Any = None
+
+    @property
+    def sites(self):
+        return models.Table(client=self.client, endpoint="tables/sdwan/sites")
+
+    @property
+    def links(self):
+        return models.Table(client=self.client, endpoint="tables/sdwan/links")
