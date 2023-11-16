fb72cb9a2af866a17643273a61c92d83581c3614
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 06:03:50 +0000

    Add package jinjarope/0.2.3/jinjarope-0.2.3.tar.gz

---
+++ packages/jinjarope/jinjarope-0.2.3.tar.gz/jinjarope-0.2.3/src/jinjarope/__init__.py
@@ -0,0 +1,48 @@
+__version__ = "0.2.3"
+
+
+from .environment import BlockNotFoundError, Environment
+from .loaders import (
+    LoaderMixin,
+    FileSystemLoader,
+    FsSpecFileSystemLoader,
+    FsSpecProtocolPathLoader,
+    ChoiceLoader,
+    ModuleLoader,
+    NestedDictLoader,
+    TemplateFileLoader,
+    PackageLoader,
+    FunctionLoader,
+    PrefixLoader,
+    DictLoader,
+)
+from .loaderregistry import LoaderRegistry
+from . import utils
+
+registry = LoaderRegistry()
+
+get_loader = registry.get_loader
+
+
+def get_loader_cls_by_id(loader_id: str):
+    loaders = {i.ID: i for i in utils.iter_subclasses(LoaderMixin) if "ID" in i.__dict__}
+    return loaders[loader_id]
+
+
+__all__ = [
+    "BlockNotFoundError",
+    "Environment",
+    "FsSpecFileSystemLoader",
+    "FsSpecProtocolPathLoader",
+    "FileSystemLoader",
+    "ChoiceLoader",
+    "ModuleLoader",
+    "NestedDictLoader",
+    "TemplateFileLoader",
+    "PackageLoader",
+    "FunctionLoader",
+    "PrefixLoader",
+    "DictLoader",
+    "get_loader",
+    "get_loader_cls_by_id",
+]
