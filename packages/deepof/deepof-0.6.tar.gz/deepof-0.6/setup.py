24c97deed2c71ea7e428a8ed6212c8833caf11a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 03:04:32 +0000

    Add package deepof/0.6/deepof-0.6.tar.gz

---
+++ packages/deepof/deepof-0.6.tar.gz/deepof-0.6/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as fp:
+    install_requires = [pkg.replace("\n", "") for pkg in fp]
+
+setuptools.setup(
+    name="deepof",
+    version="0.6",
+    author="Lucas Miranda",
+    author_email="lucas_miranda@psych.mpg.de",
+    description="A suite for postprocessing time-series extracted from videos of freely moving rodents using DeepLabCut amd SLEAP",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.mpcdf.mpg.de/lucasmir/deepof/",
+    packages=setuptools.find_packages(
+        exclude=["*.tests", "*.tests.*", "tests.*", "tests"]
+    ),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+    ],
+    python_requires=">3.8",
+    platform="Platform independent",
+    License="MIT",
+    include_package_data=True,
+    install_requires=install_requires,
+)
