e4f352a582b2fad9d5c4b4e5be73aa68902431df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 05:04:05 +0000

    Add package astmaxxx/0.1.0/astmaxxx-0.1.0.tar.gz

---
+++ packages/astmaxxx/astmaxxx-0.1.0.tar.gz/astmaxxx-0.1.0/setup.py
@@ -0,0 +1,18 @@
+import pathlib
+
+import setuptools
+
+setuptools.setup(
+    name="astmaxxx",
+    version="0.1.0",
+    description="astma codes",
+    long_description=pathlib.Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/HV4512/astma_codes",
+    author="Harsh Vardhan",
+    author_email="jobbykarn@gmail.com",
+    license="Open-Source",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    
+)
