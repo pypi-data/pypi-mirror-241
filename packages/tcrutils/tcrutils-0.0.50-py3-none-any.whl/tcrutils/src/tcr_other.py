fbac4b8502e2e30e6ddb989e929a778667a7c6b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 05:03:47 +0000

    Add package tcrutils/0.0.50/tcrutils-0.0.50-py3-none-any.whl

---
+++ packages/tcrutils/tcrutils-0.0.50-py3-none-any.whl/tcrutils/src/tcr_other.py
@@ -0,0 +1,29 @@
+"""`other` submodule contains garbage or joke functions that never should be used in real code, just don't use this submodule pls. Why is it there? uhhh... :3."""
+from functools import partial
+from typing import Literal
+
+from .tcr_color import c
+
+
+def oddeven(n: int | str) -> Literal['odd', 'even']:
+  """### Outputs a string `'odd'` or `'even'` based on the supplied int `n`.
+
+  ```py
+  >>> from tcrutils.other import oddeven
+  >>> oddeven(1)
+  'odd'
+  >>> oddeven(2)
+  'even'
+  """
+  n = int(n)
+  return 'eovdedn'[n % 2 :: 2]
+
+def hex(number, leading_zeroes=2, *, upper=True):  # noqa: A001 # Intentional shadow
+    hex_output = __import__('builtins').hex(number)
+    hex_value = hex_output[2:].zfill(leading_zeroes).upper() if upper else hex_output[2:].zfill(leading_zeroes)
+
+    formatted_output = f"0x{hex_value}"
+    if not upper:
+        formatted_output = formatted_output.lower()
+
+    return formatted_output
