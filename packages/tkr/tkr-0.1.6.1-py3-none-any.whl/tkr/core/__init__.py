4dd2c8b4c9af2c5ece1aa0571551013309ff9a5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 15:05:28 +0000

    Add package tkr/0.1.6.1/tkr-0.1.6.1-py3-none-any.whl

---
+++ packages/tkr/tkr-0.1.6.1-py3-none-any.whl/tkr/core/__init__.py
@@ -0,0 +1,13 @@
+from .changelog import changelog
+tkr_version = '0.1.6.1'
+
+def tkr_setup_hook() -> list:
+  return (tkr_version, 'tkr', 'toolkitr.tkr', 'tklr.', 'toolkitr.email@gmail.com', 'Python Toolkit')
+
+def get_changelog(version: str = tkr_version) -> str:
+  return changelog.get(version, 'Changelog not found. Try a format like this: (example: 0.0.0.0) -> version=\'0.0.0.0\'')
+
+try:
+  with open(f'.pythonlibs/lib/python3.10/site-packages/tkr/core/coreattrs.json', 'w') as file:
+    file.write(str({"version": tkr_version}))
+except: attrs: str = 'tkr.coreattrs.WriteFailed'
