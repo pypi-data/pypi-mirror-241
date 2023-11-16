bcb75a41724ed9638d75ae33b66bf0aa804f071b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 07:04:03 +0000

    Add package nice-gui-utils/0.1.5/nice_gui_utils-0.1.5-py3-none-any.whl

---
+++ packages/nice-gui-utils/nice_gui_utils-0.1.5-py3-none-any.whl/keycloak_js/keycloak.js
@@ -0,0 +1,27 @@
+export default {
+    template: "<div></div>",
+    mounted() {
+        this.keycloak = globalKeycloakInstance;
+    },
+    methods: {
+        initialize(initConfig) {
+            this.keycloak.init(initConfig).then(
+                    authenticated => {
+                        if (authenticated) {
+                            this.keycloak.onTokenExpired = this.keycloak.updateToken;
+                        }
+                    }
+
+            );
+        },
+        token() {
+            return this.keycloak.token;
+        },
+        refreshToken() {
+            return this.keycloak.refreshToken;
+        },
+        authenticated() {
+            return this.keycloak.authenticated;
+        }
+    }
+};
