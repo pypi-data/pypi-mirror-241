f6ac7d0aef30548917897f1d8a0a984bd576fe42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 06:03:58 +0000

    Add package getpaper/0.3.6/getpaper-0.3.6.tar.gz

---
+++ packages/getpaper/getpaper-0.3.6.tar.gz/getpaper-0.3.6/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.3.6'
+DESCRIPTION = 'getpaper - papers download made easy!'
+LONG_DESCRIPTION = 'A package with python functions for downloading papers'
+
+# Setting up
+setup(
+    name="getpaper",
+    version=VERSION,
+    author="antonkulaga (Anton Kulaga)",
+    author_email="<antonkulaga@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['pycomfort', 'click',
+                      'unstructured', 'unstructured-inference', 'unstructured[local-inference]', 'unstructured.PaddleOCR',
+                      'scidownl', 'Deprecated', 'semanticscholar', 'pdfminer.six', 'langchain'],
+    keywords=['python', 'utils', 'files', 'papers', 'download'],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    entry_points={
+     "console_scripts": [
+         "download=getpaper.download:app",
+         "parse=getpaper.parse:app"
+     ]
+    }
+)
