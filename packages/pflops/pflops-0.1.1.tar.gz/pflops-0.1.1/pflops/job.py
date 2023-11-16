69ca6b111034040866b909fe5638987f7ca3d851
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 16:05:08 +0000

    Add package pflops/0.1.1/pflops-0.1.1.tar.gz

---
+++ packages/pflops/pflops-0.1.1.tar.gz/pflops-0.1.1/pflops/job.py
@@ -0,0 +1,55 @@
+from typing import TypedDict
+from urllib.parse import urljoin
+
+import requests
+import typer
+from rich import print
+from typing_extensions import Annotated
+
+from . import config
+
+app = typer.Typer()
+
+
+class FileInfo(TypedDict):
+    name: str
+    size: int
+
+
+@app.command()
+def ls():
+    """
+    List currently running jobs.
+    """
+    r = requests.get(url=urljoin(config.BASE_URL, "/job/list"))
+    if r.status_code != 200:
+        print("[red]Failed to reach Petaflops server.")
+        return
+    jobs = r.json()
+    if not jobs:
+        print("No jobs are running.")
+    else:
+        for job in jobs:
+            # TODO
+            print(job)
+
+
+@app.command()
+def submit(
+    command: Annotated[
+        str,
+        typer.Argument(
+            help="Path to the executable to be submitted to a job queue.",
+        ),
+    ],
+):
+    """
+    Submit a job to run.
+    """
+    r = requests.get(url=urljoin(config.BASE_URL, f"/job/submit?{command}"))
+    if r.status_code in [200, 201]:
+        print(
+            "[green]Job submitted. You can check your running jobs with `pflops job ls`."
+        )
+    else:
+        print(f"[red]Job submission failed. Status code: {r.status_code}")
