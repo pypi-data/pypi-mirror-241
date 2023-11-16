ecc3e0e5ea390796b9d88a75c6d86dc899c688c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 03:04:15 +0000

    Add package splinepy/0.0.47/splinepy-0.0.47-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/splinepy/splinepy-0.0.47-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/splinepy/settings.py
@@ -0,0 +1,42 @@
+"""splinepy/splinepy/settings.py
+Convenient global variables.
+"""
+
+
+def __splinepy_name_to_type__():
+    """Workaround to provide flexible string to type conversion without
+    causing circular import
+    """
+    from splinepy import NURBS, Bezier, BSpline, Multipatch, RationalBezier
+
+    return {
+        "Bezier": Bezier,
+        "RationalBezier": RationalBezier,
+        "BSpline": BSpline,
+        "NURBS": NURBS,
+        "Multipatch": Multipatch,
+    }
+
+
+TOLERANCE = 1e-11
+"""
+Default tolerance for any operation that involves floats
+"""
+
+NTHREADS = 1
+"""
+Default number of threads.
+"""
+
+NAME_TO_TYPE = None
+"""
+String to Type conversion. Any function that may return different types should
+use this. If you have derived classes, replace this with your own conversion
+dict. In splinepy, this is set in __init__.
+"""
+
+CHECK_BOUNDS = True
+"""
+Bool to check bounds of queries if requested. Can be set to false to
+accelerate process
+"""
