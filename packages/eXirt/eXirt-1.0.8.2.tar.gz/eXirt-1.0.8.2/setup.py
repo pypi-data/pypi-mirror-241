837c664d1622d827068a3266c2312f508ad38154
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 14:04:12 +0000

    Add package eXirt/1.0.8.2/eXirt-1.0.8.2.tar.gz

---
+++ packages/eXirt/eXirt-1.0.8.2.tar.gz/eXirt-1.0.8.2/setup.py
@@ -0,0 +1,16 @@
+# read the contents of your README file
+from pathlib import Path
+from distutils.core import setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(name='eXirt',
+      description='Explainable Artificial Intelligence tool base in Item Response Theory.',
+      author='José de Sousa Ribeiro Filho',
+      author_email='jose.sousa.filho@gmail.com',
+      version='1.0.8.2',
+      packages=['pyexirt'],
+      license='CC0 1.0 Universal'
+      )
+
