46c7ff82a81d6547f78c8172cc68802fa946cb54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 16:05:22 +0000

    Add package pychassiscli/0.2.0/pychassiscli-0.2.0-py3-none-any.whl

---
+++ packages/pychassiscli/pychassiscli-0.2.0-py3-none-any.whl/pychassiscli/cli.py
@@ -0,0 +1,48 @@
+import os
+
+import click
+from rich import print as rich_print
+
+from pychassiscli.utils import get_directory, status, copy_files
+
+INIT_TYPE_CHOICES = ['apiflask', 'nameko']
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.option('-d', '--directory',
+              default='.',
+              show_default=True,
+              help='The directory name for the template to be placed')
+@click.option('-t', '--type', '_type',
+              default='nameko',
+              show_default=True,
+              type=click.Choice(INIT_TYPE_CHOICES, case_sensitive=False),
+              help='The types of the template')
+def init(directory, _type):
+    """
+    Initialize a project via templates.
+    """
+    template_dir = os.path.join(get_directory('templates'), _type)
+    if not os.access(template_dir, os.F_OK):
+        rich_print('No such template type {}'.format(_type))
+        return
+
+    if directory != '.':
+        if os.access(directory, os.F_OK) and os.listdir(directory):
+            rich_print('Directory {} already exists and is not empty'.format(directory))
+            return
+
+        if not os.access(directory, os.F_OK):
+            with status(f'Creating directory {os.path.abspath(directory)!r}'):
+                os.makedirs(directory)
+
+    copy_files(template_dir, directory)
+
+
+if __name__ == '__main__':
+    cli()
