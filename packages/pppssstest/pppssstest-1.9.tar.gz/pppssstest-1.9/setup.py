7923f5df2e16eaf164d24a824f885682073ff3e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 05:03:58 +0000

    Add package pppssstest/1.9/pppssstest-1.9.tar.gz

---
+++ packages/pppssstest/pppssstest-1.9.tar.gz/pppssstest-1.9/setup.py
@@ -0,0 +1,38 @@
+
+
+import setuptools
+
+def readme():
+    try:
+        with open('README.md') as f:
+            return f.read()
+    except IOError:
+        return ''
+
+
+setuptools.setup(
+name="pppssstest",
+version="1.9",
+author="author",
+author_email="author@email.com",
+install_requires=["PySimpleGUI"],
+description="Desc here",
+long_description=readme(),
+long_description_content_type="text/markdown",
+license='Free To Use But Restricted',
+keywords="pp ss tt",
+url="",
+packages=setuptools.find_packages(),
+python_requires="",
+classifiers=[
+"Framework :: PySimpleGUI :: 4",
+],
+package_data={"": 
+[""]
+        },
+entry_points={'gui_scripts': [
+"pptest=pppssstest.pppssstest:main",
+    ]
+    },
+)
+
