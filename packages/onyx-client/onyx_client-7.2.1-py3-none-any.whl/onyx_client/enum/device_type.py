f93cd7ac9706f6c7de1d66934ec96a955cde1cd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 04:03:53 +0000

    Add package onyx-client/7.2.1/onyx_client-7.2.1-py3-none-any.whl

---
+++ packages/onyx-client/onyx_client-7.2.1-py3-none-any.whl/onyx_client/enum/device_type.py
@@ -0,0 +1,45 @@
+"""Device Types of Onyx devices."""
+from enum import Enum, auto
+
+
+class DeviceType(Enum):
+    """The device types supported by Onyx."""
+
+    ROLLERSHUTTER = auto()
+    AWNING = auto()
+    RAFFSTORE_90 = auto()
+    RAFFSTORE_180 = auto()
+    WEATHER = auto()
+    VENEER = auto()
+    BASIC_LIGHT = auto()
+    CLICK = auto()
+    SWITCH = auto()
+    PERGOLA_AWNING_ROOF = auto()
+    PERGOLA_SIDE = auto()
+    PERGOLA_SLAT_ROOF = auto()
+    UNKNOWN = 9999
+
+    def string(self) -> str:
+        """Get the string representation for the Onyx API."""
+        return self.name.lower()
+
+    def is_shutter(self) -> bool:
+        """Check if the type corresponds to any kind of shutter."""
+        return self in [
+            self.ROLLERSHUTTER,
+            self.AWNING,
+            self.RAFFSTORE_90,
+            self.RAFFSTORE_180,
+            self.VENEER,
+            self.PERGOLA_AWNING_ROOF,
+            self.PERGOLA_SIDE,
+            self.PERGOLA_SLAT_ROOF,
+        ]
+
+    @staticmethod
+    def convert(lower: str):
+        """Get the device type from the Onyx API's type information."""
+        try:
+            return DeviceType[lower.upper()]
+        except (KeyError, AttributeError):
+            return None
