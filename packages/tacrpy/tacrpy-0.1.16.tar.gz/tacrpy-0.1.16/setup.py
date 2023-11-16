d5a73c9544b64870b7819f9e9062e73c20016562
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 16:05:40 +0000

    Add package tacrpy/0.1.16/tacrpy-0.1.16.tar.gz

---
+++ packages/tacrpy/tacrpy-0.1.16.tar.gz/tacrpy-0.1.16/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tacrpy',
+    version='0.1.16',
+    description='Analytická knihovna pro potřeby TA ČR',
+    long_description='Knihovna, která slouží pro práci s daty a vypracování analýz TA ČR.',
+    author='RozumDoKapsy',
+    author_email='david.sulc@tacr.cz',
+    # packages=['tacrpy', 'nlp', 'datahub']
+    packages=find_packages(),
+    project_urls={
+        'Documentation': 'https://data.tacr.cz/tacrpy/docs/index.html'
+    }
+)
