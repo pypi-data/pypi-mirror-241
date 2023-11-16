f98e4e65348e0d821ea1d2479dd9456a65f517cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 06:04:06 +0000

    Add package containers-sugar/1.9.1/containers_sugar-1.9.1-py3-none-any.whl

---
+++ packages/containers-sugar/containers_sugar-1.9.1-py3-none-any.whl/containers_sugar/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for containers-sugar."""
+
+
+__author__ = 'Ivan Ogasawara'
+__email__ = 'ivan.ogasawara@gmail.com'
+__version__ = '1.9.1'  # semantic-release
+
+
+from containers_sugar.sugar import Sugar  # noqa: F401
