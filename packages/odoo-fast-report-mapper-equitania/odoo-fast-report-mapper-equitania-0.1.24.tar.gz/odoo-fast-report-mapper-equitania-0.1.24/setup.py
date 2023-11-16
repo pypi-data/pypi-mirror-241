4cd4f28735e32a878e16fd1a44302e415d13e934
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 03:05:13 +0000

    Add package odoo-fast-report-mapper-equitania/0.1.24/odoo-fast-report-mapper-equitania-0.1.24.tar.gz

---
+++ packages/odoo-fast-report-mapper-equitania/odoo-fast-report-mapper-equitania-0.1.24.tar.gz/odoo-fast-report-mapper-equitania-0.1.24/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="odoo-fast-report-mapper-equitania",
+    version="0.1.24",
+    author="Equitania Software GmbH",
+    author_email="info@equitania.de",
+    description="A package to create FastReport entries in Odoo",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/equitania/odoo-fast-report-mapper",
+    packages=['odoo_fast_report_mapper', 'odoo_report_helper'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    entry_points='''
+    [console_scripts]
+    odoo-fr-mapper=odoo_fast_report_mapper.odoo_fast_report_mapper:start_odoo_fast_report_mapper
+    ''',
+    install_requires=[
+        'OdooRPC>=0.10.1',
+        'click>=8.1.3',
+        'PyYaml>=5.4.1'
+    ]
+)
