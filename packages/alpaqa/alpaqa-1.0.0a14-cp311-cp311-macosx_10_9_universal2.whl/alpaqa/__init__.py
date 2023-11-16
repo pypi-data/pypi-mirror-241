74a95c1e0151404dd691819f92166f6cf1d08301
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 14:04:32 +0000

    Add package alpaqa/1.0.0a14/alpaqa-1.0.0a14-cp311-cp311-macosx_10_9_universal2.whl

---
+++ packages/alpaqa/alpaqa-1.0.0a14-cp311-cp311-macosx_10_9_universal2.whl/alpaqa/__init__.py
@@ -0,0 +1,11 @@
+"""
+Augmented Lagrangian and PANOC solvers for nonconvex numerical optimization.
+"""
+__version__ = "1.0.0a14"
+
+from .alpaqa import *
+from .alpaqa import __c_version__
+
+assert __version__ == __c_version__
+
+from .pyapi import *
