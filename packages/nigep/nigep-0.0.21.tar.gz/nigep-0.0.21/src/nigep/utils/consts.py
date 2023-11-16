92dfd54e843951a2dc3e590d9ea9392ed8d3a646
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 15:05:21 +0000

    Add package nigep/0.0.21/nigep-0.0.21.tar.gz

---
+++ packages/nigep/nigep-0.0.21.tar.gz/nigep-0.0.21/src/nigep/utils/consts.py
@@ -0,0 +1,6 @@
+results_columns = ['model-name', 'train-dataset-noise', 'test-dataset-noise', 'precision(yes)', 'precision(no)',
+                   'precision(macro-avg)', 'precision(weighted-avg)', 'recall(yes)', 'recall(no)', 'recall(macro-avg)',
+                   'recall(weighted-avg)', 'f1-score(yes)', 'f1-score(no)', 'f1-score(accuracy)', 'f1-score(macro-avg)',
+                   'f1-score(weighted-avg)', 'TP', 'FP', 'FN', 'TN']
+
+NOISE_LEVELS = [0, .1, .2, .3, .4, .5, .6, .7, .8, .9]
