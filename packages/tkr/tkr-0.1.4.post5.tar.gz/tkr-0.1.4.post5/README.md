1d1bd30e8e662a4bcc202ad09025cd1e6514c43d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 15:04:45 +0000

    Add package tkr/0.1.4.post5/tkr-0.1.4.post5.tar.gz

---
+++ packages/tkr/tkr-0.1.4.post5.tar.gz/tkr-0.1.4.post5/README.md
@@ -0,0 +1,1 @@
+# tkr - Python Toolkit
---
+++ packages/tkr/tkr-0.1.4.post5.tar.gz/tkr-0.1.4.post5/tkr/abc/__init__.py
@@ -0,0 +1,64 @@
+import asyncio
+import inspect
+from ._tool import ToolProxy
+
+abc_types: tuple = (
+  'TOOL.TKR.PROXY',
+  'TOOL.TKR.CLASSIC'
+)
+
+VERSION: str = abc_types[1]
+
+class Tool(ToolProxy):
+  slots: tuple = ('name', 'version', 'description', 'method', '__name__', '__version__', '__description__', '__method__', 'type', '__type__')
+
+  def __init__(self, method: object, name: str='Tool.DefaultName', version: str='Tool.NoVersion', description: str='Tool.Description', *args, **kwargs) -> None:
+    self.name: str = name
+    self.__name__: str = self.name
+    self.version: str = version
+    self.__version__: str = self.version
+    self.description: str = description
+    self.__description__: str = self.description
+    self.method: str = method
+    self.__method__: str = self.method
+    self.type: str = VERSION
+    self.__type__: str = self.type
+
+  def run(self, *args, **kwargs):
+    if inspect.iscoroutinefunction(self.method): asyncio.run(self.method(*args, **kwargs))
+    else: self.method(*args, **kwargs)
+
+  def info(self, *args, **kwargs) -> dict:
+    return {
+      'name': self.name,
+      'version': self.version,
+      'description': self.description,
+      'method': self.method,
+    }
+
+  def __dict__(self, *args, **kwargs) -> dict:
+    return self.info()
+
+  def __tuple__(self, *args, **kwargs) -> tuple:
+    return (self.name, self.version, self.description, self.method)
+
+  def __list__(self, *args, **kwargs) -> list:
+    return [self.name, self.version, self.description, self.method]
+
+  def __repr__(self, *args, **kwargs) -> str:
+    return f'<Tool name={self.name} version={self.version} description={self.description} method={self.method}>'
+
+  def __str__(self, *args, **kwargs) -> str:
+    return self.__repr__()
+
+  def __call__(self, *args, **kwargs) -> object:
+    return self.method(*args, **kwargs)
+
+  def __int__(self, *args, **kwargs) -> int:
+    return int(str(self.version).replace('.', ''))
+
+  def __eq__(self, other, *args, **kwargs) -> bool:
+    return self.version == other.version, self.name == other.name, self.description == other.description, self.method == other.method
+
+  def __ne__(self, other, *args, **kwargs) -> bool:
+    return not self.__eq__(other)
