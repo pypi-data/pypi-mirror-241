6819ac5fd5174ad0dec0e0208f7a90f36922c872
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 04:04:35 +0000

    Add package autogen-agents/0.0.1.dev0/autogen_agents-0.0.1.dev0-py3-none-any.whl

---
+++ packages/autogen-agents/autogen_agents-0.0.1.dev0-py3-none-any.whl/autogen_agents/__about__.py
@@ -0,0 +1,2 @@
+"""Unofficial agents for Autogen framework"""
+__version__ = "0.0.1dev0"
---
+++ packages/autogen-agents/autogen_agents-0.0.1.dev0-py3-none-any.whl/autogen_agents/agents/__init__.py
@@ -0,0 +1,5 @@
+from typing import List
+
+from autogen_agents.agents.google_search_agent import GoogleSearchAgent
+
+__all__: List[str] = ["GoogleSearchAgent"]
