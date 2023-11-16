da4625a0d4d52c5b62c30ad1c1b3b848af613583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 03:05:17 +0000

    Add package geometry-alrandis/0.2/geometry_alrandis-0.2-py3-none-any.whl

---
+++ packages/geometry-alrandis/geometry_alrandis-0.2-py3-none-any.whl/figures/line.py
@@ -0,0 +1,8 @@
+from point import Point
+
+class Line():
+    def __init__(self, point1:Point, point2:Point):
+        self.point1 = point1
+        self.point2 = point2
+    def length(self):
+        return ((self.point2.x - self.point1.x)**2 + (self.point2.y - self.point1.y)**2)**0.5
