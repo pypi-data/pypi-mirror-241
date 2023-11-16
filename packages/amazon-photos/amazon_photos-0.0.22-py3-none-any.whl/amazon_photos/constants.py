205d48e6cadbfa1493b761e0bedbe91b329d3697
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 14:04:16 +0000

    Add package amazon-photos/0.0.22/amazon_photos-0.0.22-py3-none-any.whl

---
+++ packages/amazon-photos/amazon_photos-0.0.22-py3-none-any.whl/amazon_photos/constants.py
@@ -0,0 +1,6 @@
+MAX_TRASH_BATCH = 50  # max nodes to batch into trash request
+MAX_PURGE_BATCH = 50  # max nodes to batch into purge (delete) request
+MAX_DOWNLOAD_BATCH = 1200  # max nodes to batch into zip file for download
+MAX_NODES = 9999  # this is a real limit, not a large arbitrary number
+MAX_LIMIT = 200  # max number of nodes to return in a single request
+MAX_NODE_OFFSETS = [i * MAX_LIMIT for i in range(49)] + [MAX_NODES - MAX_LIMIT]
