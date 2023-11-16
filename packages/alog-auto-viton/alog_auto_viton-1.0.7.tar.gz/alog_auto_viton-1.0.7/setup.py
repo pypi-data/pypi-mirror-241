de54dd1ae23be13bb987a0c2bb8529571eb3321e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 15:05:26 +0000

    Add package alog-auto-viton/1.0.7/alog_auto_viton-1.0.7.tar.gz

---
+++ packages/alog-auto-viton/alog_auto_viton-1.0.7.tar.gz/alog_auto_viton-1.0.7/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='alog_auto_viton',
+    version='1.0.7',
+    packages=find_packages(),
+    # add any other dependencies your project needs
+    install_requires=[
+        'setuptools',
+        'requests',
+        # etc.
+    ],
+    entry_points={
+        'console_scripts': [
+            'alog_auto = cli.main:main',
+            'kol_info = cli.an_es:main',
+        ],
+    },
+)
