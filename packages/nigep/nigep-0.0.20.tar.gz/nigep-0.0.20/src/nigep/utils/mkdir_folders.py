fbcfba83e2090dfc0ef43d0159f02c326df27b62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 15:05:21 +0000

    Add package nigep/0.0.20/nigep-0.0.20.tar.gz

---
+++ packages/nigep/nigep-0.0.20.tar.gz/nigep-0.0.20/src/nigep/utils/mkdir_folders.py
@@ -0,0 +1,30 @@
+import os
+
+
+def mkdir_dataset(new_dataset_folder_name):
+    new_dataset_folder_path = f'{os.getcwd()}/dataset/{new_dataset_folder_name}'
+    if not os.path.exists(new_dataset_folder_path):
+        os.mkdir(new_dataset_folder_path)
+        print('dataset folder created inside dataset: ' + new_dataset_folder_name)
+    else:
+        print('dataset folder already exists: ' + new_dataset_folder_name)
+
+
+def mkdir_output():
+    output_folder_path = f'{os.getcwd()}/output'
+
+    try:
+        if not os.path.exists(output_folder_path):
+            os.mkdir(output_folder_path)
+
+    except FileExistsError:
+        pass
+
+
+def generate_directory_name(name, x=0):
+    dir_name = (name + (' ' + str(x) if x is not 0 else '')).strip()
+    if not os.path.exists(dir_name):
+        os.mkdir(dir_name)
+        return dir_name
+    else:
+        return generate_directory_name(name, x + 1)
