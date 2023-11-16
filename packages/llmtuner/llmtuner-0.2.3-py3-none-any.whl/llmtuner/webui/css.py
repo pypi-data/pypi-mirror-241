5e71e48ce548804a0cbc4eee2a1c05598cdb6261
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 04:04:04 +0000

    Add package llmtuner/0.2.3/llmtuner-0.2.3-py3-none-any.whl

---
+++ packages/llmtuner/llmtuner-0.2.3-py3-none-any.whl/llmtuner/webui/css.py
@@ -0,0 +1,27 @@
+CSS = r"""
+.duplicate-button {
+  margin: auto;
+  color: white;
+  background: black;
+  border-radius: 100vh;
+}
+
+.modal-box {
+  position: fixed !important;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%); /* center horizontally */
+  max-width: 1000px;
+  max-height: 750px;
+  overflow-y: auto;
+  background-color: var(--input-background-fill);
+  flex-wrap: nowrap !important;
+  border: 2px solid black !important;
+  z-index: 1000;
+  padding: 10px;
+}
+
+.dark .modal-box {
+  border: 2px solid white !important;
+}
+"""
