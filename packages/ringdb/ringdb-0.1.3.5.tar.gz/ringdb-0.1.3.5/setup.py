fd0e07307c618c5b1aa9984bb151f3e9152bf70b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 16:05:43 +0000

    Add package ringdb/0.1.3.5/ringdb-0.1.3.5.tar.gz

---
+++ packages/ringdb/ringdb-0.1.3.5.tar.gz/ringdb-0.1.3.5/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+#with open("README.md", "r", encoding="utf-8") as fh:
+#    long_description = "test"
+
+#description = '''A gravitational event database that queries and locally saves event strain samples, 
+#detector PSDs and posterior samples - all in one place'''
+
+setup(name='ringdb',
+      version='0.1.3.5',
+      long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
+      author='Asad Hussain',
+      author_email='asadh@utexas.edu',
+      url='https://github.com/potatoasad/ringdb',
+      download_url='https://github.com/Potatoasad/ringdb/archive/refs/tags/v0.1.3.5.zip',
+      license='MIT',
+      packages=['ringdb'],
+      package_data={'ringdb': ['metadb/*']},
+      install_requires=[
+            'h5py',
+            'matplotlib',
+            'numpy',
+            'pandas',
+            'tqdm']
+     )
