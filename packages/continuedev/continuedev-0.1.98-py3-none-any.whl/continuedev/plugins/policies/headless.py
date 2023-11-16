7e7ad7609aa4db9cec595049b9d01862585a49ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 06:03:48 +0000

    Add package continuedev/0.1.98/continuedev-0.1.98-py3-none-any.whl

---
+++ packages/continuedev/continuedev-0.1.98-py3-none-any.whl/continuedev/plugins/policies/headless.py
@@ -0,0 +1,21 @@
+from ...core.config import ContinueConfig
+from ...core.main import Policy, SessionState, Step
+from ...core.observation import TextObservation
+from ...core.steps import ShellCommandsStep
+from ...plugins.steps.on_traceback import DefaultOnTracebackStep
+
+
+class HeadlessPolicy(Policy):
+    command: str
+
+    def next(self, config: ContinueConfig, session_state: SessionState) -> Step:
+        if len(session_state.history) == 0:
+            return ShellCommandsStep(cmds=[self.command])
+
+        observations = session_state.history[-1].observations
+        if traceback_obs := next(
+            filter(lambda obs: isinstance(obs, TextObservation), observations), None
+        ):
+            return DefaultOnTracebackStep(output=traceback_obs.text)
+
+        return None
---
+++ packages/continuedev/continuedev-0.1.98-py3-none-any.whl/continuedev/plugins/steps/steps_on_startup.py
@@ -0,0 +1,19 @@
+from ...core.main import Step
+from ...core.sdk import ContinueSDK, Models
+
+
+class StepsOnStartupStep(Step):
+    hide: bool = True
+
+    async def describe(self, models: Models):
+        return "Running steps on startup"
+
+    async def run(self, sdk: ContinueSDK):
+        steps_on_startup = sdk.config.steps_on_startup
+
+        for step_type in steps_on_startup:
+            if isinstance(step_type, type):
+                step = step_type()
+            else:
+                step = step_type
+            await sdk.run_step(step)
