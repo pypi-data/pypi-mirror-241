c5e43b4fa89900b8ca3468b520bc2c7dca2ffde6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 15:04:38 +0000

    Add package out-and-in/1.1/out_and_in-1.1-py3-none-any.whl

---
+++ packages/out-and-in/out_and_in-1.1-py3-none-any.whl/out_and_in/__init__.py
@@ -0,0 +1,12 @@
+# __init__.py
+
+# available package level module
+from out_and_in.main import outin, one_in, one_out
+
+__version__ = "1.0"
+__author__ = 'ChenfengMeng'
+__all__ = ['outin', 'one_in', 'one_out']
+
+print("This is a package for feature selection")
+
+# initialization message
