fcbd6e450f2b19985c8cded7bf1c1df24feabfeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 15:05:19 +0000

    Add package mknodes/0.51.0/mknodes-0.51.0.tar.gz

---
+++ packages/mknodes/mknodes-0.51.0.tar.gz/mknodes-0.51.0/mknodes/basenodes/mkimageslideshow/__init__.py
@@ -0,0 +1,55 @@
+from __future__ import annotations
+
+from typing import Any
+
+from mknodes.basenodes import mknode
+from mknodes.utils import log, resources, xmlhelpers as xml
+
+
+logger = log.get_logger(__name__)
+
+# https://ruyadorno.com/simple-slider/
+
+JS_URL = "https://rawgit.com/ruyadorno/simple-slider/master/dist/simpleslider.min.js"
+
+SCRIPT = """\
+window.addEventListener('DOMContentLoaded', function () {
+  simpleslider.getSlider();
+})
+"""
+
+
+class MkImageSlideshow(mknode.MkNode):
+    """Node to show an Image slideshow (in autoplay mode)."""
+
+    ICON = "material/image-multiple"
+    JS_FILES = [resources.JSFile(JS_URL), resources.JSText(SCRIPT, "slideshow.js")]
+
+    def __init__(
+        self,
+        images: list[str],
+        **kwargs: Any,
+    ):
+        """Constructor.
+
+        Arguments:
+            images: A list of image URLs to use for the slideshow
+            kwargs: Keyword arguments passed to parent
+        """
+        super().__init__(**kwargs)
+        self.images = images
+
+    def get_element(self) -> xml.Div:
+        attrs = {"data-simple-slider": ""}
+        root = xml.Div(style="width: 100%; padding-bottom: 25%", parent=None, **attrs)
+        for img in self.images:
+            xml.Img(src=img, parent=root)
+        return root
+
+    def _to_markdown(self) -> str:
+        root = self.get_element()
+        return root.to_string()
+
+
+if __name__ == "__main__":
+    img = MkImageSlideshow(["https://picsum.photos/200", "https://picsum.photos/201"])
---
+++ packages/mknodes/mknodes-0.51.0.tar.gz/mknodes-0.51.0/mknodes/templatenodes/mkcommandoutput/__init__.py
@@ -0,0 +1,59 @@
+from __future__ import annotations
+
+from collections.abc import Sequence
+
+from typing import Any
+
+from mknodes.basenodes import mknode
+from mknodes.utils import helpers, log, resources
+
+
+logger = log.get_logger(__name__)
+
+
+TEXT = """<div data-terminal>
+  <span data-ty="input">{input}</span>
+  <span data-ty>{output}</span>
+</div>"""
+
+
+class MkCommandOutput(mknode.MkNode):
+    """Node to display the terminal output of a command."""
+
+    ICON = "material/bash"
+    STATUS = "new"
+    CSS = [resources.CSSFile("terminal.css")]
+
+    def __init__(self, call: Sequence[str], **kwargs: Any):
+        """Constructor.
+
+        Arguments:
+            call: System call to make
+            kwargs: Keyword arguments passed to parent
+        """
+        self.call = call
+        # caching on instance level as a compromise
+        self._cache: dict[str, str] = {}  # {call: output}
+        super().__init__(**kwargs)
+
+    @property
+    def output(self) -> str:
+        import pathlib
+
+        key = " ".join(self.call)
+        if key in self._cache:
+            return self._cache[key]
+        cwd = self.ctx.metadata.repository_path or pathlib.Path.cwd()
+        if output := helpers.get_output_from_call(key, cwd=cwd):
+            self._cache[key] = output  # .replace("\n", "<br>").replace(" ", "&nbsp;")
+            return self._cache[key]
+        return "**Command failed**"
+
+    def _to_markdown(self):
+        call = " ".join(self.call)
+        return TEXT.format(input=call, output=self.output)
+
+
+if __name__ == "__main__":
+    text = MkCommandOutput.with_context(["dir"])
+    print(text)
