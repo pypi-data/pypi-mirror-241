7df17d5f46d5cb2933efb51c862457422b96447a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-16 05:03:40 +0000

    Add package pdpyras/5.1.3/pdpyras-5.1.3.tar.gz

---
+++ packages/pdpyras/pdpyras-5.1.3.tar.gz/pdpyras-5.1.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+__version__ = '5.1.3'
+
+if __name__ == '__main__':
+    setup(
+        name='pdpyras',
+        description="PagerDuty Python REST API Sessions",
+        long_description="A basic REST API client for PagerDuty based on Requests' Session class",
+        py_modules=['pdpyras'],
+        version=__version__,
+        license='MIT',
+        url='https://pagerduty.github.io/pdpyras',
+        download_url='https://pypi.org/project/pdpyras/',
+        install_requires=['requests', 'urllib3', 'deprecation'],
+        author='PagerDuty',
+        author_email='support@pagerduty.com',
+        python_requires='>=3.6'
+    )
