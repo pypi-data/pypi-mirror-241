0027caa603a11036dd4a0b7d6fa3e5e455dd7dcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 16:10:39 +0000

    Add package rdfloom/0.0.5/rdfloom-0.0.5.tar.gz

---
+++ packages/rdfloom/rdfloom-0.0.5.tar.gz/rdfloom-0.0.5/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+VERSION = "0.0.5"
+
+setup(
+    name='rdfloom',
+    version=VERSION,
+    author='Andra Waagmeester',
+    author_email='andra@micel.io',
+    description='Python package for weaving RDF from tabular data',
+    long_description    =open('README.md').read(),
+    long_description_content_type='text/markdown',
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Development Status :: 4 - Beta",
+        "Operating System :: POSIX",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Developers",
+        "Topic :: Utilities",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    packages=find_packages(),
+    install_requires=[
+         "xlsxwriter",
+         'pandas',
+         'rdflib',
+         'ipywidgets',
+         'IPython',
+         'os'
+    ],
+)
