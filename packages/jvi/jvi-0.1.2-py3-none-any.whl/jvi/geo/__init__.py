34a1eec6da521ee37d45dda49c2068809237073d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 05:04:17 +0000

    Add package jvi/0.1.2/jvi-0.1.2-py3-none-any.whl

---
+++ packages/jvi/jvi-0.1.2-py3-none-any.whl/jvi/geo/__init__.py
@@ -0,0 +1,13 @@
+# from jvi.geo.point2d import Point
+# from jvi.geo.rectangle import Rect
+# from jvi.geo.size2d import Size
+
+
+def is_normalized(v: float) -> bool:
+    """判断坐标是否被归一化"""
+    return 0 <= v <= 1
+
+
+def to_zero(v: float) -> float:
+    """接近零的数据转为零"""
+    return v if abs(v) > 0.00001 else 0
