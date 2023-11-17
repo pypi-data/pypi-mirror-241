484c5f41080ad17b6b73f8d9831fc70d889efc10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 06:04:36 +0000

    Add package drone-mobile/0.2.30/drone_mobile-0.2.30-py3-none-any.whl

---
+++ packages/drone-mobile/drone_mobile-0.2.30-py3-none-any.whl/drone_mobile-0.2.30.data/scripts/demo.py
@@ -0,0 +1,24 @@
+#!python
+
+"""
+Simple script to demo the API
+"""
+
+import sys, json, pprint
+from drone_mobile import Vehicle
+
+if __name__ == "__main__":
+        if len(sys.argv) != 3:
+                raise Exception('You must specify Username and Password as arguments, e.g. demo.py test@test.com password123')
+        else:
+                vehicleObject = Vehicle(sys.argv[1], sys.argv[2]) # Username, Password   
+                vehicleObject.auth()        
+                vehicles = vehicleObject.getAllVehicles()
+
+                pprint.pprint(json.dumps(vehicles), compact=True)# Print the status of all vehicles
+                
+                # r.unlock() # Unlock the doors
+
+                # time.sleep(10) # Wait 10 seconds
+
+                # r.lock() # Lock the doors
