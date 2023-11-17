5a5ff705e0021e46b05f854a45ecb7d7c010cef9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 06:04:39 +0000

    Add package regex-custom-dict/0.2/regex_custom_dict-0.2.tar.gz

---
+++ packages/regex-custom-dict/regex_custom_dict-0.2.tar.gz/regex_custom_dict-0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+with open("README.md","r",encoding="utf-8") as f:
+    desc=f.read()
+
+setup(
+    name='regex_custom_dict',
+    version='0.2',
+    packages=find_packages(),
+    install_requires=[
+        're'
+    ],
+    long_description=desc,
+    long_description_content_type="text/markdown"
+)
