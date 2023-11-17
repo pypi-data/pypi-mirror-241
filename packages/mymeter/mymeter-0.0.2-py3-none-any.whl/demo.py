a0888e598276aa23c6fb38fdff2a42fa707211aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 06:04:32 +0000

    Add package mymeter/0.0.2/mymeter-0.0.2-py3-none-any.whl

---
+++ packages/mymeter/mymeter-0.0.2-py3-none-any.whl/demo.py
@@ -0,0 +1,58 @@
+import asyncio
+import argparse
+import aiohttp
+from datetime import datetime, timedelta
+from getpass import getpass
+
+from mymeter import MyMeter
+
+
+async def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--username",
+        help="Username for logging into the utility's website. "
+        "If not provided, you will be asked for it",
+    )
+    parser.add_argument(
+        "--password",
+        help="Password for logging into the utility's website. "
+        "If not provided, you will be asked for it",
+    )
+    parser.add_argument(
+        "--filename",
+        help="Filename to output fetched data to. Does not need file extension."
+        "If not provided, no file output will be generated.",
+    )
+    parser.add_argument(
+        "--start_date",
+        help="Start datetime for historical data. Defaults to 30 days ago",
+        type=lambda s: datetime.fromisoformat(s),
+        default=datetime.now() - timedelta(days=30),
+    )
+    parser.add_argument(
+        "--end_date",
+        help="end datetime for historical data. Defaults to now",
+        type=lambda s: datetime.fromisoformat(s),
+        default=datetime.now(),
+    )
+
+    args = parser.parse_args()
+    filename = args.filename
+    username = args.username or input("Username: ")
+    password = args.password or getpass("Password: ")
+    start_time = args.start_date
+    end_time = args.end_date
+    async with aiohttp.ClientSession() as session:
+        mymeter = MyMeter(session, username, password)
+        await mymeter.async_login()
+        results = await mymeter.async_get_usage_reads(start_time, end_time)
+        if results:
+            for result in results:
+                print(result)
+            print("Total Results:", len(results))
+        if filename:
+            await mymeter.print_data(start_time, filename, end_time)
+
+
+asyncio.run(main())
