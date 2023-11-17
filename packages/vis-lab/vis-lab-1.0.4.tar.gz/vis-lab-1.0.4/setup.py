9b5628fee849e7be9d86b2c626e6fe87af84c62c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 16:09:40 +0000

    Add package vis-lab/1.0.4/vis-lab-1.0.4.tar.gz

---
+++ packages/vis-lab/vis-lab-1.0.4.tar.gz/vis-lab-1.0.4/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+from os import path
+# twine upload dist/*
+# python setup.py bdist_wheel
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3.6',
+  'Programming Language :: Python :: 3.7',
+  'Programming Language :: Python :: 3.8',
+  'Programming Language :: Python :: 3.9',
+]
+ 
+setup(
+  name='vis-lab',
+  version='1.0.4',
+  description='a system reinforcement learning library',
+  # long_description=open(path.join(path.abspath(path.dirname(__file__)), 'README.md'), encoding='utf-8').read(),
+  url='',  
+  author='Ngo Xuan Phong',
+  author_email='phong@cht.edu.vn',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='python',
+  packages=find_packages(),
+  install_requires=[
+    'numpy == 1.21.6',
+    'numba == 0.56.3',
+    ],
+  python_requires='>=3.6',
+  include_package_data=True
+)
