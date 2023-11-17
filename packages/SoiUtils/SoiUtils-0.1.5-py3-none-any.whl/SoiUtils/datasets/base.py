9d44b2af5ae633a0a3327563a277c2ae717d3ad9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 04:04:57 +0000

    Add package SoiUtils/0.1.5/SoiUtils-0.1.5-py3-none-any.whl

---
+++ packages/SoiUtils/SoiUtils-0.1.5-py3-none-any.whl/SoiUtils/datasets/base.py
@@ -0,0 +1,29 @@
+from pybboxes import BoundingBox
+from typing import Union,List
+from dataclasses import dataclass
+import numpy as np
+import torch
+from PIL import Image
+
+@dataclass
+class Detection:
+    bbox: BoundingBox
+    cls: Union[str,int] = None
+    constructors_of_supported_formats = {'yolo': BoundingBox.from_yolo,'coco':BoundingBox.from_yolo,
+                                         'voc':BoundingBox.from_voc,'fiftyone':BoundingBox.from_fiftyone,
+                                         'albumentations':BoundingBox.from_albumentations}
+
+    @classmethod
+    def load_generic_mode(cls, bbox, cl=None, mode='yolo', **kwargs):
+        bbox = Detection.constructors_of_supported_formats[mode](*bbox, **kwargs).to_coco()
+        return cls(bbox, cl)
+        
+@dataclass
+class ImageDetectionSample:
+    image: Union[np.array, torch.Tensor, Image.Image]
+    detections: List[Detection] = None
+
+
+
+
+
