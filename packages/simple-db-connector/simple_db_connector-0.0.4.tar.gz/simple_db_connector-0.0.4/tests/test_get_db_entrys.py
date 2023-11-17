1692e231e131cc11794ece572ad53d358b54834b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 14:04:11 +0000

    Add package simple-db-connector/0.0.4/simple_db_connector-0.0.4.tar.gz

---
+++ packages/simple-db-connector/simple_db_connector-0.0.4.tar.gz/simple_db_connector-0.0.4/tests/test_get_db_entrys.py
@@ -0,0 +1,37 @@
+import os
+from simple_db_connector import database
+import decimal
+import datetime
+
+
+# Call the database class
+db = database(
+    os.environ["db_url"],
+    os.environ["db_user"],
+    os.environ["db_password"],
+    os.environ["db_port"],
+    os.environ["db_name"],
+)
+
+# Example Table Name 
+table_name =  "cars_table"  
+
+# Example Search Parameter
+search_parameter =  {
+	"manufacture" : "toyoat"
+} 
+
+# Example Table creation 
+db.get_db_entrys(table_name, search_parameter)
+
+# Example Table Name 
+table_name =  "cars_table"  
+
+# Example Search Parameter
+search_parameter =  {
+	"manufacture" : "toyoat",
+	"model" : "Aygo X yalp",
+}  
+
+# Example Table creation 
+db.get_db_entrys(table_name, search_parameter)
