9188c9319b4e2de466e3609509f4abe47b26d5c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 05:04:37 +0000

    Add package whosyouragent/1.3.1/whosyouragent-1.3.1.tar.gz

---
+++ packages/whosyouragent/whosyouragent-1.3.1.tar.gz/whosyouragent-1.3.1/CHANGELOG.md
@@ -0,0 +1,118 @@
+# Changelog
+
+## v1.3.0 (2023-11-02)
+
+#### New Features
+
+* add get_header()
+
+## v1.2.0 (2023-03-05)
+
+#### New Features
+
+* add option to return dict from get_agent()
+#### Fixes
+
+* fix vivaldi updater
+#### Others
+
+* test as_dict param for get_agent()
+
+
+## v1.1.1 (2023-02-23)
+
+#### Fixes
+
+* update chrome updater
+#### Others
+
+* build v1.1.1
+* update changelog
+* update readme
+
+
+## v1.1.0 (2023-02-16)
+
+#### New Features
+
+* implement version randomization into get_agent()
+* add func to randomize version numbers
+#### Others
+
+* build v1.1.0
+* update changelog
+
+
+## v1.0.10 (2023-02-16)
+
+#### Fixes
+
+* update chrome version updater
+#### Others
+
+* build v1.0.10
+* update changelog
+
+
+## v1.0.9 (2023-02-14)
+
+#### Fixes
+
+* update firefox version updater
+* fix dict key removal during iteration
+#### Others
+
+* build v1.0.9
+* update changelog
+
+
+## v1.0.8 (2023-02-13)
+
+#### Fixes
+
+* fix creating browserVersions.json with {} instead of "{}" lol
+#### Others
+
+* build v1.0.8
+* update changelog
+
+
+## v1.0.7 (2023-02-13)
+
+#### Fixes
+
+* fix error occuring when browserVersions.json doesn't exist
+* remove unused import
+#### Others
+
+* build v1.0.7
+* update changelog
+* update changelog
+
+
+## v1.0.6 (2023-02-13)
+
+#### Others
+
+* build v1.0.6
+* update changelog
+
+
+## v1.0.5 (2023-02-13)
+
+#### Others
+
+* build v1.0.5
+* update changelog
+
+
+## v1.0.4 (2023-02-13)
+
+#### Fixes
+
+* wrap update_all() in try/except
+#### Others
+
+* build v1.0.4
+* update changelog
+* add browserVersions.json to .gitignore
---
+++ packages/whosyouragent/whosyouragent-1.3.1.tar.gz/whosyouragent-1.3.1/src/whosyouragent/whosyouragent.py
@@ -0,0 +1,178 @@
+import json
+import random
+from concurrent.futures import ThreadPoolExecutor
+from pathlib import Path
+
+import requests
+from bs4 import BeautifulSoup
+
+
+class VersionUpdater:
+    def __init__(self):
+        self.versions_path = Path(__file__).parent / "browserVersions.json"
+        if not self.versions_path.exists():
+            self.versions_path.write_text(
+                json.dumps(
+                    {
+                        "Firefox": "119.0.1",
+                        "Chrome": "109.0.5414.165",
+                        "Edg": "119.0.2151.58",
+                        "Vivaldi": "6.4.3160.44",
+                        "OPR": "105.0.4970.13",
+                        "Safari": "17.0",
+                    }
+                )
+            )
+
+    def update_firefox(self):
+        try:
+            url = "https://www.mozilla.org/en-US/firefox/releases/"
+            soup = BeautifulSoup(requests.get(url).text, "html.parser")
+            release_list = soup.find("ol", class_="c-release-list")
+            version = release_list.ol.li.a.text
+            self.firefox = version
+        except Exception as e:
+            self.firefox = None
+
+    def update_chrome(self):
+        try:
+            url = "https://en.wikipedia.org/wiki/Google_Chrome"
+            soup = BeautifulSoup(requests.get(url).text, "html.parser")
+            info_boxes = soup.find_all("td", class_="infobox-data")
+            version = info_boxes[8].text[
+                : min([info_boxes[8].text.find("["), info_boxes[8].text.find("/")])
+            ]
+            self.chrome = version
+        except Exception as e:
+            self.chrome = None
+
+    def update_safari(self):
+        try:
+            url = "https://en.wikipedia.org/wiki/Safari_(web_browser)"
+            soup = BeautifulSoup(requests.get(url).text, "html.parser")
+            info_boxes = soup.find_all("td", class_="infobox-data")
+            version = info_boxes[2].text[: info_boxes[2].text.find("[")]
+            self.safari = version
+        except Exception as e:
+            self.safari = None
+
+    def update_edge(self):
+        try:
+            url = "https://www.techspot.com/downloads/7158-microsoft-edge.html"
+            soup = BeautifulSoup(requests.get(url).text, "html.parser")
+            version = soup.find("div", class_="subver").text
+            self.edge = version
+        except Exception as e:
+            self.edge = None
+
+    def update_vivaldi(self):
+        try:
+            url = "https://vivaldi.com/blog/"
+            soup = BeautifulSoup(requests.get(url).text, "html.parser")
+            text = soup.find("div", class_="download-vivaldi-sidebar").text
+            text = text.split(" - ")[1]
+            text = text.replace(" (", ".")
+            version = text[: text.find(")")]
+            self.vivaldi = version
+        except Exception as e:
+            self.vivaldi = None
+
+    def update_opera(self):
+        try:
+            url = "https://en.wikipedia.org/wiki/Opera_(web_browser)"
+            soup = BeautifulSoup(requests.get(url).text, "html.parser")
+            info_boxes = soup.find_all("td", class_="infobox-data")
+            version = info_boxes[2].div.text[: info_boxes[2].div.text.find("[")]
+            self.opera = version
+        except Exception as e:
+            self.opera = None
+
+    def update_all(self):
+        updaters = [
+            self.update_firefox,
+            self.update_chrome,
+            self.update_safari,
+            self.update_edge,
+            self.update_vivaldi,
+            self.update_opera,
+        ]
+        with ThreadPoolExecutor() as executor:
+            for updater in updaters:
+                executor.submit(updater)
+        versions = {
+            "Firefox": self.firefox,
+            "Chrome": self.chrome,
+            "Edg": self.edge,
+            "Vivaldi": self.vivaldi,
+            "OPR": self.opera,
+            "Safari": self.safari,
+        }
+        # Remove any keys that failed to update and keep previous version number
+        poppers = [
+            version
+            for version in versions
+            if version and not ((versions[version]).replace(".", "")).isnumeric()
+        ]
+        for popper in poppers:
+            versions.pop(popper)
+        previous_versions = json.loads(self.versions_path.read_text())
+        versions = previous_versions | versions
+        self.versions_path.write_text(json.dumps(versions))
+
+
+platforms = [
+    "(Windows NT 10.0; Win64; x64)",
+    "(x11; Ubuntu; Linux x86_64)",
+    "(Windows NT 11.0; Win64; x64)",
+    "(Macintosh; Intel Mac OS X 13_0_0)",
+]
+
+
+def randomize_version_number(version: str) -> str:
+    """Randomize a version number so that it's in between
+    the previous major version and the current one."""
+    parts = [int(part) for part in version.split(".")]
+    parts[0] = random.randint(parts[0] - 1, parts[0])
+    for i, part in enumerate(parts[1:]):
+        parts[i + 1] = random.randint(0, part)
+    return ".".join(str(part) for part in parts)
+
+
+def get_agent(as_dict: bool = False) -> str:
+    """Build and return a user agent string.
+
+    :param as_dict: If True, return {"User-Agent": useragent} instead of just the useragent string.
+    Note: Leaving this parameter in place to maintain backwards compatibility,
+    but it's advised to use the `get_header()` function instead."""
+    browsers = json.loads((Path(__file__).parent / "browserVersions.json").read_text())
+    for browser in browsers:
+        browsers[browser] = randomize_version_number(browsers[browser])
+    browser = random.choice(list(browsers.keys()))
+    if browser == "Safari":
+        platform = platforms[-1]
+        useragent = f'Mozilla/5.0 {platform} AppleWebKit/605.1.15 (KHTML, like Gecko) Version/{browsers["Safari"]} Safari/605.1.15'
+    else:
+        platform = random.choice(platforms)
+        if browser == "Firefox":
+            platform = platform[: platform.rfind(")")] + f"; rv:{browsers[browser]})"
+            useragent = (
+                f"Mozilla/5.0 {platform} Gecko/20100101 Firefox/{browsers[browser]}"
+            )
+        else:
+            useragent = f'Mozilla/5.0 {platform} AppleWebKit/537.36 (KHTML, like Gecko) Chrome/{browsers["Chrome"]} Safari/537.36'
+            if browser == "Edg":
+                useragent += f' Edg/{browsers["Edg"]}'
+            elif browser == "OPR":
+                useragent += f' OPR/{browsers["OPR"]}'
+            elif browser == "Vivaldi":
+                useragent += f' Vivaldi/{browsers["Vivaldi"]}'
+    if as_dict:
+        return {"User-Agent": useragent}
+    else:
+        return useragent
+
+
+def get_header() -> dict[str, str]:
+    """Returns a dictionary `{'User-Agent': <random user agent string>}` for convenience.
+    >>> response = requests.get(url, headers=get_header())"""
+    return {"User-Agent": get_agent()}
