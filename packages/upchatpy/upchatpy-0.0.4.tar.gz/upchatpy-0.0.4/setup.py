67d96759bf9afea196f59c45af61674bc00675f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 04:05:11 +0000

    Add package upchatpy/0.0.4/upchatpy-0.0.4.tar.gz

---
+++ packages/upchatpy/upchatpy-0.0.4.tar.gz/upchatpy-0.0.4/setup.py
@@ -0,0 +1,36 @@
+from setuptools import find_packages, setup
+
+from upgrade_chat import __version__
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="upchatpy",
+    version=__version__,
+    url="https://github.com/vertyco/upgrade-chat",
+    author="vertyco",
+    author_email="alex.c.goble@gmail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    description="A type hinted Python wrapper for the Upgrade.Chat API ",
+    packages=find_packages(),
+    keywords="upgrade.chat upgradechat upgradechatpy monetization api wrapper chat payments discord",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Framework :: Pydantic :: 1",
+        "Framework :: Pydantic :: 2",
+        "Framework :: Pytest",
+        "Operating System :: OS Independent",
+        "Topic :: Utilities",
+        "Typing :: Typed",
+        "Natural Language :: English",
+    ],
+    requires=["pydantic", "aiohttp"],
+    python_requires=">=3.10",
+)
