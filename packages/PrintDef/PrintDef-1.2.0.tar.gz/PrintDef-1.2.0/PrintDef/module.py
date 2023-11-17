b70c5902bb2936992f50be6b7a2285dc845ad196
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 05:04:30 +0000

    Add package PrintDef/1.2.0/PrintDef-1.2.0.tar.gz

---
+++ packages/PrintDef/PrintDef-1.2.0.tar.gz/PrintDef-1.2.0/PrintDef/module.py
@@ -0,0 +1,11 @@
+
+
+def HelloWorld():
+    print("\nHello, world!\n" *2)
+
+def PrintHelp():
+    print(f"print(value, ..., sep='', end='\н(n)', file=sys.stdout, flush=False)")
+
+def greet(name):
+    return f"Hello, {name}!"
+
