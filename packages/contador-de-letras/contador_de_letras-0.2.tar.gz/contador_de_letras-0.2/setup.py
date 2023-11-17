205312bbfc035a67154571d061b62d25b6a195ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 15:04:34 +0000

    Add package contador-de-letras/0.2/contador_de_letras-0.2.tar.gz

---
+++ packages/contador-de-letras/contador_de_letras-0.2.tar.gz/contador_de_letras-0.2/setup.py
@@ -0,0 +1,19 @@
+# Contenido de setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='contador_de_letras',
+    version='0.2',
+    packages=find_packages(),
+    install_requires=[
+        'tqdm',
+        'argparse',
+    ],
+    entry_points={
+        'console_scripts': [
+            'contador_de_letras = contador_de_letras.contador_de_letras:main',
+        ],
+
+    },
+)
