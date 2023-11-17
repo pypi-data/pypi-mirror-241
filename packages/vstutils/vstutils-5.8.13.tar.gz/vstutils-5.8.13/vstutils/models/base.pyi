bd3723ac1c82792ad5086059716e320dfd6977c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 14:04:41 +0000

    Add package vstutils/5.8.13/vstutils-5.8.13.tar.gz

---
+++ packages/vstutils/vstutils-5.8.13.tar.gz/vstutils-5.8.13/vstutils/models/base.pyi
@@ -0,0 +1,21 @@
+from typing import Any, Optional, Sequence, Text, Tuple
+from ..api.base import GenericViewSet
+from django.db.models.base import ModelBase
+
+
+DEFAULT_VIEW_FIELD_NAMES: Tuple[Text,Text,Text,Text,Text]
+LAZY_MODEL: object
+
+
+def get_proxy_labels(model: ModelBase) -> Tuple:
+    ...
+
+
+def get_first_match_name(field_names: Sequence[Text], default: Optional[Text] = None) -> Text:
+    ...
+
+
+class ModelBaseClass(ModelBase):
+    generated_view: GenericViewSet
+    lazy_generated_view: GenericViewSet
+    OriginalMeta: Any
