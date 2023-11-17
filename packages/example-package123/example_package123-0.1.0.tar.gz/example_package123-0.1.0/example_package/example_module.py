9b16b253adafe056c63017c4f52fe0148f12b18c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 14:04:56 +0000

    Add package example-package123/0.1.0/example_package123-0.1.0.tar.gz

---
+++ packages/example-package123/example_package123-0.1.0.tar.gz/example_package123-0.1.0/example_package/example_module.py
@@ -0,0 +1,5 @@
+# example_package/example_module.py
+
+def add_numbers(a, b):
+    return a + b
+cd
---
+++ packages/example-package123/example_package123-0.1.0.tar.gz/example_package123-0.1.0/setup.py
@@ -0,0 +1,15 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='example_package123',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={},
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
