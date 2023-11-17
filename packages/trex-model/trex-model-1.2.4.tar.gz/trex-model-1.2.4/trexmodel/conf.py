8f2e7803b7ce19f6e677c3ec4bacea19297459c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 04:04:21 +0000

    Add package trex-model/1.2.4/trex-model-1.2.4.tar.gz

---
+++ packages/trex-model/trex-model-1.2.4.tar.gz/trex-model-1.2.4/trexmodel/conf.py
@@ -0,0 +1,43 @@
+import os, config_path
+
+
+INTERNAL_MAX_FETCH_RECORD                       = 9999
+MAX_FETCH_RECORD_FULL_TEXT_SEARCH               = 1000
+MAX_FETCH_RECORD_FULL_TEXT_SEARCH_PER_PAGE      = 10
+MAX_FETCH_RECORD                                = 99999999
+MAX_FETCH_IMAGE_RECORD                          = 100
+MAX_CHAR_RANDOM_UUID4                           = 20
+PAGINATION_SIZE                                 = 2
+VISIBLE_PAGE_COUNT                              = 10
+
+DEFAULT_GMT_HOURS                               = 8
+
+GENDER_MALE_CODE                                = 'm'
+GENDER_FEMALE_CODE                              = 'f'
+GENDER_UNKNOWN_CODE                             = 'u'
+
+APPLICATION_ACCOUNT_PROVIDER                    = 'app'
+
+MODEL_PROJECT_ID                                = os.environ['GCLOUD_PROJECT_ID']
+
+DATASTORE_SERVICE_ACCOUNT_KEY_FILEPATH          = os.environ['SERVICE_ACCOUNT_KEY']
+
+DATASTORE_CREDENTIAL_PATH                       = os.path.abspath(os.path.dirname(config_path.__file__)) + '/' + DATASTORE_SERVICE_ACCOUNT_KEY_FILEPATH
+
+MERCHANT_STAT_FIGURE_UPDATE_INTERVAL_IN_MINUTES = os.environ.get('MERCHANT_STAT_FIGURE_UPDATE_INTERVAL_IN_MINUTES') or 60
+
+MESSAGE_CATEGORY_ANNOUNCEMENT       = 'announcement'
+MESSAGE_CATEGORY_ALERT              = 'alert'
+MESSAGE_CATEGORY_PROMOTION          = 'promotion'
+MESSAGE_CATEGORY_SURVEY             = 'survey'
+MESSAGE_CATEGORY_SYSTEM             = 'system'
+MESSAGE_CATEGORY_REWARD             = 'reward'
+
+MESSAGE_CATEGORIES = (MESSAGE_CATEGORY_ANNOUNCEMENT, MESSAGE_CATEGORY_ALERT, MESSAGE_CATEGORY_PROMOTION, MESSAGE_CATEGORY_SURVEY, MESSAGE_CATEGORY_SYSTEM, MESSAGE_CATEGORY_REWARD)
+
+MESSAGE_STATUS_NEW      = 'n'
+MESSAGE_STATUS_READ     = 'r'
+
+
+MESSAGE_STATUS_SET      = (MESSAGE_STATUS_NEW, MESSAGE_STATUS_READ)
+
