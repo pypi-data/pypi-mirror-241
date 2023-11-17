5d59b86679fbef470a2ebf918decb6851fee17fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 14:04:27 +0000

    Add package adala/0.0.3/adala-0.0.3-py3-none-any.whl

---
+++ packages/adala/adala-0.0.3-py3-none-any.whl/adala/memories/base.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from typing import Any, Optional, TYPE_CHECKING, Dict
+
+from pydantic import BaseModel, Field
+
+
+class Memory(BaseModel, ABC):
+
+    """
+    Base class for long-term memories.
+    Long-term memories are used to store acquired knowledge and can be shared between agents.
+    """
+
+    @abstractmethod
+    def remember(self, observation: str, experience: Any):
+        """
+        Base method for remembering experiences in long term memory.
+        """
+
+    @abstractmethod
+    def retrieve(self, observation: str) -> Any:
+        """
+        Base method for retrieving past experiences from long term memory, based on current observations
+        """
---
+++ packages/adala/adala-0.0.3-py3-none-any.whl/adala/skills/__init__.py
@@ -0,0 +1,4 @@
+from .skillset import SkillSet, LinearSkillSet, ParallelSkillSet
+from .collection.classification import ClassificationSkill
+from ._base import Skill, TransformSkill, AnalysisSkill, SynthesisSkill
+
