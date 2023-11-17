e7abe05421db9a412f33df1dd0f0a644875082db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 03:04:24 +0000

    Add package ddumpy/0.1.1/ddumpy-0.1.1-py3-none-any.whl

---
+++ packages/ddumpy/ddumpy-0.1.1-py3-none-any.whl/ddumpy/helpers/console.py
@@ -0,0 +1,19 @@
+import select
+import sys
+import termios
+import tty
+
+
+class NonBlockingConsole(object):
+    def __enter__(self):
+        self.old_settings = termios.tcgetattr(sys.stdin)
+        tty.setcbreak(sys.stdin.fileno())
+        return self
+
+    def __exit__(self, type, value, traceback):
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.old_settings)
+
+    def get_data(self):
+        if select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], []):
+            return sys.stdin.buffer.read(1)
+        return None
