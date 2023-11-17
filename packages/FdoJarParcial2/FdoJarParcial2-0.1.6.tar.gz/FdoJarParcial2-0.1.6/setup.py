968550f96b6a3a035a97a0aa80a5f1913e8b96d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 15:04:24 +0000

    Add package FdoJarParcial2/0.1.6/FdoJarParcial2-0.1.6.tar.gz

---
+++ packages/FdoJarParcial2/FdoJarParcial2-0.1.6.tar.gz/FdoJarParcial2-0.1.6/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+
+with open('./README.md','r') as fn:
+    description_1 = fn.read()
+
+setup(
+    name='FdoJarParcial2',
+    version='0.1.6',
+    packages=find_packages(include=['FdoJarParcial2']),
+    description='Suavizado de curvas',
+    long_description=description_1,
+    long_description_content_type = 'text/markdown',
+    author='Fernando Jaramillo',
+    license= 'MIT',
+    install_requirements=  ['contourpy'
+                            'cycler'
+                            'fonttools'
+                            'kiwisolver'
+                            'matplotlib'
+                            'numpy'
+                            'packaging'
+                            'pandas'
+                            'Pillow'
+                            'pyparsing'
+                            'python-dateutil'
+                            'pytz'
+                            'six'
+                            'tzdata'],
+    python_requirements='3.11.6',
+    author_email='juanf.jaramillo@udea.edu.co',
+    url='https://gitlab.com/juanf.jaramillo1/curso-fci-2023-2.git'
+    
+)
