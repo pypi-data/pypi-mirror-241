07bafc0d3ebe560fa643da676febc70cb50143e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 05:04:04 +0000

    Add package PyMemoryEditor/1.5.5/pymemoryeditor-1.5.5-py3-none-any.whl

---
+++ packages/PyMemoryEditor/pymemoryeditor-1.5.5-py3-none-any.whl/PyMemoryEditor/process/util.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+import psutil
+import sys
+
+if "win" in sys.platform:
+    from win32.win32gui import FindWindow
+    from win32.win32process import GetWindowThreadProcessId
+
+
+def get_process_id_by_process_name(process_name: str) -> int:
+    """
+    Get a process name and return its process ID.
+    """
+    for process in psutil.process_iter():
+        if process.name() == process_name:
+            return process.pid
+
+
+def get_process_id_by_window_title(window_title: str) -> int:
+    """
+    Get a window title and return its process ID.
+    """
+    if "win" not in sys.platform:
+        raise OSError("This function is compatible only with Windows OS.")
+
+    hwnd = FindWindow(None, window_title)
+    return GetWindowThreadProcessId(hwnd)[1] if hwnd else 0
+
+
+def pid_exists(pid: int) -> bool:
+    """
+    Check if the process ID exists.
+    """
+    return psutil.pid_exists(pid)
