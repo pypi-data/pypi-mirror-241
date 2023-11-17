e05c718ba03b8d3596cb29b2c4a99b9aa32ca163
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 04:04:37 +0000

    Add package pppssstest/1.10/pppssstest-1.10.tar.gz

---
+++ packages/pppssstest/pppssstest-1.10.tar.gz/pppssstest-1.10/pppssstest/pppssstest.py
@@ -0,0 +1,12 @@
+import PySimpleGUI as sg
+
+version = "1.9"
+
+
+def main():
+    sg.popup(f'Running test version {version}')
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ packages/pppssstest/pppssstest-1.10.tar.gz/pppssstest-1.10/setup.py
@@ -0,0 +1,39 @@
+
+
+import setuptools
+
+def readme():
+    try:
+        with open('README.md') as f:
+            return f.read()
+    except IOError:
+        return ''
+
+
+setuptools.setup(
+name="pppssstest",
+version="1.10",
+author="author",
+author_email="author@email.com",
+install_requires=["PySimpleGUI"],
+description="Desc here",
+long_description=readme(),
+long_description_content_type="text/markdown",
+license='Free To Use But Restricted',
+keywords="pp ss tt",
+url="",
+packages=setuptools.find_packages(),
+python_requires="",
+classifiers=[
+"Framework :: PySimpleGUI :: 4",
+"Framework :: PySimpleGUI :: 5",
+],
+package_data={"": 
+[""]
+        },
+entry_points={'gui_scripts': [
+"pptest=pppssstest.pppssstest:main",
+    ]
+    },
+)
+
