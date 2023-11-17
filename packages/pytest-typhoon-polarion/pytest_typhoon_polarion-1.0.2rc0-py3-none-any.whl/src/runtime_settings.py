1d526ab17817336165f4f0a9b1ded701259fc6b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 05:04:11 +0000

    Add package pytest-typhoon-polarion/1.0.2rc0/pytest_typhoon_polarion-1.0.2rc0-py3-none-any.whl

---
+++ packages/pytest-typhoon-polarion/pytest_typhoon_polarion-1.0.2rc0-py3-none-any.whl/src/runtime_settings.py
@@ -0,0 +1,31 @@
+
+polarion_test_cases = {}
+
+class Settings:
+    POLARION_HOST = ''
+    POLARION_USER = ''
+    POLARION_PASSWORD = ''
+    POLARION_TOKEN = ''
+    POLARION_PROJECT_ID = ''
+    POLARION_TEST_RUN = ''
+    WEB_URL = ''
+    ALLUREDIR = ''
+    ALLOW_COMMENTS = None
+    POLARION_VERIFY_CERTIFICATE = True
+
+
+class TestExecutionResult:
+    polarion_test_mapping = {}  # test_name: polarion_test_id (str: str)
+
+    result_polarion_mapping = {}  # polarion_test_id: test_results (str: list)
+    test_polarion_mapping = {}  # Inverted polarion_test_mapping dict
+
+    uid_test_mapping = {}  # test_name: uid (str: str)
+    parent_uid = None
+
+
+class PolarionTestRunRefs:
+    client = None
+    project = None
+    run = None
+    test_cases = list()
