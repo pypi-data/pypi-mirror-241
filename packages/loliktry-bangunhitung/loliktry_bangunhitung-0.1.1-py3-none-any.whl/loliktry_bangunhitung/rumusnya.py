fce6591fab69ff469b18f196ad25084e2bc5d62a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 03:04:43 +0000

    Add package loliktry-bangunhitung/0.1.1/loliktry_bangunhitung-0.1.1-py3-none-any.whl

---
+++ packages/loliktry-bangunhitung/loliktry_bangunhitung-0.1.1-py3-none-any.whl/loliktry_bangunhitung/rumusnya.py
@@ -0,0 +1,25 @@
+import math
+# Rumus bangun
+
+def luas_persegipanjang(a, b):
+    return a * b
+def luas_lingkaran(radius):
+    return math.pi * radius ** 2
+
+def keliling_lingkaran(radius):
+    return 2 * math.pi * radius
+
+
+# Rumusa hitungan
+def tambah(a, b):
+    return a + b
+
+def kurang(a, b):
+    return a - b
+
+def kali(a, b):
+    return a * b
+
+def bagi(a, b):
+    return a / b
+
