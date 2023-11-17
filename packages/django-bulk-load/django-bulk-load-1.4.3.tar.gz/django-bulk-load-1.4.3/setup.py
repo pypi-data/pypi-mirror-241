1fbdb3f7e1d09fa2b9e4ba09c7bd93d83876264f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 16:09:40 +0000

    Add package django-bulk-load/1.4.3/django-bulk-load-1.4.3.tar.gz

---
+++ packages/django-bulk-load/django-bulk-load-1.4.3.tar.gz/django-bulk-load-1.4.3/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+__version__ = "1.4.3"
+
+setuptools.setup(
+    name="django-bulk-load",
+    version=__version__,
+    author="Cedar",
+    author_email="support@cedar.com",
+    license="MIT",
+    description="Bulk load Django models",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cedar-team/django-bulk-load",
+    project_urls={},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(
+        exclude=["*.tests", "*.tests.*", "tests.*", "tests"]
+    ),
+    python_requires=">=3.6",
+    install_requires=[
+        "django>=2.2",
+        "psycopg2>=2.8.6"
+    ],
+    extras_require={
+        'test': []
+    },
+)
