ee16a728eb2122391e1aa9eba5ce95d18b2d0d4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 05:04:36 +0000

    Add package neumai/0.0.22/neumai-0.0.22.tar.gz

---
+++ packages/neumai/neumai-0.0.22.tar.gz/neumai-0.0.22/neumai/Loaders/LoaderHelper.py
@@ -0,0 +1,31 @@
+from Loaders import (
+    AutoLoader,
+    LoaderEnum,
+    MarkdownLoader,
+    NeumJSONLoader,
+    NeumCSVLoader,
+    PDFLoader,
+    HTMLLoader,
+)
+from neumai.Shared.Selector import Selector
+
+def as_loader(dct:dict):
+    if dct == None:
+        return AutoLoader()
+    loader_name = dct.get("loader_name", None)
+    loader_information = dct.get("loader_information", None)
+    selector = Selector.as_selector(dct.get("selector", None))
+    if loader_name == LoaderEnum.autoloader:
+        return AutoLoader(loader_information=loader_information, selector=selector)
+    elif loader_name == LoaderEnum.htmlloader:
+        return HTMLLoader(loader_information=loader_information, selector=selector)
+    elif loader_name == LoaderEnum.markdownloader:
+        return MarkdownLoader(loader_information=loader_information, selector=selector)
+    elif loader_name == LoaderEnum.neumjsonloader:
+        return NeumJSONLoader(loader_information=loader_information, selector=selector)
+    elif loader_name == LoaderEnum.neumcsvloader:
+        return NeumCSVLoader(loader_information=loader_information, selector=selector)
+    elif loader_name == LoaderEnum.pdfloader:
+        return PDFLoader(loader_information=loader_information, selector=selector)
+    else: 
+        return AutoLoader(loader_information=loader_information, selector=selector)
---
+++ packages/neumai/neumai-0.0.22.tar.gz/neumai-0.0.22/neumai/SinkConnectors/SinkHelper.py
@@ -0,0 +1,32 @@
+from SinkConnectors import (
+    PineconeSink,
+    WeaviateSink,
+    QdrantSink,
+    SingleStoreSink,
+    SinkConnectorEnum,
+    SupabaseSink
+)
+
+from Shared.Exceptions import InvalidSinkConnectorException
+
+available_connectors = [enum.value for enum in list(SinkConnectorEnum)]
+
+def as_sink(dct:dict):
+    if dct == None:
+        raise InvalidSinkConnectorException("Must supply a data connector configuration")
+    if not isinstance(dct, dict):
+        raise InvalidSinkConnectorException("Data connector configuration needs to be a dictionary")
+    sink_name = dct.get("sink_name", None)
+    sink_information = dct.get("sink_information", None)
+    if sink_name == SinkConnectorEnum.pineconesink:
+        return PineconeSink(sink_information=sink_information)
+    elif sink_name == SinkConnectorEnum.qdrantsink:
+        return QdrantSink(sink_information=sink_information)
+    elif sink_name == SinkConnectorEnum.singlestoresink:
+        return SingleStoreSink(sink_information=sink_information)
+    elif sink_name == SinkConnectorEnum.supabasesink:
+        return SupabaseSink(sink_information=sink_information)
+    elif sink_name == SinkConnectorEnum.weaviatesink:
+        return WeaviateSink(sink_information=sink_information)
+    else:
+        raise InvalidSinkConnectorException(f"{sink_name} is an invalid sink connector. Available connectors: {available_sink_connectors}]")
