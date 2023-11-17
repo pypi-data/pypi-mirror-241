4de69eb37101e034b0f87148331ad846566d539d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 05:04:12 +0000

    Add package django-drf-cms/0.1.25/django-drf-cms-0.1.25.tar.gz

---
+++ packages/django-drf-cms/django-drf-cms-0.1.25.tar.gz/django-drf-cms-0.1.25/drf_cms/mixins.py
@@ -0,0 +1,24 @@
+from django.db import models
+from uuslug import uuslug
+
+
+class SlugModelMixin(models.Model):
+    slugged_field = ""
+
+    slug = (
+        models.SlugField(unique=True)
+    )
+
+    class Meta:
+        abstract = True
+
+    def prepare_slug(self):
+        if not self.slug:
+            _slugged_field = getattr(self, self.slugged_field)
+            self.slug = uuslug(_slugged_field, instance=self)
+        else:
+            self.slug = uuslug(self.slug, instance=self)
+
+    def save(self, *args, **kwargs):
+        self.prepare_slug()
+        super(SlugModelMixin, self).save(*args, **kwargs)
