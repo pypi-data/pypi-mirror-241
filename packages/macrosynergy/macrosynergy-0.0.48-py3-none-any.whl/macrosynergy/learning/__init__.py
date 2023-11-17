f46cab58280914ee174409f8f3810964d7c8aae5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 03:04:22 +0000

    Add package macrosynergy/0.0.48/macrosynergy-0.0.48-py3-none-any.whl

---
+++ packages/macrosynergy/macrosynergy-0.0.48-py3-none-any.whl/macrosynergy/learning/__init__.py
@@ -0,0 +1,25 @@
+from .panel_timeseries_split import PanelTimeSeriesSplit
+from .cv_tools import panel_cv_scores
+from .transformers import LassoSelectorTransformer, MapSelectorTransformer, BenchmarkTransformer
+from .metrics import (
+    panel_significance_probability,
+    sharpe_ratio,
+    sortino_ratio,
+    regression_accuracy,
+    regression_balanced_accuracy,
+)
+from .preds_to_pnl import static_preds_to_pnl
+
+__all__ = [
+    "PanelTimeSeriesSplit",
+    "panel_cv_scores",
+    "LassoSelectorTransformer",
+    "MapSelectorTransformer",
+    "BenchmarkTransformer",
+    "panel_significance_probability",
+    "regression_accuracy",
+    "regression_balanced_accuracy",
+    "sharpe_ratio",
+    "sortino_ratio",
+    "static_preds_to_pnl"
+]
---
+++ packages/macrosynergy/macrosynergy-0.0.48-py3-none-any.whl/macrosynergy/version.py
@@ -0,0 +1,8 @@
+# THIS FILE IS GENERATED FROM macrosynergy SETUP.PY
+short_version: str = '0.0.48'
+version: str = '0.0.48'
+full_version: str = '0.0.48'
+git_revision: str = '775aa10243337be326cbe81d9f16d7a5c45850ca'
+release: bool = True
+if not release:
+    version = full_version
