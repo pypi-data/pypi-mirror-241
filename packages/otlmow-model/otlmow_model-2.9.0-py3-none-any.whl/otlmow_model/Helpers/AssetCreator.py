1a9be1a45c06544d611ebadc581b61c2b6e66d9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 06:04:26 +0000

    Add package otlmow-model/2.9.0/otlmow_model-2.9.0-py3-none-any.whl

---
+++ packages/otlmow-model/otlmow_model-2.9.0-py3-none-any.whl/otlmow_model/Helpers/AssetCreator.py
@@ -0,0 +1,48 @@
+from otlmow_model.Helpers.GenericHelper import get_titlecase_from_ns, get_ns_and_name_from_uri
+
+
+def dynamic_create_instance_from_ns_and_name(namespace: str, class_name: str,
+                                             directory: str = 'otlmow_model.Classes'):
+    """Loads the OTL class module and attempts to instantiate the class using the name and namespace of the class
+
+    :param namespace: namespace of the class
+    :type: str
+    :param class_name: class name to instantiate
+    :type: str
+    :param directory: directory where the class modules are located, defaults to OTLMOW.OTLModel.Classes
+    :type: str
+    :return: returns an instance of class_name in the given namespace, located from directory, that inherits from AIMObject or RelatieObject
+    :rtype: AIMObject, RelatieObject or None
+    """
+
+    if directory is None:
+        directory = 'otlmow_model.Classes'
+
+    if namespace is None:
+        namespace = ''
+    else:
+        namespace = get_titlecase_from_ns(namespace) + '.'
+
+    try:
+        # TODO: check https://stackoverflow.com/questions/2724260/why-does-pythons-import-require-fromlist
+        py_mod = __import__(name=f'{directory}.{namespace}{class_name}', fromlist=f'{class_name}')
+    except ModuleNotFoundError:
+        return None
+    class_ = getattr(py_mod, class_name)
+    instance = class_()
+
+    return instance
+
+
+def dynamic_create_instance_from_uri(class_uri: str, directory: str = None):
+    if directory is None:
+        directory = 'otlmow_model.Classes'
+
+    if class_uri == 'http://purl.org/dc/terms/Agent':
+        ns, name = None, 'Agent'
+    else:
+        ns, name = get_ns_and_name_from_uri(class_uri)
+    created = dynamic_create_instance_from_ns_and_name(ns, name, directory=directory)
+    if created is None:
+        raise ValueError(f'{class_uri} is likely not a valid uri, it does not result in a created instance')
+    return created
