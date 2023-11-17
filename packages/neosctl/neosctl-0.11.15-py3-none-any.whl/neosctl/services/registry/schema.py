45c47a2d0b928196ff4753fba7c34306b947679b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 03:04:03 +0000

    Add package neosctl/0.11.15/neosctl-0.11.15-py3-none-any.whl

---
+++ packages/neosctl/neosctl-0.11.15-py3-none-any.whl/neosctl/services/registry/schema.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+
+
+class RegisterCore(BaseModel):
+    name: str
+    public: bool
+
+
+class MigrateCore(BaseModel):
+    urn: str
+    account: str
+
+
+class AddCoreContact(BaseModel):
+    user_id: str
+    role: str
+
+
+class RemoveCoreContact(BaseModel):
+    user_id: str
