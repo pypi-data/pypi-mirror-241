4f9477d73a10d8177be8612f9c927c3e9ca6f23e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 03:03:52 +0000

    Add package e2b/0.11.0rc1/e2b-0.11.0rc1.tar.gz

---
+++ packages/e2b/e2b-0.11.0rc1.tar.gz/e2b-0.11.0rc1/e2b/__init__.py
@@ -0,0 +1,36 @@
+from .api import (
+    E2BApiClient,
+    client,
+)
+from .constants import (
+    SANDBOX_DOMAIN,
+    API_HOST,
+)
+from .sandbox import (
+    Sandbox,
+    FilesystemOperation,
+    FilesystemWatcher,
+    FileInfo,
+    FilesystemEvent,
+    FilesystemManager,
+    TerminalManager,
+    Terminal,
+    ProcessManager,
+    Process,
+    OpenPort,
+    Action,
+    EnvVars,
+    SandboxException,
+    TerminalException,
+    ProcessException,
+    FilesystemException,
+    RpcException,
+    ProcessMessage,
+    ProcessOutput,
+    TerminalOutput,
+    run_code,
+)
+from .templates import (
+    DataAnalysis,
+    CodeInterpreter,
+)
