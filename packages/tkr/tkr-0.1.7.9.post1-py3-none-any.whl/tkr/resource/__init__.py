6fba92daf39505dad971a2141178b42c419fae10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 14:04:42 +0000

    Add package tkr/0.1.7.9.post1/tkr-0.1.7.9.post1-py3-none-any.whl

---
+++ packages/tkr/tkr-0.1.7.9.post1-py3-none-any.whl/tkr/resource/__init__.py
@@ -0,0 +1,33 @@
+"""
+General resources used throughout tkr.
+
+Functions:
+- `deprecated`: Mark a function as deprecated.
+- `notdeprecated`: Mark a function as not deprecated.
+"""
+
+def deprecated(func: object) -> object:
+  """
+  Allows for users to see if a method is deprecated
+
+  Parameters:
+  func (object): The deprecated function
+
+  Returns:
+  object: The deprecated function
+  """
+  func.deprecated = True
+  return func
+
+def notdeprecated(func: object) -> object:
+  """
+  Allows for users to see if a method is deprecated
+
+  Parameters:
+  func (object): Non-deprecated function
+
+  Returns:
+  object: Non-deprecated function
+  """
+  func.deprecated = False
+  return func
