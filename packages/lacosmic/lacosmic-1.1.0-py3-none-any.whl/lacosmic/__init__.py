d4457618107c01bd079e5fe9bf55164f680687b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 07:04:28 +0000

    Add package lacosmic/1.1.0/lacosmic-1.1.0-py3-none-any.whl

---
+++ packages/lacosmic/lacosmic-1.1.0-py3-none-any.whl/lacosmic/__init__.py
@@ -0,0 +1,13 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+Package to remove cosmic rays from an astronomical image using the
+L.A.Cosmic algorithm (van Dokkum 2001; PASP 113, 1420).
+"""
+
+# Packages may add whatever they like to this file, but
+# should keep this content at the top.
+# ----------------------------------------------------------------------------
+from ._astropy_init import *   # noqa: F401, F403
+# ----------------------------------------------------------------------------
+
+from .core import *  # noqa: F401, F403
