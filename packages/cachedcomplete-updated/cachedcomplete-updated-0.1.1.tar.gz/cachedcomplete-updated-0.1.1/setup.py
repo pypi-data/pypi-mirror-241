7a85231e4f94c6b49c476663f97440348b76bfcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 06:04:27 +0000

    Add package cachedcomplete-updated/0.1.1/cachedcomplete-updated-0.1.1.tar.gz

---
+++ packages/cachedcomplete-updated/cachedcomplete-updated-0.1.1.tar.gz/cachedcomplete-updated-0.1.1/setup.py
@@ -0,0 +1,51 @@
+import setuptools
+import os
+
+README_PATH = 'README.md'
+
+if os.path.exists(README_PATH):
+    with open(README_PATH, 'r') as fd:
+        long_description = fd.read()
+
+setuptools.setup(
+    name='cachedcomplete-updated',
+    version='0.1.1',
+    url='https://github.com/reef-technologies/cachedcomplete-updates',
+    project_urls={
+        "Source Code": 'https://github.com/reef-technologies/cachedcomplete-updated'
+    },
+    license='MIT License',
+    author='',
+    author_email='',
+    description='Cached wrapper for python argcomplete',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'argcomplete>=3.1.6,<4'
+    ],
+    packages=setuptools.find_packages(exclude=['test']),
+    zip_safe=False,
+    include_package_data=True,
+    platforms=['MacOS X', 'Posix'],
+    test_suite='test',
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Development Status :: 4 - Beta',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Shells',
+        'Topic :: Terminals'
+    ]
+)
