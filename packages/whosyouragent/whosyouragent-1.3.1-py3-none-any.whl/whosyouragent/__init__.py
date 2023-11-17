62e6942da3f36e9f258575729e02dfb0477b7e8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 05:04:37 +0000

    Add package whosyouragent/1.3.1/whosyouragent-1.3.1-py3-none-any.whl

---
+++ packages/whosyouragent/whosyouragent-1.3.1-py3-none-any.whl/whosyouragent/__init__.py
@@ -0,0 +1,19 @@
+import os
+import time
+from pathlib import Path
+
+from .whosyouragent import VersionUpdater, get_agent, get_header
+
+__version__ = "1.3.1"
+
+browsers_path = Path(__file__).parent / "browserVersions.json"
+if (
+    not browsers_path.exists()
+    or time.time() - os.stat(str(browsers_path)).st_mtime > 604800  # 1 week
+):
+    print("Updating whosyouragent browser versions...")
+    updater = VersionUpdater()
+    try:
+        updater.update_all()
+    except Exception as e:
+        print(e)
