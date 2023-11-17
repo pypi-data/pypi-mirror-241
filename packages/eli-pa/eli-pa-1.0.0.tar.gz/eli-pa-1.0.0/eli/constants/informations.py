41cf93956efc81f82d490d91407d667d50a4590c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 04:04:19 +0000

    Add package eli-pa/1.0.0/eli-pa-1.0.0.tar.gz

---
+++ packages/eli-pa/eli-pa-1.0.0.tar.gz/eli-pa-1.0.0/eli/constants/informations.py
@@ -0,0 +1,23 @@
+# package description for argparse
+APPLICATION_DESCRIPTION = 'Eli is your AI-powered Personal Assistant that helps you with your day to day tasks.'
+
+# link to installation guide
+INSTALLATION_GUIDE = 'Check out https://github.com/AvaterClasher/eli for ' \
+                     'installation guide. '
+
+# epilog information
+EPILOG_DESCRIPTION = 'Eli is designed as an application for hacktoberfest 2023. '
+
+# `--version` option output pattern
+VERSION_INFO = '''
+                     .---.
+       __.....__     |   |.--.
+   .-''         '.   |   ||__|
+  /     .-''"'-.  `. |   |.--.
+ /     /________\   \|   ||  |
+ |                  ||   ||  |
+ \    .-------------'|   ||  |
+  \    '-.____...---.|   ||  |
+   `.             .' |   ||__|
+     `''-...... -'   '---'
+'''
