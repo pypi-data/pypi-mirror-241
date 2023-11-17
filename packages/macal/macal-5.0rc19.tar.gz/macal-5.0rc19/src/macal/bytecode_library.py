7c8503f8623159118d8882682b003af5a3a4fa78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 15:04:14 +0000

    Add package macal/5.0rc19/macal-5.0rc19.tar.gz

---
+++ packages/macal/macal-5.0rc19.tar.gz/macal-5.0rc19/src/macal/bytecode_library.py
@@ -0,0 +1,25 @@
+#
+# Product:   Macal
+# Author:    Marco Caspers
+# Email:     SamaDevTeam@westcon.com
+# License:   MIT License
+# Date:      24-10-2023
+#
+# Copyright 2023 Westcon-Comstor
+#
+
+# this is the bytecode library for the compiler and the scope.
+
+from __future__ import annotations
+from .conversion import convertToHexAddr
+
+class BytecodeLibrary:
+    def __init__(self, name: str, address: int) -> BytecodeLibrary:
+        self.name: str = name
+        self.address: int = address
+
+    def __str__(self) -> str:
+        return f"BytecodeLibrary(name={self.name}, address={convertToHexAddr(self.address)})"
+
+    def __repr__(self) -> str:
+        return self.__str__()
