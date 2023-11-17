05a65bdfbacbe60c21039fd5476934c8dc633e27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 05:04:26 +0000

    Add package ThreeForces/0.0.1/ThreeForces-0.0.1.tar.gz

---
+++ packages/ThreeForces/ThreeForces-0.0.1.tar.gz/ThreeForces-0.0.1/libname/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 16 14:46:25 2023
+
+@author: herac
+"""
+
+from ArrowGui import update_graph, arrow3D
+
+com = 'COM4'
+input1 = 'a:0:i'
+input2 = 'a:1:i'
+input3 = 'a:2:i'
+input4 = 'a:3:i'
+
+arrow3D(com,input1,input2,input3,input4)
---
+++ packages/ThreeForces/ThreeForces-0.0.1.tar.gz/ThreeForces-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = '3D arrow visualization & real-time force x,y,z graph'
+
+# Setting up
+setup(
+    name="ThreeForces",
+    version=VERSION,
+    author="Constantinos Heracleous",
+    author_email="<heracostas65@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', '3D visualization', 'force'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
