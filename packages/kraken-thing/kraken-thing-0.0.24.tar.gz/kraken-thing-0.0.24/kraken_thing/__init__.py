5f403f557a6904837b4d1c25755c3576f33f4d41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 06:04:37 +0000

    Add package kraken-thing/0.0.24/kraken-thing-0.0.24.tar.gz

---
+++ packages/kraken-thing/kraken-thing-0.0.24.tar.gz/kraken-thing-0.0.24/kraken_thing/__init__.py
@@ -0,0 +1,20 @@
+
+import os
+try:
+    import kraken_thing_methods as k
+except:
+    
+    
+    package = 'github.com/tactik8/krakenthingmethods'
+    token = 'REMOVETHIS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'
+    token = token.replace('REMOVETHIS', '')
+    string = 'pip install --upgrade git+https://{token}@{package}.git'.format(token = token, package = package)
+    os.system(string)
+    import kraken_thing_methods as k
+    
+
+
+
+from .kraken_class_observation.kraken_class_observation import Observation
+from .kraken_class_thing.kraken_class_thing import Thing
+from .kraken_class_things.kraken_class_things import Things
