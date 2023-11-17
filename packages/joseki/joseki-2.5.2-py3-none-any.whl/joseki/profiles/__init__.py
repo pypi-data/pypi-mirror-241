1a2f379ab16c91adbe5de0a29d6d8cd5c1be5ed6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 05:04:04 +0000

    Add package joseki/2.5.2/joseki-2.5.2-py3-none-any.whl

---
+++ packages/joseki/joseki-2.5.2-py3-none-any.whl/joseki/profiles/__init__.py
@@ -0,0 +1,36 @@
+"""
+Atmosphere thermophysical profiles.
+"""
+from .afgl_1986 import (
+    AFGL1986Tropical,
+    AFGL1986MidlatitudeSummer,
+    AFGL1986MidlatitudeWinter,
+    AFGL1986SubarcticSummer,
+    AFGL1986SubarcticWinter,
+    AFGL1986USStandard,
+)
+from .mipas_2007 import (
+    MIPASMidlatitudeDay,
+    MIPASMidlatitudeNight,
+    MIPASPolarSummer,
+    MIPASPolarWinter,
+    MIPASTropical,
+)
+from .ussa_1976 import USSA1976
+from .factory import factory
+
+__all__ = [
+    "AFGL1986Tropical",
+    "AFGL1986MidlatitudeSummer",
+    "AFGL1986MidlatitudeWinter",
+    "AFGL1986SubarcticSummer",
+    "AFGL1986SubarcticWinter",
+    "AFGL1986USStandard",
+    "MIPASMidlatitudeDay",
+    "MIPASMidlatitudeNight",
+    "MIPASPolarSummer",
+    "MIPASPolarWinter",
+    "MIPASTropical",
+    "USSA1976",
+    "factory",
+]
