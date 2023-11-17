6632d860e4dea1fdb243b39f025cff7c9d3a2661
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 04:05:13 +0000

    Add package upchatpy/0.0.5/upchatpy-0.0.5.tar.gz

---
+++ packages/upchatpy/upchatpy-0.0.5.tar.gz/upchatpy-0.0.5/upgrade_chat/__init__.py
@@ -0,0 +1,29 @@
+from .api import Client
+from .exceptions import (APIError, AuthenticationError, HTTPError,
+                         ResourceNotFoundError)
+from .responses.orders import (Coupon, DiscordRole, Order, OrderItem,
+                               OrderResponse, OrdersResponse, OrderUser,
+                               Product)
+from .responses.products import ProductResponse, ProductsResponse
+from .responses.users import User, UsersResponse
+
+__all__ = [
+    "APIError",
+    "AuthenticationError",
+    "HTTPError",
+    "Client",
+    "Coupon",
+    "DiscordRole",
+    "Order",
+    "OrderItem",
+    "OrderResponse",
+    "OrdersResponse",
+    "OrderUser",
+    "Product",
+    "ProductResponse",
+    "ProductsResponse",
+    "ResourceNotFoundError",
+    "User",
+    "UsersResponse",
+]
+__version__ = "0.0.5"
