a6c999462b41e420dbcd7f82c6dc9db41c89ba3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 04:04:19 +0000

    Add package NumbaML/1.0.17/NumbaML-1.0.17.tar.gz

---
+++ packages/NumbaML/NumbaML-1.0.17.tar.gz/NumbaML-1.0.17/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='NumbaML',
+    version='1.0.17',
+    packages=['numbaml'],
+    url='https://github.com/jcatankard/NumbaML',
+    author='Josh Tankard',
+    description='Linear regression with Numba',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=['numba', 'numpy', 'scipy'],
+)
