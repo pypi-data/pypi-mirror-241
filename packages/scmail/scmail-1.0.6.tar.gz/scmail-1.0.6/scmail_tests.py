094f722ec2b485ef7dfc85c2cb0fe45992986867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 06:04:37 +0000

    Add package scmail/1.0.6/scmail-1.0.6.tar.gz

---
+++ packages/scmail/scmail-1.0.6.tar.gz/scmail-1.0.6/scmail_tests.py
@@ -0,0 +1,19 @@
+#from unit_tests.unit_tests_MLSolver import *
+#from unit_tests.unit_tests_Simulator import *
+#from unit_tests.unit_tests_SpaLinSolver import *
+from unit_tests.unit_tests_TopoSearch import *
+from unit_tests.unit_tests_TopoSearchParallel import *
+from unit_tests.unit_tests_EMSolver import *
+from unit_tests.utils import *
+import sys
+import os
+
+#def main():
+
+
+if __name__ == '__main__':
+    #print(os.getcwd())
+    sys.path.append(os.path.dirname(__file__))
+    sys.path.append(os.path.join(os.path.dirname(__file__), 'helpers'))
+    print("Running tests for sc-MAIL...")
+    unittest.main()
