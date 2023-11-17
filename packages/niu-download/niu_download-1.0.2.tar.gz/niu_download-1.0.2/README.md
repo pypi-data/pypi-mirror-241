0ff6c281cfcc0e9f1c8e6cbfff01767711d78fd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 03:05:06 +0000

    Add package niu-download/1.0.2/niu_download-1.0.2.tar.gz

---
+++ packages/niu-download/niu_download-1.0.2.tar.gz/niu_download-1.0.2/README.md
@@ -0,0 +1,22 @@
+# This is a tool that can automatically download models from Hugging Face(Windows or Linux).You can download single model or make batch download.
+
+## You can use this tool in python script
+- ### Download "roberta-base" from Hugging face to "models" 
+    ```
+    from sao_download import download as sd
+    sd.download_models('models', ['gpt2'])
+    ```
+  #### And then, the whole gpt2 model in pytorch will be stored in "models" direct.
+- ### Batch Download pytorch models from Hugging Face
+    ```
+    from sao_download import download as sd
+    sd.download_models('models', ['roberta-base', 'gpt2', 'bert-base-uncased'])
+    ```
+  #### This code will download roberta-base、 gpt2、 bert-base-uncased into "models"
+
+## You can also use in cli
+
+    niudownload -m gpt2,roberta-base -d Models
+
+"-m" means the model(s) from Huggingface you want to download
+"-d" means the output dirct you want to store the model(s)
---
+++ packages/niu-download/niu_download-1.0.2.tar.gz/niu_download-1.0.2/setup.py
@@ -0,0 +1,36 @@
+from time import time
+import setuptools
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="niu_download",
+    version="1.0.2",
+    author="Peinan Feng & Junhao Ruan & Peizhuo Liu (NEUNLPLAB)",
+    author_email="fpnan@foxmail.com",
+    description="A tool that can download Hugging Face model(s)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/342215448/HF-models-Download-batch.git",
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    },
+    packages=setuptools.find_packages(where="src"),
+    install_requires=[
+            "requests",
+            "tqdm",
+            "Beautifulsoup4",
+        ],
+    entry_points={
+            "console_scripts": [
+                "niudownload = sao_download.download:cli_main",
+            ],
+        },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+
+    python_requires=">=3.6",
+)
