091455797de908699b9e51f4b92bd5056d011627
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 05:04:16 +0000

    Add package LangTorch/0.1.4/LangTorch-0.1.4-py3-none-any.whl

---
+++ packages/LangTorch/LangTorch-0.1.4-py3-none-any.whl/langtorch/tt/to_tt/__init__.py
@@ -0,0 +1,11 @@
+from .textmodule import TextModule
+from .codemodule import CodeModule
+from .chatmodule import ChatModule
+# from .linear import Linear
+from .activation import Activation, OpenAI, LLM, ActivationGPT
+from .loss import TextLoss
+# from .pooling import AvgPool, MaxPool,
+# from .dropout import Dropout
+# from .rnn import  RNNBase
+from .distance import CosineSimilarity
+# from .transformer import TextTransformer
