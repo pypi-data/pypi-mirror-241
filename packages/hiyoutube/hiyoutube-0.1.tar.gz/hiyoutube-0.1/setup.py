04738e2a8fb0986124f983ea5796d40cb27f2ded
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 14:04:29 +0000

    Add package hiyoutube/0.1/hiyoutube-0.1.tar.gz

---
+++ packages/hiyoutube/hiyoutube-0.1.tar.gz/hiyoutube-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name='hiyoutube',
+    version='0.1',
+    py_modules=['test1'],
+)
---
+++ packages/hiyoutube/hiyoutube-0.1.tar.gz/hiyoutube-0.1/test1.py
@@ -0,0 +1,98 @@
+# print("nice")
+# import test2
+
+# print(test2.engine)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from stuff import cool
+# from stuff.cool import greet
+
+# greet("Jake")
+# cool.greet("Sami")
+
+from stuff import *
+
+cool.greet("Jake")
+print(name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import stuff
+
+# stuff.greet(stuff.name)
+
+
+
+
+
+
+
+
+
+
+
+
+# import stuff
+
+# stuff.greet("Jake")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from stuff import cool, nice
+
+# niceClass = nice._Nicer()
+# print(niceClass.nicest)
+
+# print(cool.super_cool.nicest)
