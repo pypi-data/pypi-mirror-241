2ccfa82d73c2421c755bfcd88ea495d08db34acb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 14:04:22 +0000

    Add package dolpa/0.1.2/dolpa-0.1.2-py3-none-any.whl

---
+++ packages/dolpa/dolpa-0.1.2-py3-none-any.whl/dolpa/exceptions.py
@@ -0,0 +1,14 @@
+class FailedAssertion(Exception):
+    pass
+
+
+class NotAllowedComparison(Exception):
+    pass
+
+
+class InValidCallAttributeError(Exception):
+    pass
+
+
+class InValidCallAttributeModifierError(Exception):
+    pass
