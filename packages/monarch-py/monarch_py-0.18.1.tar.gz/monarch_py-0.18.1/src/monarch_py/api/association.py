b99cd7bc8609c3cb36df3a41e2ee2d685acf399d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 14:04:31 +0000

    Add package monarch-py/0.18.1/monarch_py-0.18.1.tar.gz

---
+++ packages/monarch-py/monarch_py-0.18.1.tar.gz/monarch_py-0.18.1/src/monarch_py/api/association.py
@@ -0,0 +1,52 @@
+from typing import List, Union
+
+from fastapi import APIRouter, Depends, Query
+from monarch_py.api.additional_models import PaginationParams
+from monarch_py.api.config import solr
+from monarch_py.datamodels.model import AssociationResults, MultiEntityAssociationResults
+
+router = APIRouter(
+    tags=["association"],
+    responses={404: {"description": "Not Found"}},
+)
+
+
+@router.get("")
+@router.get("/all", include_in_schema=False)  # We can remove this once the chatgpt plugin & oak aren't using it
+async def _get_associations(
+    category: Union[List[str], None] = Query(default=None),
+    subject: Union[List[str], None] = Query(default=None),
+    predicate: Union[List[str], None] = Query(default=None),
+    object: Union[List[str], None] = Query(default=None),
+    entity: Union[List[str], None] = Query(default=None),
+    direct: Union[bool, None] = Query(default=None),
+    pagination: PaginationParams = Depends(),
+) -> AssociationResults:
+    """Retrieves all associations for a given entity, or between two entities."""
+    response = solr().get_associations(
+        category=category,
+        predicate=predicate,
+        subject=subject,
+        object=object,
+        entity=entity,
+        direct=direct,
+        offset=pagination.offset,
+        limit=pagination.limit,
+    )
+    return response
+
+
+@router.get("/multi")
+async def _get_multi_entity_associations(
+    entity: Union[List[str], None] = Query(default=None),
+    counterpart_category: Union[List[str], None] = Query(default=None),
+    pagination: PaginationParams = Depends(),
+) -> List[MultiEntityAssociationResults]:
+    """Retrieves all associations between each entity and each counterpart category."""
+    response = solr().get_multi_entity_associations(
+        entity=entity,
+        counterpart_category=counterpart_category,
+        offset=pagination.offset,
+        limit_per_group=pagination.limit,
+    )
+    return response
---
+++ packages/monarch-py/monarch_py-0.18.1.tar.gz/monarch_py-0.18.1/src/monarch_py/api/config.py
@@ -0,0 +1,85 @@
+import os
+import requests as rq
+
+from functools import lru_cache
+
+from pydantic import BaseSettings
+
+from monarch_py.implementations.solr.solr_implementation import SolrImplementation
+from monarch_py.datamodels.model import TermSetPairwiseSimilarity
+
+
+class Settings(BaseSettings):
+    solr_host = os.getenv("SOLR_HOST") if os.getenv("SOLR_HOST") else "127.0.0.1"
+    solr_port = os.getenv("SOLR_PORT") if os.getenv("SOLR_PORT") else 8983
+    solr_url = os.getenv("SOLR_URL") if os.getenv("SOLR_URL") else f"http://{solr_host}:{solr_port}/solr"
+    phenio_db_path = os.getenv("PHENIO_DB_PATH") if os.getenv("PHENIO_DB_PATH") else "/data/phenio.db"
+
+    oak_server_host = os.getenv("OAK_SERVER_HOST", "127.0.0.1")
+    oak_server_port = os.getenv("OAK_SERVER_PORT", 18811)
+
+
+settings = Settings()
+
+
+@lru_cache(maxsize=1)
+def solr():
+    return SolrImplementation(settings.solr_url)
+
+
+def convert_nans(input_dict, to_value=None):
+    """
+    Given an input dict of the form {<term>: {<field>: <value>, ...}}
+    converts any <value> of 'NaN' to None.
+    """
+    for k, v in input_dict.items():
+        for ik, iv in v.items():
+            if iv == "NaN":
+                input_dict[k][ik] = None
+
+    return input_dict
+
+
+class OakHTTPRequester:
+    def compare(self, subjects, objects):
+        host = f"http://{settings.oak_server_host}:{settings.oak_server_port}"
+        path = f"/compare/{','.join(subjects)}/{','.join(objects)}"
+        url = f"{host}/{path}"
+
+        print(f"Fetching {url}...")
+        response = rq.get(url=url)
+        data = response.json()
+
+        # FIXME: currently, the response returned from semsimian_server doesn't
+        #  100% match the TermSetPairwiseSimilarity model, so we perform some
+        #  transformations below. once it does, we can remove all the code below
+        #  and just return TermSetPairwiseSimilarity(**data)
+
+        # remove these similarity maps and fold them into the _best_matches dicts
+        object_best_matches_similarity_map = convert_nans(data.pop("object_best_matches_similarity_map"))
+        subject_best_matches_similarity_map = convert_nans(data.pop("subject_best_matches_similarity_map"))
+
+        # convert to a format that can be coerced into a TermSetPairwiseSimilarity
+        converted_data = {
+            **data,
+            **{
+                # flatten the nested termset dicts
+                "subject_termset": {k: v for d in data["subject_termset"] for k, v in d.items()},
+                "object_termset": {k: v for d in data["object_termset"] for k, v in d.items()},
+                "subject_best_matches": {
+                    k: {**v, "similarity": subject_best_matches_similarity_map[k]}
+                    for k, v in data["subject_best_matches"].items()
+                },
+                "object_best_matches": {
+                    k: {**v, "similarity": object_best_matches_similarity_map[k]}
+                    for k, v in data["object_best_matches"].items()
+                },
+            },
+        }
+
+        return TermSetPairwiseSimilarity(**converted_data)
+
+
+@lru_cache(maxsize=1)
+def oak():
+    return OakHTTPRequester()
