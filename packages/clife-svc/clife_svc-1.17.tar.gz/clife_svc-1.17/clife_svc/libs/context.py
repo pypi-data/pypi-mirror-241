1455fc07af7ec5f35c0029c63aaea2498e8f9a10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 16:09:22 +0000

    Add package clife-svc/1.17/clife_svc-1.17.tar.gz

---
+++ packages/clife-svc/clife_svc-1.17.tar.gz/clife_svc-1.17/clife_svc/libs/context.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+__author__ = 'YAnho.wen'
+__mtime__ = '2021/12/21'
+
+"""
+from contextvars import ContextVar
+from typing import Optional
+
+# 申明请求ID变量
+request_id: ContextVar[Optional[str]] = ContextVar('Id of request', default="")
