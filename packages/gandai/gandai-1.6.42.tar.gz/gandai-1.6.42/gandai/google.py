26746524fd6a785bf2e1233a0e3d9b456482261d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 14:04:41 +0000

    Add package gandai/1.6.42/gandai-1.6.42.tar.gz

---
+++ packages/gandai/gandai-1.6.42.tar.gz/gandai-1.6.42/gandai/google.py
@@ -0,0 +1,33 @@
+from googleapiclient.discovery import build
+import gandai as ts
+import pandas as pd
+import googlemaps
+
+gmaps = googlemaps.Client(key=ts.secrets.access_secret_version("GOOLE_MAPS_KEY"))
+
+def search(q: str, count: int = 10) -> pd.DataFrame:
+    if count > 100:
+        print("max @ 100 for now")
+        count = 100
+
+    all_results = []
+    for i in range(0, count, 10):
+        service = build(
+            "customsearch",
+            "v1",
+            developerKey=ts.secrets.access_secret_version("GOOGLE_SEARCH_KEY"),
+        )
+        results = service.cse().list(q=q, cx="12cb7a511cc804eb0", start=i).execute()
+        all_results.extend(results["items"])
+    df = pd.DataFrame(all_results)[["title", "link", "snippet"]]
+    return df
+
+
+def get_google_places(q: str) -> pd.DataFrame:
+    results = []
+    resp = gmaps.places(
+        query=q,
+    )
+    results.extend(resp["results"])
+    results = pd.DataFrame(results)[["name", "place_id", "rating", "user_ratings_total"]]
+    return results
---
+++ packages/gandai/gandai-1.6.42.tar.gz/gandai-1.6.42/gandai/tasks.py
@@ -0,0 +1,33 @@
+import os
+from google.cloud import tasks_v2
+from threading import Thread
+
+import gandai as ts
+from gandai import main
+
+import json
+
+
+def trigger_process_event(event_id: int) -> None:
+    if os.getenv("ENV") == "local":
+        main.process_event(event_id=event_id) 
+        # Thread(target=main.process_event, kwargs={"event_id": event_id}).start()
+    else:
+        client = tasks_v2.CloudTasksClient()
+        parent = client.queue_path(
+            project=os.getenv("PROJECT_ID", "targetselect-staging"),
+            location="us-central1",
+            queue="event",
+        )
+        task = {
+            "app_engine_http_request": {
+                "http_method": tasks_v2.HttpMethod.POST,
+                "app_engine_routing": {"service": "api"},
+                "relative_uri": "/process_event",
+                "headers": {"Content-type": "application/json"},
+                "body": json.dumps({"event_id": event_id}).encode(),
+            }
+        }
+        print(task)
+        response = client.create_task(parent=parent, task=task)
+        return response
