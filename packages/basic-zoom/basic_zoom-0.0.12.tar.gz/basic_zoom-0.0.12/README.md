f22fb955d7aed22e2239c21e176ecba5e04157b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 15:04:28 +0000

    Add package basic-zoom/0.0.12/basic_zoom-0.0.12.tar.gz

---
+++ packages/basic-zoom/basic_zoom-0.0.12.tar.gz/basic_zoom-0.0.12/README.md
@@ -0,0 +1,23 @@
+# basic_zoom
+
+
+## Package Installation
+
+```bash
+pip install basic-zoom
+```
+
+
+## Example Package Usage Server-to-Server app
+
+```
+from basic_zoom import ZoomAPIClient
+
+zoomapi = ZoomAPIClient(
+    ACCOUNT_ID=" <Zoom Account ID here> ",
+    S2S_CLIENT_ID=" <Zoom S2S App Client ID here> ",
+    S2S_CLIENT_SECRET=" <Zoom S2S App Client Secret here> ",)
+
+result = zoomapi.get(endpoint_url="/phone/call_logs")
+```
+
---
+++ packages/basic-zoom/basic_zoom-0.0.12.tar.gz/basic_zoom-0.0.12/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name="basic_zoom",
+    version="0.0.12",
+    author="Justin Steinberg",
+    author_email="jsteinberg@gmail.com",
+    install_requires=["requests", "PyJWT", "requests_oauthlib", "rich"],
+    description="REST api client for Zoom",
+    long_description="REST api client for Zoom",
+    long_description_content_type="text/markdown",
+    url="https://github.com/jsteinberg1/basic_zoom",
+    packages=["basic_zoom"],
+    include_package_data=True,
+    platforms="any",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+    ],
+    python_requires=">=3.8",
+)
