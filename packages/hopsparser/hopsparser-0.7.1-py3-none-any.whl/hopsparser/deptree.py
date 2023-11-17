4c15de4e4bad94f61316c09a28dbfec593222f6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 07:04:11 +0000

    Add package hopsparser/0.7.1/hopsparser-0.7.1-py3-none-any.whl

---
+++ packages/hopsparser/hopsparser-0.7.1-py3-none-any.whl/hopsparser/deptree.py
@@ -0,0 +1,396 @@
+import collections.abc
+import itertools
+import re
+from dataclasses import dataclass
+from typing import (
+    Dict,
+    Iterable,
+    List,
+    Mapping,
+    NamedTuple,
+    Optional,
+    Sequence,
+    cast,
+)
+
+from typing_extensions import Self
+
+from loguru import logger
+
+
+# FIXME: This should be `collections.abc.Sequence[str]` as soon as we can drop py38
+class Misc(collections.abc.Sequence):
+    def __init__(self, elements: Optional[Sequence[str]] = None):
+        if elements is None:
+            elements = []
+        self._lst = list(elements)
+        self.mapping: Dict[str, str] = dict()
+        self._parse()
+
+    def _parse(self):
+        mapping = dict()
+        for e in self._lst:
+            if m := re.match("(?P<key>.+?)=(?P<value>.*)", e):
+                mapping[m.group("key")] = m.group("value")
+        self.mapping = mapping
+
+    def replace(self, mapping: Mapping[str, str]) -> Self:
+        new_elements = []
+        modified = set()
+        for e in self._lst:
+            if m := re.match("(?P<key>.+?)=(?P<value>.*)", e):
+                k = m.group("key")
+                if (new_value := mapping.get(k)) is not None:
+                    if k in modified:
+                        logger.warning(
+                            f"Extra annotation {k} has multiple labels, replacing only the first one."
+                        )
+                        new_elements.append(e)
+                    else:
+                        new_elements.append(f"{k}={new_value}")
+                        modified.add(k)
+                else:
+                    new_elements.append(e)
+        # Add new annotations
+        for k, v in mapping.items():
+            if k not in modified:
+                new_elements.append(f"{k}={v}")
+        return type(self)(new_elements)
+
+    def __getitem__(self, index):
+        return self._lst[index]
+
+    def __len__(self) -> int:
+        return len(self._lst)
+
+    def __str__(self) -> str:
+        return f"Misc({self._lst}, {self.mapping})"
+
+    def __repr__(self) -> str:
+        return f"Misc({self._lst})"
+
+    def to_conllu(self) -> str:
+        if not self._lst:
+            return "_"
+        return "|".join(self._lst)
+
+    @classmethod
+    def from_string(cls, s: str) -> Self:
+        if s == "_":
+            return cls([])
+        return cls(s.split("|"))
+
+
+class MWERange(NamedTuple):
+    start: int
+    end: int
+    form: str
+
+    def to_conll(self) -> str:
+        return f"{self.start}-{self.end}\t{self.form}\t_\t_\t_\t_\t_\t_\t_\t_"
+
+
+class EmptyNode(NamedTuple):
+    after_node: int
+    identifier: str
+    form: str
+    lemma: Optional[str]
+    upos: Optional[str]
+    xpos: Optional[str]
+    feats: Optional[str]
+    deps: Optional[str]
+    misc: Misc
+
+    def to_conll(self) -> str:
+        row = [
+            self.identifier,
+            self.form,
+            *(
+                str(c) if c is not None else "_"
+                for c in (
+                    self.lemma,
+                    self.upos,
+                    self.xpos,
+                    self.feats,
+                    None,
+                    None,
+                    self.deps,
+                )
+            ),
+            self.misc.to_conllu(),
+        ]
+        return "\t".join(row)
+
+
+@dataclass(eq=False)
+class DepNode:
+    identifier: int
+    form: str
+    lemma: Optional[str]
+    upos: Optional[str]
+    xpos: Optional[str]
+    feats: Optional[str]
+    head: Optional[int]
+    deprel: Optional[str]
+    deps: Optional[str]
+    misc: Misc
+
+    def to_conll(self) -> str:
+        row = [
+            str(self.identifier),
+            self.form,
+            *(
+                str(c) if c is not None else "_"
+                for c in (
+                    self.lemma,
+                    self.upos,
+                    self.xpos,
+                    self.feats,
+                    self.head,
+                    self.deprel,
+                    self.deps,
+                )
+            ),
+            self.misc.to_conllu(),
+        ]
+        return "\t".join(row)
+
+
+class DepGraph:
+    ROOT_TOKEN = "<root>"  # noqa: S105
+
+    def __init__(
+        self,
+        nodes: Iterable[DepNode],
+        empty_nodes: Optional[Iterable[EmptyNode]] = None,
+        mwe_ranges: Optional[Iterable[MWERange]] = None,
+        metadata: Optional[Iterable[str]] = None,
+    ):
+        self.nodes = list(nodes)
+
+        govs = {n.identifier: n.head for n in self.nodes}
+        # Only do checks on completly annotated trees
+        if None not in govs.values():
+            if 0 not in govs.values():
+                raise ValueError("Malformed tree: no root")
+            if (
+                unreachable_heads := set(govs.values())
+                .difference(govs.keys())
+                .difference((0, None))
+            ):
+                raise ValueError(f"Malformed tree: unreachable heads: {unreachable_heads}")
+
+        self.mwe_ranges = [] if mwe_ranges is None else list(mwe_ranges)
+        self.empty_nodes = [] if empty_nodes is None else list(empty_nodes)
+        self.metadata = [] if metadata is None else list(metadata)
+
+    @property
+    def words(self) -> List[str]:
+        """
+        A list where each element list[i] is the form of the word at position i.
+        """
+        return [self.ROOT_TOKEN, *(n.form for n in self.nodes)]
+
+    @property
+    def pos_tags(self) -> List[Optional[str]]:
+        """A list where each element list[i] is the upos of the word at position i."""
+        return [None, *(n.upos for n in self.nodes)]
+
+    @property
+    def heads(self) -> List[Optional[int]]:
+        """A list where each element list[i] is the index of the position of the governor of the
+        word at position i."""
+        return [None, *(n.head for n in self.nodes)]
+
+    @property
+    def deprels(self) -> List[Optional[str]]:
+        """A list where each element list[i] is the dependency label of the word at position i."""
+        return [None, *(n.deprel for n in self.nodes)]
+
+    def replace(
+        self,
+        heads: Optional[Mapping[int, int]] = None,
+        deprels: Optional[Mapping[int, str]] = None,
+        pos_tags: Optional[Mapping[int, str]] = None,
+        misc: Optional[Mapping[int, Mapping[str, str]]] = None,
+    ) -> "DepGraph":
+        """Return a new `DepGraph`, identical to `self` except for its dependencies, pos tags and
+        misc annotations (if specified). All parameters should be dicts mapping node identifiers
+        to the new value of the corresponding feature.
+
+        If no argument is provided, this returns a shallow copy of `self`.
+        """
+        if heads is None:
+            heads = dict()
+
+        if deprels is None:
+            deprels = dict()
+
+        if pos_tags is None:
+            pos_tags = dict()
+
+        if misc is None:
+            misc = dict()
+
+        new_nodes = [
+            DepNode(
+                identifier=node.identifier,
+                form=node.form,
+                lemma=node.lemma,
+                upos=pos_tags.get(node.identifier, node.upos),
+                xpos=node.xpos,
+                feats=node.feats,
+                head=heads.get(node.identifier, node.head),
+                deprel=deprels.get(node.identifier, node.deprel),
+                deps=node.deps,
+                misc=node.misc.replace(misc.get(node.identifier, dict())),
+            )
+            for node in self.nodes
+        ]
+        return type(self)(
+            empty_nodes=self.empty_nodes[:],
+            nodes=new_nodes,
+            metadata=self.metadata[:],
+            mwe_ranges=self.mwe_ranges[:],
+        )
+
+    @classmethod
+    def from_conllu(cls, istream: Iterable[str]) -> Self:
+        """Read a conll tree from an input stream"""
+        conll = []
+        metadata = []
+        for line in istream:
+            if line.startswith("#"):
+                metadata.append(line.strip())
+                continue
+            conll.append(line.strip().split("\t"))
+
+        mwe_ranges = []
+        empty_nodes = []
+        nodes = []
+        # FIXME: this is clunky, maybe write a better parser that does validation?
+        for row in conll:
+            processed_row: List[Optional[str]]
+            if "-" in row[0]:
+                mwe_start, mwe_end = row[0].split("-")
+                mwe_ranges.append(MWERange(int(mwe_start), int(mwe_end), row[1]))
+                continue
+            if len(row) < 2:
+                raise ValueError("Too few columns to build a DepNode")
+            elif len(row) < 10:
+                processed_row = [*row, *("_" for _ in range(10 - len(row)))]
+            else:
+                processed_row = list(row)
+            processed_row[2:9] = [c if c != "_" else None for c in processed_row[2:9]]
+
+            if "." in row[0]:
+                if processed_row[6] is not None:
+                    raise ValueError("Empty tokens can't have a head")
+                if processed_row[7] is not None:
+                    raise ValueError("Empty tokens can't have a deprel")
+                empty_nodes.append(
+                    EmptyNode(
+                        after_node=int(cast(str, processed_row[0]).split(".", maxsplit=1)[0]),
+                        identifier=cast(str, processed_row[0]),
+                        form=cast(str, processed_row[1]),
+                        lemma=processed_row[2],
+                        upos=processed_row[3],
+                        xpos=processed_row[4],
+                        feats=processed_row[5],
+                        deps=processed_row[8],
+                        misc=Misc.from_string(cast(str, processed_row[9])),
+                    )
+                )
+                continue
+            node = DepNode(
+                identifier=int(cast(str, processed_row[0])),
+                form=cast(str, processed_row[1]),
+                lemma=processed_row[2],
+                upos=processed_row[3],
+                xpos=processed_row[4],
+                feats=processed_row[5],
+                head=int(processed_row[6]) if processed_row[6] is not None else None,
+                deprel=processed_row[7],
+                deps=processed_row[8],
+                misc=Misc.from_string(cast(str, processed_row[9])),
+            )
+            if node.head is None and node.deprel is not None:
+                logger.warning(f"Node with empty head and nonempty deprel: {node}")
+            nodes.append(node)
+        return cls(
+            empty_nodes=empty_nodes,
+            nodes=nodes,
+            mwe_ranges=mwe_ranges,
+            metadata=metadata,
+        )
+
+    def to_conllu(self) -> str:
+        """CoNLL-U string for the dep tree"""
+        lines = self.metadata
+        for n in self.nodes:
+            mwe_list = [mwe for mwe in self.mwe_ranges if mwe.start == n.identifier]
+            for mwe in mwe_list:
+                lines.append(mwe.to_conll())
+            lines.append(n.to_conll())
+            empty_nodes_list = sorted(
+                (
+                    empty_node
+                    for empty_node in self.empty_nodes
+                    if empty_node.after_node == n.identifier
+                ),
+                key=lambda x: int(x.identifier.rsplit(".", maxsplit=1)[1]),
+            )
+            for empty_node in empty_nodes_list:
+                lines.append(empty_node.to_conll())
+        return "\n".join(lines)
+
+    def __str__(self):
+        return self.to_conllu()
+
+    def __len__(self):
+        return len(self.words)
+
+    @classmethod
+    def read_conll(
+        cls,
+        lines: Iterable[str],
+        max_tree_length: Optional[int] = None,
+    ) -> Iterable[Self]:
+        current_tree_lines: List[str] = []
+        # Add a dummy empty line to flush the last tree even if the CoNLL-U mandatory empty last
+        # line is absent
+        for line in itertools.chain(lines, [""]):
+            if not line or line.isspace():
+                if current_tree_lines:
+                    if max_tree_length is None or len(current_tree_lines) <= max_tree_length:
+                        yield cls.from_conllu(current_tree_lines)
+                    else:
+                        logger.info(
+                            f"Dropped tree with length {len(current_tree_lines)} > {max_tree_length}",
+                        )
+                    current_tree_lines = []
+            else:
+                current_tree_lines.append(line)
+
+    @classmethod
+    def from_words(
+        cls,
+        words: Iterable[str],
+    ) -> Self:
+        return cls(
+            nodes=[
+                DepNode(
+                    identifier=i,
+                    form=w,
+                    lemma=None,
+                    upos=None,
+                    xpos=None,
+                    feats=None,
+                    head=None,
+                    deprel=None,
+                    deps=None,
+                    misc=Misc(),
+                )
+                for i, w in enumerate(words, start=1)
+            ],
+        )
---
+++ packages/hopsparser/hopsparser-0.7.1-py3-none-any.whl/hopsparser/mst.py
@@ -0,0 +1,194 @@
+from typing import List, Tuple, cast
+
+import numpy as np
+
+
+# FIXME: we don't actually need this in CLE: we only need one critical cycle
+def tarjan(tree: np.ndarray) -> List[np.ndarray]:
+    """Use Tarjan's SCC algorithm to find cycles in a tree
+
+    ## Input
+
+    - `tree`: A 1d integer array such that `tree[i]` is the head of the `i`-th node
+
+    ## Output
+
+    - `cycles`: A list of 1d bool arrays such that `cycles[i][j]` is true iff the `j`-th node of
+      `tree` is in the `i`-th cycle
+    """
+    indices = -np.ones_like(tree)
+    lowlinks = -np.ones_like(tree)
+    onstack = np.zeros_like(tree, dtype=bool)
+    stack = list()
+    # I think this is in a list to be able to mutate it in the closure, even though `nonlocal` exists
+    _index = [0]
+    cycles = []
+
+    def strong_connect(i):
+        _index[0] += 1
+        index = _index[-1]  # `_index` is of length 1 so this is also `_index[0]`???
+        indices[i] = lowlinks[i] = index - 1
+        stack.append(i)
+        onstack[i] = True
+        dependents = np.where(np.equal(tree, i))[0]
+        for j in dependents:
+            if indices[j] == -1:
+                strong_connect(j)
+                lowlinks[i] = min(lowlinks[i], lowlinks[j])
+            elif onstack[j]:
+                lowlinks[i] = min(lowlinks[i], indices[j])
+
+        # There's a cycle!
+        if lowlinks[i] == indices[i]:
+            cycle = np.zeros_like(indices, dtype=bool)
+            while stack[-1] != i:
+                j = stack.pop()
+                onstack[j] = False
+                cycle[j] = True
+            stack.pop()
+            onstack[i] = False
+            cycle[i] = True
+            if cycle.sum() > 1:
+                cycles.append(cycle)
+        return
+
+    # -------------------------------------------------------------
+    for i in range(len(tree)):
+        if indices[i] == -1:
+            strong_connect(i)
+    return cycles
+
+
+# TODO: split out a `contraction` function to make this more readable
+def chuliu_edmonds(scores: np.ndarray) -> np.ndarray:
+    """Use the Chu‑Liu/Edmonds algorithm to find a maximum spanning arborescence from the weight
+    matrix of a rooted weighted directed graph
+
+    ## Input
+
+    - `scores`: A 2d numeric array such that `scores[i][j]` is the weight of the `$j→i$` edge in the
+      graph and the 0-th node is the root.
+
+    ## Output
+
+    - `tree`: A 1d integer array such that `tree[i]` is the head of the `i`-th node
+    """
+    np.fill_diagonal(scores, -float("inf"))  # prevent self-loops
+    scores[0] = -float("inf")
+    scores[0, 0] = 0
+    tree = cast(np.ndarray, np.argmax(scores, axis=1))
+    cycles = tarjan(tree)
+    if not cycles:
+        return tree
+    else:
+        # t = len(tree); c = len(cycle); n = len(noncycle)
+        # locations of cycle; (t) in [0,1]
+        cycle = cycles.pop()
+        # indices of cycle in original tree; (c) in t
+        cycle_locs = np.where(cycle)[0]
+        # heads of cycle in original tree; (c) in t
+        cycle_subtree = tree[cycle]
+        # scores of cycle in original tree; (c) in R
+        cycle_scores = scores[cycle, cycle_subtree]
+        # total score of cycle; () in R
+        total_cycle_score = cycle_scores.sum()
+
+        # locations of noncycle; (t) in [0,1]
+        noncycle = np.logical_not(cycle)
+        # indices of noncycle in original tree; (n) in t
+        noncycle_locs = np.where(noncycle)[0]
+
+        # scores of cycle's potential heads; (c x n) - (c) + () -> (n x c) in R
+        metanode_head_scores = (
+            scores[cycle][:, noncycle] - cycle_scores[:, np.newaxis] + total_cycle_score
+        )
+        # scores of cycle's potential dependents; (n x c) in R
+        metanode_dep_scores = scores[noncycle][:, cycle]
+        # best noncycle head for each cycle dependent; (n) in c
+        metanode_heads = np.argmax(metanode_head_scores, axis=0)
+        # best cycle head for each noncycle dependent; (n) in c
+        metanode_deps = np.argmax(metanode_dep_scores, axis=1)
+
+        # scores of noncycle graph; (n x n) in R
+        subscores = scores[noncycle][:, noncycle]
+        # expand to make space for the metanode (n+1 x n+1) in R
+        subscores = np.pad(subscores, ((0, 1), (0, 1)), "constant")
+        # set the contracted graph scores of cycle's potential heads; (c x n)[:, (n) in n] in R -> (n) in R
+        subscores[-1, :-1] = metanode_head_scores[metanode_heads, np.arange(len(noncycle_locs))]
+        # set the contracted graph scores of cycle's potential dependents; (n x c)[(n) in n] in R-> (n) in R
+        subscores[:-1, -1] = metanode_dep_scores[np.arange(len(noncycle_locs)), metanode_deps]
+
+        # MST with contraction; (n+1) in n+1
+        contracted_tree = chuliu_edmonds(subscores)
+        # head of the cycle; () in n
+        cycle_head = contracted_tree[-1]
+        # fixed tree: (n) in n+1
+        contracted_tree = contracted_tree[:-1]
+        # initialize new tree; (t) in 0
+        new_tree = -np.ones_like(tree)
+        # fixed tree with no heads coming from the cycle: (n) in [0,1]
+        contracted_subtree = contracted_tree < len(contracted_tree)
+        # add the nodes to the new tree (t)[(n)[(n) in [0,1]] in t] in t = (n)[(n)[(n) in [0,1]] in n] in t
+        new_tree[noncycle_locs[contracted_subtree]] = noncycle_locs[
+            contracted_tree[contracted_subtree]
+        ]
+        # fixed tree with heads coming from the cycle: (n) in [0,1]
+        contracted_subtree = np.logical_not(contracted_subtree)
+        # add the nodes to the tree (t)[(n)[(n) in [0,1]] in t] in t = (c)[(n)[(n) in [0,1]] in c] in t
+        new_tree[noncycle_locs[contracted_subtree]] = cycle_locs[metanode_deps[contracted_subtree]]
+        # add the old cycle to the tree; (t)[(c) in t] in t = (t)[(c) in t] in t
+        new_tree[cycle_locs] = tree[cycle_locs]
+        # root of the cycle; (n)[() in n] in c = () in c
+        cycle_root = metanode_heads[cycle_head]
+        # add the root of the cycle to the new tree; (t)[(c)[() in c] in t] = (c)[() in c]
+        new_tree[cycle_locs[cycle_root]] = noncycle_locs[cycle_head]
+        return new_tree
+
+
+# ===============================================================
+def chuliu_edmonds_one_root(scores: np.ndarray) -> np.ndarray:
+    """Repeatedly Use the Chu‑Liu/Edmonds algorithm to find a maximum spanning dependency tree from
+    the weight matrix of a rooted weighted directed graph.
+
+    **ATTENTION: this modifies `scores` in place.**
+
+    ## Input
+
+    - `scores`: A 2d numeric array such that `scores[i][j]` is the weight of the `$j→i$` edge in the
+      graph and the 0-th node is the root.
+
+    ## Output
+
+    - `tree`: A 1d integer array such that `tree[i]` is the head of the `i`-th node
+    """
+
+    scores = scores.astype(np.float64)
+    tree = chuliu_edmonds(scores)
+    roots_to_try = np.where(np.equal(tree[1:], 0))[0] + 1
+    if len(roots_to_try) == 1:
+        return tree
+
+    # -------------------------------------------------------------
+    def set_root(scores: np.ndarray, root: int) -> Tuple[np.ndarray, np.ndarray]:
+        """Force the `root`-th node to be the only node under the root by overwriting the weights of
+        the other children of the root."""
+        root_score = scores[root, 0]
+        scores = np.array(scores)
+        scores[1:, 0] = -float("inf")
+        scores[root] = -float("inf")
+        scores[root, 0] = 0
+        return scores, root_score
+
+    # We find the maximum spanning dependenc_treey tree by try every possible root
+    best_score, best_tree = -np.inf, None  # This is what's causing it to crash
+    for root in roots_to_try:
+        _scores, root_score = set_root(scores, root)
+        _tree = chuliu_edmonds(_scores)
+        tree_probs = _scores[np.arange(len(_scores)), _tree]
+        tree_score = (tree_probs).sum() + (root_score) if (tree_probs > -np.inf).all() else -np.inf
+        if tree_score > best_score:
+            best_score = tree_score
+            best_tree = _tree
+
+    assert best_tree is not None
+    return best_tree
