7cd988264e21246bc2fe5b304c45a00b23bd0afc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 16:10:41 +0000

    Add package ophyd-devices/0.12.0/ophyd_devices-0.12.0.tar.gz

---
+++ packages/ophyd-devices/ophyd_devices-0.12.0.tar.gz/ophyd_devices-0.12.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+__version__ = "0.12.0"
+
+if __name__ == "__main__":
+    setup(
+        install_requires=[
+            "ophyd",
+            "typeguard",
+            "prettytable",
+            "bec_lib",
+            "numpy",
+            "pyyaml",
+            "std_daq_client",
+            "pyepics",
+            "pytest",
+        ],
+        extras_require={"dev": ["pytest", "pytest-random-order", "black", "coverage"]},
+        version=__version__,
+    )
