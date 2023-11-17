4c5e26c71d687d1f43d1e420c952bdbdf43b1872
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 15:04:58 +0000

    Add package gradio/4.4.0/gradio-4.4.0.tar.gz

---
+++ packages/gradio/gradio-4.4.0.tar.gz/gradio-4.4.0/gradio/templates/frontend/assets/frontmatter-3c75adc2.js
@@ -0,0 +1,64 @@
+import { s as styleTags, f as foldNodeProp, c as foldInside, p as parseMixed, t as tags, S as StreamLanguage } from './Index-0c0b45c1.js';
+import { yaml } from './yaml-c63fc23d.js';
+import './index-2b08cc7a.js';
+import './svelte/svelte.js';
+import './Button-5e2814d0.js';
+import './Index-259f3890.js';
+import './Copy-e5864b80.js';
+import './Download-ea83bae3.js';
+import './BlockLabel-4986e1f2.js';
+import './Empty-65007a7c.js';
+import './Example-ef3222e9.js';
+
+const frontMatterFence = /^---\s*$/m;
+const frontmatter = {
+  defineNodes: [{ name: "Frontmatter", block: true }, "FrontmatterMark"],
+  props: [
+    styleTags({
+      Frontmatter: [tags.documentMeta, tags.monospace],
+      FrontmatterMark: tags.processingInstruction
+    }),
+    foldNodeProp.add({
+      Frontmatter: foldInside,
+      FrontmatterMark: () => null
+    })
+  ],
+  wrap: parseMixed((node) => {
+    const { parser } = StreamLanguage.define(yaml);
+    if (node.type.name === "Frontmatter") {
+      return {
+        parser,
+        overlay: [{ from: node.from + 4, to: node.to - 4 }]
+      };
+    }
+    return null;
+  }),
+  parseBlock: [
+    {
+      name: "Frontmatter",
+      before: "HorizontalRule",
+      parse: (cx, line) => {
+        let end = void 0;
+        const children = new Array();
+        if (cx.lineStart === 0 && frontMatterFence.test(line.text)) {
+          children.push(cx.elt("FrontmatterMark", 0, 4));
+          while (cx.nextLine()) {
+            if (frontMatterFence.test(line.text)) {
+              end = cx.lineStart + 4;
+              break;
+            }
+          }
+          if (end !== void 0) {
+            children.push(cx.elt("FrontmatterMark", end - 4, end));
+            cx.addElement(cx.elt("Frontmatter", 0, end, children));
+          }
+          return true;
+        }
+        return false;
+      }
+    }
+  ]
+};
+
+export { frontmatter };
+//# sourceMappingURL=frontmatter-3c75adc2.js.map
