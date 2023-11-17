f4770f3b35e28130cd66b7e1882ae4d7776b7d7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 04:04:51 +0000

    Add package arturotest/0.0.1.post0/arturotest-0.0.1.post0.tar.gz

---
+++ packages/arturotest/arturotest-0.0.1.post0.tar.gz/arturotest-0.0.1.post0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+import codecs
+import os
+
+
+# Setting up
+setup(
+    name="arturotest",
+    version='0.0.1.post',
+    author="rtvro",
+    author_email="<example@gmail.com>",
+    description='test package',
+    install_requires=[],
+    long_description_content_type="text/markdown",
+    long_description='test package long'
+)
+
