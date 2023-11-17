415439eee833d61917067a611b7403c12347db21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 05:04:26 +0000

    Add package shell-proc/2.1.0/shell_proc-2.1.0-py3-none-any.whl

---
+++ packages/shell-proc/shell_proc-2.1.0-py3-none-any.whl/shell_proc/__meta__.py
@@ -0,0 +1,6 @@
+name = 'shell_proc'
+version = '2.1.0'
+description = 'Continuous shell process'
+url = 'https://github.com/justengel/shell_proc'
+author = 'Justin Engel'
+author_email = 'jtengel08@gmail.com'
