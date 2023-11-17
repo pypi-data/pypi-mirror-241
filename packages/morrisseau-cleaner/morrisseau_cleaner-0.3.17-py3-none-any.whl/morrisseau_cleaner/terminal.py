2314e9b5f9614e38708aa78d0e0e117f340def0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 07:04:20 +0000

    Add package morrisseau-cleaner/0.3.17/morrisseau_cleaner-0.3.17-py3-none-any.whl

---
+++ packages/morrisseau-cleaner/morrisseau_cleaner-0.3.17-py3-none-any.whl/morrisseau_cleaner/terminal.py
@@ -0,0 +1,10 @@
+from askii_art import askii_art_main
+
+def display_menu():
+    print("Welcome to the Morrisseau Project Cleaner Module!")
+    print("  help - Show available commands")
+    print("  exit - Exit the program")
+
+def terminal_main():
+    askii_art_main()
+    display_menu()
