9164510410bbdd3d06cb8178f295f61f37eb642f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 06:04:47 +0000

    Add package poetry/1.7.1/poetry-1.7.1.tar.gz

---
+++ packages/poetry/poetry-1.7.1.tar.gz/poetry-1.7.1/tests/console/commands/env/test_remove.py
@@ -0,0 +1,108 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import pytest
+
+from poetry.core.constraints.version import Version
+
+from tests.console.commands.env.helpers import check_output_wrapper
+
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+    from cleo.testers.command_tester import CommandTester
+    from pytest_mock import MockerFixture
+
+    from tests.types import CommandTesterFactory
+
+
+@pytest.fixture
+def tester(command_tester_factory: CommandTesterFactory) -> CommandTester:
+    return command_tester_factory("env remove")
+
+
+def test_remove_by_python_version(
+    mocker: MockerFixture,
+    tester: CommandTester,
+    venvs_in_cache_dirs: list[str],
+    venv_name: str,
+    venv_cache: Path,
+) -> None:
+    check_output = mocker.patch(
+        "subprocess.check_output",
+        side_effect=check_output_wrapper(Version.parse("3.6.6")),
+    )
+
+    tester.execute("3.6")
+
+    assert check_output.called
+    assert not (venv_cache / f"{venv_name}-py3.6").exists()
+
+    expected = f"Deleted virtualenv: {venv_cache / venv_name}-py3.6\n"
+    assert tester.io.fetch_output() == expected
+
+
+def test_remove_by_name(
+    tester: CommandTester,
+    venvs_in_cache_dirs: list[str],
+    venv_name: str,
+    venv_cache: Path,
+) -> None:
+    expected = ""
+
+    for name in venvs_in_cache_dirs:
+        tester.execute(name)
+
+        assert not (venv_cache / name).exists()
+
+        expected += f"Deleted virtualenv: {venv_cache / name}\n"
+
+    assert tester.io.fetch_output() == expected
+
+
+def test_remove_all(
+    tester: CommandTester,
+    venvs_in_cache_dirs: list[str],
+    venv_name: str,
+    venv_cache: Path,
+) -> None:
+    expected = {""}
+    tester.execute("--all")
+    for name in venvs_in_cache_dirs:
+        assert not (venv_cache / name).exists()
+        expected.add(f"Deleted virtualenv: {venv_cache / name}")
+    assert set(tester.io.fetch_output().split("\n")) == expected
+
+
+def test_remove_all_and_version(
+    tester: CommandTester,
+    venvs_in_cache_dirs: list[str],
+    venv_name: str,
+    venv_cache: Path,
+) -> None:
+    expected = {""}
+    tester.execute(f"--all {venvs_in_cache_dirs[0]}")
+    for name in venvs_in_cache_dirs:
+        assert not (venv_cache / name).exists()
+        expected.add(f"Deleted virtualenv: {venv_cache / name}")
+    assert set(tester.io.fetch_output().split("\n")) == expected
+
+
+def test_remove_multiple(
+    tester: CommandTester,
+    venvs_in_cache_dirs: list[str],
+    venv_name: str,
+    venv_cache: Path,
+) -> None:
+    expected = {""}
+    removed_envs = venvs_in_cache_dirs[0:2]
+    remaining_envs = venvs_in_cache_dirs[2:]
+    tester.execute(" ".join(removed_envs))
+    for name in removed_envs:
+        assert not (venv_cache / name).exists()
+        expected.add(f"Deleted virtualenv: {venv_cache / name}")
+    for name in remaining_envs:
+        assert (venv_cache / name).exists()
+    assert set(tester.io.fetch_output().split("\n")) == expected
---
+++ packages/poetry/poetry-1.7.1.tar.gz/poetry-1.7.1/tests/console/commands/test_publish.py
@@ -0,0 +1,132 @@
+from __future__ import annotations
+
+from pathlib import Path
+from typing import TYPE_CHECKING
+from typing import Any
+
+import pytest
+import requests
+
+from poetry.publishing.uploader import UploadError
+
+
+if TYPE_CHECKING:
+    import httpretty
+
+    from cleo.testers.application_tester import ApplicationTester
+    from pytest_mock import MockerFixture
+
+    from tests.helpers import PoetryTestApplication
+
+
+def test_publish_returns_non_zero_code_for_upload_errors(
+    app: PoetryTestApplication,
+    app_tester: ApplicationTester,
+    http: type[httpretty.httpretty],
+) -> None:
+    http.register_uri(
+        http.POST, "https://upload.pypi.org/legacy/", status=400, body="Bad Request"
+    )
+
+    exit_code = app_tester.execute("publish --username foo --password bar")
+
+    assert exit_code == 1
+
+    expected_output = """
+Publishing simple-project (1.2.3) to PyPI
+"""
+    expected_error_output = """\
+HTTP Error 400: Bad Request | b'Bad Request'
+"""
+
+    assert expected_output in app_tester.io.fetch_output()
+    assert expected_error_output in app_tester.io.fetch_error()
+
+
+@pytest.mark.filterwarnings("ignore::pytest.PytestUnhandledThreadExceptionWarning")
+def test_publish_returns_non_zero_code_for_connection_errors(
+    app: PoetryTestApplication,
+    app_tester: ApplicationTester,
+    http: type[httpretty.httpretty],
+) -> None:
+    def request_callback(*_: Any, **__: Any) -> None:
+        raise requests.ConnectionError()
+
+    http.register_uri(
+        http.POST, "https://upload.pypi.org/legacy/", body=request_callback
+    )
+
+    exit_code = app_tester.execute("publish --username foo --password bar")
+
+    assert exit_code == 1
+
+    expected = str(UploadError(error=requests.ConnectionError()))
+
+    assert expected in app_tester.io.fetch_error()
+
+
+def test_publish_with_cert(
+    app_tester: ApplicationTester, mocker: MockerFixture
+) -> None:
+    publisher_publish = mocker.patch("poetry.publishing.Publisher.publish")
+
+    app_tester.execute("publish --cert path/to/ca.pem")
+
+    assert [
+        (None, None, None, Path("path/to/ca.pem"), None, False, False)
+    ] == publisher_publish.call_args
+
+
+def test_publish_with_client_cert(
+    app_tester: ApplicationTester, mocker: MockerFixture
+) -> None:
+    publisher_publish = mocker.patch("poetry.publishing.Publisher.publish")
+
+    app_tester.execute("publish --client-cert path/to/client.pem")
+    assert [
+        (None, None, None, None, Path("path/to/client.pem"), False, False)
+    ] == publisher_publish.call_args
+
+
+@pytest.mark.parametrize(
+    "options",
+    [
+        "--dry-run",
+        "--skip-existing",
+        "--dry-run --skip-existing",
+    ],
+)
+def test_publish_dry_run_skip_existing(
+    app_tester: ApplicationTester, http: type[httpretty.httpretty], options: str
+) -> None:
+    http.register_uri(
+        http.POST, "https://upload.pypi.org/legacy/", status=409, body="Conflict"
+    )
+
+    exit_code = app_tester.execute(f"publish {options} --username foo --password bar")
+
+    assert exit_code == 0
+
+    output = app_tester.io.fetch_output()
+    error = app_tester.io.fetch_error()
+
+    assert "Publishing simple-project (1.2.3) to PyPI" in output
+    assert "- Uploading simple_project-1.2.3.tar.gz" in error
+    assert "- Uploading simple_project-1.2.3-py2.py3-none-any.whl" in error
+
+
+def test_skip_existing_output(
+    app_tester: ApplicationTester, http: type[httpretty.httpretty]
+) -> None:
+    http.register_uri(
+        http.POST, "https://upload.pypi.org/legacy/", status=409, body="Conflict"
+    )
+
+    exit_code = app_tester.execute(
+        "publish --skip-existing --username foo --password bar"
+    )
+
+    assert exit_code == 0
+
+    error = app_tester.io.fetch_error()
+    assert "- Uploading simple_project-1.2.3.tar.gz File exists. Skipping" in error
