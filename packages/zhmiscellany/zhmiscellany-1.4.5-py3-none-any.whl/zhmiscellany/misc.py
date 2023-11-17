454163c6a6f860232b1fe5d558347281fbbc745f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 04:05:08 +0000

    Add package zhmiscellany/1.4.5/zhmiscellany-1.4.5-py3-none-any.whl

---
+++ packages/zhmiscellany/zhmiscellany-1.4.5-py3-none-any.whl/zhmiscellany/misc.py
@@ -0,0 +1,27 @@
+import os, signal
+from ._misc_supportfuncs import set_activity_timeout, activity
+import zhmiscellany.math
+
+
+def die():
+    os.kill(os.getpid(), signal.SIGTERM)
+
+
+def show_progress(things, total_things, extra_data=''):
+    print(f'\r{zhmiscellany.math.smart_percentage(things, total_things)}% {extra_data}', end='')
+    if things == total_things:
+        print('')
+
+
+def every_nth(number, n):
+    if number % n == 0:
+        return True
+    return False
+
+
+def smart_every_nth(number, n, total):
+    if number % n == 0:
+        return True
+    if number == total:
+        return True
+    return False
