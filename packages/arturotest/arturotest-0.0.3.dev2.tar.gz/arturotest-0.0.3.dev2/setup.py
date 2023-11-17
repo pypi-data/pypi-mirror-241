1d120902addc46b002737b59aecd6c29ac3ee8cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 05:04:07 +0000

    Add package arturotest/0.0.3.dev2/arturotest-0.0.3.dev2.tar.gz

---
+++ packages/arturotest/arturotest-0.0.3.dev2.tar.gz/arturotest-0.0.3.dev2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+import codecs
+import os
+
+
+# Setting up
+setup(
+    name="arturotest",
+    version='0.0.3-dev.2',
+    author="rtvro",
+    author_email="<example@gmail.com>",
+    description='test package',
+    install_requires=[],
+    long_description_content_type="text/markdown",
+    long_description='test package long'
+)
+
