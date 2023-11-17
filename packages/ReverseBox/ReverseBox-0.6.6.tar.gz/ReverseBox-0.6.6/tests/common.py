2e7ce73526121e60e3a0256aaad4b0ea420fb780
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 04:05:14 +0000

    Add package ReverseBox/0.6.6/ReverseBox-0.6.6.tar.gz

---
+++ packages/ReverseBox/ReverseBox-0.6.6.tar.gz/ReverseBox-0.6.6/tests/common.py
@@ -0,0 +1,47 @@
+"""
+Copyright © 2022  Bartłomiej Duda
+License: GPL-3.0 License
+"""
+
+from dataclasses import dataclass
+
+
+@dataclass
+class CRCTestEntry:
+    test_data: bytes
+    expected_int: int
+    expected_str: str
+
+
+@dataclass
+class XORTestEntry:
+    test_data: bytes
+    key: bytes
+    expected_result: bytes
+
+
+@dataclass
+class ROT13TestEntry:
+    test_data: bytes
+    key: bytes
+    expected_result: bytes
+
+
+@dataclass
+class XORRetro64ECOTestEntry:
+    test_data: bytes
+    key: int
+    expected_result: bytes
+
+
+@dataclass
+class HashTestEntry:
+    test_data: bytes
+    expected_result: bytes
+
+
+@dataclass
+class PaddingTestEntry:
+    test_offset: int
+    test_div: int
+    expected_padding: int
