c4b93f59dfb7a8c3c2c0dec921e4cb187f433f15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 03:05:18 +0000

    Add package quantagonia-api-client/0.69/quantagonia_api_client-0.69-py3-none-any.whl

---
+++ packages/quantagonia-api-client/quantagonia_api_client-0.69-py3-none-any.whl/quantagonia/enums.py
@@ -0,0 +1,28 @@
+from enum import Enum
+
+class HybridSolverConnectionType(Enum):
+	CLOUD = 0
+	LOCAL = 1
+
+class HybridSolverServers(Enum):
+    PROD = "https://api.quantagonia.com"
+    STAGING = "https://staging.quantagonia.com"
+    DEV = "https://dev.quantagonia.com"
+    DEV3 = "https://dev3.quantagonia.com"
+    LOCAL = "https://localhost:8088"
+
+class PriorityEnum(str, Enum):
+    HIGH = "high"
+    LOW = "low"
+    MEDIUM = "medium"
+
+class HybridSolverOptSenses(Enum):
+    """
+    An enumeration class representing the optimization senses for the hybrid solver.
+
+    Attributes:
+        MAXIMIZE: Holds a string representing maximization.
+        MINIMIZE: Holds a string representing minimization.
+    """
+    MAXIMIZE = "MAXIMIZE"
+    MINIMIZE = "MINIMIZE"
