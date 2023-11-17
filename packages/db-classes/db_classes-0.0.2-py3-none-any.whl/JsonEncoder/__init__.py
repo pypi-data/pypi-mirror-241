cfd791c72b0ecebef4189a66d8b68a30c3562886
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 07:04:27 +0000

    Add package db-classes/0.0.2/db_classes-0.0.2-py3-none-any.whl

---
+++ packages/db-classes/db_classes-0.0.2-py3-none-any.whl/JsonEncoder/__init__.py
@@ -0,0 +1,9 @@
+__all__ = [
+    "Decoder",
+    "Encoder",
+]
+
+from .Decoder import Decoder
+from .Encoder import Encoder
+from .Encoders import *
+from .Decoders import *
---
+++ packages/db-classes/db_classes-0.0.2-py3-none-any.whl/db_classes/DbClassLiteral.py
@@ -0,0 +1,8 @@
+from dataclasses import dataclass
+
+from .DbClass import DbClass
+
+
+@dataclass
+class DbClassLiteral(DbClass):
+    pass
