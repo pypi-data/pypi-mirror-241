8b00849c99476be2c6c49bf77670e69fb8c136ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 03:04:41 +0000

    Add package quickremi/1.0.4/quickremi-1.0.4.tar.gz

---
+++ packages/quickremi/quickremi-1.0.4.tar.gz/quickremi-1.0.4/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="quickremi",
+    version="1.0.4",
+    author="Aru Raghuvanshi",
+    author_email="sixdigitco@gmail.com",
+    description="A wrapper of REMI library to build UI front-ends purely using Python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=["ui"],
+    python_requires=">=3.7"
+)
