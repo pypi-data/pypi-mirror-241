07abf9ed4acd17809ef46777a9bc37cf0a6ef165
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 16:10:15 +0000

    Add package kuroco-py/0.0.2.8/kuroco_py-0.0.2.8-py3-none-any.whl

---
+++ packages/kuroco-py/kuroco_py-0.0.2.8-py3-none-any.whl/kuroco_embedding/CONFIG.py
@@ -0,0 +1,8 @@
+QUERY_KW: str = "vector_search"
+VECTOR_SEARCH_MODIFIER: str = "vector_search_modifier"
+
+EMBEDDING_COLUMN_VECTORS: str = "EMBEDDING_COLUMN_VECTORS"
+
+SCORE_DISTANCE_COLUMN_NAME: str = "vector_distance"
+LABEL_SCORE_COLUMN_NAME: str = "label_score"
+KEYWORDS_SCORE_COLUMN_NAME: str = "keywords_score"
