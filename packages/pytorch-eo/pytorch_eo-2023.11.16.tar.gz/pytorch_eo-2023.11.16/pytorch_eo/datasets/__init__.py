006c00598000ec4052c454959b86aedf7505a2c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 06:04:10 +0000

    Add package pytorch-eo/2023.11.16/pytorch_eo-2023.11.16.tar.gz

---
+++ packages/pytorch-eo/pytorch_eo-2023.11.16.tar.gz/pytorch_eo-2023.11.16/pytorch_eo/datasets/__init__.py
@@ -0,0 +1,9 @@
+from .RGBImageDataset import RGBImageDataset
+from .ConcatDataset import ConcatDataset
+from .SensorImageDataset import SensorImageDataset
+from .CategoricalImageDataset import CategoricalImageDataset
+from .SingleBandImageDataset import SingleBandImageDataset
+
+from .eurosat import EuroSATRGB, EuroSAT
+from .ucmerced import UCMercedLandUse
+from .sen12floods import SEN12Floods
---
+++ packages/pytorch-eo/pytorch_eo-2023.11.16.tar.gz/pytorch_eo-2023.11.16/pytorch_eo/tasks/segmentation/ImageSegmentation.py
@@ -0,0 +1,40 @@
+import torch
+import segmentation_models_pytorch as smp
+from pytorch_eo.metrics.segmentation import iou
+from ..BaseTask import BaseTask
+
+
+class ImageSegmentation(BaseTask):
+    def __init__(
+        self,
+        model=None,
+        hparams=None,
+        inputs=["image"],
+        outputs=["mask"],
+        loss_fn=None,
+        metrics=None,
+        num_classes=None,
+    ):
+        # defaults
+        if num_classes is None and model is None:
+            raise ValueError("num_classes or model must be provided")
+        if model is None:
+            model = smp.Unet(
+                encoder_name="resnet18",
+                encoder_weights="imagenet",  # imagenet
+                in_channels=3,
+                classes=num_classes,
+            )
+        loss_fn = torch.nn.BCEWithLogitsLoss() if loss_fn is None else loss_fn
+        hparams = {"optimizer": "Adam"} if hparams is None else hparams
+        if metrics is None and num_classes is None:
+            raise ValueError("num_classes must be provided if metrics is None")
+        metrics = {"iou": iou} if metrics is None else metrics
+        super().__init__(model, hparams, inputs, outputs, loss_fn, metrics)
+
+    def predict(self, batch):
+        self.eval()
+        with torch.no_grad():
+            x = self.my_prepare_data(batch)
+            y_hat = self(x)
+            return torch.sigmoid(y_hat)
