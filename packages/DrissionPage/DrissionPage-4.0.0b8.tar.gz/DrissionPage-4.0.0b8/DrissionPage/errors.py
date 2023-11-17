f8f0e70e713e1ec59db9da144f5e7a0d7ab03a8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 16:10:00 +0000

    Add package DrissionPage/4.0.0b8/DrissionPage-4.0.0b8.tar.gz

---
+++ packages/DrissionPage/DrissionPage-4.0.0b8.tar.gz/DrissionPage-4.0.0b8/DrissionPage/errors.py
@@ -0,0 +1,68 @@
+# -*- coding:utf-8 -*-
+"""
+@Author  :   g1879
+@Contact :   g1879@qq.com
+"""
+
+
+class BaseError(Exception):
+    _info = None
+
+    def __init__(self, ErrorInfo=None):
+        super().__init__(self)  # 初始化父类
+        self._info = ErrorInfo or self._info
+
+    def __str__(self):
+        return self._info
+
+
+class AlertExistsError(BaseError):
+    _info = '存在未处理的提示框。'
+
+
+class ContextLossError(BaseError):
+    _info = '页面被刷新，请操作前尝试等待页面刷新或加载完成。'
+
+
+class ElementLossError(BaseError):
+    _info = '元素对象因刷新已失效。'
+
+
+class CDPError(BaseError):
+    _info = '方法调用错误。'
+
+
+class PageClosedError(BaseError):
+    _info = '页面已关闭。'
+
+
+class ElementNotFoundError(BaseError):
+    _info = '没有找到元素。'
+
+
+class JavaScriptError(BaseError):
+    _info = 'JavaScript运行错误。'
+
+
+class NoRectError(BaseError):
+    _info = '该元素没有位置及大小。'
+
+
+class BrowserConnectError(BaseError):
+    _info = '浏览器连接失败。'
+
+
+class NoResourceError(BaseError):
+    _info = '该元素无可保存的内容或保存失败。'
+
+
+class CanNotClickError(BaseError):
+    _info = '该元素无法滚动到视口或被遮挡，无法点击。'
+
+
+class GetDocumentError(BaseError):
+    _info = '获取文档失败。'
+
+
+class WaitTimeoutError(BaseError):
+    _info = '等待失败。'
