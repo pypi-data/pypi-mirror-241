94a6b01b382948637e0e71ca6372d8728c71020d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 14:04:17 +0000

    Add package Doggel-Inc/1.0.0/Doggel-Inc-1.0.0.tar.gz

---
+++ packages/Doggel-Inc/Doggel-Inc-1.0.0.tar.gz/Doggel-Inc-1.0.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="Doggel-Inc",
+    version="1.0.0",
+    author="Lordpomind",
+    author_email="lordpomind@gmail.com",
+    description="A package with all Doggeł Inc services",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    
+    packages=find_packages(),
+    install_requires=[
+
+        "aiohttp",
+    ],
+    python_requires='>=3.8',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
