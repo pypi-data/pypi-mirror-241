3deeb8e2437fd7d55549110160ed32102a9cc14f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 16:09:50 +0000

    Add package fastapi-view/0.2.0/fastapi_view-0.2.0-py3-none-any.whl

---
+++ packages/fastapi-view/fastapi_view-0.2.0-py3-none-any.whl/fastapi_view/__init__.py
@@ -0,0 +1,15 @@
+from contextvars import ContextVar
+
+view_request: ContextVar = ContextVar("view_request", default=None)
+
+from .view import _View
+
+view = _View()
+
+from .inertia import _Inertia
+
+inertia = _Inertia()
+
+from .vite import _Vite
+
+vite = _Vite()
