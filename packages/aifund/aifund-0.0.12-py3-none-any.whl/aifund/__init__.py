d9e49df32b29437b8a50312a1e68446145edb536
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 15:04:10 +0000

    Add package aifund/0.0.12/aifund-0.0.12-py3-none-any.whl

---
+++ packages/aifund/aifund-0.0.12-py3-none-any.whl/aifund/__init__.py
@@ -0,0 +1,12 @@
+"""
+AIfund 是基于 Python 的基金量化工具
+"""
+
+__version__ = "0.0.12"
+__author__ = "tiano"
+
+from aifund.data.fund_etf_em import (
+    web_data,
+    stock_changes,
+    realtime_change,
+)
