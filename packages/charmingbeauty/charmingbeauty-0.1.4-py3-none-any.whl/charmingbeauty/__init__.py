1f50de9d71e616ced0d8fb2300dfa212b6a3bb5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 07:04:22 +0000

    Add package charmingbeauty/0.1.4/charmingbeauty-0.1.4-py3-none-any.whl

---
+++ packages/charmingbeauty/charmingbeauty-0.1.4-py3-none-any.whl/charmingbeauty/__init__.py
@@ -0,0 +1,12 @@
+"""
+Create visually appealing plots for high energy physisc/general purpose.
+"""
+__version__ = "0.1.1"
+
+from . import layout
+from . import visual
+
+from .visual import set_style_default,\
+                    set_style_present,\
+                    set_style_poster
+
