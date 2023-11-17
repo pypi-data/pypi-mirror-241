f504f7d26fa053087dde81f1a12c6a8be32628a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 07:04:13 +0000

    Add package qucircuit/1.5/qucircuit-1.5-py3-none-any.whl

---
+++ packages/qucircuit/qucircuit-1.5-py3-none-any.whl/qckt/__init__.py
@@ -0,0 +1,10 @@
+
+from qckt.QCkt import QCkt
+from qckt.Registers import QRegister, CRegister, placement
+from qckt.Job import Job
+from qckt.qException import QCktException
+
+__version__ = '1.5'
+__author__ = 'Atul Varshneya'
+__email__ = 'atul.varshneya@gmail.com'
+
