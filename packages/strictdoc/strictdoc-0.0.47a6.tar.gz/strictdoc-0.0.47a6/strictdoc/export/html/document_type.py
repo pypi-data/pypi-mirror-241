435cab74e5eb3d8c768e6bcdccda4363ebb9a41b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 16:09:50 +0000

    Add package strictdoc/0.0.47a6/strictdoc-0.0.47a6.tar.gz

---
+++ packages/strictdoc/strictdoc-0.0.47a6.tar.gz/strictdoc-0.0.47a6/strictdoc/export/html/document_type.py
@@ -0,0 +1,47 @@
+class DocumentType:
+    DOCUMENT = 1
+    TABLE = 2
+    TRACE = 3
+    DEEPTRACE = 4
+    PDF = 5
+
+    def __init__(self, document_type):
+        self.document_type = document_type
+
+    @staticmethod
+    def all():  # noqa: A003
+        return (
+            DocumentType.DOCUMENT,
+            DocumentType.TABLE,
+            DocumentType.TRACE,
+            DocumentType.DEEPTRACE,
+            DocumentType.PDF,
+        )
+
+    @staticmethod
+    def document():
+        return DocumentType(DocumentType.DOCUMENT)
+
+    @staticmethod
+    def table():
+        return DocumentType(DocumentType.TABLE)
+
+    @staticmethod
+    def trace():
+        return DocumentType(DocumentType.TRACE)
+
+    @staticmethod
+    def deeptrace():
+        return DocumentType(DocumentType.DEEPTRACE)
+
+    @staticmethod
+    def pdf():
+        return DocumentType(DocumentType.PDF)
+
+    @property
+    def is_document(self):
+        return self.document_type == DocumentType.DOCUMENT
+
+    @property
+    def is_deeptrace(self):
+        return self.document_type == DocumentType.DEEPTRACE
