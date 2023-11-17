0f94fcf432e8c875bfa06fe89b9b26646feae2e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 14:04:26 +0000

    Add package doppkit/0.3.0rc2/doppkit-0.3.0rc2-py3-none-any.whl

---
+++ packages/doppkit/doppkit-0.3.0rc2-py3-none-any.whl/doppkit/gui/cache.py
@@ -0,0 +1,37 @@
+from typing import Iterable, Union, TYPE_CHECKING
+import logging
+
+from ..app import Application
+from ..cache import Content
+from ..cache import cache as cache_generic
+
+if TYPE_CHECKING:
+    from .window import QtProgress
+
+logger = logging.getLogger(__name__)
+
+async def cache(
+        app: Application,
+        urls: Iterable[str],
+        headers: dict[str, str],
+        progress: 'QtProgress'
+) -> list[Union[Content, Exception]]:
+    """
+    Downloads URL contents from GRiD
+
+    Parameters
+    ----------
+    app
+    urls
+        List of URLs to download the contents of
+    headers
+        Header information to relay to the GRiD Server
+    progress
+        Progress Interconnect for doppkit cache function to relay
+        download progress through.
+
+    Returns
+    -------
+
+    """
+    return await cache_generic(app, urls, headers, progress=progress)
