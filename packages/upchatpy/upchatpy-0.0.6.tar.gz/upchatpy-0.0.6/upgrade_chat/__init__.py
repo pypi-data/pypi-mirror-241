4f68d32ee100cd8c9cec72b865760aa028def722
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 04:05:22 +0000

    Add package upchatpy/0.0.6/upchatpy-0.0.6.tar.gz

---
+++ packages/upchatpy/upchatpy-0.0.6.tar.gz/upchatpy-0.0.6/upgrade_chat/__init__.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+
+from .api import Client
+from .exceptions import (APIError, AuthenticationError, HTTPError,
+                         ResourceNotFoundError)
+from .responses.orders import (Coupon, DiscordRole, Order, OrderItem,
+                               OrderResponse, OrdersResponse, OrderUser,
+                               Product)
+from .responses.products import ProductResponse, ProductsResponse
+from .responses.users import User, UsersResponse
+
+__all__ = [
+    "APIError",
+    "AuthenticationError",
+    "HTTPError",
+    "Client",
+    "Coupon",
+    "DiscordRole",
+    "Order",
+    "OrderItem",
+    "OrderResponse",
+    "OrdersResponse",
+    "OrderUser",
+    "Product",
+    "ProductResponse",
+    "ProductsResponse",
+    "ResourceNotFoundError",
+    "User",
+    "UsersResponse",
+]
+__version__ = (Path(__file__).parent.parent / "version.txt").read_text()
