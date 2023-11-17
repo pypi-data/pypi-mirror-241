3f31d2bd2e52823defd47f3c6f09602e8a9588eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 04:05:24 +0000

    Add package grai-client/0.3.5/grai_client-0.3.5-py3-none-any.whl

---
+++ packages/grai-client/grai_client-0.3.5-py3-none-any.whl/grai_client/endpoints/v1/__init__.py
@@ -0,0 +1,1 @@
+from grai_client.endpoints.v1 import client, delete, get, patch, post, url, utils
---
+++ packages/grai-client/grai_client-0.3.5-py3-none-any.whl/grai_client/utilities/tests.py
@@ -0,0 +1,16 @@
+import os
+
+
+def get_test_client():
+    """ """
+    from grai_client.endpoints.v1.client import ClientV1
+
+    host = os.environ.get("GRAI_HOST", "localhost")
+    port = os.environ.get("GRAI_PORT", "8000")
+    username = os.environ.get("GRAI_USERNAME", "null@grai.io")
+    password = os.environ.get("GRAI_PASSWORD", "super_secret")
+    workspace = os.environ.get("GRAI_WORKSPACE", "default")
+
+    client = ClientV1(host, port, workspace=workspace, insecure=True, username=username, password=password)
+
+    return client
