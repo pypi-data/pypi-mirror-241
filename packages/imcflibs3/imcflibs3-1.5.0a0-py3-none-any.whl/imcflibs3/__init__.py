a0e34594a55bad388682780b143325f03a83a3af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 04:05:24 +0000

    Add package imcflibs3/1.5.0a0/imcflibs3-1.5.0a0-py3-none-any.whl

---
+++ packages/imcflibs3/imcflibs3-1.5.0a0-py3-none-any.whl/imcflibs3/__init__.py
@@ -0,0 +1,15 @@
+"""Python convenience from and for the IMCF (Imaging Core Facility).
+
+.. include:: ../../README.md
+
+.. include:: ../../TESTING.md
+
+.. include:: ../../CHANGELOG.md
+"""
+
+__version__ = "1.5.0-a.0"
+
+from . import iotools
+from . import log
+from . import pathtools
+from . import strtools
