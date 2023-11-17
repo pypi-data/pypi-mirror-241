ac7c0c119a488c4a90825a0af416830438343804
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 15:04:54 +0000

    Add package MagicSelection/0.0.17/MagicSelection-0.0.17-py3-none-any.whl

---
+++ packages/MagicSelection/MagicSelection-0.0.17-py3-none-any.whl/MagicSelection/__init__.py
@@ -0,0 +1,6 @@
+from MagicSelection.feature_selection import magic_selection
+from MagicSelection.feature_selection import upsample
+from MagicSelection.feature_selection import get_example_data
+
+if __name__ == "__main__":
+    print()
