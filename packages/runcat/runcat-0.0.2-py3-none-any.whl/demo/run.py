52f861d0ae181e4d5c691a3795f62dc51e29a1f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 16:09:58 +0000

    Add package runcat/0.0.2/runcat-0.0.2-py3-none-any.whl

---
+++ packages/runcat/runcat-0.0.2-py3-none-any.whl/demo/run.py
@@ -0,0 +1,14 @@
+import runcat
+
+
+if __name__ == '__main__':
+    # 执行多个用例文件，主程序入口
+
+    runcat.main(
+        plat="web",
+        path="tests/test_web.py",
+        host='https://app-test.qizhidao.com'
+    )
+
+
+
