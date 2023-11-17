6e39f37de0f0bb5fa9031903fbd00cbe0f6e8f8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-17 05:04:15 +0000

    Add package scbl-utils/1.0.0/scbl_utils-1.0.0-py3-none-any.whl

---
+++ packages/scbl-utils/scbl_utils-1.0.0-py3-none-any.whl/scbl_utils/utils/gdrive.py
@@ -0,0 +1,310 @@
+from pathlib import Path
+
+import gspread as gs
+import pandas as pd
+from numpy import nan
+from rich import print as rprint
+
+from .defaults import SPECIES_TO_GENOME_PATTERN, TRACKING_DF_INDEX_COL
+
+
+def login(*args, **kwargs) -> gs.Client:  # type: ignore
+    """Log into Google Drive and return gspread.Client.
+
+    :raises RuntimeError: If login impossible, raise error
+    :return: The logged in client
+    :rtype: gs.Client
+    """
+    from typer import Abort
+
+    from .defaults import DOCUMENTATION
+
+    try:
+        return gs.service_account(*args, **kwargs)
+    except Exception as e:
+        rprint(
+            f'Could not log into Google Drive. See {DOCUMENTATION} for instructions on authentication. {e}'
+        )
+        Abort()
+
+
+def load_specs(config_files: dict[str, Path]) -> tuple[dict, dict]:
+    """Load specifications from google-drive config directory
+
+    :param config_files: A dict mapping the name of a config file to its path
+    :type config_files: dict[str, Path]
+    :return: The tracking sheet specification and the metrics sheet specification, as a tuple in that order
+    :rtype: tuple[dict, dict]
+    """
+    from yaml import Loader, load
+
+    # Load in the two specification files that instruct script how to
+    # get information from Google Drive
+    specs = {
+        filename: load(path.read_text(), Loader)
+        for filename, path in config_files.items()
+        if 'spec.yml' in filename
+    }
+
+    return specs['trackingsheet-spec.yml'], specs['metricssheet-spec.yml']
+
+
+class GSheet(gs.Spreadsheet):
+    """Inherits from gspread.Spreadsheet, adding a to_df method. Constructor requires same args as gspread.SpreadSheet"""
+
+    def to_df(
+        self, sheet_idx: int = 0, col_renaming: dict[str, str] = {}, header_row: int = 0, index_col: str = TRACKING_DF_INDEX_COL, to_join: bool = True
+    ) -> pd.DataFrame:
+        # Get table and assign data and header row, then convert to df
+        table = self.get_worksheet(sheet_idx).get_values(combine_merged_cells=True)
+        data = table[header_row + 1 :]
+        columns = table[header_row]
+        df = pd.DataFrame(data=data, columns=columns)
+
+        # Filter columns and format
+        cols_to_keep = list(col_renaming.keys() & df.columns)
+        df = df[cols_to_keep].copy()
+        df.rename(columns=col_renaming, inplace=True)
+        df = df.map(lambda s: s.strip(), na_action='ignore')  # type: ignore
+        df.replace({'TRUE': True, 'FALSE': False, '': nan, '-': nan}, inplace=True)
+
+        # Set index and rename
+        df.set_index(index_col, inplace=True)
+        df.index.rename('', inplace=True)
+        
+        if to_join:
+            duplicated = df.index.duplicated()
+            df = df.loc[~duplicated].copy()
+        
+        return df
+
+    # def to_df(
+    #     self, col_renaming: dict[str, str] = {}, lib_pattern: str = LIBRARY_ID_PATTERN
+    # ) -> pd.DataFrame:
+    #     """Get a spreadsheet from Google Drive and convert to pandas.DataFrame
+
+    #     Parameters
+    #     ----------
+    #         :param worksheet_index: The index of the sheet you want to get from the spreadsheet, defaults to 0
+    #         :type worksheet_index: `int`
+    #         :param col_renaming: A mapping between the column names in the Google Sheet and the column names desired in the returned df. Only columns in this dict will be kept, defaults to {}
+    #         :type col_renaming: `dict[str, str]`, optional
+    #         :param col_types:  A mapping between the column names in the df and the type they should be converted to. Note that the keys in this dict should be the values of the col_renaming dict, defaults to {}
+    #         :type col_types: `dict[str, type]`, optional
+    #         :param **kwargs: Keyword arguments to be passed to gspread.WorkSheet.get_all_records.
+
+    #     Returns
+    #     -------
+    #         :return: The requested Google Sheet as a `pandas.DataFrame`
+    #         :rtype: pd.DataFrame
+    #     """
+    #     # Initialize list of dataframes and get all tables in
+    #     # spreadsheet as rows and as columns
+    #     dfs = []
+    #     tables = (
+    #         (
+    #             worksheet.get_values(major_dimension='rows'),
+    #             worksheet.get_values(major_dimension='columns'),
+    #         )
+    #         for worksheet in self.worksheets()
+    #     )
+
+    #     # Some worksheets have the same columns. Initialize a pd.Series
+    #     # tracking nan counts so as to pick the ones with the least
+    #     best_nan_counts = pd.Series({col: inf for col in col_renaming.values()})
+
+    #     # Iterate over table-pairings
+    #     for table_as_rows, table_as_columns in tables:
+    #         cols_in_sheet, header_row_idx, header_row = set(), 0, []
+
+    #         # Iterate over the rows of the table
+    #         for i, row in enumerate(table_as_rows):
+    #             # If this row shares elements with the desired columns
+    #             # then it's the header row.
+    #             cols_in_sheet = col_renaming.keys() & row
+    #             if cols_in_sheet:
+    #                 header_row_idx, header_row = i, row
+    #                 break
+
+    #         # If we've looped over all rows of the table and found
+    #         # no rows that contain at least one of our desired keys,
+    #         # move onto the next worksheet of this spreadsheet
+    #         if not cols_in_sheet:
+    #             continue
+
+    #         # Get a list of the columns that contain values that match
+    #         # the regex of a library ID
+    #         library_col_idxs = [
+    #             i
+    #             for i, col in enumerate(table_as_columns)
+    #             if any(match(pattern=lib_pattern, string=entry) for entry in col)
+    #         ]
+
+    #         # If it's empty, that means that this table doesn't have a
+    #         # column tracking library ID, so its useless to us because
+    #         # we can't align worksheets. # TODO: perhaps make this more
+    #         # dynamic in case one wants to retrieve a spreadsheet that
+    #         # has nothing to with libraries
+    #         if not library_col_idxs:
+    #             continue
+
+    #         # Take the first column that matches the requirement above
+    #         library_col_idx = library_col_idxs[0]
+
+    #         # Construct DataFrame, assuming all data will be after
+    #         # header row
+    #         data = table_as_rows[header_row_idx + 1 :]
+    #         df = pd.DataFrame(data=data, columns=header_row)
+
+    #         # Format df
+    #         df = df.map(lambda value: value.strip() if isinstance(value, str) else value)  # type: ignore
+    #         df.replace({'TRUE': True, 'FALSE': False}, inplace=True)
+
+    #         # The column name to index on will be the element in the
+    #         # header row in the position defined as library_col_idx
+    #         index_col_name = header_row[library_col_idx]
+    #         df.set_index(index_col_name, inplace=True)
+    #         df.index.rename(None, inplace=True)
+
+    #         # Drop rows with duplicate indexes because the
+    #         # tracking sheet has many empty rows
+    #         duplicate_mask = df.index.duplicated()
+    #         df = df.loc[~duplicate_mask]
+
+    #         # Subset to the columns we care about and rename
+    #         to_keep = list(cols_in_sheet - {index_col_name})
+    #         df = df[to_keep]
+    #         df.rename(columns=col_renaming, inplace=True)
+
+    #         # Compare the nan counts, setting to_keep = True for
+    #         # columns in this df that have less nans than the best so
+    #         # far
+    #         nan_counts = df.isna().sum()
+    #         duplicate_cols = df.columns.intersection(best_nan_counts.index)
+    #         to_keep = nan_counts[duplicate_cols] < best_nan_counts[duplicate_cols]
+
+    #         # Get the actual column names to keep and subset df again
+    #         cols_to_keep = to_keep[to_keep].index
+    #         df = df[cols_to_keep]
+
+    #         # Set the best nan counts and append df to dfs list
+    #         best_nan_counts[cols_to_keep] = nan_counts[cols_to_keep]
+    #         dfs.append(df)
+
+    #     # Concatenate the dataframes and return
+    #     return pd.concat(dfs, axis=1)
+
+
+def get_project_params(
+    df_row: pd.Series, metrics_dir_id: str, gclient: gs.Client, species_to_genome_pattern: dict[str, str] = SPECIES_TO_GENOME_PATTERN, **kwargs
+) -> pd.Series:
+    """Use with pandas.DataFrame.apply to get tool version and reference path
+
+    Parameters
+    ----------
+        :param df_row: The passed-in row of the pandas.DataFrame. It should contain the keys "project", "tool", "reference_dir", and "sample_name"
+        :type df_row: `pd.Series`
+        :param metrics_dir_id: The ID of the Google Drive folder containing delivered metrics
+        :type metrics_dir_id: `str`
+
+    Returns
+    -------
+        :return: A `dict` with keys 'tool_version' and 'reference_path'
+        :rtype: `dict[str, str]`
+    """
+    from googleapiclient.discovery import build
+
+    from .samplesheet import genomes_from_user, get_latest_version
+
+    # Get credentials, project, tool, and reference dirs
+    creds = gclient.auth
+    sample_name, project, tool, reference_dirs, species = (
+        df_row[col] for col in ('sample_name', 'project', 'tool', 'reference_dirs', 'species')
+    )
+
+    # Build service
+    service = build(serviceName='drive', version='v3', credentials=creds)
+
+    # Get all files in Google Drive folder matching criteria, sort
+    # by modified time, and get their IDs
+    result = (
+        service.files()
+        .list(
+            corpora='user',
+            q=f"fullText contains '{project}' and fullText contains '{tool}' and mimeType='application/vnd.google-apps.spreadsheet' and '{metrics_dir_id}' in parents",
+            fields='files(id, modifiedTime, mimeType, parents)',
+        )
+        .execute()
+    )
+    metrics_files = result['files']
+    metrics_files.sort(key=lambda file: file['modifiedTime'])
+    spreadsheet_ids = [file['id'] for file in metrics_files]
+    
+    genome_pattern = species_to_genome_pattern.get(species, r'.*')
+    # Iterate over all spreadsheets
+    for id in spreadsheet_ids:
+        try:
+            # Load all worksheets of file into dataframes
+            metricssheet = GSheet(client=gclient, properties={'id': id})
+            metrics_dfs = [
+                metricssheet.to_df(sheet_idx=idx, **kwargs)
+                for idx, _ in enumerate(metricssheet.worksheets())
+            ]
+        except gs.exceptions.APIError:
+            rprint(f'In trying to assign the [blue]tool_version[/] and [blue]reference_path[/] for [bold orange1]{sample_name}[/], the [red]API request-rate limit was exceeded[/]. Defaulting to latest [blue]tool_version[/] and asking you to input [blue]reference_path[/].')
+            break
+
+
+        # The below chunk of code will figure out whether each tab in
+        # in the metrics spreadsheet represents the same information 
+        # about different libraries, or different information about 
+        # the same libraries. # TODO: put in to a function
+
+        # First, figure out whether the columns are the same between 
+        # sheets
+        cols = [col for df in metrics_dfs for col in df.columns]
+        set_cols = set(cols)
+
+        # If they are, then it's the same info but for different
+        # libraries. If not, then it's different info for same
+        # libraries.
+        if len(cols) != len(set_cols):
+            metrics_df = pd.concat(metrics_dfs, axis=0, join='outer')
+        else:
+            metrics_df = pd.concat(metrics_dfs, axis=1, join='outer')
+
+        # metrics_dfs[0].join(other=metrics_dfs[1:], on='libraries', how='outer', rsuffix='1')  # type: ignore
+
+        # Filter metrics_df to contain just those projects matching this
+        # project and tool
+        project_df = metrics_df[
+            (metrics_df['project'] == project) & (metrics_df['tool'] == tool) & (metrics_df['reference'].str.match(genome_pattern, case=False))
+        ].copy()
+
+        if project_df.shape[0] < 1:
+            continue
+
+        # Construct the reference path, then convert it to a str
+        # representation
+        project_df['reference_path'] = project_df['reference'].map(
+            lambda genome: [str(ref_dir / genome) for ref_dir in reference_dirs]
+        )
+
+        # Since all rows should be the same (as they belong to the same
+        # project and have the same tool), return the first row
+        return project_df[['tool_version', 'reference_path']].iloc[0]
+
+    # Either there were no spreadsheets matching the criteria or there
+    # are no instances of the project that share this tool. Just get
+    # the latest tool version and reference genome from the user
+    params = pd.Series()
+    params['tool_version'] = get_latest_version(tool)
+
+    message = f'\nThere are no spreadsheets in the Google Drive folder at https://drive.google.com/drive/folders/{metrics_dir_id} containing the SCBL Project [bold orange1]{project}[/] and the tool [bold orange1]{tool}[/].'
+    params['reference_path'] = genomes_from_user(
+        message=message,
+        reference_dirs=reference_dirs,
+        sample_name=sample_name,
+    )
+
+    return params
---
+++ packages/scbl-utils/scbl_utils-1.0.0-py3-none-any.whl/scbl_utils/utils/samplesheet.py
@@ -0,0 +1,347 @@
+from collections.abc import Collection, Sequence
+from pathlib import Path
+from re import sub, match
+
+from numpy import nan
+import pandas as pd
+from rich import print as rprint
+from typer import Abort
+from yaml import Dumper, SequenceNode, add_representer, dump
+
+from .defaults import (
+    ANTIBODY_LIB_TYPES,
+    LIBRARY_GLOB_PATTERN,
+    PLATFORMS_TO_PROBESET,
+    SAMPLENAME_BLACKLIST_PATTERN,
+    SEP_PATTERN,
+    SIBLING_REPOSITORY,
+    SAMPLESHEET_KEYS,
+    SAMPLESHEET_SORT_KEYS,
+    SAMPLESHEET_GROUP_KEY,
+    VISIUM_DIR
+)
+
+
+def map_libs_to_fastqdirs(
+    fastqdirs: Collection[Path], glob_pattern: str = LIBRARY_GLOB_PATTERN
+) -> dict[str, str]:
+    """Go from a list of fastq dirs to a mapping of library ID to fastq dir
+
+    :param fastq_dirs: list of fastq dirs
+    :type fastq_dirs: list[Path]
+    :param glob_pattern: pattern to glob for in each fastq dir, defaults to LIBRARY_GLOB_PATTERN
+    :type glob_pattern: str, optional
+    :raises FileNotFoundError: If any of the dirs do not contain files matching the glob pattern, raise FileNotFoundError
+    :return: A dict mapping
+    :rtype: dict[str, str]
+    """
+    # Get a mapping between library names and the fastq directories
+    # they're in. Assumes a specific format for filenames
+    lib_to_fastqdir = {
+        path.name.split('_')[0]: str(fastqdir.absolute())
+        for fastqdir in fastqdirs
+        for path in fastqdir.glob(glob_pattern)
+    }
+
+    # Get the directories that do not contain any files matching the
+    # glob pattern. If there are any, raise error
+    input_dirs = {str(fastqdir.absolute()) for fastqdir in fastqdirs}
+    matching_dirs = set(lib_to_fastqdir.values())
+    bad_dirs = '\n'.join(input_dirs - matching_dirs)
+    if bad_dirs:
+        rprint(
+            f'The following directories did not contain any files that match the glob pattern [blue bold]{glob_pattern}[/]:',
+            bad_dirs,
+            sep='\n',
+        )
+        raise Abort()
+
+    # Sort dict before returning
+    sorted_libs = sorted(lib_to_fastqdir.keys())
+    sorted_lib_to_fastqdir = {lib: lib_to_fastqdir[lib] for lib in sorted_libs}
+    return sorted_lib_to_fastqdir
+
+
+# def program_from_types(library_types: tuple[str, ...], lib_types_to_program: dict[tuple[str, ...], tuple[str, str, list[str]]] = LIB_TYPES_TO_PROGRAM) -> dict[str, str]:
+#     sorted_lib_types = sorted(library_types)
+#     sorted_lib_types = tuple(sorted_lib_types)
+
+#     tool_command_refdir = lib_types_to_program.get(library_types)
+
+#     if not tool_command_refdir:
+#         sample_name = sample_df['sample_name'].iloc[0]
+#         rprint(
+#             f'The library types [bold orange1]{library_types}[/] associated with [bold orange1]{sample_name}[/] are not a valid combination. Valid combinations:\n[bold blue]',
+#             *lib_types_to_program.keys(),
+#             sep='\n',
+#         )
+#         raise Abort()
+
+
+# def program_from_lib_types(
+#     sample_df: pd.DataFrame,
+#     ref_parent_dir: Path = REF_PARENT_DIR,
+#     lib_types_to_program: dict[tuple[str, ...], tuple[str, str, list[str]]] = LIB_TYPES_TO_PROGRAM,  # type: ignore
+#     samplesheet_key_to_type: dict[str, type] = SAMPLESHEET_KEY_TO_TYPE,
+# ) -> pd.Series:
+#     """To be used as first argument to `samplesheet_df.groupby('sample_name', as_index=False).apply`. Aggregates `sample_df` (representing one sample), adding information derived from the library types
+
+#     :param sample_df: This dataframe represents one sample, with each row representing a library belonging to that sample. It must contain the column 'library_types'
+#     :type sample_df: `pd.DataFrame`
+#     :param lib_types_to_program: A dict that associates a library type combo to a tool-command-reference_dir combo, defaults to LIB_TYPES_TO_PROGRAM
+#     :type lib_types_to_program: dict[tuple[str, ...], tuple[str, str, Path]
+#     :param samplesheet_key_to_type: A mapping between each samplesheet key and its type, defaults to SAMPLESHEET_KEY_TO_TYPE
+#     :type samplesheet_key_to_type: `dict[str, type]`, optional
+#     :raises ValueError: If the combination of library types for a given sample doesn't exist, raises error
+#     :return: A `dict` that compresses the many rows of `sample_df` into one row, which will be handled by `samplesheet.groupby('sample_name').agg`
+#     :rtype: `pandas.Series`
+#     """
+#     # Initialize the output, a series aggreggating the df
+#     aggregated = pd.Series()
+#     aggregated['libraries'] = tuple(sample_df.index)
+
+#     # Get the tool-command-refdir combo based on library types
+#     # and throw error if not found
+#     library_types = tuple(sample_df['library_types'].sort_values())
+#     tool_command_refdir = lib_types_to_program.get(library_types)
+
+#     if not tool_command_refdir:
+#         sample_name = sample_df['sample_name'].iloc[0]
+#         rprint(
+#             f'The library types [bold orange1]{library_types}[/] associated with [bold orange1]{sample_name}[/] are not a valid combination. Valid combinations:\n[bold blue]',
+#             *lib_types_to_program.keys(),
+#             sep='\n',
+#         )
+#         raise Abort()
+
+#     (
+#         aggregated['tool'],
+#         aggregated['command'],
+#         aggregated['reference_dirs'],
+#     ) = tool_command_refdir
+
+#     # The list of reference dirs contains relative paths, make them
+#     # absolute
+#     aggregated['reference_dirs'] = [
+#         ref_parent_dir / ref_child_dir for ref_child_dir in aggregated['reference_dirs']
+#     ]
+
+#     # Iterate over each column of sample_df and aggregate
+#     for col, series in sample_df.items():
+#         if col == 'n_cells':
+#             aggregated[col] = series.max()
+#         # TODO: think of a better way to check for 10x_platform
+#         elif samplesheet_key_to_type.get(col) == list[str] or series.nunique() > 1 or col == '10x_platform':  # type: ignore
+#             aggregated[col] = tuple(series)
+#         else:
+#             aggregated[col] = series.iloc[0]
+
+#     return aggregated
+
+
+def get_latest_version(
+    tool: str, repository_link: str = SIBLING_REPOSITORY
+) -> (
+    str
+):  # TODO: eventually, create a more easily parsable file in the github repo that has this information.
+    """Get latest version of a given tool
+
+    :param tool: The name of the tool
+    :type tool: `str`
+    :param repository_link: Link to repository containing a table to read information from, defaults to SIBLING_REPOSITORY
+    :type repository_link: `str`, optional
+    :return: The latest version of the tool
+    :rtype: `str`
+    """
+    # Read the table from the README, rename columns, and format tool
+    df = pd.read_html(repository_link)[0]
+    df.rename(columns={col: col.lower() for col in df.columns}, inplace=True)
+    df['tool'] = df['tool'].str.replace(' ', '-').str.lower()
+
+    # Get the index of each row that actually has a tool name because
+    # some rows are blank
+    tool_idxs = df.dropna(subset='tool').index
+
+    # Append the index of the last row, as well as 1 + that index. This
+    # is relevant for the next step
+    tool_idxs = tool_idxs.append(pd.Index([df.index[-1], df.index[-1] + 1]))
+
+    # Pair each tool index with the index of the next row containing a tool name
+    idx_pairings = zip(tool_idxs, tool_idxs[1:])
+
+    # Fill the table so that there are no blank rows
+    for first_row_with_tool, last_row_with_tool in idx_pairings:
+        df.loc[range(first_row_with_tool, last_row_with_tool), 'tool'] = df.loc[first_row_with_tool, 'tool']  # type: ignore
+
+    # Group by tool, get latest version for each tool, and format
+    grouped = df.groupby('tool')
+    latest_versions = grouped['version'].max()
+
+    return latest_versions[tool]
+
+
+def genomes_from_user(
+    message: str, reference_dirs: Collection[Path], sample_name: str
+) -> list[str]:
+    from rich.prompt import Prompt
+
+    # Print a message explaining why the user is being prompted
+    rprint(message, end='\n\n')
+
+    # The reference dirs and libraries are both in order, zip them
+    reference_paths = []
+    for ref_dir in reference_dirs:
+        # Get the genome choices and sort for prettier output
+        genome_choices = [path.name for path in ref_dir.iterdir()]
+        genome_choices.sort()
+
+        # Ask the user which genome they want and construct the full
+        # ref path, appending to output
+        genome = Prompt.ask(
+            f'Choose a genome in [bold green]{ref_dir.absolute()}[/] for [bold orange1]{sample_name}[/] ->',
+            choices=genome_choices,
+        )
+        full_ref_path = str((ref_dir / genome).absolute())
+        reference_paths.append(full_ref_path)
+
+    return reference_paths
+
+
+def get_antibody_tags(
+    library_types: Collection[str], antibody_lib_types: set[str] = ANTIBODY_LIB_TYPES
+) -> tuple[str, ...] | float:
+    if not antibody_lib_types & set(library_types):
+        return nan
+
+    tags_df = pd.read_csv(
+        'https://raw.githubusercontent.com/TheJacksonLaboratory/nf-tenx/main/assets/totalseq-b_universal.csv'
+    )
+
+    return tuple(tags_df['tag_id'])
+
+
+def map_platform_to_probeset(
+    df_row: pd.Series,
+    platform_to_probset: dict[str, dict[str, str]] = PLATFORMS_TO_PROBESET,
+):
+    platforms, species = (df_row[col] for col in ('10x_platform', 'species'))
+    
+    for platform in platforms:
+        probeset_dict = platform_to_probset.get(platform)
+        if not probeset_dict:
+            continue
+        probe_set = probeset_dict.get(species)
+        if probe_set:
+            return probe_set
+
+    return nan
+
+
+def get_visium_info(
+    df_row: pd.Series,
+    visium_dir: Path = VISIUM_DIR,
+) -> pd.Series:
+    library = df_row['libraries'][0]
+    slide, area, lib_types = (df_row[col] for col in ('slide', 'area', 'library_types'))
+
+    new_data = df_row.copy()
+    for lib in (library.upper(), library.lower()):
+        shared_patterns = {
+            'roi_json': rf'{lib}\.json',
+            'image': rf'{lib}\.tiff',
+            'manual_alignment': rf'{lib}[_-]{slide}[_-]{area}\.json',
+        }
+        cytassist_pattern = {'cyta_image': rf'CAV.*{lib}.*\.tif'}
+        lib_type_to_patterns = {
+            ('CytAssist Gene Expression',): shared_patterns | cytassist_pattern,
+            ('Spatial Gene Expression',): shared_patterns,
+        }
+        patterns = lib_type_to_patterns.get(lib_types)
+        if not patterns:
+            return df_row
+
+        paths_with_lib = list(visium_dir.rglob(f'*{lib}*'))
+        for key, pattern in patterns.items():
+            for path in paths_with_lib:
+                if match(pattern=pattern, string=path.name):
+                    new_data[key] = str(path.absolute())
+                    break
+    return new_data
+
+
+def sanitize_samplename(sample_name: str) -> str:
+    legal = sub(pattern=SAMPLENAME_BLACKLIST_PATTERN, repl='', string=sample_name)
+    sep_replaced = sub(pattern=SEP_PATTERN, repl='-', string=legal)
+    return sep_replaced
+
+
+def sequence_representer(dumper: Dumper, data: list | tuple) -> SequenceNode:
+    item = data[0]
+    if isinstance(item, Collection) and not isinstance(item, str):
+        return dumper.represent_list(data)
+    else:
+        return dumper.represent_sequence(
+            tag='tag:yaml.org,2002:seq', sequence=data, flow_style=True
+        )
+    
+
+def get_design(lib: str, multiplexing_df: pd.DataFrame) -> dict[str, dict[str, str]] | float:
+    if lib not in multiplexing_df.index:
+        return nan
+        
+    lib_df = multiplexing_df.loc[lib].copy()
+    design = {tag: {'name': name, 'description': description} for tag, name, description in lib_df[['tag_id', 'sub_sample_name', 'description']].itertuples(index=False)}
+    return design
+
+
+def is_valid(value: Collection[str] | str | int | bool | None | float):
+    if isinstance(value, Collection) and len(value) == 0:
+        return False
+    if value is None:
+        return False
+    if value != value:
+        return False
+    return True
+
+
+def samplesheet_from_df(
+    df: pd.DataFrame,
+    output_cols: Sequence[str] = SAMPLESHEET_KEYS,
+    cols_as_str: Collection[str] = [],
+    sortby: list[str] | str = SAMPLESHEET_SORT_KEYS,
+    groupby: list[str] | str = SAMPLESHEET_GROUP_KEY,
+    delimiter: str = '#' * 80,
+) -> str:
+    # Drop unnecessary columns and sort by defined order
+    to_keep = [col for col in output_cols if col in df.columns]
+    df = df[to_keep].copy()
+
+    # Convert lists to strings if desired and sort values
+    for col in cols_as_str:
+        df[col] = df[col].apply(lambda lst: lst[0] if len(lst) == 1 else lst)
+    df.sort_values(sortby, inplace=True)
+
+    # Add custom representer for sequences
+    for sequence_type in (list, tuple):
+        add_representer(data_type=sequence_type, representer=sequence_representer)
+
+    # Generate output string
+    yml = ''
+    for _, group in df.groupby(groupby):
+        # Convert to records and filter out nans
+        records = group.to_dict(orient='records')
+        records = [
+            {key: value for key, value in rec.items() if is_valid(value)}
+            for rec in records
+        ]
+
+        # Dump and write delimiter between groups
+        yml += dump(
+            data=records,
+            Dumper=Dumper,
+            default_flow_style=False,
+            sort_keys=False,
+        )
+        yml += f'\n{delimiter}\n\n'
+
+    return yml
