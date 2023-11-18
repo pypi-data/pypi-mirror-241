382a21cb4b9d001ca59de8bc610e5d26043d932f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-18 15:04:40 +0000

    Add package ascii-img-stuff/0.2.0/ascii_img_stuff-0.2.0.tar.gz

---
+++ packages/ascii-img-stuff/ascii_img_stuff-0.2.0.tar.gz/ascii_img_stuff-0.2.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+def long_desc():
+    with open('readme.md', 'r') as f:
+        return f.read()
+    
+setup(
+    name='ascii_img_stuff',
+    version='0.2.0',
+    author='hashirkz',
+    author_email='hashirxkhan1@gmail.com',
+    description='python3 cli tool to render *.jpg *.jpeg *.png etc imgs as ascii .txt files',
+    long_description=long_desc(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/ascii_stuff',
+    packages=find_packages('ascii_img_stuff'),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: POSIX',
+    ],
+    entry_points={
+        'console_scripts': [
+            'ascii = ascii_img_stuff.app:main'
+        ],
+    },
+    install_requires=[
+        'numpy==1.23.5',
+        'matplotlib==3.3.0',
+        'scikit-image==0.19.3',
+    ],
+)
