99671399b81c4512bdf3c71c8ef3f5aec49f44a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-18 16:05:00 +0000

    Add package apysc/4.0.63/apysc-4.0.63-py3-none-any.whl

---
+++ packages/apysc/apysc-4.0.63-py3-none-any.whl/apysc/_display/css_text_align.py
@@ -0,0 +1,15 @@
+"""The enum definitions for the text-align CSS property.
+"""
+
+from enum import Enum
+
+
+class CssTextAlign(Enum):
+    """
+    The enum definitions for the text-align CSS property.
+    """
+
+    LEFT = "left"
+    CENTER = "center"
+    RIGHT = "right"
+    JUSTIFY = "justify"
