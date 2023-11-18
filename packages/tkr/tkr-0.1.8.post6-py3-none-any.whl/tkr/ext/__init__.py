badb44bce6c6a7b1e71f437d0c2d5c2235d4bf1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-18 15:04:44 +0000

    Add package tkr/0.1.8.post6/tkr-0.1.8.post6-py3-none-any.whl

---
+++ packages/tkr/tkr-0.1.8.post6-py3-none-any.whl/tkr/ext/__init__.py
@@ -0,0 +1,8 @@
+"""
+The tkr.ext folder contains extension tools for interacting with tkr.
+
+Classes:
+- `tests.Driver`: The github testing folder webdriver for tkr. 
+"""
+
+from . import exttests as tests
