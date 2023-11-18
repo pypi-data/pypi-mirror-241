e1c24788161742df25a0d1263be22bead781bda0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-18 06:07:17 +0000

    Add package windmill-api/1.210.0/windmill_api-1.210.0-py3-none-any.whl

---
+++ packages/windmill-api/windmill_api-1.210.0-py3-none-any.whl/windmill_api/models/edit_auto_invite_json_body.py
@@ -0,0 +1,58 @@
+from typing import Any, Dict, List, Type, TypeVar, Union
+
+from attrs import define as _attrs_define
+from attrs import field as _attrs_field
+
+from ..types import UNSET, Unset
+
+T = TypeVar("T", bound="EditAutoInviteJsonBody")
+
+
+@_attrs_define
+class EditAutoInviteJsonBody:
+    """
+    Attributes:
+        operator (Union[Unset, bool]):
+    """
+
+    operator: Union[Unset, bool] = UNSET
+    additional_properties: Dict[str, Any] = _attrs_field(init=False, factory=dict)
+
+    def to_dict(self) -> Dict[str, Any]:
+        operator = self.operator
+
+        field_dict: Dict[str, Any] = {}
+        field_dict.update(self.additional_properties)
+        field_dict.update({})
+        if operator is not UNSET:
+            field_dict["operator"] = operator
+
+        return field_dict
+
+    @classmethod
+    def from_dict(cls: Type[T], src_dict: Dict[str, Any]) -> T:
+        d = src_dict.copy()
+        operator = d.pop("operator", UNSET)
+
+        edit_auto_invite_json_body = cls(
+            operator=operator,
+        )
+
+        edit_auto_invite_json_body.additional_properties = d
+        return edit_auto_invite_json_body
+
+    @property
+    def additional_keys(self) -> List[str]:
+        return list(self.additional_properties.keys())
+
+    def __getitem__(self, key: str) -> Any:
+        return self.additional_properties[key]
+
+    def __setitem__(self, key: str, value: Any) -> None:
+        self.additional_properties[key] = value
+
+    def __delitem__(self, key: str) -> None:
+        del self.additional_properties[key]
+
+    def __contains__(self, key: str) -> bool:
+        return key in self.additional_properties
