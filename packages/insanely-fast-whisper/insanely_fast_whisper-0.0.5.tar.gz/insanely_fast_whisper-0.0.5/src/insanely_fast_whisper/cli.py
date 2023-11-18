d9a771325d48b9439623b2560b19d1e8d40fd9f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-18 03:04:34 +0000

    Add package insanely-fast-whisper/0.0.5/insanely_fast_whisper-0.0.5.tar.gz

---
+++ packages/insanely-fast-whisper/insanely_fast_whisper-0.0.5.tar.gz/insanely_fast_whisper-0.0.5/src/insanely_fast_whisper/cli.py
@@ -0,0 +1,122 @@
+import json
+
+import argparse
+import torch
+from transformers import pipeline
+from rich.progress import Progress, TimeElapsedColumn, BarColumn
+
+parser = argparse.ArgumentParser(description="Automatic Speech Recognition")
+parser.add_argument(
+    "--file-name",
+    required=True,
+    type=str,
+    help="Path or URL to the audio file to be transcribed.",
+)
+parser.add_argument(
+    "--device-id",
+    required=False,
+    default="0",
+    type=str,
+    help='Device ID for your GPU (just pass the device ID number). (default: "0")',
+)
+parser.add_argument(
+    "--transcript-path",
+    required=False,
+    default="output.json",
+    type=str,
+    help="Path to save the transcription output. (default: output.json)",
+)
+parser.add_argument(
+    "--model-name",
+    required=False,
+    default="openai/whisper-large-v3",
+    type=str,
+    help="Name of the pretrained model/ checkpoint to perform ASR. (default: openai/whisper-large-v3)",
+)
+parser.add_argument(
+    "--task",
+    required=False,
+    default="transcribe",
+    type=str,
+    choices=["transcribe", "translate"],
+    help="Task to perform: transcribe or translate to another language. (default: transcribe)",
+)
+parser.add_argument(
+    "--language",
+    required=False,
+    type=str,
+    default="en",
+    help='Language of the input audio. (default: "en" (English))',
+)
+parser.add_argument(
+    "--batch-size",
+    required=False,
+    type=int,
+    default=24,
+    help="Number of parallel batches you want to compute. Reduce if you face OOMs. (default: 24)",
+)
+parser.add_argument(
+    "--flash",
+    required=False,
+    type=bool,
+    default=False,
+    help="Number of parallel batches you want to compute. Reduce if you face OOMs. (default: 24)",
+)
+parser.add_argument(
+    "--timestamp",
+    required=False,
+    type=str,
+    default="chunk",
+    choices=["chunk", "word"],
+    help="Number of parallel batches you want to compute. Reduce if you face OOMs. (default: 24)",
+)
+
+
+def main():
+    args = parser.parse_args()
+
+    if args.flash == True:
+        pipe = pipeline(
+            "automatic-speech-recognition",
+            model=args.model_name,
+            torch_dtype=torch.float16,
+            device=f"cuda:{args.device_id}",
+            model_kwargs={"use_flash_attention_2": True},
+        )
+    else:
+        pipe = pipeline(
+            "automatic-speech-recognition",
+            model=args.model_name,
+            torch_dtype=torch.float16,
+            device=f"cuda:{args.device_id}",
+        )
+
+        pipe.model = pipe.model.to_bettertransformer()
+
+    if args.timestamp == "word":
+        ts = "word"
+
+    else:
+        ts = True
+
+    with Progress(
+        TextColumn("🤗 [progress.description]{task.description}"),
+        BarColumn(style="yellow1", pulse_style="white"),
+        TimeElapsedColumn(),
+    ) as progress:
+        progress.add_task("[yellow]Transcribing...", total=None)
+
+        outputs = pipe(
+            args.file_name,
+            chunk_length_s=30,
+            batch_size=args.batch_size,
+            generate_kwargs={"task": args.task, "language": args.language},
+            return_timestamps=ts,
+        )
+
+    with open(args.transcript_path, "w", encoding="utf8") as fp:
+        json.dump(outputs, fp, ensure_ascii=False)
+
+    print(
+        f"Voila! Your file has been transcribed go check it out over here! {args.transcript_path}"
+    )
