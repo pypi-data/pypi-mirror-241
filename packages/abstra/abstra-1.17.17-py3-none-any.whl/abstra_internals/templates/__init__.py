422b0273bf04bdd7f657c767a04a10a4ab97f93f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-18 04:05:32 +0000

    Add package abstra/1.17.17/abstra-1.17.17-py3-none-any.whl

---
+++ packages/abstra/abstra-1.17.17-py3-none-any.whl/abstra_internals/templates/__init__.py
@@ -0,0 +1,21 @@
+import pathlib, json
+from pkg_resources import get_distribution
+
+abstraignore = (pathlib.Path(__file__).parent / "abstraignore").read_text()
+new_form_code = (pathlib.Path(__file__).parent / "new_form.py").read_text()
+new_hook_code = (pathlib.Path(__file__).parent / "new_hook.py").read_text()
+new_job_code = (pathlib.Path(__file__).parent / "new_job.py").read_text()
+new_dash_code = (pathlib.Path(__file__).parent / "new_dash.py").read_text()
+new_script_code = (pathlib.Path(__file__).parent / "new_script.py").read_text()
+new_dash_layout = json.load(
+    (pathlib.Path(__file__).parent / "new_dash_layout.json").open()
+)
+vscode_launch_json = json.load(
+    (pathlib.Path(__file__).parent / "vscode_launch.json").open()
+)
+
+
+def ensure_abstraignore(dir: pathlib.Path):
+    path = dir / ".abstraignore"
+    if not path.exists():
+        path.write_text(abstraignore, encoding="utf-8")
