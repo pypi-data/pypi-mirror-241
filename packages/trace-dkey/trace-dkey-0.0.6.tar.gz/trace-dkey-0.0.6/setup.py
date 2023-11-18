040ad951aadf0c77db8db681181e1ee07e9f1174
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-18 03:05:02 +0000

    Add package trace-dkey/0.0.6/trace-dkey-0.0.6.tar.gz

---
+++ packages/trace-dkey/trace-dkey-0.0.6.tar.gz/trace-dkey-0.0.6/setup.py
@@ -0,0 +1,35 @@
+import os
+
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="trace-dkey",
+    author="Prince Roshan",
+    version='0.0.6',
+    author_email="princekrroshan01@gmail.com",
+    url="https://github.com/Agent-Hellboy/trace-dkey",
+    description=("Python library to trace path of a particular key inside a nested dict"),
+    long_description=read("README.rst"),
+    license="MIT",
+    package_dir={'': 'src'},
+    packages=['trace_dkey'],
+    keywords=[
+        "tracer","dict-key-path-finder"
+    ],
+    install_requires=["click"],
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+)
