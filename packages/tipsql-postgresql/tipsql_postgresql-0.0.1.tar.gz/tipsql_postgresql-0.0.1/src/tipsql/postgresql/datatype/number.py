b5ea41abb3a357b1c3acf9bad7949297b56cab44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-18 15:04:53 +0000

    Add package tipsql-postgresql/0.0.1/tipsql_postgresql-0.0.1.tar.gz

---
+++ packages/tipsql-postgresql/tipsql_postgresql-0.0.1.tar.gz/tipsql_postgresql-0.0.1/src/tipsql/postgresql/datatype/number.py
@@ -0,0 +1,13 @@
+from tipsql.core.operator.eq_operator import EqOperator
+from tipsql.core.operator.not_eq_operator import NotEqOperator
+from tipsql.core.operator.order_operator import OrderOperator
+from tipsql.core.relation.column import ColumnType
+
+
+class Bigint[Precision: int, Scale: int](
+    ColumnType[int],
+    EqOperator[int, int],
+    OrderOperator[int, int],
+    NotEqOperator[int, int],
+):
+    __slots__ = ()
