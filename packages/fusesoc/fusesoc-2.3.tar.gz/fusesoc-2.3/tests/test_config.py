5029477ebf088cd36f460ab7f0910d64802a0be6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-18 03:05:02 +0000

    Add package fusesoc/2.3/fusesoc-2.3.tar.gz

---
+++ packages/fusesoc/fusesoc-2.3.tar.gz/fusesoc-2.3/tests/test_config.py
@@ -0,0 +1,108 @@
+# Copyright FuseSoC contributors
+# Licensed under the 2-Clause BSD License, see LICENSE for details.
+# SPDX-License-Identifier: BSD-2-Clause
+
+import os
+import os.path
+import tempfile
+
+from test_common import cache_root, cores_root, library_root
+
+from fusesoc.config import Config
+
+build_root = "test_build_root"
+
+EXAMPLE_CONFIG = """
+[main]
+build_root = {build_root}
+cache_root = {cache_root}
+cores_root = {cores_root}
+library_root = {library_root}
+
+[library.test_lib]
+location = {library_root}/test_lib
+auto-sync = false
+sync-uri = https://github.com/fusesoc/fusesoc-cores
+"""
+
+
+def test_config():
+    tcf = tempfile.NamedTemporaryFile(mode="w+")
+    tcf.write(
+        EXAMPLE_CONFIG.format(
+            build_root=build_root,
+            cache_root=cache_root,
+            cores_root=cores_root,
+            library_root=library_root,
+        )
+    )
+    tcf.seek(0)
+
+    conf = Config(tcf.name)
+
+    assert conf.library_root == library_root
+
+
+def test_config_relative_path():
+    with tempfile.TemporaryDirectory() as td:
+        config_path = os.path.join(td, "fusesoc.conf")
+        with open(config_path, "w") as tcf:
+            tcf.write(
+                EXAMPLE_CONFIG.format(
+                    build_root="build_root",
+                    cache_root="cache_root",
+                    cores_root="cores_root",
+                    library_root="library_root",
+                )
+            )
+
+        conf = Config(tcf.name)
+        for name in ["build_root", "cache_root", "library_root"]:
+            abs_td = os.path.abspath(td)
+            assert getattr(conf, name) == os.path.join(abs_td, name)
+
+
+def test_config_libraries():
+    tcf = tempfile.NamedTemporaryFile(mode="w+")
+    tcf.write(
+        EXAMPLE_CONFIG.format(
+            build_root=build_root,
+            cache_root=cache_root,
+            cores_root=cores_root,
+            library_root=library_root,
+        )
+    )
+    tcf.seek(0)
+
+    conf = Config(tcf.name)
+
+    lib = None
+    for library in conf.libraries:
+        if library.name == "test_lib":
+            lib = library
+    assert lib
+
+    assert lib.location == os.path.join(library_root, "test_lib")
+    assert lib.sync_uri == "https://github.com/fusesoc/fusesoc-cores"
+    assert not lib.auto_sync
+
+
+def test_config_write():
+    tcf = tempfile.NamedTemporaryFile(mode="w+", delete=False)
+    tcf.write(
+        EXAMPLE_CONFIG.format(
+            build_root=build_root,
+            cache_root=cache_root,
+            cores_root=cores_root,
+            library_root=library_root,
+        )
+    )
+    tcf.flush()
+
+    with Config(tcf.name) as c:
+        c.build_root = "/tmp"
+
+    conf = Config(tcf.name)
+
+    assert conf.build_root == "/tmp"
+    os.remove(tcf.name)
---
+++ packages/fusesoc/fusesoc-2.3.tar.gz/fusesoc-2.3/tests/test_edalizer.py
@@ -0,0 +1,149 @@
+# Copyright FuseSoC contributors
+# Licensed under the 2-Clause BSD License, see LICENSE for details.
+# SPDX-License-Identifier: BSD-2-Clause
+
+
+def test_tool_or_flow():
+    import os
+
+    from fusesoc.config import Config
+    from fusesoc.coremanager import CoreManager
+    from fusesoc.edalizer import Edalizer
+    from fusesoc.librarymanager import Library
+    from fusesoc.vlnv import Vlnv
+
+    tests_dir = os.path.dirname(__file__)
+    cores_dir = os.path.join(tests_dir, "capi2_cores", "misc")
+
+    lib = Library("edalizer", cores_dir)
+
+    cm = CoreManager(Config())
+    cm.add_library(lib, [])
+
+    core = cm.get_core(Vlnv("::flow"))
+
+    ref_edam = {
+        "version": "0.2.1",
+        "dependencies": {"::flow:0": []},
+        "files": [],
+        "hooks": {},
+        "name": "flow_0",
+        "parameters": {},
+        "tool_options": {},
+        "toplevel": "unused",
+        "vpi": [],
+        "flow_options": {},
+    }
+
+    edam = Edalizer(
+        toplevel=core.name,
+        flags={"target": "nothing"},
+        core_manager=cm,
+        work_root=".",
+    ).run()
+    assert edam == ref_edam
+
+    edam = Edalizer(
+        toplevel=core.name,
+        flags={"target": "flowonly"},
+        core_manager=cm,
+        work_root=".",
+    ).run()
+    assert edam == ref_edam
+
+    edam = Edalizer(
+        toplevel=core.name,
+        flags={"target": "emptyflowoptions"},
+        core_manager=cm,
+        work_root=".",
+    ).run()
+    assert edam == ref_edam
+
+    edam = Edalizer(
+        toplevel=core.name,
+        flags={"target": "toolonly"},
+        core_manager=cm,
+        work_root=".",
+    ).run()
+    assert edam == ref_edam
+
+    edam = Edalizer(
+        toplevel=core.name,
+        flags={"target": "flowandtool"},
+        core_manager=cm,
+        work_root=".",
+    ).run()
+    assert edam == ref_edam
+
+    edam = Edalizer(
+        toplevel=core.name,
+        flags={"target": "flowoptions"},
+        core_manager=cm,
+        work_root=".",
+    ).run()
+
+    ref_edam["flow_options"] = {
+        "tool1": {"someoption": "somevalue"},
+        "tool2": {"otheroption": ["detroit", 442]},
+    }
+    assert edam == ref_edam
+
+
+def test_generators():
+    import os
+    import shutil
+    import tempfile
+
+    from fusesoc.config import Config
+    from fusesoc.coremanager import CoreManager
+    from fusesoc.edalizer import Edalizer
+    from fusesoc.librarymanager import Library
+    from fusesoc.vlnv import Vlnv
+
+    tests_dir = os.path.dirname(__file__)
+    cores_dir = os.path.join(tests_dir, "capi2_cores", "misc", "generate")
+
+    lib = Library("edalizer", cores_dir)
+
+    cm = CoreManager(Config())
+    cm.add_library(lib, [])
+
+    core = cm.get_core(Vlnv("::generate"))
+
+    build_root = tempfile.mkdtemp(prefix="export_")
+    export_root = os.path.join(build_root, "exported_files")
+
+    edalizer = Edalizer(
+        toplevel=core.name,
+        flags={"tool": "icarus"},
+        core_manager=cm,
+        work_root=os.path.join(build_root, "work"),
+        export_root=export_root,
+        system_name=None,
+    )
+    edalizer.run()
+    edalizer.export()
+
+    name_to_core = {str(core.name): core for core in edalizer.cores}
+    for flavour in ["testgenerate_with_params", "testgenerate_without_params"]:
+        core_name = f"::generate-{flavour}:0"
+        assert core_name in name_to_core
+        core = name_to_core[core_name]
+
+        # ttptttg temporary directory should be removed by now
+        assert not os.path.isdir(core.core_root)
+
+    # Test generator input cache and file_input_params
+    core_name = f"::generate-testgenerate_with_cache:0"
+    assert core_name in name_to_core
+    core = name_to_core[core_name]
+    assert os.path.isdir(core.core_root)
+
+    hash = ""
+    with open(os.path.join(core.core_root, ".fusesoc_file_input_hash")) as f:
+        hash = f.read()
+
+    # SHA256 hash of test file content
+    assert hash == "da265f9dccc9d9e64d059f677508f9550b403c99e6ce5df07c6fb1d711d0ee99"
+
+    shutil.rmtree(core.core_root, ignore_errors=True)
