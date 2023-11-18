f568959af3fd304131e658610c6b45d25793f46e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-18 15:05:01 +0000

    Add package RailsStringMethods/0.4/RailsStringMethods-0.4.tar.gz

---
+++ packages/RailsStringMethods/RailsStringMethods-0.4.tar.gz/RailsStringMethods-0.4/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+setup(
+  name = 'RailsStringMethods',
+  packages = [],
+  version = '0.4',
+  license='MIT',
+  description = 'A library that allows Python devs to mimic the useful string methods available in Ruby on Rails.',
+  author = 'KayLa Thomas',
+  author_email = 'kaylathomas.dev@gmail.com',
+  url = 'https://github.com/kaylathomas/RailsStringMethods',
+  download_url = 'https://github.com/kaylathomas/RailsStringMethods/archive/refs/tags/v_0.4.tar.gz',
+  keywords = ['ruby on rails', 'ruby', 'python', 'string library', 'strings'],
+  install_requires=[
+      'inflect'
+  ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
