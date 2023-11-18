34a14b6dc9fbd282db049a67b65bf6db92c150bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-18 04:05:23 +0000

    Add package powerCS/0.1/powerCS-0.1.tar.gz

---
+++ packages/powerCS/powerCS-0.1.tar.gz/powerCS-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="powerCS",
+    version="0.1",
+    packages=find_packages(),
+    # Add more metadata as needed:
+    author="Fotis Tsakiroglou",
+    author_email="tsakirogf@hotmail.com",
+    description="A library to estimate system power consumption in a period of time (CPU, GPU, Memory)",
+    keywords="utility machine learning power consumption",
+    url="https://github.com/tbd",
+    # More configurations can be added here
+)
