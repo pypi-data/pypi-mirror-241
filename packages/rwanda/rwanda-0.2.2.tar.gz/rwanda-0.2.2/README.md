95580610fbb60b1768bc278ff4c0692faa0c6410
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-18 05:04:46 +0000

    Add package rwanda/0.2.2/rwanda-0.2.2.tar.gz

---
+++ packages/rwanda/rwanda-0.2.2.tar.gz/rwanda-0.2.2/README.md
@@ -0,0 +1,6 @@
+
+# Rwanda
+
+
+Repo is distributed under the MIT license. Please see the `LICENSE` for more information.
+
---
+++ packages/rwanda/rwanda-0.2.2.tar.gz/rwanda-0.2.2/rwanda/locations/__init__.py
@@ -0,0 +1,17 @@
+import json
+import os
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+my_file = os.path.join(THIS_FOLDER, 'locations.json')
+
+
+class Rwanda():
+
+    def __init__(self, province_name=None):
+        with open(my_file, 'r') as fp:
+            data = json.load(fp)
+        self.datas = data
+        self.provinces_names = [province['name']
+                                for province in self.datas['provinces']]
+
+    def provinces(self):
+        return self.provinces_names
