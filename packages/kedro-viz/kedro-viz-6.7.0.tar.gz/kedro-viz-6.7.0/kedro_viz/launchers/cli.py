3bb9d11a1f04396ff7b0e007514739515ad8de31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-18 03:05:17 +0000

    Add package kedro-viz/6.7.0/kedro-viz-6.7.0.tar.gz

---
+++ packages/kedro-viz/kedro-viz-6.7.0.tar.gz/kedro-viz-6.7.0/kedro_viz/launchers/cli.py
@@ -0,0 +1,175 @@
+"""`kedro_viz.launchers.cli` launches the viz server as a CLI app."""
+
+import multiprocessing
+import traceback
+from pathlib import Path
+from typing import Dict
+
+import click
+from kedro.framework.cli.project import PARAMS_ARG_HELP
+from kedro.framework.cli.utils import KedroCliError, _split_params
+from packaging.version import parse
+from watchgod import RegExpWatcher, run_process
+
+from kedro_viz import __version__
+from kedro_viz.constants import DEFAULT_HOST, DEFAULT_PORT
+from kedro_viz.integrations.pypi import get_latest_version, is_running_outdated_version
+from kedro_viz.launchers.utils import _check_viz_up, _start_browser, _wait_for
+
+_VIZ_PROCESSES: Dict[str, int] = {}
+
+
+@click.group(name="Kedro-Viz")
+def commands():  # pylint: disable=missing-function-docstring
+    pass
+
+
+@commands.command(context_settings={"help_option_names": ["-h", "--help"]})
+@click.option(
+    "--host",
+    default=DEFAULT_HOST,
+    help="Host that viz will listen to. Defaults to localhost.",
+)
+@click.option(
+    "--port",
+    default=DEFAULT_PORT,
+    type=int,
+    help="TCP port that viz will listen to. Defaults to 4141.",
+)
+@click.option(
+    "--browser/--no-browser",
+    default=True,
+    help="Whether to open viz interface in the default browser or not. "
+    "Browser will only be opened if host is localhost. Defaults to True.",
+)
+@click.option(
+    "--load-file",
+    default=None,
+    help="Load Kedro-Viz using JSON files from the specified directory.",
+)
+@click.option(
+    "--save-file",
+    default=None,
+    type=click.Path(dir_okay=False, writable=True),
+    help="Save all API responses from the backend as JSON files in the specified directory.",
+)
+@click.option(
+    "--pipeline",
+    type=str,
+    default=None,
+    help="Name of the registered pipeline to visualise. "
+    "If not set, the default pipeline is visualised",
+)
+@click.option(
+    "--env",
+    "-e",
+    type=str,
+    default=None,
+    multiple=False,
+    envvar="KEDRO_ENV",
+    help="Kedro configuration environment. If not specified, "
+    "catalog config in `local` will be used",
+)
+@click.option(
+    "--autoreload",
+    "-a",
+    is_flag=True,
+    help="Autoreload viz server when a Python or YAML file change in the Kedro project",
+)
+@click.option(
+    "--ignore-plugins",
+    is_flag=True,
+    help="A flag to ignore all installed plugins in the Kedro Project",
+)
+@click.option(
+    "--params",
+    type=click.UNPROCESSED,
+    default="",
+    help=PARAMS_ARG_HELP,
+    callback=_split_params,
+)
+# pylint: disable=import-outside-toplevel, too-many-locals
+def viz(
+    host,
+    port,
+    browser,
+    load_file,
+    save_file,
+    pipeline,
+    env,
+    autoreload,
+    ignore_plugins,
+    params,
+):
+    """Visualise a Kedro pipeline using Kedro viz."""
+    from kedro_viz.server import run_server
+
+    installed_version = parse(__version__)
+    latest_version = get_latest_version()
+    if is_running_outdated_version(installed_version, latest_version):
+        click.echo(
+            click.style(
+                "WARNING: You are using an old version of Kedro Viz. "
+                f"You are using version {installed_version}; "
+                f"however, version {latest_version} is now available.\n"
+                "You should consider upgrading via the `pip install -U kedro-viz` command.\n"
+                "You can view the complete changelog at "
+                "https://github.com/kedro-org/kedro-viz/releases.",
+                fg="yellow",
+            ),
+        )
+
+    click.echo(
+        click.style(
+            "WARNING: The `kedro viz` command will be deprecated with the release of "
+            "Kedro-Viz 7.0.0. `kedro viz run` will be the new way to run the tool.",
+            fg="yellow",
+        ),
+    )
+
+    try:
+        if port in _VIZ_PROCESSES and _VIZ_PROCESSES[port].is_alive():
+            _VIZ_PROCESSES[port].terminate()
+
+        run_server_kwargs = {
+            "host": host,
+            "port": port,
+            "load_file": load_file,
+            "save_file": save_file,
+            "pipeline_name": pipeline,
+            "env": env,
+            "autoreload": autoreload,
+            "ignore_plugins": ignore_plugins,
+            "extra_params": params,
+        }
+        if autoreload:
+            project_path = Path.cwd()
+            run_server_kwargs["project_path"] = project_path
+            run_process_kwargs = {
+                "path": project_path,
+                "target": run_server,
+                "kwargs": run_server_kwargs,
+                "watcher_cls": RegExpWatcher,
+                "watcher_kwargs": {"re_files": r"^.*(\.yml|\.yaml|\.py|\.json)$"},
+            }
+            viz_process = multiprocessing.Process(
+                target=run_process, daemon=False, kwargs={**run_process_kwargs}
+            )
+        else:
+            viz_process = multiprocessing.Process(
+                target=run_server, daemon=False, kwargs={**run_server_kwargs}
+            )
+
+        viz_process.start()
+        _VIZ_PROCESSES[port] = viz_process
+
+        _wait_for(func=_check_viz_up, host=host, port=port)
+
+        print("Kedro Viz Backend Server started successfully...")
+
+        if browser:
+            _start_browser(host, port)
+
+    except Exception as ex:  # pragma: no cover
+        traceback.print_exc()
+        raise KedroCliError(str(ex)) from ex
