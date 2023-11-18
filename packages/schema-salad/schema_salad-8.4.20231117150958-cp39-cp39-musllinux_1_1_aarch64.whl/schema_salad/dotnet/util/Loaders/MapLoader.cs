a1f0dfa59274db4086df48c674982aa5fc47f9de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-18 04:05:23 +0000

    Add package schema-salad/8.4.20231117150958/schema_salad-8.4.20231117150958-cp39-cp39-musllinux_1_1_aarch64.whl

---
+++ packages/schema-salad/schema_salad-8.4.20231117150958-cp39-cp39-musllinux_1_1_aarch64.whl/schema_salad/dotnet/util/Loaders/MapLoader.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+
+namespace ${project_name};
+
+internal class MapLoader<T> : ILoader<Dictionary<string, T>>
+{
+    private readonly ILoader valueLoader;
+
+    public MapLoader(in ILoader valueLoader)
+    {
+        this.valueLoader = valueLoader;
+    }
+
+    public Dictionary<string, T> Load(in object doc, in string baseuri, in LoadingOptions loadingOptions, in string? docRoot = null)
+    {
+        if (doc == null)
+        {
+            throw new ValidationException("Expected non null");
+        }
+
+        if (doc is not IDictionary)
+        {
+            throw new ValidationException("Expected list");
+        }
+
+        IDictionary docDictionary = (IDictionary)doc;
+        Dictionary<string, T> returnValue = new();
+        List<ILoader> loaders = new()
+        {
+            this,
+            valueLoader
+        };
+        ILoader<object> unionLoader = new UnionLoader(loaders);
+        List<ValidationException> errors = new();
+
+        foreach (KeyValuePair<string, T> item in docDictionary)
+        {
+            try
+            {
+                dynamic loadedField = unionLoader.LoadField(item.Value, baseuri, loadingOptions);
+                returnValue[item.Key] = loadedField;
+            }
+            catch (ValidationException e)
+            {
+                errors.Add(e);
+            }
+        }
+
+        if (errors.Count > 0)
+        {
+            throw new ValidationException("", errors);
+        }
+
+        return returnValue;
+    }
+
+    object ILoader.Load(in object doc, in string baseuri, in LoadingOptions loadingOptions, in string? docRoot)
+    {
+        return Load(doc, baseuri, loadingOptions, docRoot);
+    }
+}
