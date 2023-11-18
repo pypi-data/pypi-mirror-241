8fac73bfca7746190929c567b3bc0f80c368d88c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-18 05:05:04 +0000

    Add package nodestream/0.8.19/nodestream-0.8.19-py3-none-any.whl

---
+++ packages/nodestream/nodestream-0.8.19-py3-none-any.whl/nodestream/databases/query_executor.py
@@ -0,0 +1,59 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import Iterable
+
+from ..model import (
+    IngestionHook,
+    MatchStrategy,
+    Node,
+    NodeIdentityShape,
+    RelationshipIdentityShape,
+    RelationshipWithNodes,
+    TimeToLiveConfiguration,
+)
+from ..schema.indexes import FieldIndex, KeyIndex
+
+
+@dataclass(slots=True, frozen=True)
+class OperationOnNodeIdentity:
+    node_identity: NodeIdentityShape
+    match_strategy: MatchStrategy
+
+
+@dataclass(slots=True, frozen=True)
+class OperationOnRelationshipIdentity:
+    from_node: OperationOnNodeIdentity
+    to_node: OperationOnNodeIdentity
+    relationship_identity: RelationshipIdentityShape
+
+
+class QueryExecutor(ABC):
+    @abstractmethod
+    async def upsert_nodes_in_bulk_with_same_operation(
+        self, operation: OperationOnNodeIdentity, nodes: Iterable[Node]
+    ):
+        raise NotImplementedError
+
+    @abstractmethod
+    async def upsert_relationships_in_bulk_of_same_operation(
+        self,
+        shape: OperationOnRelationshipIdentity,
+        relationships: Iterable[RelationshipWithNodes],
+    ):
+        raise NotImplementedError
+
+    @abstractmethod
+    async def upsert_key_index(self, index: KeyIndex):
+        raise NotImplementedError
+
+    @abstractmethod
+    async def upsert_field_index(self, index: FieldIndex):
+        raise NotImplementedError
+
+    @abstractmethod
+    async def perform_ttl_op(self, config: TimeToLiveConfiguration):
+        raise NotImplementedError
+
+    @abstractmethod
+    async def execute_hook(self, hook: IngestionHook):
+        raise NotImplementedError
