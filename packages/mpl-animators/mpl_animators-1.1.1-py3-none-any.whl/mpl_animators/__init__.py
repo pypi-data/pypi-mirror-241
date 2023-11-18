f3e9a71bd90f0e968b6f95e4519e8532085e3302
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-18 06:08:28 +0000

    Add package mpl-animators/1.1.1/mpl_animators-1.1.1-py3-none-any.whl

---
+++ packages/mpl-animators/mpl_animators-1.1.1-py3-none-any.whl/mpl_animators/__init__.py
@@ -0,0 +1,7 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+from mpl_animators.base import *
+from mpl_animators.image import *
+from mpl_animators.line import *
+from mpl_animators.wcs import *
+
+from .version import __version__
