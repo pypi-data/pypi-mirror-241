bf250a6ab24198908afadbe21c242f7c599d9472
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-18 06:07:51 +0000

    Add package mudderpy/0.1.4/mudderpy-0.1.4.tar.gz

---
+++ packages/mudderpy/mudderpy-0.1.4.tar.gz/mudderpy-0.1.4/mudder/src/lib.rs
@@ -0,0 +1,679 @@
+use num_bigint::BigInt;
+use num_traits::cast::ToPrimitive;
+use std::cmp::Ordering;
+use std::collections::HashMap;
+use std::num::NonZeroUsize;
+
+pub fn long_div(numerator: Vec<usize>, denominator: usize, base: usize) -> (Vec<usize>, usize) {
+    let mut result = Vec::new();
+    let mut remainder = 0;
+
+    for current in numerator {
+        // Calculate the temporary value for the current iteration
+        let temp_value = current + remainder * base;
+
+        // Perform the division and store the result
+        result.push(temp_value / denominator);
+
+        // Calculate the remainder for the current iteration
+        remainder = temp_value % denominator;
+    }
+
+    // Return the result and the remainder
+    (result, remainder)
+}
+
+#[derive(Debug)]
+pub struct DivisionResult {
+    result: Vec<usize>,
+    remainder: usize,
+    denominator: usize,
+}
+
+fn long_sub_same_len(
+    a: &Vec<usize>,
+    b: &Vec<usize>,
+    base: usize,
+    remainder: Option<(usize, usize)>,
+    denominator: usize,
+) -> Result<(Vec<usize>, usize), &'static str> {
+    if a.len() != b.len() {
+        return Err("a and b should have same length");
+    }
+
+    let mut a = a.clone();
+    let mut b = b.clone();
+
+    if let Some(rem) = remainder {
+        a.push(rem.0);
+        b.push(rem.1);
+    }
+
+    let mut ret = vec![0; a.len()];
+
+    for i in (0..a.len()).rev() {
+        if a[i] >= b[i] {
+            ret[i] = a[i] - b[i];
+            continue;
+        }
+        if i == 0 {
+            return Err("Cannot go negative");
+        }
+        let mut do_break = false;
+        // look for a digit to the left to borrow from
+        for j in (0..i).rev() {
+            if a[j] > 0 {
+                // found a non-zero digit. Decrement it
+                a[j] -= 1;
+                // increment all digits to the right by `base-1`
+                for k in j + 1..i {
+                    a[k] += base - 1;
+                }
+                // until you reach the digit you couldn't subtract
+                ret[i] =
+                    a[i] + if remainder.is_some() && i == a.len() - 1 {
+                        denominator
+                    } else {
+                        base
+                    } - b[i];
+                do_break = true;
+                break;
+            }
+        }
+        if do_break {
+            continue;
+        }
+        return Err("Failed to find digit to borrow from");
+    }
+    if remainder.is_some() {
+        // result, remainder
+        Ok((ret[..ret.len() - 1].to_vec(), ret[ret.len() - 1]))
+    } else {
+        Ok((ret, 0))
+    }
+}
+
+pub fn long_add_same_len(
+    a: &Vec<usize>,
+    b: &Vec<usize>,
+    base: usize,
+    remainder: usize,
+    denominator: usize,
+) -> Result<DivisionResult, &'static str> {
+    if a.len() != b.len() {
+        return Err("a and b should have same length");
+    }
+
+    let mut _remainder = remainder;
+
+    let mut carry = remainder >= denominator;
+    if carry {
+        _remainder -= denominator;
+    }
+
+    let mut res = b.clone();
+
+    for (i, ai) in a.iter().enumerate().rev() {
+        let result = ai + b[i] + carry as usize;
+        carry = result >= base;
+        res[i] = if carry { result - base } else { result };
+    }
+
+    Ok(DivisionResult {
+        result: res,
+        remainder: _remainder,
+        denominator,
+    })
+}
+
+pub fn pad_right(vector: &mut Vec<usize>, to_length: usize, pad_value: usize) {
+    if to_length > vector.len() {
+        vector.resize(to_length, pad_value);
+    }
+}
+
+pub fn pad_left(arr: Vec<usize>, to_length: usize, val: usize) -> Vec<usize> {
+    // Calculate the number of padding elements needed
+    let pad_len = to_length.saturating_sub(arr.len());
+
+    // Create a new vector from the original array
+    let mut result = arr;
+
+    // If padding is needed, insert the padding at the start of the array
+    if pad_len > 0 {
+        result.splice(0..0, vec![val; pad_len]);
+    }
+
+    result
+}
+
+pub fn long_linspace(
+    a: Vec<usize>,
+    b: Vec<usize>,
+    base: usize,
+    n: NonZeroUsize,
+    m: usize,
+) -> Result<Vec<DivisionResult>, &'static str> {
+    let mut a = a.clone();
+    let mut b = b.clone();
+
+    if a.len() < b.len() {
+        pad_right(&mut a, b.len(), 0);
+    } else if b.len() < a.len() {
+        pad_right(&mut b, a.len(), 0);
+    }
+
+    if a == b {
+        return Err("Start and end strings are lexicographically inseperable");
+    }
+    let (a_div, a_div_rem) = long_div(a.clone(), m, base);
+    let (b_div, b_div_rem) = long_div(b.clone(), m, base);
+
+    let (mut a_prev, mut a_prev_rem) =
+        long_sub_same_len(&a, &a_div, base, Some((0, a_div_rem)), m)?;
+    let (mut b_prev, mut b_prev_rem) = (b_div.clone(), b_div_rem);
+
+    let mut ret = Vec::new();
+    for _i in 1..=(n.into()) {
+        let result = long_add_same_len(&a_prev, &b_prev, base, a_prev_rem + b_prev_rem, m)?;
+        ret.push(result);
+
+        let (a_prev_temp, a_prev_rem_temp) =
+            long_sub_same_len(&a_prev, &a_div, base, Some((a_prev_rem, a_div_rem)), m)?;
+        a_prev = a_prev_temp;
+        a_prev_rem = a_prev_rem_temp;
+
+        let b_temp = long_add_same_len(&b_prev, &b_div, base, b_prev_rem + b_div_rem, m)?;
+        b_prev = b_temp.result;
+        b_prev_rem = b_temp.remainder;
+    }
+    Ok(ret)
+}
+
+/// Returns a slice of `water` up to the first index `i` where `water[i]` is not zero and
+/// `water[i]` is not equal to `rock[i]`. If no such index exists, returns a copy of `water`.
+pub fn chop_digits(rock: &[usize], water: &[usize]) -> Vec<usize> {
+    // Iterate over the indices and values of `water`
+    for (i, &value) in water.iter().enumerate() {
+        // If `value` is not zero and `value` is not equal to the corresponding element in `rock`
+        if let Some(&rock_value) = rock.get(i) {
+            if value != 0 && value != rock_value {
+                // Return a slice of `water` up to and including the current index
+                return water[..=i].to_vec();
+            }
+        } else if value != 0 {
+            // If `i` exceeds the length of `rock` and `value` is not zero
+            // Return a slice of `water` up to and including the current index
+            return water[..=i].to_vec();
+        }
+    }
+    // If none of the elements in `water` meet the conditions, return a copy of `water`
+    water.to_vec()
+}
+
+/// Compare two arrays lexicographically.
+///
+/// Returns true if `a` is lexicographically less than `b`.
+pub fn lexicographic_less_than_array(a: &[usize], b: &[usize]) -> bool {
+    // Iterate over both arrays simultaneously
+    for (&x, &y) in a.iter().zip(b.iter()) {
+        match x.cmp(&y) {
+            // If elements are equal, continue to the next iteration
+            Ordering::Equal => continue,
+            // If a[i] is less than b[i], return true
+            Ordering::Less => return true,
+            // If a[i] is greater than b[i], return false
+            Ordering::Greater => return false,
+        }
+    }
+
+    // If all elements are equal, compare the lengths of the arrays
+    a.len() < b.len()
+}
+
+pub fn chop_successive_digits(strings: &mut Vec<Vec<usize>>) -> Vec<Vec<usize>> {
+    let reversed = !lexicographic_less_than_array(&strings[0], &strings[1]);
+    if reversed {
+        strings.reverse();
+    }
+    let mut result = Vec::new();
+    result.push(strings[0].clone());
+
+    for i in 1..strings.len() {
+        let new = chop_digits(result.last().unwrap(), &strings[i]);
+        result.push(new);
+    }
+
+    if reversed {
+        result.reverse();
+    }
+    result
+}
+
+pub struct SymbolTable {
+    num2sym: Vec<char>,
+    sym2num: HashMap<char, usize>,
+    base: usize,
+}
+
+impl SymbolTable {
+    pub fn new(symbols: Vec<char>) -> Self {
+        let mut sym2num = HashMap::new();
+        for (i, c) in symbols.iter().enumerate() {
+            sym2num.insert(c.clone(), i);
+        }
+
+        let base = symbols.len();
+
+        SymbolTable {
+            num2sym: symbols,
+            sym2num,
+            base,
+        }
+    }
+
+    pub fn from_str(symbols: &str) -> Self {
+        Self::new(symbols.chars().collect())
+    }
+
+    pub fn decimal() -> Self {
+        Self::new(('0'..='9').collect())
+    }
+
+    pub fn alphabetic() -> Self {
+        Self::new(('a'..='z').collect())
+    }
+
+    pub fn base36() -> Self {
+        Self::new(('0'..='9').chain('a'..='z').collect())
+    }
+
+    pub fn base62() -> Self {
+        Self::new(('0'..='9').chain('A'..='Z').chain('a'..='z').collect())
+    }
+
+    pub fn hex() -> Self {
+        Self::new(('0'..='9').chain('a'..='f').collect())
+    }
+
+    pub fn number_to_digits(&self, mut num: usize) -> Vec<usize> {
+        // Unwrap the base, defaulting to self.base if None.
+        let mut digits = Vec::new();
+
+        // Convert the number to the specified base by repeatedly dividing it by the base and pushing the remainder onto the digits vector.
+        while num >= 1 {
+            digits.push(num % self.base);
+            num /= self.base;
+        }
+
+        // Reverse the digits vector because the digits were pushed on in least-significant to most-significant order.
+        digits.reverse();
+
+        // If the digits vector is empty (i.e., if the input number was 0), return a vector containing a single 0. Otherwise, return the digits vector.
+        if digits.is_empty() {
+            vec![0]
+        } else {
+            digits
+        }
+    }
+
+    pub fn digits_to_string(&self, digits: &[usize]) -> String {
+        digits
+            .iter()
+            .map(|&n| self.num2sym.get(n).expect("Invalid digit provided").clone())
+            .collect::<String>()
+    }
+
+    pub fn chars_to_digits(&self, string: Vec<char>) -> Vec<usize> {
+        string
+            .iter()
+            .filter_map(|c| self.sym2num.get(c))
+            .cloned()
+            .collect()
+    }
+
+    pub fn digits_to_number(&self, digits: &[usize]) -> usize {
+        // Initialize the current base and the result
+        let mut current_base = 1;
+        let mut result = 0;
+
+        // Iterate over the digits in reverse order
+        for &digit in digits.iter().rev() {
+            // Add the current digit times the current base to the result
+            result += digit * current_base;
+
+            // Multiply the current base by the base
+            current_base *= self.base;
+        }
+
+        // Return the result
+        result
+    }
+
+    pub fn number_to_string(&self, num: usize) -> String {
+        self.digits_to_string(&self.number_to_digits(num))
+    }
+
+    pub fn string_to_number(&self, num: Vec<char>) -> usize {
+        self.digits_to_number(&self.chars_to_digits(num))
+    }
+
+    pub fn round_fraction(
+        &self,
+        numerator: usize,
+        denominator: usize,
+        base: Option<usize>,
+    ) -> Result<Vec<usize>, &'static str> {
+        let base = base.unwrap_or(self.base);
+        let places = (denominator as f64).log(base as f64).ceil() as usize;
+        let scale = base.pow(places as u32);
+
+        let numerator_big = BigInt::from(numerator);
+        let denominator_big = BigInt::from(denominator);
+        let scale_big = BigInt::from(scale);
+
+        let scaled = ((numerator_big * scale_big) / denominator_big)
+            .to_biguint()
+            .unwrap()
+            .to_usize()
+            .unwrap();
+        let digits = self.number_to_digits(scaled);
+
+        Ok(pad_left(digits, places, 0))
+    }
+
+    pub fn mudder(
+        &self,
+        n: usize,
+        start: Option<&str>,
+        end: Option<&str>,
+    ) -> Result<Vec<String>, &'static str> {
+        let n = NonZeroUsize::new(n).ok_or("n must be greater than 0")?;
+
+        let start_vec = match start {
+            Some(start) => start.chars().collect(),
+            None => vec![self.num2sym[0].clone()],
+        };
+
+        let end_vec = match end {
+            Some(end) => end.chars().collect(),
+            None => vec![self.num2sym[self.num2sym.len() - 1].clone(); start_vec.len() + 6],
+        };
+
+        assert!(
+            !start_vec.is_empty(),
+            "Empty start string not supported. Use None instead."
+        );
+        assert!(
+            !end_vec.is_empty(),
+            "Empty end string not supported. Use None instead."
+        );
+
+        let num_divisions = n.get() + 1;
+
+        let ad = self.chars_to_digits(start_vec);
+        let bd = self.chars_to_digits(end_vec);
+        let mut intermediate_digits =
+            long_linspace(ad.clone(), bd.clone(), self.base, n, num_divisions)?;
+
+        let mut final_digits = vec![];
+
+        for digit_items in &mut intermediate_digits {
+            let mut v = digit_items.result.clone();
+            v.extend(self.round_fraction(
+                digit_items.remainder,
+                digit_items.denominator,
+                Some(self.base),
+            )?);
+            final_digits.push(v);
+        }
+
+        final_digits.insert(0, ad);
+        final_digits.push(bd);
+
+        final_digits = chop_successive_digits(&mut final_digits);
+
+        let slice = &final_digits[1..final_digits.len() - 1];
+
+        let strings = slice
+            .iter()
+            .map(|v| self.digits_to_string(&v))
+            .collect::<Vec<_>>();
+
+        Ok(strings)
+    }
+
+    pub fn mudder_one(
+        &self,
+        start: Option<&str>,
+        end: Option<&str>,
+    ) -> Result<String, &'static str> {
+        let strings = self.mudder(1, start, end)?;
+        Ok(strings[0].clone())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn all_less_than(strings: &Vec<String>) {
+        for i in 0..strings.len() - 1 {
+            assert!(strings[i] < strings[i + 1]);
+        }
+    }
+
+    #[test]
+    fn numeric_basics() {
+        let table = SymbolTable::decimal();
+        let res = table.mudder_one(Some("1"), Some("2")).unwrap();
+
+        assert_eq!(res, "15");
+    }
+
+    #[test]
+    fn repeated_subdivision() {
+        let right = 'z';
+
+        for _ in 0..50 {
+            let newr = SymbolTable::alphabetic()
+                .mudder_one(Some("a"), Some(&right.to_string()))
+                .unwrap();
+            assert_ne!('a', newr.chars().next().unwrap());
+            assert_ne!(right, newr.chars().next().unwrap());
+        }
+    }
+
+    #[test]
+    fn no_start() {
+        let table = SymbolTable::alphabetic();
+        let res = table.mudder_one(None, Some("z")).unwrap();
+
+        assert_eq!(res, "m");
+    }
+
+    #[test]
+    fn no_end() {
+        let table = SymbolTable::alphabetic();
+        let res = table.mudder_one(Some("z"), None).unwrap();
+
+        assert_eq!(res, "zm");
+    }
+
+    #[test]
+    fn alphabetic_basics() {
+        let table = SymbolTable::alphabetic();
+        let res = table.mudder_one(Some("a"), Some("b")).unwrap();
+
+        assert_eq!(res, "an");
+    }
+
+    #[test]
+    fn base36_basics() {
+        let table = SymbolTable::base36();
+        let res = table.mudder_one(Some("1"), Some("2")).unwrap();
+
+        assert_eq!(res, "1i");
+    }
+
+    #[test]
+    fn base62_basics() {
+        let table = SymbolTable::base62();
+        let res = table.mudder_one(Some("1"), Some("2")).unwrap();
+
+        assert_eq!(res, "1V");
+    }
+
+    #[test]
+    fn empty_start_and_end() {
+        let table = SymbolTable::decimal();
+        let res = table.mudder_one(None, None).unwrap();
+
+        assert_eq!(res, "4");
+    }
+
+    #[test]
+    fn same_start_and_end() {
+        let table = SymbolTable::decimal();
+        let res = table.mudder_one(Some("1"), Some("1"));
+
+        assert!(res.is_err());
+    }
+
+    #[test]
+    fn chars_to_digits_test() {
+        let table = SymbolTable::alphabetic();
+        let res = table.chars_to_digits(vec!['a', 'b', 'c']);
+
+        assert_eq!(res, vec![0, 1, 2]);
+    }
+
+    #[test]
+    fn digits_to_string_test() {
+        let table = SymbolTable::alphabetic();
+        let res = table.digits_to_string(&[0, 1, 2]);
+
+        assert_eq!(res, "abc");
+    }
+
+    #[test]
+    fn number_to_string_test() {
+        let table = SymbolTable::decimal();
+        let res = table.number_to_string(123);
+
+        assert_eq!(res, "123");
+    }
+
+    #[test]
+    fn string_to_number_test() {
+        let table = SymbolTable::decimal();
+        let res = table.string_to_number(vec!['1', '2', '3']);
+
+        assert_eq!(res, 123);
+    }
+
+    #[test]
+    fn number_to_digits_test() {
+        let table = SymbolTable::decimal();
+        let res = table.number_to_digits(123);
+
+        assert_eq!(res, vec![1, 2, 3]);
+    }
+
+    #[test]
+    fn test_single_digit_to_string() {
+        let table = SymbolTable::decimal();
+        let res = table.digits_to_string(&[5]);
+
+        assert_eq!(res, "5");
+    }
+
+    #[test]
+    fn test_zero_number_to_string() {
+        let table = SymbolTable::decimal();
+        let res = table.number_to_string(0);
+
+        assert_eq!(res, "0");
+    }
+
+    #[test]
+    fn test_single_digit_string_to_number() {
+        let table = SymbolTable::decimal();
+        let res = table.string_to_number(vec!['5']);
+
+        assert_eq!(res, 5);
+    }
+
+    #[test]
+    fn test_zero_number_to_digits() {
+        let table = SymbolTable::decimal();
+        let res = table.number_to_digits(0);
+
+        assert_eq!(res, vec![0]);
+    }
+
+    #[test]
+    fn test_invalid_chars_to_digits() {
+        let table = SymbolTable::decimal();
+        let res = table.chars_to_digits(vec!['a']);
+
+        assert_eq!(res, vec![]);
+    }
+
+    #[test]
+    fn test_empty_digit_to_string() {
+        let table = SymbolTable::decimal();
+        let res = table.digits_to_string(&[]);
+
+        assert_eq!(res, "");
+    }
+
+    #[test]
+    fn test_empty_number_to_string() {
+        let table = SymbolTable::decimal();
+        let res = table.number_to_string(0);
+
+        assert_eq!(res, "0");
+    }
+
+    #[test]
+    fn test_new() {
+        let symbols = vec!['a', 'b', 'c'];
+        let table = SymbolTable::new(symbols.clone());
+        assert_eq!(table.num2sym, symbols);
+        assert_eq!(
+            table.sym2num,
+            [('a', 0), ('b', 1), ('c', 2)].iter().cloned().collect()
+        );
+        assert_eq!(table.base, 3);
+    }
+
+    #[test]
+    fn test_mudder_multiple_strings() {
+        let table = SymbolTable::base62();
+        let res = table.mudder(3, Some("1"), Some("z")).unwrap();
+        assert_eq!(res, vec!["G", "V", "k"]);
+    }
+
+    #[test]
+    fn test_fine_divisions() {
+        let fine = SymbolTable::decimal().mudder(100, Some("9"), None).unwrap();
+        let partial_fine = SymbolTable::decimal().mudder(101, Some("9"), None).unwrap();
+        let coarse = SymbolTable::decimal().mudder(5, Some("9"), None).unwrap();
+
+        all_less_than(&fine);
+        all_less_than(&partial_fine);
+        all_less_than(&coarse);
+    }
+
+    #[test]
+    fn test_default_end() {
+        let a = SymbolTable::base36()
+            .mudder_one(Some(&"z".repeat(10)), None)
+            .unwrap();
+        let b = SymbolTable::base36()
+            .mudder_one(Some(&"z".repeat(15)), None)
+            .unwrap();
+        assert!(a < b);
+    }
+}
---
+++ packages/mudderpy/mudderpy-0.1.4.tar.gz/mudderpy-0.1.4/mudderpy/README.md
@@ -0,0 +1,91 @@
+# Mudder
+
+Generate lexicographically-spaced strings between two strings from pre-defined alphabets.
+
+This package is a rewrite of (Mudder.js)[https://github.com/fasiha/mudderjs/tree/main] with a Rust core that is used to generate bindings to Python (via [Pyo3](https://github.com/PyO3/pyo3)) and JS/TS (via [wasm-pack](https://github.com/rustwasm/wasm-pack)).
+
+# Quickstart
+
+- Rust: `cargo add mudder`
+- Python: `pip install mudderpy` or `poetry add mudderpy`
+- JS/TS: `npm install mudderjs` or `yarn add mudderjs`
+
+# API
+
+The API is the same for all three languages. Note that there are a few differences in usage compared to the original:
+
+- The `SymbolTable` constructor takes in a `str`/`string` instead of a `char[]`/`string[]`. Each member of the symbol table is assumed to be a character.
+- When calling `mudder`, optional values for the `start` and `end` parameters use `None` or `undefined` instead of empty strings.
+- No method overloads.
+
+## Constructor
+
+Create a new `SymbolTable` by passing in a string. The characters in the string will be used as the alphabet for the `SymbolTable`, with the first character being the "zero" character and, the second being the "one" character, and so on.
+
+In Rust, the `new` method takes in a Vector of `char`s. The `from_str` method takes in a `&str`. In Python and JS/TS, the constructor takes in a `str`/`string`.
+
+```rust
+use mudder::SymbolTable;
+
+let table = SymbolTable::from_str("abc");
+```
+
+```python
+from mudderpy import SymbolTable
+
+table = SymbolTable("abc")
+```
+
+```typescript
+import { SymbolTable } from "mudderjs";
+
+const table = new SymbolTable("abc");
+```
+
+## Default `SymbolTable`s
+
+For convenience, there are a few default `SymbolTable`s that can be used.
+
+- `SymbolTable::decimal`: `0123456789`
+- `SymbolTable::alphabetic`: `abcdefghijklmnopqrstuvwxyz`
+- `SymbolTable::base36`: `0123456789abcdefghijklmnopqrstuvwxyz`
+- `SymbolTable::base62`: `0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz`
+- `SymbolTable::hex`: `0123456789abcdef`
+
+## `SymbolTable` methods
+
+A `SymbolTable` has the following methods:
+
+- `SymbolTable::mudder(n: usize, start: Option<&str>, end: Option<&str>) -> Result<Vec<String>,  &'static str>`: Generate `n` strings between `start` and `end`. If `start` is `None`, the first string will be the first string in the `SymbolTable`. If `end` is `None`, the last string will be the last string in the `SymbolTable`, repeated `k+6` times where `k=len(start)`.
+
+- `SymbolTable::mudder_one(start: Option<&str>, end: Option<&str>) -> Result<String,  &'static str>`: Convenience method for calling `mudder` with `n=1` and returning the first element of the resulting vector.
+
+Note that for Python and JS, the return type is just a list of strings or a single string.
+
+## Examples
+
+```rust
+use mudder::SymbolTable;
+
+let table = SymbolTable::from_str("abc");
+// let table = SymbolTable::new(vec!['a', 'b', 'c']);
+
+let strings = table.mudder(5, None, None).unwrap();
+assert_eq!(strings, vec!["ab", "ac", "b", "bc", "c"]);
+```
+
+```python
+from mudderpy import SymbolTable
+
+table = SymbolTable("abc")
+strings = table.mudder(5)
+assert strings == ['ab', 'ac', 'b', 'bc', 'c']
+```
+
+```typescript
+import { SymbolTable } from "mudderjs";
+
+const table = new SymbolTable("abc");
+const strings = table.mudder(5);
+assert(strings == ["ab", "ac", "b", "bc", "c"]);
+```
