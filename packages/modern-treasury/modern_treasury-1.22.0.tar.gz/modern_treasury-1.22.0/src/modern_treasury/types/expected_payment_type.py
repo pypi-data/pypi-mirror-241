15a5ffea38766e88d2be2d7921c8c618fc200099
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-18 14:04:42 +0000

    Add package modern-treasury/1.22.0/modern_treasury-1.22.0.tar.gz

---
+++ packages/modern-treasury/modern_treasury-1.22.0.tar.gz/modern_treasury-1.22.0/src/modern_treasury/types/expected_payment_type.py
@@ -0,0 +1,36 @@
+# File generated from our OpenAPI spec by Stainless.
+
+from typing import Optional
+from typing_extensions import Literal
+
+__all__ = ["ExpectedPaymentType"]
+
+ExpectedPaymentType = Optional[
+    Literal[
+        "ach",
+        "au_becs",
+        "bacs",
+        "book",
+        "card",
+        "chats",
+        "check",
+        "cross_border",
+        "dk_nets",
+        "eft",
+        "interac",
+        "masav",
+        "neft",
+        "nics",
+        "nz_becs",
+        "provxchange",
+        "rtp",
+        "se_bankgirot",
+        "sen",
+        "sepa",
+        "sg_giro",
+        "sic",
+        "signet",
+        "wire",
+        "zengin",
+    ]
+]
