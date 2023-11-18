4c4f9f6ad79f5a5b879a26caa4426717ea46e942
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-18 14:04:36 +0000

    Add package PBIAT-SELENIUM/1.1/PBIAT_SELENIUM-1.1.tar.gz

---
+++ packages/PBIAT-SELENIUM/PBIAT_SELENIUM-1.1.tar.gz/PBIAT_SELENIUM-1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+setup(
+    name="PBIAT_SELENIUM",
+    version=1.1,
+    description="Pacote para automatizar rotinas PowerBi",
+    author='Andre Ailton',
+    author_email='andre.ailtonf.10@gmail.com',
+    keywords=['Selenium','PowerBi','Automatization','automation'],
+    packages=find_packages()
+)
