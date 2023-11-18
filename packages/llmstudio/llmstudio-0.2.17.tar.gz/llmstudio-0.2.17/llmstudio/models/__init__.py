439532069ab0bfe46e5bf321e5c729b68fb1e646
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-18 04:05:07 +0000

    Add package llmstudio/0.2.17/llmstudio-0.2.17.tar.gz

---
+++ packages/llmstudio/llmstudio-0.2.17.tar.gz/llmstudio-0.2.17/llmstudio/models/__init__.py
@@ -0,0 +1,5 @@
+from .anthropic import AnthropicClient
+from .bedrock import BedrockClient
+from .models import LLMClient, LLMCompare, LLMModel
+from .openai import OpenAIClient
+from .vertexai import VertexAIClient
