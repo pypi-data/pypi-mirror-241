03ee6ca77b7f8e52738d19e47c7a49cfcd918d13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-18 04:05:15 +0000

    Add package stac-fastapi.extensions/2.4.9/stac-fastapi.extensions-2.4.9.tar.gz

---
+++ packages/stac-fastapi.extensions/stac-fastapi.extensions-2.4.9.tar.gz/stac-fastapi.extensions-2.4.9/stac_fastapi/extensions/core/context.py
@@ -0,0 +1,36 @@
+"""Context extension."""
+from typing import List, Optional
+
+import attr
+from fastapi import FastAPI
+
+from stac_fastapi.types.extension import ApiExtension
+
+
+@attr.s
+class ContextExtension(ApiExtension):
+    """Context Extension.
+
+    The Context extension adds a JSON object to ItemCollection responses (`/search`,
+    `/collections/{collection_id}/items`) which includes the number of items matched,
+    returned, and the limit requested.
+    https://github.com/stac-api-extensions/context
+    """
+
+    conformance_classes: List[str] = attr.ib(
+        factory=lambda: ["https://api.stacspec.org/v1.0.0-rc.2/item-search#context"]
+    )
+    schema_href: Optional[str] = attr.ib(
+        default="https://raw.githubusercontent.com/stac-api-extensions/context/v1.0.0-rc.2/json-schema/schema.json"
+    )
+
+    def register(self, app: FastAPI) -> None:
+        """Register the extension with a FastAPI application.
+
+        Args:
+            app: target FastAPI application.
+
+        Returns:
+            None
+        """
+        pass
