12123b671a2baee0335219eb17c0d40e5617fcd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-18 15:04:30 +0000

    Add package kytest/0.0.5/kytest-0.0.5.tar.gz

---
+++ packages/kytest/kytest-0.0.5.tar.gz/kytest-0.0.5/kytest/core/android/__init__.py
@@ -0,0 +1,10 @@
+from .element import AdrElem
+from .driver import AdrDriver, \
+    connected_list, first_connected
+
+__all__ = [
+    "AdrElem",
+    "AdrDriver",
+    "connected_list",
+    "first_connected"
+]
---
+++ packages/kytest/kytest-0.0.5.tar.gz/kytest-0.0.5/kytest/core/ios/__init__.py
@@ -0,0 +1,11 @@
+from .element import IosElem
+from .driver import IosDriver, \
+    connected_list, first_connected
+
+__all__ = [
+    "IosElem",
+    "IosDriver",
+    "connected_list",
+    "first_connected"
+]
+
