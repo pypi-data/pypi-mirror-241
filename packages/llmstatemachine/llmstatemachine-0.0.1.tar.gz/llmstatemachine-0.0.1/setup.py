51cff9359506edb75bc5afaa1b90ce9a5c444c90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-18 06:08:07 +0000

    Add package llmstatemachine/0.0.1/llmstatemachine-0.0.1.tar.gz

---
+++ packages/llmstatemachine/llmstatemachine-0.0.1.tar.gz/llmstatemachine-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='llmstatemachine',
+    version='0.0.1',
+    description='A Large Language Model State Machine package',
+    url='https://github.com/robocorp/llm_state_machine',
+    author='Mikko Korpela',
+    author_email='mikko@robocorp.com',
+    license='Apache 2.0',
+    packages=['llmstatemachine'],
+    zip_safe=False
+)
+
