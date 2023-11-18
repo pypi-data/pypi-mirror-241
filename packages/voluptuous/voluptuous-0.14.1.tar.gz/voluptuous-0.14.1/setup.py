bddc68a240d062a29d64e4e32aaacb86a2c67432
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-18 03:06:08 +0000

    Add package voluptuous/0.14.1/voluptuous-0.14.1.tar.gz

---
+++ packages/voluptuous/voluptuous-0.14.1.tar.gz/voluptuous-0.14.1/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+
+import sys
+import io
+sys.path.insert(0, '.')
+version = __import__('voluptuous').__version__
+
+
+with io.open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='voluptuous',
+    url='https://github.com/alecthomas/voluptuous',
+    download_url='https://pypi.python.org/pypi/voluptuous',
+    version=version,
+    description='Python data validation library',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='BSD-3-Clause',
+    platforms=['any'],
+    packages=['voluptuous'],
+    package_data={
+        'voluptuous': ['py.typed'],
+    },
+    author='Alec Thomas',
+    author_email='alec@swapoff.org',
+    python_requires=">=3.7",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    setup_requires=['wheel']
+)
