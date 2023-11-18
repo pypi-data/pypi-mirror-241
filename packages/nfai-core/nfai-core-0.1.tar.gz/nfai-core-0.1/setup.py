fa6b92c0b2e0ef4eca2bb862fef9c56beedac935
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-18 04:05:18 +0000

    Add package nfai-core/0.1/nfai-core-0.1.tar.gz

---
+++ packages/nfai-core/nfai-core-0.1.tar.gz/nfai-core-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nfai-core',
+    version='0.1',
+    author='NeuralFabric',
+    description='Core library for NeuralFabric',
+    packages=find_packages(),
+    install_requires=[
+        # List any dependencies your package requires
+    ],
+)
