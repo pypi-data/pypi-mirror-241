9b959070b33e1af98121d826ec040c4cedbca471
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-18 14:04:34 +0000

    Add package libgen-scraper/0.1.2/libgen_scraper-0.1.2-py3-none-any.whl

---
+++ packages/libgen-scraper/libgen_scraper-0.1.2-py3-none-any.whl/libgen_scraper/__init__.py
@@ -0,0 +1,14 @@
+from .non_fiction import (  # noqa
+    search_non_fiction,
+    NonFictionColumns,
+    NonFictionSearchField,
+    NonFictionResults,
+)
+from .fiction import (  # noqa
+    search_fiction,
+    FictionColumns,
+    FictionSearchCriteria,
+    FictionSearchFormat,
+    FictionResults,
+)
+from .articles import search_articles, ArticlesColumns, ArticlesResults  # noqa
