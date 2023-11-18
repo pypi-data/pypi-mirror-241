107f3f092474ca629f13d6115dd9a7715ffaf1b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-18 03:05:28 +0000

    Add package txrm2tiff/2.1.2/txrm2tiff-2.1.2-py3-none-any.whl

---
+++ packages/txrm2tiff/txrm2tiff-2.1.2-py3-none-any.whl/txrm2tiff/__init__.py
@@ -0,0 +1,4 @@
+from .info import __version__, __author__, __email__
+
+from .txrm.main import open_txrm
+from .main import convert_and_save
---
+++ packages/txrm2tiff/txrm2tiff-2.1.2-py3-none-any.whl/txrm2tiff/info.py
@@ -0,0 +1,3 @@
+__version__ = "2.1.2"
+__author__ = "Thomas Fish"
+__email__ = "thomas.fish@diamond.ac.uk"
