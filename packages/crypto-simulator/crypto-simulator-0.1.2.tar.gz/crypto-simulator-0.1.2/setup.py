227ccd83f8082ccc686360bd2db6b2ec69c48a3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-18 06:07:55 +0000

    Add package crypto-simulator/0.1.2/crypto-simulator-0.1.2.tar.gz

---
+++ packages/crypto-simulator/crypto-simulator-0.1.2.tar.gz/crypto-simulator-0.1.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='crypto-simulator',
+    version='0.1.2',
+    packages=['src'],
+    install_requires=[
+        'ccxt==4.1.50',
+        'matplotlib==3.8.1',
+        'pandas==2.1.3',
+        'tk==0.1.0',
+    ],
+    entry_points={
+        'console_scripts': [
+            'crypto-simulator = src.main:main',
+        ],
+    },
+    author='Max Paul',
+    author_email='maxkpaul21@gmail.com',
+    description='A crypto trading simulator',
+    url='https://github.com/max-paul/crypto-simulator',
+)
