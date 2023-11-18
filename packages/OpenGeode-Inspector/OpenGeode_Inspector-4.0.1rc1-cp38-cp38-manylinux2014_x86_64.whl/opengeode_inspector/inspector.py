390b2bb8834e816b6e9f98cfd644db1a484c1e74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-18 04:05:00 +0000

    Add package OpenGeode-Inspector/4.0.1rc1/OpenGeode_Inspector-4.0.1rc1-cp38-cp38-manylinux2014_x86_64.whl

---
+++ packages/OpenGeode-Inspector/OpenGeode_Inspector-4.0.1rc1-cp38-cp38-manylinux2014_x86_64.whl/opengeode_inspector/inspector.py
@@ -0,0 +1,11 @@
+#
+# Copyright (c) 2019 - 2023 Geode-solutions. All rights reserved.
+#
+
+import opengeode
+import opengeode_io
+import opengeode_geosciences
+import opengeode_geosciencesio
+
+from .lib64.opengeode_inspector_py_inspector import *
+InspectorInspectorLibrary.initialize()
