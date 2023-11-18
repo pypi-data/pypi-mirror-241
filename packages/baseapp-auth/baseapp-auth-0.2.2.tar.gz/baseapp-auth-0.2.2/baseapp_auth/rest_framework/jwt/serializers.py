0bd0ccc0ee3a6fe7cfcb203edc6e1838bab7d498
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-18 05:05:21 +0000

    Add package baseapp-auth/0.2.2/baseapp-auth-0.2.2.tar.gz

---
+++ packages/baseapp-auth/baseapp-auth-0.2.2.tar.gz/baseapp-auth-0.2.2/baseapp_auth/rest_framework/jwt/serializers.py
@@ -0,0 +1,32 @@
+from baseapp_auth.rest_framework.login.serializers import LoginPasswordExpirationMixin
+from django.utils.module_loading import import_string
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+
+class BaseJwtLoginSerializer(LoginPasswordExpirationMixin, TokenObtainPairSerializer):
+    _claim_serializer_class = ""
+
+    @classmethod
+    def get_claim_serializer_class(cls):
+        try:
+            return import_string(cls._claim_serializer_class)
+        except ImportError:
+            msg = "Could not import serializer '%s'" % cls._claim_serializer_class
+            raise ImportError(msg)
+
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        if cls._claim_serializer_class:
+            data = cls.get_claim_serializer_class()(user).data
+            for key, value in data.items():
+                token[key] = value
+
+        return token
+
+    def validate(self, data):
+        validated_data = super().validate(data)
+        self.check_password_expiration(self.user)
+        return validated_data
