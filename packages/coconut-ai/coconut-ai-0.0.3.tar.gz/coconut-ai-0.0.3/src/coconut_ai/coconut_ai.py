5200be149b72bab14fc0ba4ade1d65538ff56116
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-18 03:04:56 +0000

    Add package coconut-ai/0.0.3/coconut-ai-0.0.3.tar.gz

---
+++ packages/coconut-ai/coconut-ai-0.0.3.tar.gz/coconut-ai-0.0.3/src/coconut_ai/coconut_ai.py
@@ -0,0 +1,47 @@
+from typing import TypedDict
+
+
+class TextToImageOptions(TypedDict):
+    text: str
+    output_path: str
+
+
+class ImageToTextOptions(TypedDict):
+    image_path: str
+
+
+class TextToTextOptions(TypedDict):
+    text: str
+
+
+class ImageToImageOptions(TypedDict):
+    image_path: str
+    output_path: str
+
+
+class CoconutAI:
+    def __init__(self) -> None:
+        pass
+
+    def __name__(self) -> str:
+        return "coconut_ai"
+
+    def text_to_image(self, options: TextToImageOptions) -> None:
+        print("---Text To Image---")
+        print(f"Text: {options['text']}")
+        print(f"Output Image Path: {options['output_path']}")
+
+    def image_to_text(self, options: ImageToTextOptions) -> str:
+        print("---Image To Text---")
+        print(f"Image Path: {options['image_path']}")
+        return "Text"
+
+    def text_to_text(self, options: TextToTextOptions) -> str:
+        print("---Text To Text---")
+        print(f"Text: {options['text']}")
+        return options["text"]
+
+    def image_to_image(self, options: ImageToImageOptions) -> None:
+        print("---Image To Image---")
+        print(f"Image Path: {options['image_path']}")
+        print(f"Output Image Path: {options['output_path']}")
