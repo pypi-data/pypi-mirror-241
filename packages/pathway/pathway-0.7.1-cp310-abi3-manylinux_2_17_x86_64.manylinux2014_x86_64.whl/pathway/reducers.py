dfc4e253ba4106d29ff9690454a649ac23071d5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-18 03:04:38 +0000

    Add package pathway/0.7.1/pathway-0.7.1-cp310-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pathway/pathway-0.7.1-cp310-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pathway/reducers.py
@@ -0,0 +1,39 @@
+# Copyright © 2023 Pathway
+
+from pathway.internals.reducers import (
+    any,
+    argmax,
+    argmin,
+    avg,
+    count,
+    int_sum,
+    max,
+    min,
+    ndarray,
+    npsum,
+    sorted_tuple,
+    stateful_many,
+    stateful_single,
+    sum,
+    tuple_reducer as tuple,
+    unique,
+)
+
+__all__ = [
+    "any",
+    "argmax",
+    "argmin",
+    "avg",
+    "count",
+    "int_sum",
+    "max",
+    "min",
+    "ndarray",
+    "npsum",
+    "sorted_tuple",
+    "stateful_many",
+    "stateful_single",
+    "sum",
+    "tuple",
+    "unique",
+]
