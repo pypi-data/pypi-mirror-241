3fb056ab2e96341ce25ff014722ae6f33965cbb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-18 03:05:47 +0000

    Add package Myfreegpt/1.0.4/Myfreegpt-1.0.4-py3-none-any.whl

---
+++ packages/Myfreegpt/Myfreegpt-1.0.4-py3-none-any.whl/Myfreegpt/AsyncClient/prodia.py
@@ -0,0 +1,51 @@
+"""
+Myfreegpt's prodia module
+"""
+
+from random import randint
+from aiohttp import ClientSession, ClientError
+import requests
+
+
+class Generation:
+    """
+    This class provides methods for generating images based on prompts.
+    """
+
+    async def create(self, prompt):
+        """
+        Create a new image generation based on the given prompt.
+
+        Args:
+            prompt (str): The prompt for generating the image.
+
+        Returns:
+            resp: The generated image content
+        """
+        headers = {
+            "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.79 Safari/537.36",
+        }
+        url = "https://api.prodia.com/v1/sd/generate"
+        try:
+            async with ClientSession() as session:
+                async with session.get(
+                    "https://api.prodia.com/v1/sd/generate",
+                    params= {
+    "prompt": "prompt",
+    "model": "dreamshaper_6BakedVae.safetensors [114c8abb]",
+    "negative_prompt": "(nsfw:1.5),verybadimagenegative_v1.3, ng_deepnegative_v1_75t, (ugly face:0.5),cross-eyed,sketches, (worst quality:2), (low quality:2.1), (normal quality:2), lowres, normal quality, ((monochrome)), ((grayscale)), skin spots, acnes, skin blemishes, bad anatomy, DeepNegative, facing away, tilted head, {Multiple people}, lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worstquality, low quality, normal quality, jpegartifacts, signature, watermark, username, blurry, bad feet, cropped, poorly drawn hands, poorly drawn face, mutation, deformed, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, extra fingers, fewer digits, extra limbs, extra arms,extra legs, malformed limbs, fused fingers, too many fingers, long neck, cross-eyed,mutated hands, polar lowres, bad body, bad proportions, gross proportions, text, error, missing fingers, missing arms, missing legs, extra digit, extra arms, extra leg, extra foot, repeating hair",
+    "steps": 50,
+    "cfg_scale": 9.5,
+    "seed": randint(1, 10000),
+    "sampler": "Euler",
+    "aspect_ratio": "square"
+},
+                    headers = {
+    "accept": "application/json",
+    "content-type": "application/json"
+},
+                    timeout=30,
+                ) as resp:
+                    requests.post(url, json=params, headers=headers)
+        except ClientError as exc:
+            raise ClientError("Unable to fetch the response.") from exc
