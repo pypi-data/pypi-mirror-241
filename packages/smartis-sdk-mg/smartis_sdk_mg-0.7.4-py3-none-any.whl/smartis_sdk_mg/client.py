944f15f862ac30005f94042b38b45c300e0016d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-18 04:04:45 +0000

    Add package smartis-sdk-mg/0.7.4/smartis_sdk_mg-0.7.4-py3-none-any.whl

---
+++ packages/smartis-sdk-mg/smartis_sdk_mg-0.7.4-py3-none-any.whl/smartis_sdk_mg/client.py
@@ -0,0 +1,165 @@
+import json
+import logging
+import time
+from enum import Enum
+
+import requests
+from pydantic import ValidationError
+
+from .common import Method
+from .entity import Ads, Ad, Payload, Keyword
+from .entity import CrmCustomFields, CrmCustomField, CrmCustomFieldGroups, CrmCustomFieldGroup
+from .entity import Campaigns, Campaign
+from .entity import Channels, Placements
+from .entity import FieldsRootEntity, FieldsEntity, RootEntity
+from .entity import Keywords
+from .errors import ApiLimitError, ApiInternalError
+from .utils import clean_column_ids, ColumnType
+
+
+class ContentType(Enum):
+    application = "application/json"
+
+
+def status_code_handler(response: requests.Response) -> None:
+    """Status code preprocessing"""
+
+    match response.status_code:
+        case 200:
+            return
+        case 400 | 403:
+            resp_body: dict = response.json()
+            message = resp_body.get("error")
+            raise ConnectionError(f"{response.status_code}:{response.text}. details: {message}")
+        case 429:
+            if int(response.headers['X-Ratelimit-Remaining']) == 0:
+                logging.warning(f"Status code: {response.status_code}. "
+                                f"Reason: {response.reason}. "
+                                f"Retry after: {response.headers['Retry-After']} sec.")
+                raise ApiLimitError(int(response.headers['Retry-After']), response.text)
+        case 500 | 502:
+            logging.warning(f"Status code: {response.status_code}. "
+                            f"Reason: {response.reason}")
+            raise ApiInternalError(response.text)
+        case _:
+            logging.warning(f" Unknown exception. "
+                            f"Status code: {response.status_code}. "
+                            f"Reason: {response.reason}. ")
+            logging.info(response.headers)
+            time.sleep(60)
+
+
+class Client:
+    TRY_REQUEST = 10
+    REQUEST_PAUSE = 1
+    FORCE_PAUSE = 600
+
+    def __init__(self, api_key: str, crm_token: str = None, dev: bool = False) -> None:
+        self.api_key = api_key
+        self.crm_token = crm_token
+        self.header = {'Authorization': f"Bearer {api_key}", "Content-Type": ContentType.application.value, }
+        if dev:
+            self.host = "https://dev.smartis.bi/api/"
+        else:
+            self.host = "https://my.smartis.bi/api/"
+
+    def get_other(self, method: Method) -> []:
+        response = self._prepare(method)
+        status_code_handler(response)
+        return response.json()[method.value.location]
+
+    def _prepare(self, method: Method, parameters: str | None = None) -> requests.Response:
+        if parameters:
+            resp = requests.post(f"{self.host}{method.value.method}", headers=self.header, data=parameters)
+        else:
+            resp = requests.post(f"{self.host}{method.value.method}", headers=self.header)
+        status_code_handler(resp)
+
+        return resp
+
+    def get_channels(self) -> Channels:
+        response = self._prepare(Method.GET_CHANNELS)
+        try:
+            return Channels.model_validate(response.json())
+        except ValidationError as e:
+            raise ValueError from e
+
+    def get_placements(self) -> Placements:
+        response = self._prepare(Method.GET_PLACEMENTS)
+        try:
+            return Placements.model_validate(response.json())
+        except ValidationError as e:
+            raise ValueError from e
+
+    def _get_entity(self, ids: list, method: Method,
+                    model: type[RootEntity] | type[FieldsRootEntity]) -> RootEntity | FieldsRootEntity:
+        params = {"ids": ids}
+        if method in [Method.GET_CRM_CUSTOM_FIELDS, Method.GET_CRM_CUSTOM_FIELD_GROUPS]:
+            params["smartis_crm_token"] = self.crm_token
+        params_json = json.dumps(params)
+        response = self._prepare(method, parameters=params_json)
+        status_code_handler(response)
+        try:
+            return model.model_validate(response.json())
+        except ValidationError as e:
+            raise ValueError from e
+
+    def get_campaigns(self, campaigns_ids: list) -> list[Campaign]:
+        items: RootEntity = self._get_entity(campaigns_ids, Method.GET_CAMPAIGNS, Campaigns)
+        return items.items
+
+    def get_ads(self, ads_ids: list) -> list[Ad]:
+        items: RootEntity = self._get_entity(ads_ids, Method.GET_ADS, Ads)
+        return items.items
+
+    def get_keywords(self, keywords_ids: list) -> list[Keyword]:
+        items: RootEntity = self._get_entity(keywords_ids, Method.GET_KEYWORDS, Keywords)
+        return items.items
+
+    def _get_fields(self, method: Method, model: type[FieldsRootEntity], fields_ids: list) -> list[FieldsEntity]:
+        if self.crm_token is None:
+            raise ValueError("Not found crm token")
+        fields_ids = clean_column_ids(fields_ids, ColumnType.CustomField)
+        return self._get_entity(fields_ids, method, model).items
+
+    def get_crm_custom_fields(self, fields_ids: list) -> list[CrmCustomField]:
+        return self._get_fields(Method.GET_CRM_CUSTOM_FIELDS, CrmCustomFields, fields_ids)
+
+    def get_crm_custom_field_groups(self, fields_ids: list) -> list[CrmCustomFieldGroup]:
+        return self._get_fields(Method.GET_CRM_CUSTOM_FIELD_GROUPS, CrmCustomFieldGroups, fields_ids)
+
+    def get_report(self, payload: Payload, retry: int = TRY_REQUEST) -> requests.Response:
+        payload_json = payload.to_json()
+        try:
+            time.sleep(self.REQUEST_PAUSE)
+            answer = requests.post(f"{self.host}reports/getReport", headers=self.header, data=payload_json)
+            status_code_handler(answer)
+        except ApiLimitError as apiLimErr:
+            if retry == 0:
+                return self.retry_get_report(payload)
+            logging.warning(f"error: {apiLimErr.message}")
+            logging.info(
+                f"Retry after {apiLimErr.retry_after} sec.: {self.TRY_REQUEST - retry + 1}/{self.TRY_REQUEST}")
+            time.sleep(apiLimErr.retry_after)
+            return self.get_report(payload, retry=retry - 1)
+        except ConnectionError:
+            return answer
+        except ApiInternalError as apiIntErr:
+            if retry == 0:
+                return self.retry_get_report(payload)
+            # logging.warning(f"error: {apiIntErr.message}")
+            logging.info(f"Повтор запроса: {self.TRY_REQUEST - retry + 1}/{self.TRY_REQUEST}")
+            return self.get_report(payload, retry=retry - 1)
+        except Exception as er:
+            if retry == 0:
+                return self.retry_get_report(payload)
+            logging.warning(f"Unknown exception. error: {er}")
+            logging.info(f"Повтор запроса: {self.TRY_REQUEST - retry + 1}/{self.TRY_REQUEST}")
+            return self.get_report(payload, retry=retry - 1)
+        else:
+            return answer
+
+    def retry_get_report(self, payload: Payload) -> requests.Response:
+        logging.warning("Количество ошибок больше заданного! Пауза 10 мин")
+        time.sleep(self.FORCE_PAUSE)
+        return self.get_report(payload)
---
+++ packages/smartis-sdk-mg/smartis_sdk_mg-0.7.4-py3-none-any.whl/smartis_sdk_mg/entity.py
@@ -0,0 +1,162 @@
+import json
+from datetime import datetime
+from typing import TypeVar
+
+from pydantic import BaseModel, Field
+
+from .common import AttributionModel, GroupBy, TypeReport, FilterCategory
+from .utils import normalize_date
+
+
+class ChannelItem(BaseModel):
+    id: int
+    title: str
+
+
+class Channels(BaseModel):
+    items: list[ChannelItem] = Field(alias='channels')
+
+
+class Channel(BaseModel):
+    id: int
+    title: str
+    channel_id: int
+
+
+class Placement(BaseModel):
+    id: int
+    title: str
+    channel: Channel
+
+
+class Placements(BaseModel):
+    items: list[Placement] = Field(alias='placements')
+
+
+class Campaign(BaseModel):
+    id: int
+    placement_id: int
+    title: str
+
+
+class Campaigns(BaseModel):
+    items: list[Campaign] = Field(alias='campaigns')
+
+
+class Keyword(BaseModel):
+    id: int
+    keyword: str
+
+
+class Keywords(BaseModel):
+    items: list[Keyword]= Field(alias='keywords')
+
+
+class CrmCustomField(BaseModel):
+    id: int
+    crm_account_id: int
+    element_type_id: int
+    custom_field_title: str
+    field_type_id: int
+    is_multiple: int
+    group_id: int
+    description: str
+    status: int
+    is_filter: int
+    filter_param_id: int
+    default_visibility: int
+
+class CrmCustomFieldGroup(BaseModel):
+    id: int
+    title: str
+    crm_account_id: int
+    default_visibility: int
+    sort: int
+
+
+class CrmCustomFields(BaseModel):
+    items: list[CrmCustomField] = Field(alias='crmCustomFields')
+
+class CrmCustomFieldGroups(BaseModel):
+    items: list[CrmCustomFieldGroup] = Field(alias='crmCustomFieldGroups')
+
+
+class Ad(BaseModel):
+    id: int
+    external_id: str
+    placement_id: int
+    campaign_id: int | None
+    external_campaign_id: str | None
+    type: str
+    title: str | None
+    text: str | None
+    text1: str | None
+    text2: str | None
+    preview_url: str | None
+    href: str | None
+    device: str | None
+    created_at: str
+
+
+class Ads(BaseModel):
+    items: list[Ad] = Field(alias='ads')
+
+
+Entity = TypeVar('Entity', ChannelItem, Placement, Campaign, Keyword, CrmCustomField, CrmCustomFieldGroup, Ad)
+FieldsEntity = TypeVar('FieldsEntity', CrmCustomField, CrmCustomFieldGroup)
+RootEntity = TypeVar('RootEntity', Channels, Placements, Campaigns, Keywords, CrmCustomFields, CrmCustomFieldGroups, Ads)
+FieldsRootEntity = TypeVar('FieldsRootEntity', CrmCustomFields, CrmCustomFieldGroups)
+
+
+class Attribution:
+
+    def __init__(self, model_id: AttributionModel, period: int, with_direct: bool):
+        self.model_id = model_id.value
+        self.period = period
+        self.with_direct = with_direct
+
+    def __str__(self):
+        return f"{self.model_id} {self.period} {self.with_direct}"
+
+
+class Filter:
+    def __init__(self, category: FilterCategory, value: int, operand: str = "="):
+        self.category = category.value
+        self.value = value
+        self.operand = operand
+
+
+class Payload:
+    """Базовый класс параметров для генерации параметров к запросам"""
+
+    def __init__(self, project: str, metrics: [str], datetime_from: datetime, datetime_to: datetime,
+                 group_by: GroupBy, attribution: Attribution = None, type_report: TypeReport = TypeReport.AGGREGATED,
+                 filters: list[Filter] | None = None, fields: list[str] | None = None):
+        self.project = project
+        self.metrics = ";".join(metrics) if metrics else ""
+        self.datetime_from: str = normalize_date(datetime_from)
+        self.datetime_to: str = normalize_date(datetime_to)
+        self.group_by = group_by
+        self.type = type_report
+        self.filters = filters
+        self.attribution = attribution
+        self.fields = fields
+
+    def __str__(self):
+        return f"{self.datetime_from}//{self.datetime_to}"
+
+    def to_json(self) -> str:
+        payload = {"project": self.project,
+                   "metrics": self.metrics,
+                   "datetimeFrom": self.datetime_from,
+                   "datetimeTo": self.datetime_to,
+                   "groupBy": self.group_by.value,
+                   "type": self.type.value}
+        if self.filters:
+            payload["filters"] = self.filters
+        if self.attribution:
+            payload["attribution"] = self.attribution.__dict__
+
+        if self.fields:
+            payload["fields"] = self.fields
+        return json.dumps(payload)
