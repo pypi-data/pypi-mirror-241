b7e487bb6ce933f4723f872c19a9629393b18281
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-18 14:04:24 +0000

    Add package upsonic/0.5.0/upsonic-0.5.0.tar.gz

---
+++ packages/upsonic/upsonic-0.5.0.tar.gz/upsonic-0.5.0/upsonic/remote/interface.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+
+
+def encrypt(key, message):
+    from cryptography.fernet import Fernet
+    import base64
+    import hashlib
+    import dill
+    fernet_key = base64.urlsafe_b64encode(hashlib.sha256(key.encode()).digest())
+    fernet = Fernet(fernet_key)
+    encrypted_message = fernet.encrypt(dill.dumps(message))
+    return encrypted_message
+
+def decrypt(key, message):
+    from cryptography.fernet import Fernet
+    import base64
+    import hashlib    
+    import dill
+    fernet = Fernet(base64.urlsafe_b64encode(hashlib.sha256(key.encode()).digest()))
+    decrypted_message = dill.loads(fernet.decrypt(message))
+    return decrypted_message
+
+
+
+def Upsonic_Cloud_Free(database_name, access_key):
+    from upsonic import Upsonic_Remote
+    return Upsonic_Remote(
+        database_name, "https://cloud_1.upsonic.co", access_key, verify=True
+    )  # pragma: no cover
+
+
+def Upsonic_Cloud_Pro(database_name, access_key):
+    from upsonic import Upsonic_Remote
+    return Upsonic_Remote(
+        database_name, "https://cloud_2.upsonic.co", access_key, verify=True
+    )  # pragma: no cover
+
+
+def Upsonic_Cloud_Premium(database_name, access_key):
+    from upsonic import Upsonic_Remote
+    return Upsonic_Remote(
+        database_name, "https://cloud_3.upsonic.co", access_key, verify=True
+    )  # pragma: no cover
+
+def Upsonic_Cloud_Startup(database_name, access_key):
+    from upsonic import Upsonic_Remote
+    return Upsonic_Remote(
+        database_name, "https://cloud_4.upsonic.co", access_key, verify=True
+    )  # pragma: no cover
+
+
+
+
+def Upsonic_Cloud_Readonly(database_name, access_key):
+    from upsonic import Upsonic_Remote
+    return Upsonic_Remote(
+        database_name, "https://cloud_0.upsonic.co", access_key, verify=True
+    )  # pragma: no cover
