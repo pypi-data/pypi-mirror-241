adbddae0c5370a2fa46a711f16a0da06ef711c7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-18 14:04:40 +0000

    Add package pygna2/0.3.12/pygna2-0.3.12-py3-none-any.whl

---
+++ packages/pygna2/pygna2-0.3.12-py3-none-any.whl/pygna2/__init__.py
@@ -0,0 +1,9 @@
+"""PyGNA2 is based on `PyGNA <https://bmcbioinformatics.biomedcentral.com/articles/10.1186/s12859-020-03801-1>`_. It uses the same statistical tests, but improves the way results are reported.
+
+PyGNA is a tool that can perform several kinds of statistical tests on gene networks and gene sets.
+
+- Publication: https://bmcbioinformatics.biomedcentral.com/articles/10.1186/s12859-020-03801-1
+- Documentation: https://pygna.readthedocs.io/en/latest/index.html
+- Github: https://github.com/stracquadaniolab/pygna
+- Workflow github: https://github.com/stracquadaniolab/workflow-pygna
+"""
