446cdeff450fd7ded8b75c46509138db851de36f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-18 05:05:21 +0000

    Add package symlib/1.3.16/symlib-1.3.16.tar.gz

---
+++ packages/symlib/symlib-1.3.16.tar.gz/symlib-1.3.16/symlib/__init__.py
@@ -0,0 +1,37 @@
+# Data types
+from .lib import SUBHALO_DTYPE, HISTORY_DTYPE, BRANCH_DTYPE, CORE_DTYPE, UM_DTYPE, PARTICLE_DTYPE, GALAXY_DTYPE, GALAXY_HISTORY_DTYPE
+# I/O function
+from .lib import read_subhalos, read_cores, read_branches, read_tree, read_particles, read_um, read_galaxies, ParticleInfo
+from .lib import Particles
+# Utilities
+from .lib import simulation_parameters, parameter_table, scale_factors, pristine_merger_indices, merger_stats, propagate_parent_indices, colossus_parameters, suite_names, merger_lookup_table, find_merger_branch, find_all_merger_branches, is_real_confirmed, read_symfind, read_rockstar, pre_infall_host, transform_smooth_particles
+# TODO: better names for pristine_merger_indices and propagate_parent_indices
+from .match import match_subhalos, plot_matched_subhalos
+
+# Abstract models
+from .star_tagging import ProfileModel, RHalfModel, MStarModel, AbstractRanking
+# Profile models
+from .star_tagging import PlummerProfile
+# R_half models
+from .star_tagging import Nadler2020RHalf, Jiang2019RHalf, FixedRHalf
+# Metallicity models
+from .star_tagging import Kirby2013Metallicity
+# M_star models
+from .star_tagging import UniverseMachineMStarFit, UniverseMachineMStar
+# Ranking schemes
+from .star_tagging import RadialEnergyRanking
+# Galaxy-halo models
+from .star_tagging import GalaxyHaloModel
+# Utility functions
+from .star_tagging import clean_particles, tag_stars, look_back_orbital_time, profile_info, rmax_vmax, retag_stars
+
+# Unit conversion
+from .util import set_units_halos, set_units_x, set_units_v, set_units_parameters, set_units_histories
+# Halo names
+from .util import  get_host_directory, n_hosts
+
+# These are functions that are only really useful for the the tutorial
+from .util import plot_circle
+
+# File management
+from .file_management import pack_files, download_packed_files, download_files, unpack_files
