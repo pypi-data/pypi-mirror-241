fdc319a751024536144c2933d3c926e6c05cfa5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-18 16:05:15 +0000

    Add package tipsql-sqlite3/0.0.3/tipsql_sqlite3-0.0.3-py3-none-any.whl

---
+++ packages/tipsql-sqlite3/tipsql_sqlite3-0.0.3-py3-none-any.whl/tipsql/sqlite3/plugin.py
@@ -0,0 +1,25 @@
+from typing import Literal, override
+
+from tipsql.core.dataclasses import extra_forbid_dataclass
+from tipsql.core.plugins.database_plugin import TipsqlDatabasePlugin
+
+
+@extra_forbid_dataclass
+class Sqlite3DatabaseConfig:
+    type: Literal["sqlite3"]
+
+
+class Sqlite3DatabasePlugin(TipsqlDatabasePlugin):
+    @property
+    def database_name(self) -> str:
+        return "sqlite3"
+
+    @property
+    def database_config(self) -> type[Sqlite3DatabaseConfig]:
+        return Sqlite3DatabaseConfig
+
+    @override
+    def sync_database(self, config: Sqlite3DatabaseConfig) -> None:
+        print("syncing sqlite3 database")
+
+        return None
