df90e05f78081e18553e5278102a1d394f0ec0c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-18 06:07:31 +0000

    Add package modules-cicd/1.1/modules_cicd-1.1.tar.gz

---
+++ packages/modules-cicd/modules_cicd-1.1.tar.gz/modules_cicd-1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(name='modules_cicd',
+version='1.1',
+description='Testing installation of Package',
+author='novateva',
+author_email='author@email.com',
+license='MIT',
+packages=find_packages(),
+zip_safe=False)
