80dbe63ab3a1465abab15e491af7664981d1b920
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-18 03:05:02 +0000

    Add package glances-api/0.5.0/glances_api-0.5.0.tar.gz

---
+++ packages/glances-api/glances_api-0.5.0.tar.gz/glances_api-0.5.0/glances_api/exceptions.py
@@ -0,0 +1,25 @@
+"""Exceptions for the client."""
+
+
+class GlancesApiError(Exception):
+    """General GlancesApiError exception occurred."""
+
+    pass
+
+
+class GlancesApiConnectionError(GlancesApiError):
+    """When a connection error is encountered."""
+
+    pass
+
+
+class GlancesApiAuthorizationError(GlancesApiError):
+    """When a connection error is encountered."""
+
+    pass
+
+
+class GlancesApiNoDataAvailable(GlancesApiError):
+    """When no data is available."""
+
+    pass
