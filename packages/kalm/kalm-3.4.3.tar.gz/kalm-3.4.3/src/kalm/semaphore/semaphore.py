a5bfb7a60e1a1811ae69e6f8d5c41e767de8593b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-18 06:07:30 +0000

    Add package kalm/3.4.3/kalm-3.4.3.tar.gz

---
+++ packages/kalm/kalm-3.4.3.tar.gz/kalm-3.4.3/src/kalm/semaphore/semaphore.py
@@ -0,0 +1,24 @@
+# main file for semaphore
+
+import os
+import sys
+import time
+import json
+import requests
+
+
+def server():
+    print("server")
+    return 0
+
+def setup():
+    print("setup")
+    return 0
+
+def test():
+    print("test")
+    return 0
+
+def version():
+    print("0.0.2")
+    return 0
