895180e4b64ba2d2a5c36c9637f7c6488341b4e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-18 04:05:21 +0000

    Add package nbmetaclean/0.0.7/nbmetaclean-0.0.7-py3-none-any.whl

---
+++ packages/nbmetaclean/nbmetaclean-0.0.7-py3-none-any.whl/nbmetaclean/app.py
@@ -0,0 +1,60 @@
+import argparse
+from pathlib import Path
+
+from .clean import CleanConfig, clean_nb_file
+from .core import get_nb_names
+
+parser = argparse.ArgumentParser(
+    prog="nbclean",
+    description="Clean metadata and execution_count from Jupyter notebooks.",
+)
+parser.add_argument(
+    "path",
+    default=".",
+    nargs="*",
+    help="Path for nb or folder with notebooks.",
+)
+parser.add_argument(
+    "-s",
+    "--silent",
+    action="store_true",
+    help="Silent mode.",
+)
+parser.add_argument(
+    "--not-pt",
+    action="store_true",
+    help="Do not preserve timestamp.",
+)
+
+
+def app() -> None:
+    """Clean metadata and execution_count from Jupyter notebook."""
+    cfg = parser.parse_args()
+    path_list = cfg.path if isinstance(cfg.path, list) else [cfg.path]
+    nb_files: list[Path] = []
+    if not cfg.silent:
+        print(f"Path: {', '.join(cfg.path)}, preserve timestamp: {not cfg.not_pt}")
+    for path in path_list:
+        try:
+            nb_files.extend(get_nb_names(path))
+        except FileNotFoundError:
+            print(f"{path} not exists!")
+    if not cfg.silent:
+        print(f"notebooks to check: {len(nb_files)} ")
+    cleaned, errors = clean_nb_file(
+        nb_files,
+        CleanConfig(
+            silent=cfg.silent,
+            preserve_timestamp=not cfg.not_pt,
+        ),
+    )
+    if not cfg.silent:
+        print(f"cleaned nbs: {len(cleaned)}")
+        if errors:
+            print(f"with errors: {len(errors)}")
+            for nb, exc in errors:
+                print(f"{nb}: {exc}")
+
+
+if __name__ == "__main__":
+    app()
---
+++ packages/nbmetaclean/nbmetaclean-0.0.7-py3-none-any.whl/nbmetaclean/core.py
@@ -0,0 +1,91 @@
+from __future__ import annotations
+
+import json
+from pathlib import Path
+from typing import Optional
+
+from .typing import NbNode, PathOrStr
+
+
+def read_nb(path: PathOrStr) -> NbNode:
+    """Read notebook from filename.
+
+    Args:
+        path (Union[str, PosixPath): Notebook filename.
+
+    Returns:
+        Notebook: Jupyter Notebook as dict.
+    """
+    return json.load(open(path, "r", encoding="utf-8"))
+
+
+def write_nb(
+    nb: NbNode,
+    path: PathOrStr,
+) -> Path:
+    """Write notebook to file
+
+    Args:
+        nb (Notebook): Notebook to write
+        path (Union[str, PosixPath]): filename to write
+    Returns:
+        Path: Filename of writed Nb.
+    """
+    filename = Path(path)
+    if filename.suffix != ".ipynb":
+        filename = filename.with_suffix(".ipynb")
+    with filename.open("w", encoding="utf-8") as fh:
+        fh.write(
+            json.dumps(
+                nb,
+                indent=1,
+                separators=(",", ": "),
+                ensure_ascii=False,
+                sort_keys=True,
+            )
+            + "\n",
+        )
+    return filename
+
+
+def get_nb_names(
+    path: Optional[PathOrStr] = None,
+    recursive: bool = True,
+    hidden: bool = False,
+) -> list[Path]:
+    """Return list of notebooks from `path`. If no `path` return notebooks from current folder.
+
+    Args:
+        path (Union[Path, str, None]): Path for nb or folder with notebooks.
+        recursive bool: Recursive search.
+        hidden bool: Skip or not hidden paths, defaults to False.
+
+    Raises:
+        sys.exit: If filename or dir not exists or not nb file.
+
+    Returns:
+        List[Path]: List of notebooks names.
+    """
+    nb_path = Path(path or ".")
+
+    if not nb_path.exists():
+        raise FileNotFoundError(f"{nb_path} not exists!")
+
+    if nb_path.is_dir():
+        result = []
+        for item in nb_path.iterdir():
+            if item.is_file() and item.suffix == ".ipynb":
+                if not hidden and item.name.startswith("."):
+                    continue
+                result.append(item)
+            if item.is_dir():
+                if recursive:
+                    if not hidden and item.name.startswith("."):
+                        continue
+                    if "checkpoint" in item.name:
+                        continue
+                    result.extend(get_nb_names(item, recursive, hidden))
+
+        return result
+
+    return [nb_path]
