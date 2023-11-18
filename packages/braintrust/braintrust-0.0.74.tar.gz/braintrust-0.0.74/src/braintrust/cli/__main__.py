21329ea2757054cb46c6473467fd1f52f4205f0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-18 06:07:47 +0000

    Add package braintrust/0.0.74/braintrust-0.0.74.tar.gz

---
+++ packages/braintrust/braintrust-0.0.74.tar.gz/braintrust-0.0.74/src/braintrust/cli/__main__.py
@@ -0,0 +1,37 @@
+import argparse
+import logging
+import os
+import sys
+import textwrap
+
+from . import eval, install
+
+
+def main(args=None):
+    """The main routine."""
+
+    # Add the current working directory to sys.path, similar to python's
+    # unittesting frameworks.
+    sys.path.insert(0, os.getcwd())
+
+    if args is None:
+        args = sys.argv[1:]
+
+    parent_parser = argparse.ArgumentParser(add_help=False)
+    parent_parser.add_argument("--verbose", "-v", default=False, action="store_true")
+
+    parser = argparse.ArgumentParser(description="braintrust is a cli tool to work with Braintrust.")
+    subparsers = parser.add_subparsers(help="sub-command help", dest="subcommand", required=True)
+
+    for module in [eval, install]:
+        module.build_parser(subparsers, parent_parser)
+
+    args = parser.parse_args(args=args)
+    level = logging.DEBUG if args.verbose else logging.INFO
+    logging.basicConfig(format="%(asctime)s %(levelname)s [%(name)s]: %(message)s", level=level)
+
+    return args.func(args)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
