d82799737de92b039fc3c06709e84a6bad661b98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-18 04:05:21 +0000

    Add package nestargs/1.1.0/nestargs-1.1.0.tar.gz

---
+++ packages/nestargs/nestargs-1.1.0.tar.gz/nestargs-1.1.0/src/nestargs/__init__.py
@@ -0,0 +1,2 @@
+from ._version import __version__  # noqa: F401
+from .parser import NestedArgumentParser  # noqa: F401
---
+++ packages/nestargs/nestargs-1.1.0.tar.gz/nestargs-1.1.0/tests/test_parser.py
@@ -0,0 +1,37 @@
+import nestargs
+import pytest
+
+
+class TestNestedArgumentParser:
+    def test_parse_args(self):
+        parser = nestargs.NestedArgumentParser()
+        parser.add_argument("some.a")
+        parser.add_argument("some.b")
+        parser.add_argument("some.c.d")
+
+        args = parser.parse_args(["1", "2", "3"])
+        assert args.some.a == "1"
+        assert args.some.b == "2"
+        assert args.some.c.d == "3"
+        assert vars(args.some).keys() == {"a", "b", "c"}
+        assert vars(args.some.c).keys() == {"d"}
+
+    def test_parse_args_with_another_delimiter(self):
+        parser = nestargs.NestedArgumentParser(delimiter="/")
+        parser.add_argument("some/a")
+        parser.add_argument("some/b")
+        parser.add_argument("some/c/d")
+
+        args = parser.parse_args(["1", "2", "3"])
+        assert args.some.a == "1"
+        assert args.some.b == "2"
+        assert args.some.c.d == "3"
+        assert vars(args.some).keys() == {"a", "b", "c"}
+        assert vars(args.some.c).keys() == {"d"}
+
+    def test_parse_args_with_empty_parent(self):
+        parser = nestargs.NestedArgumentParser()
+        parser.add_argument(".empty")
+
+        with pytest.raises(ValueError):
+            parser.parse_args([""])
