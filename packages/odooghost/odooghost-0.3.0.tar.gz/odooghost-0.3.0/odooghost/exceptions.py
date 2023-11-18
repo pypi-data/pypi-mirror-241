196d11f1d954b31139a66123e8f6d32b7bd9fdfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-18 05:04:27 +0000

    Add package odooghost/0.3.0/odooghost-0.3.0.tar.gz

---
+++ packages/odooghost/odooghost-0.3.0.tar.gz/odooghost-0.3.0/odooghost/exceptions.py
@@ -0,0 +1,74 @@
+class OdooGhostException(Exception):
+    ...
+
+
+class StreamOutputError(OdooGhostException):
+    ...
+
+
+class StreamParseError(OdooGhostException):
+    ...
+
+
+class ContextAlreadySetupError(OdooGhostException):
+    ...
+
+
+class CommonNetworkEnsureError(OdooGhostException):
+    ...
+
+
+class StackException(OdooGhostException):
+    ...
+
+
+class StackConfigError(StackException):
+    ...
+
+
+class StackImageBuildError(StackException):
+    ...
+
+
+class StackAlreadyExistsError(StackException):
+    ...
+
+
+class StackNotFoundError(StackException):
+    ...
+
+
+class StackImageEnsureError(StackException):
+    ...
+
+
+class StackImagePullError(StackImageEnsureError):
+    ...
+
+
+class StackVolumeCreateError(StackException):
+    ...
+
+
+class StackContainerCreateError(StackException):
+    ...
+
+
+class StackContainerGetError(StackException):
+    ...
+
+
+class StackContainerNotFound(StackException):
+    ...
+
+
+class StackContainerStartError(StackException):
+    ...
+
+
+class AddonsError(StackException):
+    ...
+
+
+class InvalidAddonsPathError(AddonsError):
+    ...
