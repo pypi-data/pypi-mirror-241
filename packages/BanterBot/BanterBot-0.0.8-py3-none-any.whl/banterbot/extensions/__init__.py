5d80ac3a343ec7b98fd98438367215eadb1721d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 04:04:30 +0000

    Add package BanterBot/0.0.8/BanterBot-0.0.8-py3-none-any.whl

---
+++ packages/BanterBot/BanterBot-0.0.8-py3-none-any.whl/banterbot/extensions/__init__.py
@@ -0,0 +1,9 @@
+from banterbot.extensions.interface import Interface
+from banterbot.extensions.option_selector import OptionSelector
+from banterbot.extensions.persona import Persona
+
+__all__ = [
+    "Interface",
+    "OptionSelector",
+    "Persona",
+]
