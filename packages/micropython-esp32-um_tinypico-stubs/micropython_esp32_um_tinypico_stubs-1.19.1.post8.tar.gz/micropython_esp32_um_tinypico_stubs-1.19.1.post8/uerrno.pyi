8011345b9b3118aba0143f2b4b99313cfac1edbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 04:04:42 +0000

    Add package micropython-esp32-um_tinypico-stubs/1.19.1.post8/micropython_esp32_um_tinypico_stubs-1.19.1.post8.tar.gz

---
+++ packages/micropython-esp32-um_tinypico-stubs/micropython_esp32_um_tinypico_stubs-1.19.1.post8.tar.gz/micropython_esp32_um_tinypico_stubs-1.19.1.post8/uerrno.pyi
@@ -0,0 +1,36 @@
+"""
+System error codes.
+
+MicroPython module: https://docs.micropython.org/en/v1.19.1/library/errno.html
+
+CPython module: :mod:`python:errno` https://docs.python.org/3/library/errno.html .
+
+This module provides access to symbolic error codes for `OSError` exception.
+A particular inventory of codes depends on :term:`MicroPython port`.
+"""
+from _typeshed import Incomplete
+from typing import Dict
+
+EACCES: int
+EADDRINUSE: int
+EAGAIN: int
+EALREADY: int
+EBADF: int
+ECONNABORTED: int
+ECONNREFUSED: int
+ECONNRESET: int
+EEXIST: int
+EHOSTUNREACH: int
+EINPROGRESS: int
+EINVAL: int
+EIO: int
+EISDIR: int
+ENOBUFS: int
+ENODEV: int
+ENOENT: int
+ENOMEM: int
+ENOTCONN: int
+EOPNOTSUPP: int
+EPERM: int
+ETIMEDOUT: int
+errorcode: dict
