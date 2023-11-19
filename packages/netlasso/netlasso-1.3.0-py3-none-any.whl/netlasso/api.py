aa605a3c114a08e48ec25fb27d60121a77a47584
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 04:04:21 +0000

    Add package netlasso/1.3.0/netlasso-1.3.0-py3-none-any.whl

---
+++ packages/netlasso/netlasso-1.3.0-py3-none-any.whl/netlasso/api.py
@@ -0,0 +1,90 @@
+import requests
+
+from .coreutils import log, __version__
+from .messages import message
+
+
+class Api:
+    def __init__(self, netlas_api_endpoint: str, github_api_endpoint: str):
+        self.search_endpoint = f"{netlas_api_endpoint}/responses/?q=%s&start=%s"
+        self.updates_endpoint = (
+            f"{github_api_endpoint}/repos/rly0nheart/netlasso/releases/latest"
+        )
+
+    @staticmethod
+    def get_data(endpoint: str) -> dict:
+        """
+        Fetches JSON data from a given API endpoint.
+
+        :param endpoint: The API endpoint to fetch data from.
+        :return: A JSON object from the endpoint if request was successful.
+            An empty JSON object if request unsuccessful.
+        """
+        try:
+            with requests.get(url=endpoint) as response:
+                if response.status_code == 200:
+                    return response.json()
+                else:
+                    log.error(
+                        message(
+                            message_type="error",
+                            message_key="api_error",
+                            error_message=response.json(),
+                        )
+                    )
+                    return {}
+        except requests.exceptions.RequestException as error:
+            log.error(
+                message(
+                    message_type="error",
+                    message_key="http_error",
+                    error_message=error,
+                )
+            )
+            return {}
+        except Exception as error:
+            log.critical(
+                message(
+                    message_type="error",
+                    message_key="unexpected_error",
+                    error_message=error,
+                )
+            )
+            return {}
+
+    def check_updates(self):
+        """
+        Checks for updates from the program's releases.
+        If the release version does not match the current program version, assume the program is outdated.
+        """
+        from rich.markdown import Markdown
+        from rich import print
+
+        response = self.get_data(endpoint=self.updates_endpoint)
+        if response.get("tag_name"):
+            remote_version = response.get("tag_name")
+            if remote_version != __version__:
+                log.info(
+                    message(
+                        message_type="info",
+                        message_key="update_found",
+                        program_name="Net Lasso",
+                        program_call_name="netlasso",
+                        release_version=remote_version,
+                        current_version=__version__,
+                    )
+                )
+
+                release_notes = Markdown(response.get("body"))
+                print(release_notes)
+
+    def search(self, query: str, page: int):
+        """
+        Searches Netlas.io and fetches search results that match a given query.
+
+        :param query: Search query string.
+        :param page: Page number to get results from (default is 0)
+        :return: A list of results that matched the query.
+        """
+        search_results = self.get_data(endpoint=self.search_endpoint % (query, page))
+        return search_results.get("items", [])
---
+++ packages/netlasso/netlasso-1.3.0-py3-none-any.whl/netlasso/coreutils.py
@@ -0,0 +1,173 @@
+import argparse
+import csv
+import json
+import logging
+import os
+
+from rich.logging import RichHandler
+from rich.markdown import Markdown
+from rich_argparse import RichHelpFormatter
+
+from . import __description__, __epilog__, __version__
+from .messages import message
+
+
+def path_finder():
+    """
+    Creates file directories if they don't already exist.
+    """
+    file_directories = [CSV_DIRECTORY, JSON_DIRECTORY]
+    for directory in file_directories:
+        os.makedirs(directory, exist_ok=True)
+
+
+def setup_logging(debug_mode: bool) -> logging.getLogger:
+    """
+    Configure and return a logging object with the specified log level.
+
+    :param debug_mode: A boolean value indicating whether debug mode should be enabled or not.
+    :return: A logging object configured with the specified log level.
+    """
+    logging.basicConfig(
+        level="NOTSET" if debug_mode else "INFO",
+        format="%(message)s",
+        handlers=[
+            RichHandler(
+                markup=True, log_time_format="%I:%M:%S %p", show_level=debug_mode
+            )
+        ],
+    )
+    return logging.getLogger("Net Lasso")
+
+
+def data_broker(api_data: dict, data_file: str) -> dict:
+    """
+    Formats API data based on a key mapping from a JSON file.
+
+    :param api_data: Dictionary containing raw data from the API.
+    :param data_file: Path to the JSON file that contains the key mapping.
+
+    :returns: A Formatted JSON object with human-readable keys.
+    """
+    # Construct path to the mapping data file
+    mapping_data_file = os.path.join(CURRENT_FILE_DIRECTORY, "data", data_file)
+
+    # Load the mapping from the specified file
+    with open(mapping_data_file, "r", encoding="utf-8") as file:
+        mapping_data = json.load(file)
+
+    # Initialize an empty dictionary to hold the formatted data
+    formatted_data = {}
+
+    # Map API data to human-readable format using the mapping
+    for api_data_key, mapping_data_key in mapping_data.items():
+        formatted_data[mapping_data_key] = api_data.get(api_data_key, "N/A")
+
+    return formatted_data
+
+
+def save_data(
+    data: dict,
+    filename: str,
+    save_to_json: bool = False,
+    save_to_csv: bool = False,
+):
+    """
+    Save the given data to JSON and/or CSV files based on the arguments.
+
+    :param data: The data to be saved, which is a list of dictionaries.
+    :param filename: The base filename to use when saving.
+    :param save_to_json: A boolean value to indicate whether to save data as a JSON file.
+    :param save_to_csv: A boolean value to indicate whether to save data as a CSV file.
+    """
+
+    # Save to JSON if save_json is True
+    if save_to_json:
+        with open(os.path.join(JSON_DIRECTORY, f"{filename}.json"), "w") as json_file:
+            json.dump(data, json_file)
+        log.info(
+            message(
+                message_type="info",
+                message_key="data_saved",
+                data_type="JSON",
+                file_path=json_file.name,
+            )
+        )
+
+    # Save to CSV if save_csv is True
+    if save_to_csv:
+        with open(
+            os.path.join(CSV_DIRECTORY, f"{filename}.csv"), "w", newline=""
+        ) as csv_file:
+            writer = csv.writer(csv_file)
+            # Write the header based on keys from the first dictionary
+            header = data.keys()
+            writer.writerow(header)
+
+            # Write each row
+            writer.writerow(data.values())
+        log.info(
+            message(
+                message_type="info",
+                message_key="data_saved",
+                data_type="CSV",
+                file_path=csv_file.name,
+            )
+        )
+
+
+def create_parser() -> argparse.ArgumentParser:
+    """
+    Creates and configures an argument parser for the command line arguments.
+
+    :return: A configured argparse.ArgumentParser object ready to parse the command line arguments.
+    """
+    parser = argparse.ArgumentParser(
+        description=Markdown(__description__, style="argparse.text"),
+        epilog=Markdown(__epilog__, style="argparse.text"),
+        formatter_class=RichHelpFormatter,
+    )
+    parser.add_argument("-q", "--query", help="Search query")
+    parser.add_argument(
+        "-p",
+        "--page",
+        type=int,
+        default=0,
+        help="Page number to get results from (default: %(default)s)",
+    )
+    parser.add_argument(
+        "-l",
+        "--limit",
+        type=int,
+        default=10,
+        help="Number of results to return (each page returns 20 results. default is %(default)s)",
+    )
+    parser.add_argument("-d", "--debug", help="Enable debug mode", action="store_true")
+    parser.add_argument(
+        "-r", "--raw", help="Return results in raw JSON format", action="store_true"
+    )
+
+    parser.add_argument(
+        "-j", "--json", help="Write results to a JSON file", action="store_true"
+    )
+    parser.add_argument(
+        "-c", "--csv", help="Write results to a CSV file", action="store_true"
+    )
+    parser.add_argument(
+        "-v", "--version", version=f"Net Lasso v{__version__}", action="version"
+    )
+
+    return parser
+
+
+args = create_parser().parse_args()
+log = setup_logging(debug_mode=args.debug)
+
+# Construct path to the program's directory
+PROGRAM_DIRECTORY = os.path.expanduser(os.path.join("~", "netlasso"))
+
+# Construct path to the current file's directory
+CURRENT_FILE_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
+# Construct paths to directories of CSV and JSON files.
+CSV_DIRECTORY = os.path.join(PROGRAM_DIRECTORY, "csv")
+JSON_DIRECTORY = os.path.join(PROGRAM_DIRECTORY, "json")
