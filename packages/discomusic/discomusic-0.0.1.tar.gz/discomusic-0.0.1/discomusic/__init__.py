787edb7a9f6c6aa870e01408348f27dbfe285e83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 03:05:54 +0000

    Add package discomusic/0.0.1/discomusic-0.0.1.tar.gz

---
+++ packages/discomusic/discomusic-0.0.1.tar.gz/discomusic-0.0.1/discomusic/__init__.py
@@ -0,0 +1,1 @@
+from .discomusic import setup
---
+++ packages/discomusic/discomusic-0.0.1.tar.gz/discomusic-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="discomusic",
+    version="0.0.1",
+    packages=find_packages(),
+    #install_requires = [
+    #    ""
+    #],
+    author="crzydev",
+    description="This is the perfect library for creating a music player discord bot!"
+)
