35a2ec1659b91b7f8c9864d4f56782a9dae73ccf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 03:06:13 +0000

    Add package t1847441/0.11/t1847441-0.11.tar.gz

---
+++ packages/t1847441/t1847441-0.11.tar.gz/t1847441-0.11/setup.py
@@ -0,0 +1,23 @@
+import sys
+
+from setuptools import setup, find_packages
+
+sys.path.append('src')
+
+
+setup(
+    name='t1847441',
+    version='0.11',
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    entry_points={
+        'console_scripts': [
+            't1847441=main:main',
+        ],
+    },
+    include_package_data=True,
+    package_data={
+        '': ['static/*'],
+    },
+)
+
