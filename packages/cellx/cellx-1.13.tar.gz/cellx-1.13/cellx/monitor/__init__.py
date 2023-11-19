d747999cb2883da8ebeded1c93883dc9dc2bc014
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 03:06:10 +0000

    Add package cellx/1.13/cellx-1.13.tar.gz

---
+++ packages/cellx/cellx-1.13.tar.gz/cellx-1.13/cellx/monitor/__init__.py
@@ -0,0 +1,9 @@
+from .color import *
+from .null import *
+from .sdl import *
+from .sdl_filter import *
+from .postscript import *
+try:
+    from .opengl import *
+except (ImportError, AttributeError) as e:
+    print('Failed to import cellx.monitor.opengl module.  Continue without OpenGL support...')
