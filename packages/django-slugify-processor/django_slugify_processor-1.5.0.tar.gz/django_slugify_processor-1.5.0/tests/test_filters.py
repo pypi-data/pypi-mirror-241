66821a2e0143c5a2981f2d315bd055fd859dcf93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 04:04:18 +0000

    Add package django-slugify-processor/1.5.0/django_slugify_processor-1.5.0.tar.gz

---
+++ packages/django-slugify-processor/django_slugify_processor-1.5.0.tar.gz/django_slugify_processor-1.5.0/tests/test_filters.py
@@ -0,0 +1,30 @@
+import typing as t
+
+from django.template import Context, Template
+
+
+def test_slugify_via_builtin_override(settings: t.Any) -> None:
+    settings.SLUGIFY_PROCESSORS = ["test_app.coding.slugify_programming"]
+    settings.TEMPLATES = [
+        {
+            "BACKEND": "django.template.backends.django.DjangoTemplates",
+            "APP_DIRS": True,
+            "OPTIONS": {
+                "builtins": ["django_slugify_processor.templatetags.slugify_processor"]
+            },
+        }
+    ]
+
+    template = Template("{{'c++'|slugify}}")
+    assert template.render(Context({})) == "cpp"
+
+
+def test_slugify_via_load_templatetags(settings: t.Any) -> None:
+    settings.SLUGIFY_PROCESSORS = ["test_app.coding.slugify_programming"]
+    settings.INSTALLED_APPS = ["django_slugify_processor"]
+    settings.TEMPLATES = [
+        {"BACKEND": "django.template.backends.django.DjangoTemplates", "APP_DIRS": True}
+    ]
+
+    template = Template('{% load slugify_processor %}{{"c++"|slugify}}')
+    assert template.render(Context({})) == "cpp"
