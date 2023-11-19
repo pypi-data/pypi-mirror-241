83e5f865a4a4f956c1dbe179f96fb701978b9f5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 04:04:17 +0000

    Add package kandinsky-api-gigachat/1.0.0/kandinsky-api-gigachat-1.0.0.tar.gz

---
+++ packages/kandinsky-api-gigachat/kandinsky-api-gigachat-1.0.0.tar.gz/kandinsky-api-gigachat-1.0.0/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+	name='kandinsky-api-gigachat',
+	version='1.0.0',
+	author='__token__',
+	author_email='aleksfolt@ya.ru',
+	description='100 percent free kandinsky',
+	packages=['kandinsky-api-gigachat'],
+	include_package_data=True,
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
