eaf01867b8319c81062a92f436a4d09edbecaa82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 14:05:28 +0000

    Add package local-first-linguist/0.2.1/local-first-linguist-0.2.1.tar.gz

---
+++ packages/local-first-linguist/local-first-linguist-0.2.1.tar.gz/local-first-linguist-0.2.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='local-first-linguist',
+    version='0.2.1',
+    author_email='me@liangrunda.com',
+    license='MIT',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'translate=src.cli:cli'
+        ]
+    },
+    install_requires=[
+        'tqdm>=4.0.0'
+    ]
+)
