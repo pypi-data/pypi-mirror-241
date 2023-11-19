308ad70f5e9794112cb4100ae4f315cd07887bd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 14:05:45 +0000

    Add package hTrackerIU/0.2/hTrackerIU-0.2-py3-none-any.whl

---
+++ packages/hTrackerIU/hTrackerIU-0.2-py3-none-any.whl/hTrackerIU/api/main.py
@@ -0,0 +1,41 @@
+import os
+from pathlib import Path
+
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+
+from .routes import router
+
+environment = os.getenv("ENV", "prod")
+
+app = FastAPI()
+
+if environment != "prod":
+    origins = [
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+    ]
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+app.include_router(router, prefix="/api")
+
+if environment == "prod":
+    app.mount("/static", StaticFiles(directory="src/hTrackerIU/static/static"), name="static")
+
+    @app.get("/{full_path:path}", response_class=HTMLResponse)
+    async def catch_all(request: Request):
+        if not request.url.path.startswith("/api"):
+            return Path("src/hTrackerIU/static/index.html").read_text()
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
