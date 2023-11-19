2f5cde1b310264cb6d547e9043803f968b615398
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 15:05:04 +0000

    Add package allianceauth-auth-stats/0.0.1b3/allianceauth-auth-stats-0.0.1b3.tar.gz

---
+++ packages/allianceauth-auth-stats/allianceauth-auth-stats-0.0.1b3.tar.gz/allianceauth-auth-stats-0.0.1b3/authstats/managers.py
@@ -0,0 +1,7 @@
+import logging
+
+from allianceauth.eveonline.models import EveCorporationInfo
+from django.core.exceptions import ObjectDoesNotExist
+from django.db import models
+
+logger = logging.getLogger(__name__)
