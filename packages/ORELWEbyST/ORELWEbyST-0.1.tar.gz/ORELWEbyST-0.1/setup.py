bb8c536ff2d5caac2be27dddb24bf020ee4204d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 15:04:53 +0000

    Add package ORELWEbyST/0.1/ORELWEbyST-0.1.tar.gz

---
+++ packages/ORELWEbyST/ORELWEbyST-0.1.tar.gz/ORELWEbyST-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+name = 'ORELWEbyST',
+version = '0.1',
+author = 'Jaehwan Park',
+author_email = 'jpark127@utk.edu',
+python_requires = '>=3.6'
+)
