76ea4a446983221098fb813147943549ca90b047
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 14:05:28 +0000

    Add package mhacks1/0.9/mhacks1-0.9.tar.gz

---
+++ packages/mhacks1/mhacks1-0.9.tar.gz/mhacks1-0.9/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="mhacks1",
+    version="0.9",
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A small example package",
+    url="http://example.com/my_package",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'openai',
+    ],
+)
