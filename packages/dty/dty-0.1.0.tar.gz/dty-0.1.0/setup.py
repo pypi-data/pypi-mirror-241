08535bd91da7b5884f70eab646c4e4433c550477
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 04:04:30 +0000

    Add package dty/0.1.0/dty-0.1.0.tar.gz

---
+++ packages/dty/dty-0.1.0.tar.gz/dty-0.1.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dty',
+    version='0.1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    description='A tool to convert directory contents to YAML.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/adriangalilea/dty',
+    author='Adrian Galilea Delgado',
+    author_email='adriangalilea@gmail.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        'rich',
+        'pyperclip',
+        'pyyaml'
+    ],
+    entry_points={
+        'console_scripts': [
+            'dtj=dtj.main:main',
+        ],
+    },
+)
