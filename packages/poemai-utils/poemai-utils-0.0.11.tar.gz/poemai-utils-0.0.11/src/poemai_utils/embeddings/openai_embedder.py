5d98771a245c8a9eeeeb34f40660e46f89546f98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 04:05:10 +0000

    Add package poemai-utils/0.0.11/poemai-utils-0.0.11.tar.gz

---
+++ packages/poemai-utils/poemai-utils-0.0.11.tar.gz/poemai-utils-0.0.11/src/poemai_utils/embeddings/openai_embedder.py
@@ -0,0 +1,23 @@
+import numpy as np
+from poemai_utils.embeddings.embedder_base import EbedderBase
+
+
+class OpenAIEmbedder(EbedderBase):
+    def __init__(self, model_name="text-embedding-ada-002"):
+        try:
+            from openai import OpenAI
+        except ImportError:
+            raise ImportError(
+                "You must install openai to use this function. Try: pip install openai"
+            )
+
+        super().__init__()
+
+        self.model_name = model_name
+        self.client = OpenAI()
+
+    def calc_embedding(self, text, is_query: bool = False):
+        response = self.client.embeddings.create(input=text, model=self.model_name)
+        embedding = response.data[0].embedding
+        embedding = np.array(embedding, dtype=np.float32)
+        return embedding
