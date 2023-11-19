c60b3bb2b3ecabbe10a27fd328bc940fad242656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 04:04:20 +0000

    Add package cli-requests/0.2.1/cli-requests-0.2.1.tar.gz

---
+++ packages/cli-requests/cli-requests-0.2.1.tar.gz/cli-requests-0.2.1/setup.py
@@ -0,0 +1,33 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="cli-requests",
+    version="0.2.1",
+    description="simple cli program to send requests",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Sabberian/cli_requests",
+    author="sabberian",
+    license="MIT",
+    entry_points={
+        'console_scripts': [
+            'cli-requests=cli_requests.__main__:main',
+        ],
+    },
+    python_requires='>=3.8',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests==2.31.0',
+        'colorama==0.4.6',
+    ],
+)
