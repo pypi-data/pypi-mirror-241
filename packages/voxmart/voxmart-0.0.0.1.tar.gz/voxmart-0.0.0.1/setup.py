f0f09bd26c50243c880b86cf16734cbad9ace985
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 03:06:35 +0000

    Add package voxmart/0.0.0.1/voxmart-0.0.0.1.tar.gz

---
+++ packages/voxmart/voxmart-0.0.0.1.tar.gz/voxmart-0.0.0.1/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup
+
+setup(name='voxmart', version='0.0.0.1', description='voxmart api manager', author='ImaniMrosso', packages=['voxmart'], zip_safe=False)
---
+++ packages/voxmart/voxmart-0.0.0.1.tar.gz/voxmart-0.0.0.1/voxmart/__init__.py
@@ -0,0 +1,1 @@
+from .voxmart import PersonalBMI
