8e2dbf2e8fbbfb3070daa86b4fc944159e2f9bda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 14:05:40 +0000

    Add package PythAMS/0.0.5/PythAMS-0.0.5.tar.gz

---
+++ packages/PythAMS/PythAMS-0.0.5.tar.gz/PythAMS-0.0.5/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pythams',
+    version='0.0.5',
+    author='Will S, Sara A',
+    author_email='wrs225@stanford.edu',
+    description='A Python package for synthesizing and simulating fixed-point models of continuous systems.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
