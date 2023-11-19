d9e713dd2135fa47f71e63fc19001f1faba40f8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 15:04:51 +0000

    Add package GPTPlugins4All/1.0.2/GPTPlugins4All-1.0.2.tar.gz

---
+++ packages/GPTPlugins4All/GPTPlugins4All-1.0.2.tar.gz/GPTPlugins4All-1.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='GPTPlugins4All',
+    version='1.0.2',
+    packages=find_packages(),
+    description='GPT Plugins for 4all',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author_email='trevor@zenithai.io',
+    url='https://github.com/tcmartin/gpt-plugins-4all',
+    install_requires=[
+        'PyYAML', 'requests', 'openapi-spec-validator'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    
+)
