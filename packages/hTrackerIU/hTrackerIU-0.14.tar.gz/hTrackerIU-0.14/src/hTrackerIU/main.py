901176bc734fcc23daa1839626eb758d73a867a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 15:04:49 +0000

    Add package hTrackerIU/0.14/hTrackerIU-0.14.tar.gz

---
+++ packages/hTrackerIU/hTrackerIU-0.14.tar.gz/hTrackerIU-0.14/src/hTrackerIU/main.py
@@ -0,0 +1,32 @@
+import webbrowser
+from threading import Timer
+
+import uvicorn
+from sqlalchemy import create_engine, inspect
+
+from .infrastructure.config import Config
+from .init_db import init_database
+
+
+def open_browser():
+    webbrowser.open_new("http://localhost:8095")
+
+
+def main():
+    engine = create_engine(Config.DATABASE_URL)
+    inspector = inspect(engine)
+
+    if not inspector.get_table_names():
+        print("init_database...")
+        init_database()
+    else:
+        print(inspector.get_table_names())
+
+    Timer(1, open_browser).start()
+
+    uvicorn.run("hTrackerIU.api.main:app",
+                host="0.0.0.0", port=8095, reload=True)
+
+
+if __name__ == "__main__":
+    main()
