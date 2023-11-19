3b21ea3a4926347e1c9f916a8b712ad47fe82a09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 03:06:44 +0000

    Add package filum-utils/0.1.17/filum-utils-0.1.17.tar.gz

---
+++ packages/filum-utils/filum-utils-0.1.17.tar.gz/filum-utils-0.1.17/setup.py
@@ -0,0 +1,38 @@
+import os
+import sys
+
+from setuptools import setup, find_packages
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'filum_analytics'))
+
+VERSION = "0.1.17"
+DESCRIPTION = "Filum Utils"
+LONG_DESCRIPTION = "Filum Utils"
+
+install_requires = [
+    "requests==2.25.1",
+    "filum-analytics-python==1.1.1",
+    "glom==20.11.0",
+    "pyexcel==0.7.0",
+    "pyexcel-xls==0.7.0",
+    "pyexcel-xlsx==0.6.0",
+    "openpyxl==3.0.10",
+]
+
+setup(
+    name="filum-utils",
+    version=VERSION,
+    author="Hiep Nguyen",
+    author_email="<hnguyen@filum.ai>",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=install_requires,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ]
+)
