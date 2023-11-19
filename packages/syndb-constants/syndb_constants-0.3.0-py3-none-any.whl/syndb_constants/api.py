9e4501b8af098194ddfe1838d8c6e044924eb3e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 04:04:14 +0000

    Add package syndb-constants/0.3.0/syndb_constants-0.3.0-py3-none-any.whl

---
+++ packages/syndb-constants/syndb_constants-0.3.0-py3-none-any.whl/syndb_constants/api.py
@@ -0,0 +1,10 @@
+
+TEST_USERNAME: str = "caniko@syndb.xyz"
+TEST_PASSWORD: str = "systar"
+
+ANIMAL_SPECIES_CSV_NAME = "animal_species.csv"
+ANIMAL_CULTURAL_NAME_CSV_NAME = "animal_cultural_name.csv"
+BRAIN_REGION_CSV_NAME = "brain_region.csv"
+MUTATION_CSV_NAME = "mutation.csv"
+MICROSCOPY_CSV_NAME = "microscopy.csv"
+PUBLICATION_CSV_NAME = "publication.csv"
