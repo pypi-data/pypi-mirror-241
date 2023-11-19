8976afa5d3156a32cded02ebd280b379eacbf064
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 14:05:25 +0000

    Add package hillcipher/0.0.2/hillcipher-0.0.2.tar.gz

---
+++ packages/hillcipher/hillcipher-0.0.2.tar.gz/hillcipher-0.0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2'
+DESCRIPTION = 'Hill Cipher (Encryption and Decryption)'
+# LONG_DESCRIPTION = 'A package that allows to encrypt and decrypt the text using the Hill Cipher technique. Checkout the repository(https://github.com/Kirandeep2806/Hill-Cipher) for codebase and snippets.'
+
+# Setting up
+setup(
+    name="hillcipher",
+    version=VERSION,
+    author="NooB c0deR (Kiran Deep)",
+    author_email="kirandeep102030@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=open("README.md", "rt").read(),
+    packages=find_packages(),
+    install_requires=['numpy', 'mathmatrix'],
+    keywords=['python', 'cryptography', 'hill', 'hillcipher', 'encryption', 'decryption', 'symmetric encryption'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Security :: Cryptography",
+        "Intended Audience :: Education",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
