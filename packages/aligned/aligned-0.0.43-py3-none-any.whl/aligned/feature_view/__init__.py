3ee34f928ac5c62f5e9dc28dc8d1b6c66b3ea4a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 15:05:17 +0000

    Add package aligned/0.0.43/aligned-0.0.43-py3-none-any.whl

---
+++ packages/aligned/aligned-0.0.43-py3-none-any.whl/aligned/feature_view/__init__.py
@@ -0,0 +1,15 @@
+from aligned.feature_view.combined_view import (
+    CombinedFeatureView,
+    CombinedFeatureViewMetadata,
+    combined_feature_view,
+)
+from aligned.feature_view.feature_view import FeatureView, FeatureViewMetadata, feature_view
+
+__all__ = [
+    'FeatureView',
+    'FeatureViewMetadata',
+    'CombinedFeatureView',
+    'CombinedFeatureViewMetadata',
+    'feature_view',
+    'combined_feature_view',
+]
