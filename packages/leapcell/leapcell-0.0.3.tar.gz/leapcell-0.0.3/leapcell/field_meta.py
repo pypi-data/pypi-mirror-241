6863c0b2b13411fd8ba827700435ec7f485b92aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 15:05:10 +0000

    Add package leapcell/0.0.3/leapcell-0.0.3.tar.gz

---
+++ packages/leapcell/leapcell-0.0.3.tar.gz/leapcell-0.0.3/leapcell/field_meta.py
@@ -0,0 +1,31 @@
+from typing import Dict
+
+
+class FieldMeta:
+    def __init__(self, obj: Dict) -> None:
+        self._id: str = obj["id"]
+        self._type: str = obj["type"]
+        self._name: str = obj["name"]
+        pass
+
+    @property
+    def type(self):
+        return self._type
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def name(self):
+        return self._name
+
+    def __str__(self) -> str:
+        return "<field id: {} name: {}, type: {}>".format(
+            self.id,
+            self.name,
+            self.type,
+        )
+
+    def __repr__(self) -> str:
+        return self.__str__()
