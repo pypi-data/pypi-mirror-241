57af66d6e6af629e647c9ebd0bf25fe71b819d52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 14:05:30 +0000

    Add package pytreegrav/1.1.4/pytreegrav-1.1.4.tar.gz

---
+++ packages/pytreegrav/pytreegrav-1.1.4.tar.gz/pytreegrav-1.1.4/setup.py
@@ -0,0 +1,33 @@
+import setuptools, os
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+thelibFolder = os.path.dirname(os.path.realpath(__file__))
+requirementPath = thelibFolder + "/requirements.txt"
+install_requires = []
+if os.path.isfile(requirementPath):
+    with open(requirementPath) as f:
+        install_requires = f.read().splitlines()
+
+setuptools.setup(
+    name="pytreegrav",
+    version="1.1.4",
+    author="Mike Grudic",
+    author_email="mike.grudich@gmail.com",
+    description="Fast approximate gravitational force and potential calculations",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    project_urls={
+        "Bug Tracker": "https://github.com/mikegrudic/pytreegrav",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires=install_requires,
+)
