a8d26f481d9e56454e2c2e9d30f86a46f5cdc5b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 14:05:40 +0000

    Add package torch-optimi/0.1.1/torch_optimi-0.1.1-py3-none-any.whl

---
+++ packages/torch-optimi/torch_optimi-0.1.1-py3-none-any.whl/optimi/__init__.py
@@ -0,0 +1,10 @@
+from importlib.metadata import version
+
+__version__ = version("torch-optimi")
+
+from .adam import Adam, adam
+from .adamw import AdamW, adamw
+from .adan import Adan, adan
+from .lion import Lion, lion
+from .sgd import SGD, sgd
+from .stableadamw import StableAdamW, stableadamw
