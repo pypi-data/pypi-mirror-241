a02de0010b6afeccfc4a43dc005b84415f155098
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 03:05:57 +0000

    Add package hello-func/0.0.3/hello-func-0.0.3.tar.gz

---
+++ packages/hello-func/hello-func-0.0.3.tar.gz/hello-func-0.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+with open ("app/README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+    name="hello-func",
+    version='0.0.3',
+    description="Noway, this prints hello! OMG 360 NOSCOPE MLG",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="AlphaPotat",
+    author_email="luchinin.n@yandex.ru",
+    package_dir={"":"app"},
+    packages=find_packages(where="app"),
+    url="https://github.com/Alpha-Potat/hello",
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent"
+    ],
+    install_requires=[
+        "twine", "python-dotenv", "wheel"
+        ],
+)
