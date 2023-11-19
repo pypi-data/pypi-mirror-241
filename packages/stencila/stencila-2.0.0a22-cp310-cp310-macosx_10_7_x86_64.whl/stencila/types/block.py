610c928551db285a47df27ed19ec34f6e2e39d67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 14:05:26 +0000

    Add package stencila/2.0.0a22/stencila-2.0.0a22-cp310-cp310-macosx_10_7_x86_64.whl

---
+++ packages/stencila/stencila-2.0.0a22-cp310-cp310-macosx_10_7_x86_64.whl/stencila/types/block.py
@@ -0,0 +1,49 @@
+# Generated file; do not edit. See the Rust `schema-gen` crate.
+
+from .prelude import *
+
+Admonition = ForwardRef("Admonition")
+Call = ForwardRef("Call")
+Claim = ForwardRef("Claim")
+CodeBlock = ForwardRef("CodeBlock")
+CodeChunk = ForwardRef("CodeChunk")
+Division = ForwardRef("Division")
+Figure = ForwardRef("Figure")
+For = ForwardRef("For")
+Form = ForwardRef("Form")
+Heading = ForwardRef("Heading")
+If = ForwardRef("If")
+Include = ForwardRef("Include")
+List = ForwardRef("List")
+MathBlock = ForwardRef("MathBlock")
+Paragraph = ForwardRef("Paragraph")
+QuoteBlock = ForwardRef("QuoteBlock")
+Section = ForwardRef("Section")
+Table = ForwardRef("Table")
+ThematicBreak = ForwardRef("ThematicBreak")
+
+
+Block = Union[
+    Admonition,
+    Call,
+    Claim,
+    CodeBlock,
+    CodeChunk,
+    Division,
+    Figure,
+    For,
+    Form,
+    Heading,
+    If,
+    Include,
+    List,
+    MathBlock,
+    Paragraph,
+    QuoteBlock,
+    Section,
+    Table,
+    ThematicBreak,
+]
+"""
+Union type in block content node types.
+"""
