584ed5e30f9020dbb8baadee1ea4879b4a565141
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 14:05:40 +0000

    Add package mhacks1/1.0/mhacks1-1.0-py3-none-any.whl

---
+++ packages/mhacks1/mhacks1-1.0-py3-none-any.whl/mhacks1/testing1.py
@@ -0,0 +1,40 @@
+import sys
+import subprocess
+import openai
+
+def explain_error_with_gpt(error_message):
+    openai.api_key = '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'
+    try:
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=f"Explain the following Python error and suggest possible solutions:\n\n{error_message}",
+            max_tokens=250,
+        )
+        return response.choices[0].text.strip()
+    except Exception as e:
+        return f"Error in contacting OpenAI API: {str(e)}"
+
+
+def run_script(script_path):
+    try:
+        output = subprocess.check_output([sys.executable, script_path], stderr=subprocess.STDOUT)
+        print(output.decode())
+    except subprocess.CalledProcessError as e:
+        error_output = e.output.decode()
+        explanation = explain_error_with_gpt(error_output)
+        print("titanic")
+        print(error_output)
+        print("titanic")
+        print(explanation)
+        print("titanic")
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: mhacks-run <script_path>")
+        sys.exit(1)
+
+    script_path = sys.argv[1]
+    run_script(script_path)
+
+if __name__ == "__main__":
+    main()
