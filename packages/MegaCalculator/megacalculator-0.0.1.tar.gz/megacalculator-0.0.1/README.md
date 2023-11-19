20a1eefb71c39da7cd06ea8f4bf2ca1c331843ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 04:05:19 +0000

    Add package MegaCalculator/0.0.1/megacalculator-0.0.1.tar.gz

---
+++ packages/MegaCalculator/megacalculator-0.0.1.tar.gz/megacalculator-0.0.1/README.md
@@ -0,0 +1,35 @@
+This is a simple calculator package.
+
+Supported commands are:
+
+MegaCalculator.addition(x, y)
+
+MegaCalculator.subtraction(x, y)
+    
+MegaCalculator.multiplication(x, y)
+    
+MegaCalculator.division(x, y)
+    
+MegaCalculator.addition3(x, y, z)
+
+MegaCalculator.subtraction3(x, y, z)
+    
+MegaCalculator.multiplication3(x, y, z)
+    
+MegaCalculator.division3(x, y, z)
+    
+MegaCalculator.addition4(x, y, z, q)
+
+MegaCalculator.subtraction4(x, y, z, q)
+    
+MegaCalculator.multiplication4(x, y, z, q)
+    
+MegaCalculator.division4(x, y, z, q)
+    
+MegaCalculator.addition5(x, y, z, q, w)
+
+MegaCalculator.subtraction5(x, y, z, q, w)
+    
+MegaCalculator.multiplication5(x, y, z, q, w)
+    
+MegaCalculator.division5(x, y, z, q, w)
---
+++ packages/MegaCalculator/megacalculator-0.0.1.tar.gz/megacalculator-0.0.1/src/MegaCalculator/MegaCalculator.py
@@ -0,0 +1,47 @@
+def addition(x, y):
+    print(x + y)
+
+def subtraction(x, y):
+    print(x - y)
+    
+def multiplication(x, y):
+    print(x * y)
+    
+def division(x, y):
+    print(x / y)
+    
+def addition3(x, y, z):
+    print(x + y + z)
+
+def subtraction3(x, y, z):
+    print(x - y - z)
+    
+def multiplication3(x, y, z):
+    print(x * y * z)
+    
+def division3(x, y, z):
+    print(x / y / z)
+    
+def addition4(x, y, z, q):
+    print(x + y + z + q)
+
+def subtraction4(x, y, z, q):
+    print(x - y - z - q)
+    
+def multiplication4(x, y, z, q):
+    print(x * y * z * q)
+    
+def division4(x, y, z, q):
+    print(x / y / z / q)
+    
+def addition5(x, y, z, q, w):
+    print(x + y + z + q + w)
+
+def subtraction5(x, y, z, q, w):
+    print(x - y - z - q - w)
+    
+def multiplication5(x, y, z, q, w):
+    print(x * y * z * q * w)
+    
+def division5(x, y, z, q, w):
+    print(x / y / z / q / w)
