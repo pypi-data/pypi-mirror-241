ece9be3784e9e14af1f2afffd97553d928eefa85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 14:05:28 +0000

    Add package pyleb128/0.2.3/pyleb128-0.2.3.tar.gz

---
+++ packages/pyleb128/pyleb128-0.2.3.tar.gz/pyleb128-0.2.3/pyleb128/_base.py
@@ -0,0 +1,51 @@
+# --------------------------------------------------------------------------------------
+#  Copyright(C) 2023 yntha                                                             -
+#                                                                                      -
+#  This program is free software: you can redistribute it and/or modify it under       -
+#  the terms of the GNU General Public License as published by the Free Software       -
+#  Foundation, either version 3 of the License, or (at your option) any later          -
+#  version.                                                                            -
+#                                                                                      -
+#  This program is distributed in the hope that it will be useful, but WITHOUT ANY     -
+#  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A     -
+#  PARTICULAR PURPOSE. See the GNU General Public License for more details.            -
+#                                                                                      -
+#  You should have received a copy of the GNU General Public License along with        -
+#  this program. If not, see <http://www.gnu.org/licenses/>.                           -
+# --------------------------------------------------------------------------------------
+
+# maximum byte length for a LEB128.
+LEB128_MAX_SIZE = 5
+
+
+class InvalidVarint(ValueError):
+    pass
+
+
+class _LEB128(int):
+    def __new__(cls, num: int, *args, **kwargs):
+        if num is None:
+            num = 0
+
+        return super().__new__(cls, num)
+
+    def __init__(self, num: int):
+        self.value = num
+        self.size = self.calcsize(num)
+
+    def __str__(self):
+        return str(self.value)
+
+    @staticmethod
+    def calcsize(n):
+        s = 0
+
+        while True:
+            n >>= 7
+
+            if n <= 0:
+                break
+
+            s += 1
+
+        return max(s, 1)
