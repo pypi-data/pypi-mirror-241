03ea4226a70602a1b7f20022e7a105990224c152
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 15:05:17 +0000

    Add package tarik-cryptography-rsa/0.1/tarik_cryptography_rsa-0.1.tar.gz

---
+++ packages/tarik-cryptography-rsa/tarik_cryptography_rsa-0.1.tar.gz/tarik_cryptography_rsa-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tarik_cryptography_rsa',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        "cryptography"
+    ],
+)
