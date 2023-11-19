fbb9ac634db0bf3c6f1e29ee1e1fc238e894a979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 03:05:32 +0000

    Add package open-hypergraphs/0.1.0/open_hypergraphs-0.1.0.tar.gz

---
+++ packages/open-hypergraphs/open_hypergraphs-0.1.0.tar.gz/open_hypergraphs-0.1.0/open_hypergraphs/array/__init__.py
@@ -0,0 +1,11 @@
+"""Array backends for :ref:`open_hypergraphs.finite_function`.
+
+.. warning::
+   This part of the API is likely to change significantly in future releases.
+
+.. autosummary::
+    :toctree: _autosummary
+    :recursive:
+
+    open_hypergraphs.array.numpy
+"""
