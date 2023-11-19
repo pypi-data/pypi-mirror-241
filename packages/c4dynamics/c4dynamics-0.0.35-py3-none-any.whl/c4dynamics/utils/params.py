e6cdea387070eb5d6a2a7f541cf1d63ea9689069
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 04:04:16 +0000

    Add package c4dynamics/0.0.35/c4dynamics-0.0.35-py3-none-any.whl

---
+++ packages/c4dynamics/c4dynamics-0.0.35-py3-none-any.whl/c4dynamics/utils/params.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+''' global parameters '''
+pi          = np.pi
+g           = 9.80665  # 32.2        # 
+
+''' conversion variables '''
+ft2m        = 0.3048    # 1             # 
+lbft2kgm    = 4.88243 # 47.8803 to include gravity        # 1      
+r2d         = 180 / np.pi 
+d2r         = np.pi / 180 
+
+''' math functions  '''
+sin         = np.sin
+cos         = np.cos
+tan         = np.tan
+arcsin      = np.arcsin
+arccos      = np.arccos
+arctan      = np.arctan
+
+
+
+
