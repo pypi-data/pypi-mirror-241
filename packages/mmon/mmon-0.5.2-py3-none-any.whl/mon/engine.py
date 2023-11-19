03fae15a34e846c75ca8efc7d6388bb6b2933f31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 03:05:29 +0000

    Add package mmon/0.5.2/mmon-0.5.2-py3-none-any.whl

---
+++ packages/mmon/mmon-0.5.2-py3-none-any.whl/mon/engine.py
@@ -0,0 +1,20 @@
+import openai
+from langchain.agents import AgentType, Tool, initialize_agent
+
+from mon.tools import load_tools
+
+# openai.log = "debug"
+
+
+class Engine:
+    def __init__(self, llm, verbose_level=0):
+        self.agent = initialize_agent(
+            load_tools(llm),
+            llm,
+            agent=AgentType.OPENAI_MULTI_FUNCTIONS,
+            verbose=verbose_level > 1,
+        )
+
+    def run(self, prompt: str) -> str:
+        response = self.agent.run(prompt)
+        return response
