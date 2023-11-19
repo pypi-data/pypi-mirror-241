0739035d7b671fe1844d7ad39709fb690bdbf84c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 15:05:12 +0000

    Add package panel-modal/0.4.0/panel_modal-0.4.0-py3-none-any.whl

---
+++ packages/panel-modal/panel_modal-0.4.0-py3-none-any.whl/panel_modal/__init__.py
@@ -0,0 +1,18 @@
+"""This package provides a Panel Modal that works both on the server and in the notebook.
+
+..code-block::
+
+    import panel as pn
+    from panel_modal import Modal
+
+    pn.extension()
+
+    modal = Modal(pn.panel("Hi. I am the Panel Modal!", width=200))
+
+    pn.Column(modal.param.open, modal).servable()
+"""
+from .modal import Modal
+
+VERSION = "0.4.0"
+
+__all__ = ["Modal", "VERSION"]
