de6bb4f5eb2580d997316b1065bc3d26f84cc7f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 04:05:07 +0000

    Add package pyalex/0.13/pyalex-0.13.tar.gz

---
+++ packages/pyalex/pyalex-0.13.tar.gz/pyalex-0.13/pyalex/__init__.py
@@ -0,0 +1,50 @@
+try:
+    from pyalex._version import __version__
+    from pyalex._version import __version_tuple__
+except ImportError:
+    __version__ = "0.0.0"
+    __version_tuple__ = (0, 0, 0)
+
+from pyalex.api import Author
+from pyalex.api import Authors
+from pyalex.api import Concept
+from pyalex.api import Concepts
+from pyalex.api import Funder
+from pyalex.api import Funders
+from pyalex.api import Institution
+from pyalex.api import Institutions
+from pyalex.api import Journals
+from pyalex.api import People
+from pyalex.api import Publisher
+from pyalex.api import Publishers
+from pyalex.api import Source
+from pyalex.api import Sources
+from pyalex.api import Venue
+from pyalex.api import Venues
+from pyalex.api import Work
+from pyalex.api import Works
+from pyalex.api import config
+from pyalex.api import invert_abstract
+
+__all__ = [
+    "Works",
+    "Work",
+    "Authors",
+    "Author",
+    "Sources",
+    "Source",
+    "Funder",
+    "Funders",
+    "Publishers",
+    "Publisher",
+    "Venues",
+    "Venue",
+    "Institutions",
+    "Institution",
+    "Concepts",
+    "Concept",
+    "People",
+    "Journals",
+    "config",
+    "invert_abstract",
+]
