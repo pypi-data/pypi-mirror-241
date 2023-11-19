11ced949cb76d171604c0286d1046d0d97dcfdad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 14:05:40 +0000

    Add package einnovationlabs-python-lib/0.0.1a1/einnovationlabs-python-lib-0.0.1a1.tar.gz

---
+++ packages/einnovationlabs-python-lib/einnovationlabs-python-lib-0.0.1a1.tar.gz/einnovationlabs-python-lib-0.0.1a1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="einnovationlabs-python-lib",
+    version="0.0.1-alpha-1",
+    author="Developer Team",
+    author_email="dev@einnovationlabs.com",
+    description="A package for e-innovation labs' utility functions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/einnovationlabs/einnovationlabs-python-lib",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
