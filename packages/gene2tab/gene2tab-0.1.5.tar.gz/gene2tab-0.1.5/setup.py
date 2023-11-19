d61e90b8dc194aa39e646c2236dcced5def1e6c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 03:04:42 +0000

    Add package gene2tab/0.1.5/gene2tab-0.1.5.tar.gz

---
+++ packages/gene2tab/gene2tab-0.1.5.tar.gz/gene2tab-0.1.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import codecs
+
+setup(
+    name="gene2tab",
+    install_requires=[
+        "tabulate",
+        "pandas",
+    ],
+    version="0.1.5",
+    author="Iker García-Ferrero",
+    author_email="igarciaf896@gmail.com",
+    license="Apache License 2.0",
+    description="Creates a table with the presence/absence of genes in samples",
+    long_description=codecs.open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "gene2tab=src.gene2tab:main",
+        ],
+    },
+    url="https://github.com/ikergarcia1996/Gene2Tab",
+)
