d1aa073c73c82e762dd8a90cd1e36a8d29f408bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 03:05:52 +0000

    Add package tipsql-postgresql/0.0.12/tipsql_postgresql-0.0.12.tar.gz

---
+++ packages/tipsql-postgresql/tipsql_postgresql-0.0.12.tar.gz/tipsql_postgresql-0.0.12/src/tipsql/postgresql/plugin.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+from typing import Literal, override
+
+from tipsql.core.dataclasses import extra_forbid_dataclass
+from tipsql.core.plugins.database_plugin import TipsqlDatabasePlugin
+from tipsql.postgresql.sync.table_generator import TableGenerator
+
+
+@extra_forbid_dataclass
+class PostgresqlDatabaseConfig:
+    type: Literal["postgresql"]
+
+
+class PostgresqlDatabasePlugin(TipsqlDatabasePlugin):
+    @property
+    def database_name(self) -> str:
+        return "postgresql"
+
+    @property
+    def database_config(self) -> type[PostgresqlDatabaseConfig]:
+        return PostgresqlDatabaseConfig
+
+    @override
+    def sync_database(self, config: PostgresqlDatabaseConfig) -> list[Path]:
+        import tipsql.postgresql
+
+        paths = []
+
+        connection = tipsql.postgresql.Connection.from_env()
+
+        tables_path = Path("tables.py")
+        paths.append(tables_path)
+        with open(tables_path, "w") as f:
+            f.write(TableGenerator(connection._connection).generate())
+
+        return paths
