98589d5a65f4e35bd97c40c562bc5ab7317661bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 04:04:53 +0000

    Add package drivers/0.0.1/drivers-0.0.1-py3-none-any.whl

---
+++ packages/drivers/drivers-0.0.1-py3-none-any.whl/drivers/pdf/__main__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+__all__ = ['main']
+
+import sys
+import argparse
+
+from . import pdf_to_text
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv[1:]
+    parser = argparse.ArgumentParser('input-pdf')
+    parser.add_argument('path', nargs='?')
+    args = parser.parse_args(argv)
+
+    if args.path is not None:
+        file = open(args.path, 'rb')
+    else:
+        file = sys.stdin.buffer
+
+    text = pdf_to_text(file)
+    print(text)
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
