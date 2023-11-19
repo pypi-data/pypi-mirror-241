04e5fe3cedc07b9fd77902a885ea07cfed75e73e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 03:04:42 +0000

    Add package pyiron-gui/0.0.11/pyiron_gui-0.0.11.tar.gz

---
+++ packages/pyiron-gui/pyiron_gui-0.0.11.tar.gz/pyiron_gui-0.0.11/setup.py
@@ -0,0 +1,45 @@
+"""
+Setuptools based setup module
+"""
+from setuptools import setup, find_packages
+
+import versioneer
+
+setup(
+    name='pyiron_gui',
+    version=versioneer.get_version(),
+    description='Repository for GUI plugins to the pyiron IDE.',
+    long_description='http://pyiron.org',
+
+    url='https://github.com/pyiron/pyiron_gui',
+    author='Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department',
+    author_email='siemer@mpie.de',
+    license='BSD',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Topic :: Scientific/Engineering :: Physics',
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10'
+    ],
+
+    keywords='pyiron',
+    packages=find_packages(exclude=["*tests*", "*docs*", "*binder*", "*conda*", "*notebooks*", "*.ci_support*"]),
+    install_requires=[
+        'pyiron_base>=0.6.9',
+        'pyiron_atomistics>=0.3.6',
+        'ipywidgets>=8.1.1',
+        'matplotlib>=3.8.1',
+        'nbconvert>=7.11.0',
+        'nbformat>=5.9.2',
+        'numpy>=1.26.0',
+        'pandas>=2.1.3',
+    ],
+    cmdclass=versioneer.get_cmdclass(),
+)
