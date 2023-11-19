95acf2f8bf3551c4941a01ae648de3013df5bdad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 03:05:09 +0000

    Add package fake-bpy-module-3.1/20231118/fake_bpy_module_3.1-20231118-py3-none-any.whl

---
+++ packages/fake-bpy-module-3.1/fake_bpy_module_3.1-20231118-py3-none-any.whl/blend_render_info/__init__.py
@@ -0,0 +1,20 @@
+import sys
+import typing
+
+GenericType = typing.TypeVar("GenericType")
+
+
+def main():
+    ''' 
+
+    '''
+
+    pass
+
+
+def read_blend_rend_chunk(path):
+    ''' 
+
+    '''
+
+    pass
