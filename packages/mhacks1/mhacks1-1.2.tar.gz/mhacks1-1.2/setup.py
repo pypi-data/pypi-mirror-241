7bb54cacfe36dfa7e29c408164b25cc5721c5315
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-19 14:05:28 +0000

    Add package mhacks1/1.2/mhacks1-1.2.tar.gz

---
+++ packages/mhacks1/mhacks1-1.2.tar.gz/mhacks1-1.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+setuptools.setup(
+    name="mhacks1",
+    version="1.2",
+    author="aarham",
+    author_email="aarham.wasit@gmail.com",
+    description="mhacks devtools",
+    url="https://pypi.org/project/mhacks1",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'openai',
+    ],
+    entry_points={
+        'console_scripts': [
+            'mhacks=mhacks1.testing1:main',
+        ],
+    },
+)
