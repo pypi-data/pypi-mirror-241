65ea4a2347c858806d8c70bde12ce76418e434cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 03:04:54 +0000

    Add package abstract-gui/0.0.61.66/abstract_gui-0.0.61.66.tar.gz

---
+++ packages/abstract-gui/abstract_gui-0.0.61.66.tar.gz/abstract_gui-0.0.61.66/src/abstract_gui/__init__.py
@@ -0,0 +1,5 @@
+from .abstract_gui import *
+from .abstract_browser import AbstractBrowser
+from .abstract_window_manager import AbstractWindowManager
+from .abstract_next_read_manager import NextReadManager
+from .RightClickManager import RightClickManager
